b08dd48915614e63b2486e1e7b81af0b1dbfb70f
Pedro Piñera <pepibumur@gmail.com>
2018-04-17 19:56:02 +0200

    [spm] Remove tests from xcbuddykitTests target

--- App/cli/main.swift
+++ App/cli/main.swift
@@ -1,15 +1,14 @@
 import Foundation
-import SwiftCLI
 import xcbuddykit
 
-let cli = CLI(name: "xcbuddy", version: App().version, description: "Xcode projects at scale")
-
-cli.commands = [
-    UpdateCommand(),
-    DumpCommand(),
-    // generate-xcodeproj
-    // build
-    // test
-]
-
-cli.goAndExit()
+//let cli = CLI(name: "xcbuddy", version: App().version, description: "Xcode projects at scale")
+//
+//cli.commands = [
+//    UpdateCommand(),
+//    DumpCommand(),
+//    // generate-xcodeproj
+//    // build
+//    // test
+//]
+//
+//cli.goAndExit()
