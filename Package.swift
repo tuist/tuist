390c2e3b401399a77561bcc66df675ee8a22de82
Pedro Piñera <pepibumur@gmail.com>
2018-04-17 17:37:40 +0200

    [spm] Remove old dependencies

--- Package.swift
+++ Package.swift
@@ -6,11 +6,7 @@ let package = Package(
     name: "xcbuddy",
     dependencies: [
         .package(url: "git@github.com:xcodeswift/xcproj.git", .upToNextMinor(from: "4.2.0")),
-        .package(url: "git@github.com:jakeheis/SwiftCLI.git", .upToNextMinor(from: "4.0.0")),
-        .package(url: "git@github.com:httpswift/swifter.git", .upToNextMinor(from: "1.4.0")),
-        .package(url: "git@github.com:kareman/SwiftShell.git", .upToNextMinor(from: "4.0.0")),
-        .package(url: "git@github.com:mtynior/ColorizeSwift.git", .upToNextMinor(from: "1.2.0")),
-        .package(url: "git@github.com:JohnSundell/Unbox.git", .upToNextMinor(from: "2.5.0")),
+        .package(url: "git@github.com:apple/swift-package-manager.git", .upToNextMinor(from: "0.2.0")),
     ],
     targets: [
         .target(
@@ -22,7 +18,7 @@ let package = Package(
         ),
         .target(
             name: "Dependencies",
-            dependencies: ["xcproj", "SwiftCLI", "Swifter", "SwiftShell", "ColorizeSwift", "Unbox"]
+            dependencies: ["xcproj", "Utility"]
         ),
     ]
 )
