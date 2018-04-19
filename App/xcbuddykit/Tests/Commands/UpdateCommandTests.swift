72d1437b38c56a6eb7b822e62ce0dd5e2cbc0573
Pedro Piñera <pepibumur@gmail.com>
2018-04-19 16:35:45 +0200

    Add UpdateCommandTests

---
+++ App/xcbuddykit/Tests/Commands/UpdateCommandTests.swift
@@ -0,0 +1,9 @@
+//
+//  UpdateCommandTests.swift
+//  xcbuddykitTests
+//
+//  Created by Pedro Piñera Buendía on 19.04.18.
+//  Copyright © 2018 ppinera.es. All rights reserved.
+//
+
+import Foundation
