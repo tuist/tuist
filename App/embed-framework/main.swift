2b4ccc570b0472d411b5cc8d5b923e63a9dcdbd6
Pedro Piñera <pepibumur@gmail.com>
2018-04-27 17:38:44 +0200

    Add embed-framework

---
+++ App/embed-framework/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  embed-framework
+//
+//  Created by Pedro Piñera Buendía on 27.04.18.
+//  Copyright © 2018 ppinera.es. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
