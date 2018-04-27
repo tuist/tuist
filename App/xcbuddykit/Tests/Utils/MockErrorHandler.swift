7aeafe9ee2c1e0a7b236817fa833263ef23293f3
Pedro Piñera <pepibumur@gmail.com>
2018-04-27 16:21:05 +0200

    Regenerate docs

--- App/xcbuddykit/Tests/Utils/MockErrorHandler.swift
+++ App/xcbuddykit/Tests/Utils/MockErrorHandler.swift
@@ -1,14 +1,11 @@
 import Foundation
+@testable import xcbuddykit
 import XCTest
 
-@testable import xcbuddykit
-
 final class MockErrorHandler: ErrorHandling {
-
     var fatalErrorArgs: [FatalError] = []
 
     func fatal(error: FatalError) {
         fatalErrorArgs.append(error)
     }
-
 }
