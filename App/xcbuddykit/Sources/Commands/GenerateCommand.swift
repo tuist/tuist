6b80d6fba5211469f68f607fdfdacffa15d573dd
Pedro <pepibumur@gmail.com>
2018-05-14 18:29:10 +0200

    Update GenerateCommand to accept a config argument

--- App/xcbuddykit/Sources/Commands/GenerateCommand.swift
+++ App/xcbuddykit/Sources/Commands/GenerateCommand.swift
@@ -34,6 +34,9 @@ public class GenerateCommand: NSObject, Command {
     /// Path argument.
     let pathArgument: OptionArgument<String>
 
+    /// Config argument.
+    let configArgument: OptionArgument<String>
+
     /// Initializes the generate command with the argument parser.
     ///
     /// - Parameter parser: argument parser.
@@ -66,8 +69,13 @@ public class GenerateCommand: NSObject, Command {
         pathArgument = subParser.add(option: "--path",
                                      shortName: "-p",
                                      kind: String.self,
-                                     usage: "The path where the Project.swift file will be generated",
+                                     usage: "The path where the Project.swift file will be generated.",
                                      completion: .filename)
+        configArgument = subParser.add(option: "--config",
+                                       shortName: "-c",
+                                       kind: String.self,
+                                       usage: "The configuration that will be generated.",
+                                       completion: .filename)
     }
 
     /// Runs the command.
@@ -76,12 +84,25 @@ public class GenerateCommand: NSObject, Command {
     /// - Throws: an error if the command cannot be executed.
     public func run(with arguments: ArgumentParser.Result) {
         context.errorHandler.try {
+            // Path
             var path: AbsolutePath! = arguments.get(pathArgument).map({ AbsolutePath($0) })
             if path == nil {
                 path = AbsolutePath.current
             }
+
+            // Config
+            var config: BuildConfiguration = .debug
+            if let configString = arguments.get(configArgument) {
+                guard let buildConfiguration = BuildConfiguration(rawValue: configString) else {
+                    let error = ArgumentParserError.invalidValue(argument: "config",
+                                                                 error: ArgumentConversionError.custom("config can only be debug or release"))
+                    self.context.errorHandler.fatal(error: FatalError.abort(error))
+                    return
+                }
+                config = buildConfiguration
+            }
             let context = try GeneratorContext(graph: graphLoader.load(path: path))
-            try workspaceGenerator.generate(path: path, context: context, options: GenerationOptions())
+            try workspaceGenerator.generate(path: path, context: context, options: GenerationOptions(buildConfiguration: config))
             self.context.printer.print(section: "Generate command succeeded 🎉")
         }
     }
