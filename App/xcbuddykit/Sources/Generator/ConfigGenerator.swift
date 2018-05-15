800a6bfe14a201b44b9d11d0b4ec2e00f54cb79a
Pedro <pepibumur@gmail.com>
2018-05-15 10:32:58 +0200

    Remove if/else

--- App/xcbuddykit/Sources/Generator/ConfigGenerator.swift
+++ App/xcbuddykit/Sources/Generator/ConfigGenerator.swift
@@ -21,7 +21,7 @@ protocol ConfigGenerating: AnyObject {
                                sourceRootPath: AbsolutePath,
                                context: GeneratorContexting,
                                options: GenerationOptions) throws -> PBXObjectReference
-
+    
     /// Generates the manifests target configuration.
     ///
     /// - Parameters:
@@ -31,7 +31,7 @@ protocol ConfigGenerating: AnyObject {
     /// - Returns: the configuration list reference.
     /// - Throws: an error if the generation fails.
     func generateManifestsConfig(pbxproj: PBXProj, context: GeneratorContexting, options: GenerationOptions) throws -> PBXObjectReference
-
+    
     /// Generates the target configuration list and configurations.
     ///
     /// - Parameters:
@@ -55,14 +55,14 @@ protocol ConfigGenerating: AnyObject {
 final class ConfigGenerator: ConfigGenerating {
     /// File generator.
     let fileGenerator: FileGenerating
-
+    
     /// Default config generator constructor.
     ///
     /// - Parameter fileGenerator: generator used to generate files.
     init(fileGenerator: FileGenerating = FileGenerator()) {
         self.fileGenerator = fileGenerator
     }
-
+    
     /// Generates the project configuration list and configurations.
     ///
     /// - Parameters:
@@ -83,29 +83,26 @@ final class ConfigGenerator: ConfigGenerating {
         /// Configuration list
         let configurationList = XCConfigurationList(buildConfigurations: [])
         let configurationListReference = pbxproj.objects.addObject(configurationList)
-
-        if options.buildConfiguration == .debug {
-            try generateProjectSettingsFor(buildConfiguration: .debug,
-                                           configuration: project.settings?.debug,
-                                           project: project,
-                                           groups: groups,
-                                           sourceRootPath: sourceRootPath,
-                                           context: context,
-                                           pbxproj: pbxproj,
-                                           configurationList: configurationList)
-        } else if options.buildConfiguration == .release {
-            try generateProjectSettingsFor(buildConfiguration: .release,
-                                           configuration: project.settings?.release,
-                                           project: project,
-                                           groups: groups,
-                                           sourceRootPath: sourceRootPath,
-                                           context: context,
-                                           pbxproj: pbxproj,
-                                           configurationList: configurationList)
-        }
+        
+        try generateProjectSettingsFor(buildConfiguration: .debug,
+                                       configuration: project.settings?.debug,
+                                       project: project,
+                                       groups: groups,
+                                       sourceRootPath: sourceRootPath,
+                                       context: context,
+                                       pbxproj: pbxproj,
+                                       configurationList: configurationList)
+        try generateProjectSettingsFor(buildConfiguration: .release,
+                                       configuration: project.settings?.release,
+                                       project: project,
+                                       groups: groups,
+                                       sourceRootPath: sourceRootPath,
+                                       context: context,
+                                       pbxproj: pbxproj,
+                                       configurationList: configurationList)
         return configurationListReference
     }
-
+    
     /// Generate the project settings for a given configuration (e.g. Debug or Release)
     ///
     /// - Parameters:
@@ -129,12 +126,12 @@ final class ConfigGenerator: ConfigGenerating {
         let variant: BuildSettingsProvider.Variant = (buildConfiguration == .debug) ? .debug : .release
         let defaultConfigSettings = BuildSettingsProvider.projectDefault(variant: variant)
         let defaultSettingsAll = BuildSettingsProvider.projectDefault(variant: .all)
-
+        
         var settings: [String: Any] = [:]
         extend(buildSettings: &settings, with: defaultSettingsAll)
         extend(buildSettings: &settings, with: project.settings?.base ?? [:])
         extend(buildSettings: &settings, with: defaultConfigSettings)
-
+        
         let variantBuildConfiguration = XCBuildConfiguration(name: buildConfiguration.rawValue.capitalized)
         if let variantConfig = configuration {
             extend(buildSettings: &settings, with: variantConfig.settings)
@@ -150,7 +147,7 @@ final class ConfigGenerator: ConfigGenerating {
         let debugConfigurationReference = pbxproj.objects.addObject(variantBuildConfiguration)
         configurationList.buildConfigurations.append(debugConfigurationReference)
     }
-
+    
     /// Generates the configuration for the manifests target.
     ///
     /// - Parameters:
@@ -162,7 +159,7 @@ final class ConfigGenerator: ConfigGenerating {
     func generateManifestsConfig(pbxproj: PBXProj, context: GeneratorContexting, options: GenerationOptions) throws -> PBXObjectReference {
         let configurationList = XCConfigurationList(buildConfigurations: [])
         let configurationListReference = pbxproj.objects.addObject(configurationList)
-
+        
         let addSettings: (XCBuildConfiguration) throws -> Void = { configuration in
             let frameworkParentDirectory = try context.resourceLocator.projectDescription().parentDirectory
             configuration.buildSettings["FRAMEWORK_SEARCH_PATHS"] = frameworkParentDirectory.asString
@@ -184,7 +181,7 @@ final class ConfigGenerator: ConfigGenerating {
         }
         return configurationListReference
     }
-
+    
     /// Generates the target configuration list and configurations.
     ///
     /// - Parameters:
@@ -207,9 +204,9 @@ final class ConfigGenerator: ConfigGenerating {
         // TODO:
         return configurationListReference
     }
-
+    
     // MARK: - Private
-
+    
     /// Extends build settings with other build settings.
     ///
     /// - Parameters:
