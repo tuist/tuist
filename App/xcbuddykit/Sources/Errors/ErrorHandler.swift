9b4ed23ad5cf98acd9a97e40af75d5e18a6dce5f
Pedro Piñera <pepibumur@gmail.com>
2018-05-18 17:55:30 +0200

    Some code style fixes

--- App/xcbuddykit/Sources/Errors/ErrorHandler.swift
+++ App/xcbuddykit/Sources/Errors/ErrorHandler.swift
@@ -64,18 +64,18 @@ final class ErrorHandler: ErrorHandling {
     ///
     /// - Parameter error: error.
     func fatal(error: FatalError) {
-        let silent = error.type == .abortSilent || error.type == .bugSilent
-        let bug = error.type == .bug || error.type == .bugSilent
-        if !error.description.isEmpty && !silent {
+        let isSilent = error.type == .abortSilent || error.type == .bugSilent
+        let isBug = error.type == .bug || error.type == .bugSilent
+        if !error.description.isEmpty && !isSilent {
             printer.print(errorMessage: error.description)
-        } else if silent {
+        } else if isSilent {
             let message = """
             An unexpected error happened. We've opened an issue to fix it as soon as possible.
             We are sorry for any inconviniences it might have caused.
             """
             printer.print(errorMessage: message)
         }
-        if bug {
+        if isBug {
             let event = Event(level: .debug)
             event.message = error.description
             let semaphore = DispatchSemaphore(value: 0)
