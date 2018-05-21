5055f9f59bde41ff3ec280c7999fff40bae56661
Juanpe Catalán <juan.catalan@odigeo.com>
2018-05-21 13:52:10 +0200

    test: Create MockInputRequerer

--- App/xcbuddykit/Tests/Utils/Mocks/MockInputRequester.swift
+++ App/xcbuddykit/Tests/Utils/Mocks/MockInputRequester.swift
@@ -1,5 +1,24 @@
 @testable import xcbuddykit
 
-class MockInputRequester: InputRequesting {
+class MockInputRequester: InputRequering {
     
+    enum Mode {
+        case positive
+        case negative
+    }
+    
+    var mode: Mode = .positive
+    let printer: Printing = MockPrinter()
+    
+    func requestBoolUserInput(message: String) -> Bool {
+        return mode == .positive ? true : false
+    }
+    
+    func requestRequiredUserInput(message: String, errorMessage: String) -> String {
+        return "required"
+    }
+    
+    func requestOptionalUserInput(message: String) -> String? {
+        return mode == .positive ? "optional" : nil
+    }
 }
