68d2ef4bee2e920a598fb1258dc77d26da25eac0
Pedro Piñera <pepibumur@gmail.com>
2018-06-01 18:00:56 +0200

    Update xcodeproj dependency

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "xpm",
     dependencies: [
-        .package(url: "git@github.com:xcode-project-manager/xcodeproj.git", .revision("0c2d7389cf2d0b5a89cce492e501623dfe0c71ae")),
+        .package(url: "git@github.com:xcode-project-manager/xcodeproj.git", .revision("d003a30d0b0e3b0d422f6c19be5ec7b90e901475")),
         .package(url: "https://github.com/apple/swift-package-manager", from: "0.2.0"),
     ],
     targets: [
