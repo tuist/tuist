6f96181901d747a6120c39308a622637593b9042
Pedro Piñera <pepibumur@gmail.com>
2018-06-24 15:22:19 +0200

    Add xpmtools target

--- Package.swift
+++ Package.swift
@@ -4,10 +4,18 @@ import PackageDescription
 
 let package = Package(
     name: "xpm",
+    products: [
+        .executable(name: "xpm", targets: ["xpm"]),
+        .executable(name: "xpmembed", targets: ["xpmembed"]),
+        .executable(name: "xpmenv", targets: ["xpmenv"]),
+        .library(name: "ProjectDescription",
+                 type: .dynamic,
+                 targets: ["ProjectDescription"])
+    ],
     dependencies: [
         .package(url: "git@github.com:xcode-project-manager/xcodeproj.git", .revision("1ad8b0739963d9cc9592b9616a2f027ade7c93ab")),
         .package(url: "https://github.com/apple/swift-package-manager", from: "0.2.0"),
-    ],
+        ],
     targets: [
         .target(
             name: "xpmkit",
@@ -29,7 +37,9 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "ProjectDescriptionTests",
-            dependencies: ["ProjectDescription"]
-        ),
-    ]
+            dependencies: ["ProjectDescription"]),
+        .target(
+            name: "xpmtools",
+            dependencies: ["Utility"]),
+        ]
 )
---
+++ Sources/xpmtools/main.swift
@@ -0,0 +1,1 @@
+print("Running tools")
