0bdc440d0e775445725f22b7a78963c8b72b4eae
Pedro Piñera <pepibumur@gmail.com>
2018-07-14 15:38:55 +0200

    Add MockSettingsController

---
+++ Tests/xpmenvkitTests/Settings/Mocks/MockSettingsController.swift
@@ -0,0 +1,20 @@
+import Foundation
+@testable import xpmenvkit
+
+final class MockSettingsController: SettingsControlling {
+    var settingsCount: UInt = 0
+    var settingsStub: Settings?
+    var setSettingsStub: ((Settings) throws -> Void)?
+    var setSettingsCount: UInt = 0
+
+    func settings() throws -> Settings {
+        settingsCount += 1
+        if let settingsStub = settingsStub { return settingsStub }
+        return Settings()
+    }
+
+    func set(settings: Settings) throws {
+        setSettingsCount += 1
+        try setSettingsStub?(settings)
+    }
+}
