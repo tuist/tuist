2376b02494b03d5cafe58cec122bc6f44d6112bc
Pedro Piñera <pepibumur@gmail.com>
2018-07-14 15:36:07 +0200

    Add more tests

--- Tests/xpmenvkitTests/Settings/SettingsTests.swift
+++ Tests/xpmenvkitTests/Settings/SettingsTests.swift
@@ -3,6 +3,11 @@ import XCTest
 @testable import xpmenvkit
 
 final class SettingsTests: XCTestCase {
+    func test_coding_keys() {
+        XCTAssertEqual(Settings.CodingKeys.lastTimeUpdatesChecked.rawValue, "last_time_updates_checked")
+        XCTAssertEqual(Settings.CodingKeys.canaryReference.rawValue, "canary_reference")
+    }
+
     func test_equatable() {
         let date = Date()
         let settingsA = Settings(lastTimeUpdatesChecked: date, canaryReference: "ref")
