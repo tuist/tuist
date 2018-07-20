fa1f4e8b0f95b21042a102dfbe8650ad9336cd25
Pedro Piñera <pepibumur@gmail.com>
2018-07-20 18:29:55 -0400

    Update Init command to support platform and product and generate the Info.plist files automatically

--- Sources/xpmcore/Extensions/AbsolutePath+Extras.swift
+++ Sources/xpmcore/Extensions/AbsolutePath+Extras.swift
@@ -16,25 +16,6 @@ extension AbsolutePath {
     }
 
     public func glob(_ pattern: String) -> [AbsolutePath] {
-        var gt = glob_t()
-        let cPattern = strdup(appending(RelativePath(pattern)).asString)
-        defer {
-            globfree(&gt)
-            free(cPattern)
-        }
-
-        let flags = GLOB_TILDE | GLOB_BRACE | GLOB_MARK
-        if systemGlob(cPattern, flags, nil, &gt) == 0 {
-            let matchc = gt.gl_matchc
-            return (0 ..< Int(matchc)).compactMap { index in
-                if let path = String(validatingUTF8: gt.gl_pathv[index]!) {
-                    return AbsolutePath(path)
-                }
-                return nil
-            }
-        }
-
-        // GLOB_NOMATCH
-        return []
+        return Glob(pattern: appending(RelativePath(pattern)).asString).paths.map({ AbsolutePath($0) })
     }
 }
--- Sources/xpmkit/Commands/InitCommand.swift
+++ Sources/xpmkit/Commands/InitCommand.swift
@@ -35,22 +35,54 @@ public class InitCommand: NSObject, Command {
     public static let command = "init"
 
     /// Command description.
-    public static let overview = "Initializes a Project.swift in the current folder."
+    public static let overview = "Initializes a project in the current directory."
 
-    /// Path argument.
-    let pathArgument: OptionArgument<String>
+    /// Platform argument.
+    let platformArgument: OptionArgument<String>
 
-    /// Context
-    let context: CommandsContexting
+    /// Product argument.
+    let productArgument: OptionArgument<String>
 
-    public required init(parser: ArgumentParser) {
+    /// File handler.
+    let fileHandler: FileHandling
+
+    /// Printer.
+    let printer: Printing
+
+    /// Info.plist provisioner
+    let infoplistProvisioner: InfoPlistProvisioning
+
+    public required convenience init(parser: ArgumentParser) {
+        self.init(parser: parser,
+                  fileHandler: FileHandler(),
+                  printer: Printer(),
+                  infoplistProvisioner: InfoPlistProvisioner())
+    }
+
+    init(parser: ArgumentParser,
+         fileHandler: FileHandling,
+         printer: Printing,
+         infoplistProvisioner: InfoPlistProvisioning) {
         let subParser = parser.add(subparser: InitCommand.command, overview: InitCommand.overview)
-        pathArgument = subParser.add(option: "--path",
-                                     shortName: "-p",
-                                     kind: String.self,
-                                     usage: "The path where the Project.swift file will be generated",
-                                     completion: .filename)
-        context = CommandsContext()
+        productArgument = subParser.add(option: "--product",
+                                        shortName: nil,
+                                        kind: String.self,
+                                        usage: "The product (app or framework) the generated project will build.",
+                                        completion: ShellCompletion.values([
+                                            (value: "app", description: "Application"),
+                                            (value: "framework", description: "Framework"),
+        ]))
+        platformArgument = subParser.add(option: "--platform",
+                                         shortName: nil,
+                                         kind: String.self,
+                                         usage: "The platform (ios or macos) the product will be for.",
+                                         completion: ShellCompletion.values([
+                                             (value: "ios", description: "iOS platform"),
+                                             (value: "macos", description: "macOS platform"),
+        ]))
+        self.fileHandler = fileHandler
+        self.printer = printer
+        self.infoplistProvisioner = infoplistProvisioner
     }
 
     /// Runs the command.
@@ -58,43 +90,32 @@ public class InitCommand: NSObject, Command {
     /// - Parameter arguments: input arguments.
     /// - Throws: throws an error if the execution fails.
     public func run(with arguments: ArgumentParser.Result) throws {
-        var path: AbsolutePath! = arguments
-            .get(pathArgument)
-            .map({ AbsolutePath($0) })
-            .map({ $0.appending(component: Constants.Manifest.project) })
-        if path == nil {
-            path = AbsolutePath.current.appending(component: Constants.Manifest.project)
-        }
-        if context.fileHandler.exists(path) {
-            throw InitCommandError.alreadyExists(path)
-        }
-        guard let projectName = path.parentDirectory.components.last else {
-            throw InitCommandError.ungettableProjectName(path)
-        }
-        let projectSwift = self.projectSwift(name: projectName)
-        try projectSwift.write(toFile: path.asString,
-                               atomically: true,
-                               encoding: .utf8)
-        context.printer.print(section: "Project.swift generated at path \(path.asString)")
+        let product = try self.product(arguments: arguments)
+        let platform = try self.platform(arguments: arguments)
+        let name = try self.name()
+        try generateProjectSwift(name: name, platform: platform, product: product)
+        try generateSources(name: name, platform: platform, product: product)
+        try generateTests(name: name)
+        try generatePlists(platform: platform, product: product)
     }
 
-    fileprivate func projectSwift(name: String) -> String {
-        return """
+    fileprivate func generateProjectSwift(name: String, platform: Platform, product: Product) throws {
+        let content = """
         import ProjectDescription
         
-         let project = Project(name: "{{NAME}}",
+         let project = Project(name: "\(name)",
                       schemes: [
                           /* Project schemes are defined here */
-                          Scheme(name: "{{NAME}}",
+                          Scheme(name: "\(name)",
                                  shared: true,
-                                 buildAction: BuildAction(targets: ["{{NAME}}"])),
+                                 buildAction: BuildAction(targets: ["\(name)"])),
                       ],
                       settings: Settings(base: [:]),
                       targets: [
-                          Target(name: "{{NAME}}",
-                                 platform: .iOS,
-                                 product: .app,
-                                 bundleId: "com.xcodepm.{{NAME}}",
+                          Target(name: "\(name)",
+                                 platform: .\(platform.rawValue),
+                                 product: .\(product.rawValue),
+                                 bundleId: "com.xcodepm.\(name)",
                                  infoPlist: "Info.plist",
                                  dependencies: [
                                      /* Target dependencies can be defined here */
@@ -102,12 +123,173 @@ public class InitCommand: NSObject, Command {
                                  ],
                                  settings: nil,
                                  buildPhases: [
-                                    
-                                     .sources([.sources("./Sources/*")]),
+                                     .sources([.sources("./Sources/**/*.swift")]),
                                      /* Other build phases can be added here */
                                      /* .resources([.include(["./Resources/**/*"])]) */
                                 ]),
+                          Target(name: "\(name)Tests",
+                                 platform: .\(platform.rawValue),
+                                 product: .unitTests,
+                                 bundleId: "com.xcodepm.\(name)Tests",
+                                 infoPlist: "Tests.plist",
+                                 dependencies: [
+                                   .target(name: "\(name)")
+                                 ],
+                                 settings: nil,
+                                 buildPhases: [
+                                     .sources([.sources("./Tests/**/*.swift")]),
+                                ]),
+        
+                        
                     ])
-        """.replacingOccurrences(of: "{{NAME}}", with: name)
+        """
+        try content.write(to: fileHandler.currentPath.appending(component: "Project.swift").url, atomically: true, encoding: .utf8)
+    }
+
+    fileprivate func generatePlists(platform: Platform, product: Product) throws {
+        try infoplistProvisioner.generate(path: fileHandler.currentPath.appending(component: "Info.plist"),
+                                          platform: platform,
+                                          product: product)
+        try infoplistProvisioner.generate(path: fileHandler.currentPath.appending(component: "Tests.plist"),
+                                          platform: platform,
+                                          product: .unitTests)
+    }
+
+    fileprivate func generateSources(name: String, platform: Platform, product: Product) throws {
+        let path = fileHandler.currentPath.appending(component: "Sources")
+
+        if fileHandler.exists(path) {
+            throw InitCommandError.alreadyExists(path)
+        }
+        try fileHandler.createFolder(path)
+
+        var content: String!
+        var filename: String!
+
+        if platform == .macOS && product == .app {
+            filename = "AppDelegate.swift"
+            content = """
+            import Cocoa
+            
+            @NSApplicationMain
+            class AppDelegate: NSObject, NSApplicationDelegate {
+            
+                @IBOutlet weak var window: NSWindow!
+            
+                func applicationDidFinishLaunching(_ aNotification: Notification) {
+                    // Insert code here to initialize your application
+                }
+            
+                func applicationWillTerminate(_ aNotification: Notification) {
+                    // Insert code here to tear down your application
+                }
+            
+            }
+            """
+        } else if platform == .iOS && product == .app {
+            filename = "AppDelegate.swift"
+
+            content = """
+            import UIKit
+            
+            @UIApplicationMain
+            class AppDelegate: UIResponder, UIApplicationDelegate {
+            
+                var window: UIWindow?
+            
+                func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+                    window = UIWindow(frame: UIScreen.main.bounds)
+                    let viewController = UIViewController()
+                    viewController.view.backgroundColor = .white
+                    window?.rootViewController = viewController
+                    window?.makeKeyAndVisible()
+                    return true
+                }
+            
+            }
+            """
+        } else {
+            filename = "\(name).swift"
+            content = """
+            import Foundation
+            
+            class \(name) {
+            
+            }
+            """
+        }
+
+        try content.write(to: path.appending(component: filename).url, atomically: true, encoding: .utf8)
+    }
+
+    /// Generates the tests folder with a base tests file.
+    ///
+    /// - Parameter name: project name.
+    /// - Throws: an error if the tests folder cannot be created.
+    fileprivate func generateTests(name: String) throws {
+        let path = fileHandler.currentPath.appending(component: "Tests")
+
+        if fileHandler.exists(path) {
+            throw InitCommandError.alreadyExists(path)
+        }
+        try fileHandler.createFolder(path)
+
+        let content = """
+        import Foundation
+        import XCTest
+        
+        @testable import \(name)
+
+        final class \(name)Tests: XCTestCase {
+        
+        }
+        """
+        try content.write(to: path.appending(component: "\(name)Tests.swift").url, atomically: true, encoding: .utf8)
+    }
+
+    /// Returns the name that should be used for the project.
+    ///
+    /// - Returns: project name.
+    /// - Throws: an error if the name cannot be obtained.
+    fileprivate func name() throws -> String {
+        if let name = fileHandler.currentPath.components.last {
+            return name
+        } else {
+            throw InitCommandError.ungettableProjectName(AbsolutePath.current)
+        }
+    }
+
+    /// Returns the product by parsing the arguments.
+    ///
+    /// - Parameter arguments: argument parser result.
+    /// - Returns: the product that should be used for the project.
+    fileprivate func product(arguments: ArgumentParser.Result) throws -> Product {
+        if let productString = arguments.get(self.productArgument) {
+            let valid = ["app", "framework"]
+            if valid.contains(productString), let product = Product(rawValue: productString) {
+                return product
+            } else {
+                throw ArgumentParserError.invalidValue(argument: "product", error: .custom("Product should be either app or framework"))
+            }
+        } else {
+            return .app
+        }
+    }
+
+    /// Returns the platform by parsing the arguments.
+    ///
+    /// - Parameter arguments: argument parser result.
+    /// - Returns: the platform that should be used for the project.
+    fileprivate func platform(arguments: ArgumentParser.Result) throws -> Platform {
+        if let platformString = arguments.get(self.platformArgument) {
+            let valid = ["ios", "macos"]
+            if valid.contains(platformString) {
+                return (platformString == "ios") ? .iOS : .macOS
+            } else {
+                throw ArgumentParserError.invalidValue(argument: "platform", error: .custom("Platform should be either ios or macos"))
+            }
+        } else {
+            return .iOS
+        }
     }
 }
