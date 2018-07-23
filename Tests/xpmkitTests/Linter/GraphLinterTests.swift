d93beff01fb6a4e05b9dd5a554119c64f4173286
Pedro Piñera <pepibumur@gmail.com>
2018-07-23 07:07:09 -0400

    Some improvements

---
+++ Tests/xpmkitTests/Linter/GraphLinterTests.swift
@@ -0,0 +1,13 @@
+import Basic
+import Foundation
+import XCTest
+@testable import xpmkit
+
+final class GraphLinterTests: XCTestCase {
+    var subject: GraphLinter!
+
+    override func setUp() {
+        super.setUp()
+        subject = GraphLinter()
+    }
+}
