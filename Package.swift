8ec3c00253c6bae603595732eea1b226b808ee7f
Pedro Piñera <pepibumur@gmail.com>
2018-07-26 16:09:12 -0400

    Update SwiftShell version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     dependencies: [
         .package(url: "git@github.com:tuist/xcodeproj.git", .revision("549d67686d90ef8e45fccdca147682f185af2ad0")),
         .package(url: "git@github.com:apple/swift-package-manager.git", .revision("3e71e57db41ebb32ccec1841a7e26c428a9c08c5")),
-        .package(url: "git@github.com:tuist/SwiftShell.git", .revision("b9afd2eb8cc5b0cd5713d6ed4e649948ec70ff8e")),
+        .package(url: "git@github.com:tuist/SwiftShell.git", .revision("71b7bc18af32114176e89f6637188ac3957011f6")),
     ],
     targets: [
         .target(
