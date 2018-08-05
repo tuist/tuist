325867207d0f86458bef142d03233ebf5c9a8626
Pedro Piñera <pepibumur@gmail.com>
2018-08-05 23:08:50 +0200

    Update formula

--- Formula/tuist.rb
+++ Formula/tuist.rb
@@ -2,8 +2,6 @@ class Tuist < Formula
   desc "Generate, maintain, and interact with Xcode projects easily"
   homepage "https://tuist.io"
   url "https://github.com/tuist/tuist/raw/0.2.0/bin/tuistenv"
-  version "0.2.0"
-  # make sha256
   sha256 "6769e784ade1a6c5726a71afc78b13dd1509f2b421deb6719fd2ff9ab796afda"
 
   def install
@@ -12,6 +10,9 @@ class Tuist < Formula
   end
 
   test do
+    # Shows all available commands
+    system "#{bin}/tuist", "--help-env"
+    # Pins tuist to the given version
     system "#{bin}/tuist", "local", "0.2.0"
   end
 end
