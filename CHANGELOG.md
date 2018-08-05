cdddbbe30ddabca50b5539e62c6c5bac07d2ba01
Pedro Piñera <pepibumur@gmail.com>
2018-08-05 22:12:26 +0200

    Version 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,8 @@ Please, check out guidelines: https://keepachangelog.com/en/1.0.0/
 
 ## Next version
 
+## 0.2.0
+
 ### Added
 
 - Install command https://github.com/tuist/tuist/pull/83 by @pepibumur.
--- Sources/TuistCore/Constants.swift
+++ Sources/TuistCore/Constants.swift
@@ -5,7 +5,7 @@ public class Constants {
     public static let binFolderName = ".tuist-bin"
     public static let binName = "tuist"
     public static let gitRepositorySSH = "git@github.com:tuist/tuist.git"
-    public static let version = "0.1.0"
+    public static let version = "0.2.0"
     public static let swiftVersion = "4.1.2"
 
     public struct Manifest {
