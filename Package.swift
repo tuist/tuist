48cbba359ad0309f9742ace90f3017fd77a2a072
Pedro Piñera Buendía <pepibumur@gmail.com>
2018-08-17 12:50:46 +0200

    Use https links (#91)
    * Use https links
    
    * Replace missing link
    
    * Update changelog

--- Package.swift
+++ Package.swift
@@ -12,9 +12,9 @@ let package = Package(
                  targets: ["ProjectDescription"]),
     ],
     dependencies: [
-        .package(url: "git@github.com:tuist/xcodeproj.git", .revision("549d67686d90ef8e45fccdca147682f185af2ad0")),
-        .package(url: "git@github.com:carthage/ReactiveTask.git", .revision("57d221b82270b05380d66117e07ac4069b78a4e9")),
-        .package(url: "git@github.com:apple/swift-package-manager.git", .revision("3e71e57db41ebb32ccec1841a7e26c428a9c08c5")),
+        .package(url: "https://github.com/tuist/xcodeproj.git", .revision("549d67686d90ef8e45fccdca147682f185af2ad0")),
+        .package(url: "https://github.com/carthage/ReactiveTask.git", .revision("57d221b82270b05380d66117e07ac4069b78a4e9")),
+        .package(url: "https://github.com/apple/swift-package-manager.git", .revision("3e71e57db41ebb32ccec1841a7e26c428a9c08c5")),
     ],
     targets: [
         .target(
--- Sources/TuistCore/Constants.swift
+++ Sources/TuistCore/Constants.swift
@@ -4,7 +4,7 @@ public class Constants {
     public static let versionFileName = ".tuist-version"
     public static let binFolderName = ".tuist-bin"
     public static let binName = "tuist"
-    public static let gitRepositorySSH = "git@github.com:tuist/tuist.git"
+    public static let gitRepositorySSH = "https://github.com/tuist/tuist.git"
     public static let version = "0.2.0"
     public static let swiftVersion: String = "4.1.2"
 
