2fdc2e4401e28c962f7316fcefbab47486f3eee1
Pedro Piñera <pepibumur@gmail.com>
2018-08-24 09:58:32 +0200

    Regenerate tuistenv and bump version

--- Formula/tuist.rb
+++ Formula/tuist.rb
@@ -1,8 +1,8 @@
 class Tuist < Formula
   desc "Generate, maintain, and interact with Xcode projects easily"
   homepage "https://tuist.io"
-  url "https://github.com/tuist/tuist/raw/0.3.0/bin/tuistenv"
-  sha256 "43915c5373f9fe22ac04be730aca625d8320bb27688d3acd8b1687a4728af8cb"
+  url "https://github.com/tuist/tuist/raw/0.4.0/bin/tuistenv"
+  sha256 "c2bb0a1e3416f71132e8ff12fb9dfe1aefe086022d89aea878c0de91ac3498c3"
 
   def install
     File.rename("tuistenv", "tuist")
