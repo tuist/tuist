1e6acc2c7ed7ccf0552593596c78bbd08417a68a
Pedro Piñera <pepibumur@gmail.com>
2018-08-26 20:38:15 +0200

    Fix tests

--- Sources/ProjectDescription/Scheme.swift
+++ Sources/ProjectDescription/Scheme.swift
@@ -8,6 +8,15 @@ public class Scheme: Codable {
     public let buildAction: BuildAction?
     public let testAction: TestAction?
     public let runAction: RunAction?
+
+    public enum CodingKeys: String, CodingKey {
+        case name
+        case shared
+        case buildAction = "build_action"
+        case testAction = "test_action"
+        case runAction = "run_action"
+    }
+
     public init(name: String,
                 shared: Bool = true,
                 buildAction: BuildAction? = nil,
@@ -26,6 +35,12 @@ public class Scheme: Codable {
 public class Arguments: Codable {
     public let environment: [String: String]
     public let launch: [String: Bool]
+
+    public enum CodingKeys: String, CodingKey {
+        case environment
+        case launch
+    }
+
     public init(environment: [String: String],
                 launch: [String: Bool]) {
         self.environment = environment
@@ -37,6 +52,11 @@ public class Arguments: Codable {
 
 public class BuildAction: Codable {
     public let targets: [String]
+
+    public enum CodingKeys: String, CodingKey {
+        case targets
+    }
+
     public init(targets: [String]) {
         self.targets = targets
     }
@@ -49,6 +69,14 @@ public class TestAction: Codable {
     public let arguments: Arguments?
     public let config: BuildConfiguration
     public let coverage: Bool
+
+    public enum CodingKeys: String, CodingKey {
+        case targets
+        case arguments
+        case config
+        case coverage
+    }
+
     public init(targets: [String],
                 arguments: Arguments? = nil,
                 config: BuildConfiguration = .debug,
@@ -66,6 +94,13 @@ public class RunAction: Codable {
     public let config: BuildConfiguration
     public let executable: String?
     public let arguments: Arguments?
+
+    public enum CodingKeys: String, CodingKey {
+        case config
+        case executable
+        case arguments
+    }
+
     public init(config: BuildConfiguration = .debug,
                 executable: String? = nil,
                 arguments: Arguments? = nil) {
