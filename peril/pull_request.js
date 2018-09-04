1c33231cd50e6c4a7c1d9e8fa2c77efec4b0f813
Pedro Piñera Buendía <pedro+work@ppinera.es>
2018-09-04 18:42:41 +0200

    Add Danger file (#82)
    * Add pull_request.js
    
    * Test
    
    * test
    
    * changes

---
+++ peril/pull_request.js
@@ -0,0 +1,16 @@
+import { schedule, danger, warn, fail } from 'danger'
+import { includes } from 'lodash'
+
+// Ensure that the tuistenv binary is generated when tuistenv files are modified
+// const hasTuistEnvBin = includes(danger.git.modified_files, 'bin/tuistenv')
+// const tuistEnvFiles = danger.git.modified_files.filter(path => {
+//   return path.includes('Sources/TuistEnvKit')
+// })
+
+// if (tuistEnvFiles.length > 0 && !hasTuistEnvBin) {
+//   fail(
+//     "bin/tuistenv needs to be regenerated. Run 'make build-env' and commit the changes."
+//   )
+// }
+
+fail('fail')
