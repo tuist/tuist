b88e0036eb8abcf8859e1ff3e79cfa216e83cbcb
Pedro Piñera <pepibumur@gmail.com>
2018-09-05 16:11:42 +0200

    test danger check

--- Tests/ProjectDescriptionTests/PlatformTests .swift
+++ Tests/ProjectDescriptionTests/PlatformTests .swift
@@ -4,6 +4,11 @@ import XCTest
 
 final class PlatformTests: XCTestCase {
     func test_toJSON() {
+      do {
+        /* Nothing */
+      } catch {
+        
+      }
         assertCodableEqualToJson([Platform.iOS], "[\"ios\"]")
         assertCodableEqualToJson([Platform.macOS], "[\"macos\"]")
         assertCodableEqualToJson([Platform.watchOS], "[\"watchos\"]")
