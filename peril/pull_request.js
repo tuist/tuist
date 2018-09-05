223817d812515054c6a6d9a50f9dd77f5df1bd68
Pedro Piñera <pepibumur@gmail.com>
2018-09-05 16:10:47 +0200

    Add error handling check

--- peril/pull_request.js
+++ peril/pull_request.js
@@ -1,2 +1,39 @@
 import { schedule, danger, warn, fail } from 'danger'
 import { includes } from 'lodash'
+
+// Variables
+
+const modified = danger.git.modified_files
+const newFiles = danger.git.created_files
+const files = [danger.git.modified_files, danger.git.created_files].flat
+
+// Utilities
+
+const apiParams = () => ({
+  ...danger.github.thisPR,
+  ref: danger.github.pr.head.ref,
+})
+
+const getContent = path => {
+  return await danger.github.api.repos.getContent({ ...apiParams(), path })
+}
+
+// Error handling
+const checkErrorHandling = async () => {
+  // do { /* some swift code */ } catch { /* some rescue logic here */ }
+  const regex = /do\s*\{[\s\S]*\}\s*catch\s*{[\s\S]*\}/
+  await files.forEach((path) => {
+    const content = await getContent(path)
+    if (regex.test(content)) {
+      fail(`The file ${path} is handling errors without a concrete list`)
+    }
+  })
+}
+
+
+// Schedule
+
+schedule(async () => {
+  await checkErrorHandling()
+})
+
