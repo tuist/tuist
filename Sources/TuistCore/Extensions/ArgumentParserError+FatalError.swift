57159867b5b8cc9135eb4668cc56b6d51065c0e7
Pedro Piñera Buendía <pedro+work@ppinera.es>
2018-09-28 12:43:25 +0200

    Extract Core into its own repository (#141)

--- Sources/TuistCore/Extensions/ArgumentParserError+FatalError.swift
+++
@@ -1,7 +0,0 @@
-import Utility
-
-extension ArgumentParserError: FatalError {
-    public var type: ErrorType {
-        return .abort
-    }
-}
--- Sources/TuistCoreTesting/Extensions/NSError+TestData.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public extension NSError {
-    public static func test() -> NSError {
-        return NSError(domain: "test", code: 1, userInfo: nil)
-    }
-}
