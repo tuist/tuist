bfd5de7ce71fa5ae983fa3addf88127a2883bf9c
Pedro Piñera <pedro@ppinera.es>
2018-12-19 09:21:32 +0100

    Support cpp source files

---
+++ Tests/TuistKitTests/Models/TargetTests.swift
@@ -0,0 +1,9 @@
+import Foundation
+@testable import TuistKit
+import XCTest
+
+final class TargetTests: XCTestCase {
+    func test_validSourceExtensions() {
+        XCTAssertEqual(Target.validSourceExtensions, ["m", "swift", "mm", "cpp"])
+    }
+}
