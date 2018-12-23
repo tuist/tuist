30f6ff42cb7cd3a81b61255198c351ae428a6be0
Pedro Piñera <pedro@ppinera.es>
2018-12-23 21:10:54 +0100

    Remove byebug line

--- features/step_definitions/shared/tuist.rb
+++ features/step_definitions/shared/tuist.rb
@@ -5,5 +5,4 @@ end
 Then(/tuist generates the project/) do
   system("swift", "run", "tuist", "generate", "--path", @dir)
   @workspace_path = Dir.glob(File.join(@dir, "*.xcworkspace")).first
-  # require 'byebug'; byebug;
 end
