90a6a90ad8afe14cbffcc242dd34f01fc8e074f2
Pedro Piñera <pedro@ppinera.es>
2018-12-24 10:07:39 +0100

    Fix test

--- features/step_definitions/init.rb
+++ features/step_definitions/init.rb
@@ -1,3 +1,3 @@
 When(/I initialize a (.+) (.+) named (.+)/) do |platform, product, name|
-  Tuist::System.run("swift", "run", "tuist", "init", "--path", @dir, "--platform", platform, "--product", product, "--name", name)
+  system("swift", "run", "tuist", "init", "--path", @dir, "--platform", platform, "--product", product, "--name", name)
 end
