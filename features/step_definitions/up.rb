9afb5bd70a3d67098b9ee2c3731cafb17940a7de
Marcin Iwanicki <miwanicki1@bloomberg.net>
2019-02-04 19:38:47 +0000

    Fix up.rb formatting

--- features/step_definitions/up.rb
+++ features/step_definitions/up.rb
@@ -1,9 +1,9 @@
 
 Then(/I should have (.+) installed/) do |tool_path|
-    @tool_path = tool_path
-    assert(File.exist?(tool_path), "#{tool_path} was not installed")
+  @tool_path = tool_path
+  assert(File.exist?(tool_path), "#{tool_path} was not installed")
 end
 
 After do
-    FileUtils.rm_r(@tool_path) unless @tool_path.nil?
+  FileUtils.rm_r(@tool_path) unless @tool_path.nil?
 end
