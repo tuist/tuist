c88bbf0db12d64ef6654552567ddc350430266c6
Marcin Iwanicki <miwanicki1@bloomberg.net>
2019-02-07 14:37:08 +0000

    Fix issues found by Robocop

--- features/step_definitions/shared/tuist.rb
+++ features/step_definitions/shared/tuist.rb
@@ -16,7 +16,7 @@ end
 
 Then(/tuist generates reports error "(.+)"/) do |error|
   expected_msg = error.sub!("${ARG_PATH}", @dir)
-  stdin, stdout, stderr, wait_thr = Open3.popen3("swift", "run", "tuist", "generate", "--path", @dir) 
+  _, _, stderr, wait_thr = Open3.popen3("swift", "run", "tuist", "generate", "--path", @dir)
   actual_msg = stderr.gets.to_s.strip
   assert_equal(expected_msg, actual_msg)
   assert_equal(wait_thr.value.exitstatus, 1)
