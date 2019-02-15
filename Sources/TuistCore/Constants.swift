840035b9476570d90e1ee28b7fd4978d254e474c
Oliver Atkinson <oliveratkinson@me.com>
2019-02-15 15:17:46 +0000

    Change the swift version to 4.2
    Why:
    
    * 4.2.1 is an invalid specification for SWIFT_VERSION.

--- Sources/TuistCore/Constants.swift
+++ Sources/TuistCore/Constants.swift
@@ -6,6 +6,6 @@ public class Constants {
     public static let binName = "tuist"
     public static let gitRepositoryURL = "https://github.com/tuist/tuist.git"
     public static let version = "0.11.0"
-    public static let swiftVersion: String = "4.2.1"
+    public static let swiftVersion: String = "4.2"
     public static let bundleName: String = "tuist.zip"
 }
