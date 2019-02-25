9b6f66d49a1b49eb562b087e9da6e699aa495e6a
Kassem Wridan <kas@mxpr.net>
2019-02-25 21:16:40 +0000

    Adding more tests for `GraphManifestLoader`

--- Tests/TuistKitTests/Graph/GraphManifestLoaderTests.swift
+++ Tests/TuistKitTests/Graph/GraphManifestLoaderTests.swift
@@ -40,34 +40,124 @@ final class GraphManifestLoaderTests: XCTestCase {
                                       deprecator: deprecator)
     }
 
-    func test_load_when_swift() throws {
+    func test_loadProject() throws {
+        // Given
         let content = """
         import ProjectDescription
         let project = Project(name: "tuist")
         """
-
+        
         let manifestPath = fileHandler.currentPath.appending(component: Manifest.project.fileName)
         try content.write(to: manifestPath.url,
                           atomically: true,
                           encoding: .utf8)
-
+        
+        // When
         let got = try subject.loadProject(at: fileHandler.currentPath)
+        
+        // Then
+        
         XCTAssertEqual(got.name, "tuist")
     }
-
-    func test_manifestPath_when_swift() throws {
-        try fileHandler.touch(fileHandler.currentPath.appending(component: Manifest.project.fileName))
-        let got = try subject.manifestPath(at: fileHandler.currentPath, manifest: .project)
-
-        XCTAssertEqual(got, fileHandler.currentPath.appending(component: Manifest.project.fileName))
+    
+    func test_loadWorkspace() throws {
+        // Given
+        let content = """
+        import ProjectDescription
+        let workspace = Workspace(name: "tuist", projects: [])
+        """
+        
+        let manifestPath = fileHandler.currentPath.appending(component: Manifest.workspace.fileName)
+        try content.write(to: manifestPath.url,
+                          atomically: true,
+                          encoding: .utf8)
+        
+        // When
+        let got = try subject.loadWorkspace(at: fileHandler.currentPath)
+        
+        // Then
+        XCTAssertEqual(got.name, "tuist")
+    }
+    
+    func test_loadSetup() throws {
+        // Given
+        let content = """
+        import ProjectDescription
+        let setup = Setup([
+                        .custom(name: "hello", meet: ["a", "b"], isMet: ["c"])
+                    ])
+        """
+        
+        let manifestPath = fileHandler.currentPath.appending(component: Manifest.setup.fileName)
+        try content.write(to: manifestPath.url,
+                          atomically: true,
+                          encoding: .utf8)
+        
+        // When
+        let got = try subject.loadSetup(at: fileHandler.currentPath)
+        
+        // Then
+        let customUp = got.first as? UpCustom
+        XCTAssertEqual(got.count, 1)
+        XCTAssertEqual(customUp?.name, "hello")
+        XCTAssertEqual(customUp?.meet, ["a", "b"])
+        XCTAssertEqual(customUp?.isMet, ["c"])
+    }
+    
+    func test_load_invalidFormat() throws {
+        // Given
+        let content = """
+        import ABC
+        let project
+        """
+        
+        let manifestPath = fileHandler.currentPath.appending(component: Manifest.project.fileName)
+        try content.write(to: manifestPath.url,
+                          atomically: true,
+                          encoding: .utf8)
+        
+        // When / Then
+        XCTAssertThrowsError(
+            try subject.loadProject(at: fileHandler.currentPath)
+        )
+    }
+    
+    func test_load_missingManifest() throws {
+        XCTAssertThrowsError(
+            try subject.loadProject(at: fileHandler.currentPath)
+        ) { error in
+            XCTAssertEqual(error as? GraphManifestLoaderError, GraphManifestLoaderError.manifestNotFound(.project, fileHandler.currentPath))
+        }
     }
 
-    func test_manifestsAt_when_swift() throws {
+    func test_manifestPath() throws {
+        // Given
+        let manifestsPaths = Manifest.allCases.map {
+            fileHandler.currentPath.appending(component: $0.fileName)
+        }
+        try manifestsPaths.forEach { try fileHandler.touch($0) }
+        
+        // When
+        let got = try Manifest.allCases.map {
+            try subject.manifestPath(at: fileHandler.currentPath, manifest: $0)
+        }
+
+        // Then
+        XCTAssertEqual(got, manifestsPaths)
+    }
+
+    func test_manifestsAt() throws {
+        // Given
         try fileHandler.touch(fileHandler.currentPath.appending(component: "Project.swift"))
         try fileHandler.touch(fileHandler.currentPath.appending(component: "Workspace.swift"))
+        try fileHandler.touch(fileHandler.currentPath.appending(component: "Setup.swift"))
+        
+        // When
         let got = subject.manifests(at: fileHandler.currentPath)
 
+        // Then
         XCTAssertTrue(got.contains(.project))
         XCTAssertTrue(got.contains(.workspace))
+        XCTAssertTrue(got.contains(.setup))
     }
 }
