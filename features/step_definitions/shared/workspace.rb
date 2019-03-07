7f3ab27eaa4c411e642e75f8558af7b5bf5013d2
Pedro Piñera <pedro@ppinera.es>
2019-03-07 23:56:49 +0100

    Delete copying the working directory

--- features/step_definitions/shared/workspace.rb
+++ features/step_definitions/shared/workspace.rb
@@ -8,10 +8,5 @@ And(/I have a working directory/) do
 end
 
 After do |scenario|
-  if scenario.failed? && !@dir.nil?
-    tmp_dir = Dir.mktmpdir
-    FileUtils.cp_r(@dir, tmp_dir)
-    puts "The failing working directory has been copied into: #{tmp_dir}"
-  end
   FileUtils.rm_r(@dir) unless @dir.nil?
 end
