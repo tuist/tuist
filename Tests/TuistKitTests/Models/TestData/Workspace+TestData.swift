ccda2c54412109195b27830a59cdd4f3f12a85aa
Kassem Wridan <kwridan@bloomberg.net>
2019-03-20 23:53:52 +0000

    Adding WorkspaceStructureGenerator

---
+++ Tests/TuistKitTests/Models/TestData/Workspace+TestData.swift
@@ -0,0 +1,13 @@
+import Foundation
+import Basic
+@testable import TuistKit
+
+extension Workspace {
+    static func test(name: String = "test",
+                     projects: [AbsolutePath] = [],
+                     additionalFiles: [Workspace.Element] = []) -> Workspace {
+        return Workspace(name: name,
+                         projects: projects,
+                         additionalFiles: additionalFiles)
+    }
+}
