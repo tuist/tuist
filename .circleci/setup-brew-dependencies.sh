b5ca794c659213b1948233a272ffd932f0e2734a
Kassem Wridan <kwridan@bloomberg.net>
2019-03-24 09:18:57 +0000

    Updating script

--- .circleci/setup-brew-dependencies.sh
+++ .circleci/setup-brew-dependencies.sh
@@ -4,6 +4,6 @@ function version {
     echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }';
 }
 
-if [ $(version $(swiftformat --version)) -lt $(version $(cat ../.swiftformat-version)) ]; then
+if [ $(version $(swiftformat --version)) -lt $(version $(cat .swiftformat-version)) ]; then
     brew upgrade swiftformat
 fi
