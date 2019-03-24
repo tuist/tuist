00119119658c60ef2dc5176ddbdcf9d9cdc2104b
Kassem Wridan <kwridan@bloomberg.net>
2019-03-24 09:07:24 +0000

    Apply patch to conditionally upgrade swiftformat (by @ollieatkinson)

---
+++ .circleci/setup-brew-dependencies.sh
@@ -0,0 +1,7 @@
+function version {
+    echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }';
+}
+
+if [ $(version $(swiftformat --version)) -lt $(version $(cat ../.swiftformat-version)) ]; then
+    brew upgrade swiftformat
+fi
