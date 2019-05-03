d345265117f9089e258acb4ba3c028fd594b5016
Kassem Wridan <kas@mxpr.net>
2019-05-03 21:02:43 +0100

    Improving doc comments for clock

--- Sources/TuistCore/Utils/Clock.swift
+++ Sources/TuistCore/Utils/Clock.swift
@@ -1,14 +1,26 @@
 import Foundation
 
-public protocol ClockTimer {
-    func stop() -> TimeInterval
-}
-
+/// A clock can be used to obtain the current date
+/// as well as create `ClockTimers` to measure
+/// time intervals between events.
 public protocol Clock {
     var now: Date { get }
     func startTimer() -> ClockTimer
 }
 
+/// A clock timer can be used to measure
+/// time intervals between events
+///
+/// A timer can be obtaine from a `Clock`
+/// by calling `startTimer()`. Calling
+/// `stop()` on the timer will return
+/// the time interval since the start.
+public protocol ClockTimer {
+    func stop() -> TimeInterval
+}
+
+/// A wall clock is the default implementation of
+/// the `Clock` interface
 public class WallClock: Clock {
     public var now: Date {
         return Date()
