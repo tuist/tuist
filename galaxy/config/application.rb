3d1271ae01798fa6c4c5e67d3eb53bb0471f043d
Pedro Piñera <pedro@ppinera.es>
2019-07-01 16:34:09 +0200

    Merge galaxy into the Tuist repository

---
+++ galaxy/config/application.rb
@@ -0,0 +1,21 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Galaxy
+  class Application < Rails::Application
+    config.i18n.load_path += Dir[config.root.join('frontend/components/**/*.yml')]
+    config.autoload_paths << config.root.join('frontend/components')
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
---
+++ galaxy/frontend/components/index.js
@@ -0,0 +1,1 @@
+import "components/button/button";
