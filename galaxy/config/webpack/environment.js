50336ff152ce330bf433495363fdc7d052ef90c7
Pedro Piñera <pedro@ppinera.es>
2019-07-01 16:43:50 +0200

    Add bootstrap

--- galaxy/config/webpack/environment.js
+++ galaxy/config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.set(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default'],
+  })
+)
+
 module.exports = environment
