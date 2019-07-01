03749c114330e77eaa04004bff78b1e35b0df0ad
Pedro Piñera <pedro@ppinera.es>
2019-07-01 18:02:56 +0200

    configure Sentry

--- galaxy/config/application.rb
+++ galaxy/config/application.rb
@@ -27,5 +27,7 @@ module Galaxy
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.filter_parameters << :password
   end
 end
