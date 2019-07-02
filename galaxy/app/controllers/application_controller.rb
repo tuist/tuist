2c6faab49e0d0c5f849dec5d0e0975747c7753e0
Pedro Piñera Buendía <pedro@ppinera.es>
2019-07-02 08:56:38 +0200

    Add Sentry for crash reporting (#429)
    * configure Sentry
    
    * Remove application.js from javascript/packs
    
    * Use Ruby 2.6.3 on CircleCI

--- galaxy/app/controllers/application_controller.rb
+++ galaxy/app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_raven_context
+
+  private
+
+  def set_raven_context
+    Raven.user_context(id: session[:current_user_id]) # or anything else in session
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+  end
 end
---
+++ galaxy/config/initializers/sentry.rb
@@ -0,0 +1,6 @@
+if ENV.key?("SENTRY_DSN")
+  Raven.configure do |config|
+    config.dsn = ENV["SENTRY_DSN"]
+    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  end
+end
