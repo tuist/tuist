daac41c07e1f8bb307e4e0e6a38d851f7a187f92
Oliver Atkinson <oliveratkinson@me.com>
2019-07-03 22:28:20 +0100

    Update Fixture

--- fixtures/ios_app_with_frameworks/Framework1/Project.swift
+++ fixtures/ios_app_with_frameworks/Framework1/Project.swift
@@ -2,7 +2,7 @@ import ProjectDescription
 
 let project = Project(name: "Framework1",
                       targets: [
-                          Target(name: "Framework1-iOS",
+                          Target(name: "Framework1",
                                  platform: .iOS,
                                  product: .framework,
                                  productName: "Framework1",
@@ -12,16 +12,6 @@ let project = Project(name: "Framework1",
                                  dependencies: [
                                      .project(target: "Framework2-iOS", path: "../Framework2"),
                           ]),
-                          Target(name: "Framework1-macOS",
-                                 platform: .macOS,
-                                 product: .framework,
-                                 productName: "Framework1",
-                                 bundleId: "io.tuist.Framework1",
-                                 infoPlist: "Config/Framework1-Info.plist",
-                                 sources: "Sources/**",
-                                 dependencies: [
-                                    .project(target: "Framework2-macOS", path: "../Framework2"),
-                            ]),
                           Target(name: "Framework1Tests",
                                  platform: .iOS,
                                  product: .unitTests,
@@ -30,6 +20,6 @@ let project = Project(name: "Framework1",
                                  infoPlist: "Config/Framework1Tests-Info.plist",
                                  sources: "Tests/**",
                                  dependencies: [
-                                     .target(name: "Framework1-iOS"),
+                                     .target(name: "Framework1"),
                           ]),
 ])
