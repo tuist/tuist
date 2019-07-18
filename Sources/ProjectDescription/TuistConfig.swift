19a2cb5b73879fe56b69f9f008e6e8084d116b6a
Pedro Piñera <pedro@ppinera.es>
2019-07-18 11:09:55 +0200

    Add TuistConfig manifest model

---
+++ Sources/ProjectDescription/TuistConfig.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+/// This model allows to configure Tuist.
+public class TuistConfig: Codable {
+    /// Contains options related to the project generation.
+    ///
+    /// - generateManifestElement: When passed, Tuist generates the projects, targets and schemes to compile the project manifest.
+    public enum GenerationOption: String, Codable {
+        case generateManifestElement
+    }
+
+    /// Generation options.
+    let generationOptions: [GenerationOption]
+
+    /// Initializes the tuist cofiguration.
+    ///
+    /// - Parameter generationOptions: Generation options.
+    public init(generationOptions: [GenerationOption]) {
+        self.generationOptions = generationOptions
+        dumpIfNeeded(self)
+    }
+}
