6c91bb20c76a79219065e4b2e6ba7299b5dc5abc
Pedro Piñera <pedro@ppinera.es>
2019-08-03 04:17:06 -0400

    Add model changes to ProjectDescription

---
+++ Sources/ProjectDescription/CompatibleXcodeVersions.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+/// Enum that represents all the Xcode versions that a project or set of projects is compatible with.
+public enum CompatibleXcodeVersions: ExpressibleByArrayLiteral, Codable, Equatable {
+    /// The project supports all Xcode versions.
+    case all
+
+    /// List of versions that are supported by the project.
+    case list([String])
+
+    // MARK: - ExpressibleByArrayLiteral
+
+    public init(arrayLiteral elements: [String]) {
+        self = .list(elements)
+    }
+
+    public init(arrayLiteral elements: String...) {
+        self = .list(elements)
+    }
+
+    enum CodignKeys: String, CodingKey {
+        case type
+        case value
+    }
+
+    // MARK: - Codable
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodignKeys.self)
+        switch self {
+        case .all:
+            try container.encode("all", forKey: .type)
+        case let .list(versions):
+            try container.encode("list", forKey: .type)
+            try container.encode(versions, forKey: .value)
+        }
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodignKeys.self)
+        let type = try container.decode(String.self, forKey: .type)
+
+        switch type {
+        case "all":
+            self = .all
+        case "list":
+            self = .list(try container.decode([String].self, forKey: .value))
+        default:
+            throw DecodingError.dataCorruptedError(forKey: CodignKeys.type, in: container, debugDescription: "Invalid type \(type)")
+        }
+    }
+}
---
+++ Tests/ProjectDescriptionTests/CompatibleXcodeVersionsTests.swift
@@ -0,0 +1,23 @@
+import Foundation
+import XCTest
+
+@testable import ProjectDescription
+@testable import TuistCoreTesting
+
+final class CompatibleXcodeVersionsTests: XCTestCase {
+    func test_codable_when_all() {
+        // Given
+        let subject = CompatibleXcodeVersions.all
+
+        // Then
+        XCTAssertCodable(subject)
+    }
+
+    func test_codable_when_list() {
+        // Given
+        let subject = CompatibleXcodeVersions.list(["10.3"])
+
+        // Then
+        XCTAssertCodable(subject)
+    }
+}
