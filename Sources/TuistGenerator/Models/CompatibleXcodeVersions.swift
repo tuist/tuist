5c182fe24d5cb22b64aa21c5d42a499d9112277f
Pedro Piñera <pedro@ppinera.es>
2019-08-03 04:23:53 -0400

    Add models to the TuistGenerator target

---
+++ Sources/TuistGenerator/Models/CompatibleXcodeVersions.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+/// Enum that represents all the Xcode versions that a project or set of projects is compatible with.
+public enum CompatibleXcodeVersions: Equatable, Hashable {
+    /// The project supports all Xcode versions.
+    case all
+
+    /// List of versions that are supported by the project.
+    case list([String])
+
+    // MARK: - ExpressibleByArrayLiteral
+
+    public init(arrayLiteral elements: [String]) {
+        self = .list(elements)
+    }
+
+    public init(arrayLiteral elements: String...) {
+        self = .list(elements)
+    }
+}
