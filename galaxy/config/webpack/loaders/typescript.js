000668f8e4eac3003c464df373d449beb3d024d3
Pedro Piñera <pedro@ppinera.es>
2019-09-17 11:22:54 +0200

    Fix

--- galaxy/config/webpack/loaders/typescript.js
+++ galaxy/config/webpack/loaders/typescript.js
@@ -1,11 +1,16 @@
-const PnpWebpackPlugin = require('pnp-webpack-plugin')
+const path = require('path')
+// const PnpWebpackPlugin = require('pnp-webpack-plugin')
 
+const configFile = path.join(__dirname, '../../../tsconfig.json')
 module.exports = {
   test: /\.(ts|tsx)?(\.erb)?$/,
   use: [
     {
       loader: 'ts-loader',
-      options: PnpWebpackPlugin.tsLoaderOptions()
-    }
-  ]
+      options: {
+        // ...PnpWebpackPlugin.tsLoaderOptions(),
+        configFile,
+      },
+    },
+  ],
 }
