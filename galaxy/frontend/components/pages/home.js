e22c0f4edc2e3e712a9b752939f8f6f58e2ed39e
Pedro Piñera <pedro@ppinera.es>
2019-09-18 13:14:20 +0200

    Changes

---
+++ galaxy/frontend/components/pages/home.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+class HomePage extends React.Component {
+    render() {
+        return React.createElement(React.Fragment, null, "Hello world. How are you??");
+    }
+}
+export default HomePage;
+//# sourceMappingURL=home.js.map
---
+++ galaxy/frontend/components/pages/home.test.js
@@ -0,0 +1,6 @@
+describe('this is just a sample test', () => {
+    test('2 should be equal to 2', () => {
+        expect(2).toBe(2);
+    });
+});
+//# sourceMappingURL=home.test.js.map
