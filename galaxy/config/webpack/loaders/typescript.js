9742064479df07f2ecacf618ea179a38e061a282
Pedro Piñera <pedro@ppinera.es>
2019-09-18 12:55:21 +0200

    Fix Galaxy deployment

--- galaxy/config/webpack/loaders/typescript.js
+++ galaxy/config/webpack/loaders/typescript.js
@@ -2,12 +2,7 @@ const path = require('path')
 
 const configFile = path.join(__dirname, '../../../tsconfig.json')
 module.exports = {
-  test: function(modulePath) {
-    return (
-      (modulePath.endsWith('.ts') || modulePath.endsWith('.tsx')) &&
-      !(modulePath.endsWith('test.ts') || modulePath.endsWith('test.tsx'))
-    )
-  },
+  test: /\.tsx?$/,
   use: [
     {
       loader: 'ts-loader',
