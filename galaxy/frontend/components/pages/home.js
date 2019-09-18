dc5945c95862d30141ba2c7b35e01c05ef2375ab
Pedro Piñera <pedro@ppinera.es>
2019-09-18 15:23:40 +0200

    Fix it. Yes!

--- galaxy/frontend/components/pages/home.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-class HomePage extends React.Component {
-    render() {
-        return React.createElement(React.Fragment, null, "Hello world. How are you??");
-    }
-}
-export default HomePage;
-//# sourceMappingURL=home.js.map
