7246d710af2d07801e7e1aafd5d56b5fd532ad32
Pedro Piñera Buendía <pedro@ppinera.es>
2019-09-20 08:48:09 +0200

    Fix build phases not being generated in the right positions (#506)
    * Fix build phases not being generated in the right positions
    
    * Remove extra space
    
    * Add CHANGELOG entry
    
    * Fix step
    
    * Fix tests
    
    * Address comments
    
    * Add missing line

--- features/step_definitions/shared/tuist.rb
+++ features/step_definitions/shared/tuist.rb
@@ -7,16 +7,17 @@ end
 Then(/tuist generates the project/) do
   system("swift", "run", "tuist", "generate", "--path", @dir)
   @workspace_path = Dir.glob(File.join(@dir, "*.xcworkspace")).first
+  @xcodeproj_path = Dir.glob(File.join(@dir, "*.xcodeproj")).first
 end
 
 Then(/tuist sets up the project/) do
   system("swift", "run", "tuist", "up", "--path", @dir)
   @workspace_path = Dir.glob(File.join(@dir, "*.xcworkspace")).first
+  @xcodeproj_path = Dir.glob(File.join(@dir, "*.xcodeproj")).first
 end
 
-Then(/tuist generates yields error "(.+)"/) do |error|
-  expected_msg = error.sub!("${ARG_PATH}", @dir)
-  system("swift", "build")
+Then(/tuist generate yields error "(.+)"/) do |error|
+  expected_msg = error.gsub("${ARG_PATH}", @dir)
   _, stderr, status = Open3.capture3("swift", "run", "--skip-build", "tuist", "generate", "--path", @dir)
   actual_msg = stderr.strip
 
@@ -27,7 +28,7 @@ Then(/tuist generates yields error "(.+)"/) do |error|
     Does not contain the expected:
       #{error}
   EOD
-  assert actual_msg.include?(error), error_message
+  assert actual_msg.include?(expected_msg), error_message
   refute status.success?
 end
 
--- features/step_definitions/shared/xcode.rb
+++ features/step_definitions/shared/xcode.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'simctl'
+require 'xcodeproj'
 
 Then(/I should be able to (.+) the scheme (.+)/) do |action, scheme|
   @derived_data_path = File.join(@dir, "DerivedData")
@@ -36,4 +37,24 @@ Then(/the scheme (.+) has a build setting (.+) with value (.+) for the configura
 
   search_for = "#{key} = #{value}"
   assert(out.include?(search_for), "Couldn't find '#{search_for}'")
+end
+
+Then(/the target (.+) should have the build phase (.+) in the first position/) do |target_name, phase_name|
+  project = Xcodeproj::Project.open(@xcodeproj_path)
+  targets = project.targets
+  target = targets.detect { |t| t.name == target_name }
+  flunk("Target #{target_name} not found in the project") if target.nil?
+  build_phase = target.build_phases.first
+  flunk("The target #{target_name} doesn't have build phases") if build_phase.nil?
+  assert_equal phase_name, build_phase.name
+end
+
+Then(/the target (.+) should have the build phase (.+) in the last position/) do |target_name, phase_name|
+  project = Xcodeproj::Project.open(@xcodeproj_path)
+  targets = project.targets
+  target = targets.detect { |t| t.name == target_name }
+  flunk("Target #{target_name} not found in the project") if target.nil?
+  build_phase = target.build_phases.last
+  flunk("The target #{target_name} doesn't have build phases") if build_phase.nil?
+  assert_equal phase_name, build_phase.name
 end
