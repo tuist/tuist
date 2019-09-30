cc5407ad980bd1034d9f6fb2032a0cbcab0acc46
Pedro Piñera <pedro@ppinera.es>
2019-09-30 18:15:03 +0200

    Remove webpack, jest, and javascript dependencies

--- galaxy-backend/config/webpack/environment.js
+++
@@ -1,7 +0,0 @@
-const { environment } = require('@rails/webpacker')
-const typescript = require('./loaders/typescript')
-
-const webpack = require('webpack')
-
-environment.loaders.prepend('typescript', typescript)
-module.exports = environment
