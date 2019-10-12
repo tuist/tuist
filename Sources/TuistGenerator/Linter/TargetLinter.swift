9aabecc6ed4493a27f877e02ad13fa20b8d0de4f
Daniel Jankowski <daniell.jankowskii@gmail.com>
2019-10-12 21:36:56 +0200

    Move a regex utility function to `TuistCore`

--- Sources/TuistGenerator/Linter/TargetLinter.swift
+++ Sources/TuistGenerator/Linter/TargetLinter.swift
@@ -156,12 +156,3 @@ class TargetLinter: TargetLinting {
         return []
     }
 }
-
-extension String {
-    func matches(pattern: String) -> Bool {
-        guard let range = self.range(of: pattern, options: .regularExpression) else {
-            return false
-        }
-        return range == self.range(of: self)
-    }
-}
