6c3320117a3f90777938f4f48d44fc65caa6d25f
Pedro Piñera <pedro@ppinera.es>
2019-10-15 13:26:56 +0200

    Fix code highlighting

--- docs/src/gatsby-theme-docz/theme/index.js
+++ docs/src/gatsby-theme-docz/theme/index.js
@@ -1,7 +1,11 @@
 import baseTheme from 'gatsby-theme-docz/src/theme/index'
 import { merge } from 'lodash/fp'
+import nightOwl from '@theme-ui/prism/presets/night-owl.json'
 
 export default merge(baseTheme, {
   styles: {
+    pre: {
+      ...nightOwl
+    }
   }
 })
