166272b4933ff52967fbaab06112fe75226e2e0b
Pedro Piñera Buendía <pedro@ppinera.es>
2019-10-15 15:27:42 +0200

    Migrate the documentation website to Docz 2 (#550)
    * Update dependencies
    
    * Fixes
    
    * More fixes
    
    * Fix code highlighting
    
    * Fix sizes

--- docs/doczrc.js
+++ docs/doczrc.js
@@ -1,4 +1,5 @@
 import doczPluginNetlify from 'docz-plugin-netlify'
+import prismTheme from "prism-react-renderer/themes/nightOwl"
 
 export default {
   title: 'Tuist Documentation',
@@ -7,53 +8,9 @@ export default {
   plugins: [doczPluginNetlify()],
   // https://github.com/pedronauck/docz/issues/793
   hashRouter: true,
-  themeConfig: {
-    codemirrorTheme: 'swifty',
-    colors: {
-      blue: '#3495E8',
-      darkBlue: '#12344F',
-      lightGray: '#F8F8F8',
-      gray: '#A3A3A3',
-      purple: '#7768AF',
-      darkPurple: '#52428E',
-      green: '#3EB270',
-      darkGreen: '#207E49',
-      yellow: '#FFC107',
-      primary: '#3495E8',
-      link: 'green',
-      sidebarHighlight: 'purple',
-    },
-    logo: {
-      src: '/public/logo.png',
-      width: 100,
-    },
-    styles: {
-      body: `
-        font-family: -apple-system, system-ui, "Helvetica Neue", Helvetica, Arial, Verdana, sans-serif;
-      `,
-      code: `
-        font-size: 13px;
-        font-family: Menlo, Consolas, Monaco, "Courier New", monospace, serif;
-        background-color: white;
-      `,
-    },
-  },
   public: './public',
-  htmlContext: {
-    favicon: 'public/favicon.ico',
-    head: {
-      links: [
-        {
-          rel: 'stylesheet',
-          href: '/public/swifty.css',
-        },
-        {
-          rel: 'stylesheet',
-          href:
-            '//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css',
-        },
-      ],
-    },
+  themeConfig: {
+    prismTheme: prismTheme
   },
   menu: [
     'Getting started',
