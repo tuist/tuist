d6402799f5e11a67e153a24c4ca854ad7cc30cd9
Pedro Piñera <pedro@ppinera.es>
2019-10-29 20:05:17 +0100

    Fix pipeline (again)

---
+++ galaxy/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
