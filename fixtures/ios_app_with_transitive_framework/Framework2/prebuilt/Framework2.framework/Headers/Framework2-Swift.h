d6711c8703097463fcee076f94e0ac6090bbac3a
Pedro Piñera <pedro@ppinera.es>
2019-12-06 17:59:25 +0100

    Precompile frameworks & libraries with Swift 5.1.2

--- fixtures/ios_app_with_transitive_framework/Framework2/prebuilt/Framework2.framework/Headers/Framework2-Swift.h
+++ fixtures/ios_app_with_transitive_framework/Framework2/prebuilt/Framework2.framework/Headers/Framework2-Swift.h
@@ -194,7 +194,7 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 #if __has_attribute(external_source_symbol)
 # pragma push_macro("any")
 # undef any
-# pragma clang attribute push(__attribute__((external_source_symbol(language="Swift", defined_in="PrebuiltStaticFramework",generated_declaration))), apply_to=any(function,enum,objc_interface,objc_category,objc_protocol))
+# pragma clang attribute push(__attribute__((external_source_symbol(language="Swift", defined_in="Framework2",generated_declaration))), apply_to=any(function,enum,objc_interface,objc_category,objc_protocol))
 # pragma pop_macro("any")
 #endif
 
