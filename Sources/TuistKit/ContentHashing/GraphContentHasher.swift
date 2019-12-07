e071b496628e1c68ed59a33df3e3a142ae1330f8
Andrea Cipriani <andrea.cipriani@soundcloud.com>
2019-12-07 19:19:30 +0100

    Hash productName, platform and make sure sources are sorted

--- Sources/TuistKit/ContentHashing/GraphContentHasher.swift
+++ Sources/TuistKit/ContentHashing/GraphContentHasher.swift
@@ -22,14 +22,29 @@ public final class GraphContentHasher: GraphContentHashing {
     }
     
     private func makeContentHash(of targetNode: TargetNode) throws -> String {
-        return try hashSources(of: targetNode)
         //TODO: extend function to consider build settings, compiler flags, dependencies, and a lot more
+        let sourcesHash = try hashSources(of: targetNode)
+        let productHash = try hash(string: targetNode.target.productName)
+        let platformHash = try hash(string: targetNode.target.platform.rawValue)
+        return try hash(strings: [sourcesHash, productHash, platformHash])
     }
     
     private func hashSources(of targetNode: TargetNode) throws -> String {
-        let hashes = try targetNode.target.sources.map(md5)
-        guard let joinedHash = hashes.joined().checksum(algorithm: .md5) else {
-            throw ContentHashingError.stringHashingFailed(hashes.joined())
+        let hashes = try targetNode.target.sources.sorted(by: { $0.path < $1.path }).map(md5)
+        let joinedHash = try hash(strings: hashes)
+        return joinedHash
+    }
+    
+    private func hash(string: String) throws -> String {
+        guard let hash = string.checksum(algorithm: .md5) else {
+            throw ContentHashingError.stringHashingFailed(string)
+        }
+        return hash
+    }
+    
+    private func hash(strings: [String]) throws -> String {
+        guard let joinedHash = strings.joined().checksum(algorithm: .md5) else {
+            throw ContentHashingError.stringHashingFailed(strings.joined())
         }
         return joinedHash
     }
@@ -38,9 +53,9 @@ public final class GraphContentHasher: GraphContentHashing {
         guard let sourceData = try? fileHandler.readFile(source.path) else {
             throw ContentHashingError.fileNotFound(source.path)
         }
-        guard let checksum = sourceData.checksum(algorithm: .md5) else {
+        guard let hash = sourceData.checksum(algorithm: .md5) else {
             throw ContentHashingError.fileHashingFailed(source.path)
         }
-        return checksum
+        return hash
     }
 }
