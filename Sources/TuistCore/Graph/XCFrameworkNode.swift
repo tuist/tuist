efe26d93cb95e05dbdef29e685417d1d9c0cd808
ldindu <lakpadindu@hotmail.com>
2019-12-08 18:36:52 +0000

    Add tests for XCFrameworkNode

--- Sources/TuistCore/Graph/XCFrameworkNode.swift
+++ Sources/TuistCore/Graph/XCFrameworkNode.swift
@@ -4,6 +4,10 @@ import TuistSupport
 
 public class XCFrameworkNode: PrecompiledNode {
     
+    enum XCFrameworkNodeCodingKeys: String, CodingKey {
+        case libraries
+    }
+
     public let libraries: [XCFrameworkInfoPlist.Library]
     public let primaryBinaryPath: AbsolutePath
 
@@ -22,9 +26,11 @@ public class XCFrameworkNode: PrecompiledNode {
     }
 
     override public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(path.pathString, forKey: .path)
-        try container.encode(name, forKey: .name)
-        try container.encode("precompiled", forKey: .type)
+        var parentContainer = encoder.container(keyedBy: CodingKeys.self)
+        try parentContainer.encode(path.pathString, forKey: .path)
+        try parentContainer.encode(name, forKey: .name)
+        try parentContainer.encode("precompiled", forKey: .type)
+        var container = encoder.container(keyedBy: XCFrameworkNodeCodingKeys.self)
+        try container.encode(libraries, forKey: .libraries)
     }
 }
