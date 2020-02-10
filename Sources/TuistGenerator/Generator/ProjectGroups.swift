3580c871b3467b54e179dd387eb0cf9753626b8f
Adam Khazi <akhazi@bloomberg.net>
2020-02-10 10:41:56 +0000

    xcodegen comment

--- Sources/TuistGenerator/Generator/ProjectGroups.swift
+++ Sources/TuistGenerator/Generator/ProjectGroups.swift
@@ -64,6 +64,8 @@ class ProjectGroups {
         return group
     }
 
+    // The sorting implementation was taken from https://github.com/yonaskolb/XcodeGen/blob/d64cfff8a1ca01fd8f18cbb41f72230983c4a192/Sources/XcodeGenKit/PBXProjGenerator.swift
+    // We require exactly the same sort which places groups over files while using the PBXGroup from Xcodeproj.
     static func sortGroups(group: PBXGroup) {
         let children = group.children
             .sorted { (child1, child2) -> Bool in
