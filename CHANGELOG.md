993e635f2099fc6084ca69319931dea116cb398c
Kas <kwridan@users.noreply.github.com>
2020-02-12 10:59:23 +0000

    Fix static products false positive lint warning (#981)
    Resolves https://github.com/tuist/tuist/issues/973
    
    - In the event a static product was linked more than once in a graph a warning was raised
    - The `GraphLinter` had a step to check for this scenario during its graph traversal
    - The previous method could accidentally flag false positives in the event multiple test targets depended on the same static product
    - Additionally, depending on the order the entry nodes of a graph were evaluated there were scenarios which the check wouldn't catch due to skipping evaluated nodes
    - A new `StaticProductsGraphLinter` component is being introduced to perform this check
    - The new component treats each entry node graph path independantly
    
    Test Plan:
    
    - run `tuist generate` within `fixtures/ios_app_with_static_frameworks`
    - verify no warnings are displayed
    - manually modify the fixture to introduce an error
    
    e.g.
    
    ```swift
    // Modules/D/Project.swift
    import ProjectDescription
    
    let project = Project(name: "D",
                          targets: [
                              Target(name: "D",
                                     platform: .iOS,
                                     product: .framework,
                                     bundleId: "io.tuist.D",
                                     infoPlist: "Info.plist",
                                     sources: "Sources/**",
                                     dependencies: [
                                           .project(target: "B", path: "../B") // <-- B is transitively linked by App
                              ])
    ])
    
    ```
    
    - Run `tuist generate` and verify a warning is displayed:
    
    ```
    Warning: - Target "B" has been linked against ["App", "D"], it is a static product so may introduce unwanted side effects.
    ```

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,8 @@ Please, check out guidelines: https://keepachangelog.com/en/1.0.0/
 
 ### Fixed
 
+- Fix static products false positive lint warning by https://github.com/tuist/tuist/pull/981 @kwridan.
+
 ### Changed
 
 ## 1.2.0
---
+++ Sources/TuistGenerator/Linter/StaticProductsGraphLinter.swift
@@ -0,0 +1,220 @@
+import Basic
+import Foundation
+import TuistCore
+
+/// Static Products Graph Linter
+///
+/// A linter that identifies potential issues in a graph where
+/// static products are linked multiple times.
+///
+protocol StaticProductsGraphLinting {
+    func lint(graph: Graphing) -> [LintingIssue]
+}
+
+class StaticProductsGraphLinter: StaticProductsGraphLinting {
+    func lint(graph: Graphing) -> [LintingIssue] {
+        let nodes = graph.entryNodes
+        return warnings(in: nodes)
+            .sorted()
+            .map(lintIssue)
+    }
+
+    private func warnings(in nodes: [GraphNode]) -> Set<StaticDependencyWarning> {
+        var warnings = Set<StaticDependencyWarning>()
+        let cache = Cache()
+        nodes.forEach { node in
+            // Skip already evaluated nodes
+            guard cache.results(for: node) == nil else {
+                return
+            }
+            let results = buildStaticProductsMap(visiting: node,
+                                                 cache: cache)
+            warnings.formUnion(results.linked.flatMap(staticDependencyWarning))
+        }
+        return warnings
+    }
+
+    ///
+    /// Builds a static products map to enable performing some validation/lint checks.
+    ///
+    /// The map consists of all linked static products as follows:
+    /// `StaticProducts.linked`:
+    /// - MyStaticFrameworkA > [MyDynamicFrameworkA, MyTestsTarget]
+    /// - MyStaticFrameworkB > [MyDynamicFrameworkA, MyTestsTarget]
+    ///
+    /// The map is constructed by traversing the graph from the given node using
+    /// a depth first approach reaching to the leaf nodes. Once there, working
+    /// backwards all nodes are evaluated as follows:
+    ///
+    /// There are two "buckets", `StaticProducts.unlinked` and `StaticProducts.linked`
+    ///
+    /// - In the event a node is a static product it adds itself to the unlinked bucket
+    /// - In the event a node is a node capable of linking static products, it removes all the nodes
+    ///   from the unlinked bucket and places them in the linked bucket in format of _staticNode > [linkingNode]_.
+    ///
+    private func buildStaticProductsMap(visiting node: GraphNode,
+                                        cache: Cache) -> StaticProducts {
+        if let cachedResult = cache.results(for: node) {
+            return cachedResult
+        }
+
+        // Collect dependency results traversing the graph (dfs)
+        var results = dependencies(for: node).reduce(StaticProducts()) { results, node in
+            buildStaticProductsMap(visiting: node, cache: cache).merged(with: results)
+        }
+
+        // Static node case
+        if nodeIsStaticProduct(node) {
+            results.unlinked.insert(node)
+            cache.cache(results: results, for: node)
+            return results
+        }
+
+        // Linking node case
+        guard let linkingNode = node as? TargetNode,
+            linkingNode.target.canLinkStaticProducts() else {
+            return results
+        }
+
+        while let staticProduct = results.unlinked.popFirst() {
+            results.linked[staticProduct, default: Set()].insert(linkingNode)
+        }
+
+        cache.cache(results: results,
+                    for: node)
+
+        return results
+    }
+
+    private func staticDependencyWarning(staticProduct: GraphNode,
+                                         linkedBy: Set<TargetNode>) -> [StaticDependencyWarning] {
+        // Common dependencies between test bundles and their host apps are automatically omitted
+        // during generation - as such those shouldn't be flagged
+        //
+        // reference: https://github.com/tuist/tuist/pull/664
+        let apps: Set<GraphNode> = linkedBy.filter { $0.target.product == .app }
+        let hostedTestBundles = linkedBy
+            .filter { $0.target.product.testsBundle }
+            .filter { $0.dependencies.contains(where: { apps.contains($0) }) }
+
+        let links = linkedBy.subtracting(hostedTestBundles)
+
+        guard links.count > 1 else {
+            return []
+        }
+
+        let sortedLinks = links.sorted(by: { $0.name < $1.name })
+        return [
+            .init(staticProduct: staticProduct,
+                  linkingNodes: sortedLinks),
+        ]
+    }
+
+    private func dependencies(for node: GraphNode) -> [GraphNode] {
+        guard let targetNode = node as? TargetNode else {
+            return []
+        }
+        return targetNode.dependencies.filter { canVisit(node: $0, from: targetNode) }
+    }
+
+    private func canVisit(node: GraphNode, from: TargetNode) -> Bool {
+        guard let to = node as? TargetNode else {
+            return true
+        }
+        switch (from.target.product, to.target.product) {
+        case (.uiTests, .app):
+            // UITest bundles are hosted in a separate app (App-TestRunner) as such
+            // it should be treated as a separate graph that isn't connected to the main
+            // app's graph. It's an unfortunate side effect of declaring a target application
+            // of a UI test bundle as a dependency.
+            return false
+        default:
+            return true
+        }
+    }
+
+    private func nodeIsStaticProduct(_ node: GraphNode) -> Bool {
+        switch node {
+        case is PackageProductNode:
+            // Swift package products are currently assumed to be static
+            return true
+        case is LibraryNode:
+            return true
+        case let targetNode as TargetNode where targetNode.target.product.isStatic:
+            return true
+        default:
+            return false
+        }
+    }
+
+    private func lintIssue(from warning: StaticDependencyWarning) -> LintingIssue {
+        let staticProduct = nodeDescription(warning.staticProduct)
+        let names = warning.linkingNodes.map(\.name)
+        return LintingIssue(reason: "\(staticProduct) has been linked against \(names), it is a static product so may introduce unwanted side effects.",
+                            severity: .warning)
+    }
+
+    private func nodeDescription(_ node: GraphNode) -> String {
+        switch node {
+        case is PackageProductNode:
+            return "Package \"\(node.name)\""
+        case is LibraryNode:
+            return "Library \"\(node.name)\""
+        case is TargetNode:
+            return "Target \"\(node.name)\""
+        default:
+            return node.name
+        }
+    }
+}
+
+// MARK: - Helper Types
+
+extension StaticProductsGraphLinter {
+    private struct StaticDependencyWarning: Hashable, Comparable {
+        var staticProduct: GraphNode
+        var linkingNodes: [TargetNode]
+
+        var debugDescription: String {
+            stringDescription
+        }
+
+        private var stringDescription: String {
+            "\(staticProduct.name) > \(linkingNodes.map(\.name))"
+        }
+
+        static func < (lhs: StaticDependencyWarning,
+                       rhs: StaticDependencyWarning) -> Bool {
+            lhs.stringDescription < rhs.stringDescription
+        }
+    }
+
+    private struct StaticProducts {
+        // Unlinked static products
+        var unlinked: Set<GraphNode> = Set()
+
+        // Map of Static product to nodes that link it
+        // e.g.
+        //    - MyStaticFrameworkA > [MyDynamicFrameworkA, MyTestsTarget]
+        //    - MyStaticFrameworkB > [MyDynamicFrameworkA, MyTestsTarget]
+        var linked: [GraphNode: Set<TargetNode>] = [:]
+
+        func merged(with other: StaticProducts) -> StaticProducts {
+            StaticProducts(unlinked: unlinked.union(other.unlinked),
+                           linked: linked.merging(other.linked, uniquingKeysWith: { $0.union($1) }))
+        }
+    }
+
+    private class Cache {
+        private var cachedResults: [GraphNode: StaticProducts] = [:]
+
+        func results(for node: GraphNode) -> StaticProducts? {
+            cachedResults[node]
+        }
+
+        func cache(results: StaticProducts,
+                   for node: GraphNode) {
+            cachedResults[node] = results
+        }
+    }
+}
