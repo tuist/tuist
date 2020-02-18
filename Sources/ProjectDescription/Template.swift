da5f92922481d76db4ee89f88d2f23a213bc2c27
Marek Fořt <marek.fort@ackee.cz>
2020-02-18 08:20:01 +0100

    Add argument to Template.

--- Sources/ProjectDescription/Template.swift
+++ Sources/ProjectDescription/Template.swift
@@ -1,15 +1,29 @@
 public struct Template: Codable {
     public let description: String
+    public let arguments: [Argument]
     public let files: [File]
     public let directories: [String]
     
     public init(description: String,
+                arguments: [Argument] = [],
                 files: [File] = [],
                 directories: [String] = []) {
         self.description = description
+        self.arguments = arguments
         self.files = files
         self.directories = directories
         dumpIfNeeded(self)
+    }
+}
+
+public extension String.StringInterpolation {
+    mutating func appendInterpolation(_ value: Argument) {
+        switch value {
+        case let .required(name):
+            appendInterpolation("{{ \(name) }}")
+        case let .optional(name, default: defaultValue):
+            appendInterpolation("{{ \(name) ?? \(defaultValue) }}")
+        }
     }
 }
 
@@ -22,3 +36,43 @@ public struct File: Codable {
         self.contents = contents
     }
 }
+
+public enum Argument: Codable {
+    case required(String)
+    case optional(String, default: String)
+    
+    enum CodingKeys: String, CodingKey {
+        case type
+        case name
+        case `default`
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let name = try container.decode(String.self, forKey: .name)
+        let type = try container.decode(String.self, forKey: .type)
+        if type == "required" {
+            self = .required(name)
+        } else if type == "optional" {
+            let defaultValue = try container.decode(String.self, forKey: .default)
+            self = .optional(name, default: defaultValue)
+        } else {
+            fatalError("Argument '\(type)' not supported")
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        switch self {
+        case let .required(name):
+            try container.encode("required", forKey: .type)
+            try container.encode(name, forKey: .name)
+        case let .optional(name, default: defaultValue):
+            try container.encode("optional", forKey: .type)
+            try container.encode(name, forKey: .name)
+            try container.encode(defaultValue, forKey: .default)
+        }
+    }
+}
+
