eef7b48b4f03ce951ecb6e7b065335b6340a6d96
Pedro Piñera Buendía <pedro@ppinera.es>
2020-04-03 19:45:14 +0200

    Update Sources/TuistKit/Commands/Cloud/CloudLogoutCommand.swift
    Co-Authored-By: Andrea Cipriani <andreacipriani89@gmail.com>

--- Sources/TuistKit/Commands/Cloud/CloudLogoutCommand.swift
+++ Sources/TuistKit/Commands/Cloud/CloudLogoutCommand.swift
@@ -9,7 +9,7 @@ class CloudLogoutCommand: NSObject, Command {
     // MARK: - Attributes
 
     static let command = "logout"
-    static let overview = "Removes any existing session to authenticate on the server with he URL defined in the Config.swift file."
+    static let overview = "Removes any existing session to authenticate on the server with the URL defined in the Config.swift file."
     private let service = CloudLogoutService()
 
     // MARK: - Init
