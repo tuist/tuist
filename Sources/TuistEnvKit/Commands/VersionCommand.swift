48af30fed826edcd4e5143d3128acf702f1f5dd4
Marek Fořt <marek.fort@ackee.cz>
2020-04-10 13:21:55 +0200

    Revert "Migrate Version command."
    This reverts commit b4a69d89da1f5eccb7a6fabea1d29dd625439e47.

--- Sources/TuistEnvKit/Commands/VersionCommand.swift
+++ Sources/TuistEnvKit/Commands/VersionCommand.swift
@@ -1,16 +1,23 @@
 import Basic
 import Foundation
-import ArgumentParser
+import SPMUtility
+import TuistSupport
 
-struct VersionCommand: ParsableCommand {
-    static var configuration: CommandConfiguration {
-        CommandConfiguration(commandName: "envversion",
-                             abstract: "Outputs the current version of tuist env")
+class VersionCommand: NSObject, Command {
+    // MARK: - Command
+
+    static let command = "envversion"
+    static let overview = "Outputs the current version of tuist env."
+
+    // MARK: - Init
+
+    required init(parser: ArgumentParser) {
+        parser.add(subparser: VersionCommand.command, overview: VersionCommand.overview)
     }
 
     // MARK: - Command
 
     func run(with _: ArgumentParser.Result) {
-        try VersionService().run()
+        logger.notice("\(Constants.version)")
     }
 }
--- Sources/TuistKit/Services/VersionService.swift
+++
@@ -1,9 +0,0 @@
-import Basic
-import Foundation
-import TuistSupport
-
-final class VersionService {
-    func run() throws {
-        logger.notice("\(Constants.version)")
-    }
-}
