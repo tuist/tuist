30460b6d00c8da6930f5c448a1336614df2d2d1f
Kassem Wridan <kwridan@bloomberg.net>
2020-04-16 07:44:21 +0100

    remove redundant class

--- Sources/TuistLoader/Loaders/ManifestModelConverter.swift
+++ Sources/TuistLoader/Loaders/ManifestModelConverter.swift
@@ -8,10 +8,3 @@ public protocol ManifestModelConverting {
     func convert(manifest: ProjectDescription.Workspace, path: AbsolutePath) throws -> TuistCore.Workspace
     func convert(manifest: ProjectDescription.Project, path: AbsolutePath) throws -> TuistCore.Project
 }
-
-public class ManifestModelConverter {
-    private let converter: ManifestModelConverting
-    public init(converter: ManifestModelConverting) {
-        self.converter = converter
-    }
-}
