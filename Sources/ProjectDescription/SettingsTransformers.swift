d0afab412ecec2389fbf22d0acc48e897199c563
Natan Rolnik <natanrolnik@gmail.com>
2020-04-18 23:34:46 +0300

    Apply PR comments - part 1

--- Sources/ProjectDescription/SettingsTransformers.swift
+++ Sources/ProjectDescription/SettingsTransformers.swift
@@ -1,14 +1,12 @@
 import Foundation
 
 extension Dictionary {
-    mutating func merge(with dict: [Key: Value]) {
-        dict.forEach { self[$0.key] = $0.value }
+    mutating func merge(with other: [Key: Value]) {
+        merge(other) { $1 }
     }
 
-    func merging(with dict: [Key: Value]) -> Dictionary {
-        var copy = self
-        copy.merge(with: dict)
-        return copy
+    func merging(with other: [Key: Value]) -> Dictionary {
+        merging(other) { $1 }
     }
 }
 
@@ -38,16 +36,6 @@ public enum SwiftOptimizationLevel: String {
 public extension SettingsDictionary {
     // MARK: - Deployment target and SDKROOT
 
-    /// Sets "IPHONEOS_DEPLOYMENT_TARGET" to `value`
-    func iPhoneOSDeploymentTarget(_ value: String) -> SettingsDictionary {
-        merging(with: ["IPHONEOS_DEPLOYMENT_TARGET": value.asSettingValue])
-    }
-
-    /// Sets "SDKROOT" to "iphoneos"
-    func iPhoneOSasSDKRoot() -> SettingsDictionary {
-        merging(with: ["SDKROOT": "iphoneos"])
-    }
-
     /// Sets "SDKROOT" to `value`
     func sdkRoot(_ value: String) -> SettingsDictionary {
         merging(with: ["SDKROOT": value.asSettingValue])
@@ -64,23 +52,23 @@ public extension SettingsDictionary {
     func automaticCodeSigning(devTeam: String) -> SettingsDictionary {
         merging(with: [
             "CODE_SIGN_STYLE": "Automatic",
-            "DEVELOPMENT_TEAM": devTeam.asSettingValue
+            "DEVELOPMENT_TEAM": devTeam.asSettingValue,
         ])
     }
 
-    ///Sets "CODE_SIGN_IDENTITY" to "Apple Development"
+    /// Sets "CODE_SIGN_IDENTITY" to "Apple Development"
     func codeSignIdentityAppleDevelopment() -> SettingsDictionary {
         codeSignIdentity("Apple Development")
     }
 
-    ///Sets "CODE_SIGN_IDENTITY" to `identity`
+    /// Sets "CODE_SIGN_IDENTITY" to `identity`
     func codeSignIdentity(_ identity: String) -> SettingsDictionary {
         merging(with: ["CODE_SIGN_IDENTITY": identity.asSettingValue])
     }
 
     // MARK: - Versioning and Product Name
 
-    ///Sets "PRODUCT_NAME" to `name`
+    /// Sets "PRODUCT_NAME" to `name`
     func productName(_ name: String) -> SettingsDictionary {
         merging(with: ["PRODUCT_NAME": name.asSettingValue])
     }
@@ -106,14 +94,14 @@ public extension SettingsDictionary {
 
     // MARK: - Swift Settings
 
-    ///Sets "SWIFT_VERSION" to `version`
+    /// Sets "SWIFT_VERSION" to `version`
     func swiftVersion(_ version: String) -> SettingsDictionary {
         merging(with: ["SWIFT_VERSION": version.asSettingValue])
     }
 
-    ///Sets "OTHER_SWIFT_FLAGS" to `flags`
-    func otherSwiftFlags(_ flags: String) -> SettingsDictionary {
-        merging(with: ["OTHER_SWIFT_FLAGS": flags.asSettingValue])
+    /// Sets "OTHER_SWIFT_FLAGS" to `flags`
+    func otherSwiftFlags(_ flags: String...) -> SettingsDictionary {
+        merging(with: ["OTHER_SWIFT_FLAGS": flags.joined(separator: " ").asSettingValue])
     }
 
     /// Sets "SWIFT_COMPILATION_MODE" to the available `SwiftCompilationMode` ("singlefile" or "wholemodule")
@@ -128,19 +116,14 @@ public extension SettingsDictionary {
 
     // MARK: - Bitcode
 
-    ///Sets "ENABLE_BITCODE" to "YES" or "NO"
+    /// Sets "ENABLE_BITCODE" to "YES" or "NO"
     func bitcodeEnabled(_ enabled: Bool) -> SettingsDictionary {
         merging(with: ["ENABLE_BITCODE": enabled.asSettingValue])
     }
 
     // MARK: - Catalyst
 
-    ///Sets "SUPPORTS_MACCATALYST" to "YES" or "NO"
-    func supportsMacCatalist(_ enabled: Bool) -> SettingsDictionary {
-        merging(with: ["SUPPORTS_MACCATALYST": enabled.asSettingValue])
-    }
-
-    ///Sets "DERIVE_MACCATALYST_PRODUCT_BUNDLE_IDENTIFIER" to "YES" or "NO"
+    /// Sets "DERIVE_MACCATALYST_PRODUCT_BUNDLE_IDENTIFIER" to "YES" or "NO"
     func deriveMacCatalystProductBundleId(_ enabled: Bool) -> SettingsDictionary {
         merging(with: ["DERIVE_MACCATALYST_PRODUCT_BUNDLE_IDENTIFIER": enabled.asSettingValue])
     }
