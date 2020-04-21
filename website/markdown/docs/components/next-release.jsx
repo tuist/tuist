f84117b31099d80e32e1e29bfcd6fd9c1ca5a9dc
Pedro Piñera <pedro@ppinera.es>
2020-04-21 10:20:46 +0200

    Change release cadence to weekly

--- website/markdown/docs/components/next-release.jsx
+++ website/markdown/docs/components/next-release.jsx
@@ -5,16 +5,7 @@ import moment from 'moment'
 
 const NextRelease = () => {
   const week = moment().week()
-  let releaseDate
-  if (week % 2 == 0) {
-    releaseDate = moment()
-      .endOf('week')
-      .day(5)
-  } else {
-    releaseDate = moment()
-      .endOf('week')
-      .day(-2)
-  }
+  let releaseDate = moment().endOf('week').day(-2)
   return (
     <Styled.p>
       The next version of might get released on September{' '}
