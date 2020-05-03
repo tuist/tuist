06ca70957872b98f51f3cce2800cfbdf5b176fe6
Marek Fořt <marekfort@me.com>
2020-05-03 20:22:17 +0200

    Add errors to certificateparser.

--- Sources/TuistSigning/Certificate/CertificateParser.swift
+++ Sources/TuistSigning/Certificate/CertificateParser.swift
@@ -2,6 +2,27 @@ import Foundation
 import TuistSupport
 import TSCBasic
 
+enum CertificateParserError: FatalError, Equatable {
+    case nameParsingFailed(AbsolutePath, String)
+    case developmentTeamParsingFailed(AbsolutePath, String)
+    
+    var type: ErrorType {
+        switch self {
+        case .nameParsingFailed, .developmentTeamParsingFailed:
+            return .abort
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case let .nameParsingFailed(path, input):
+            return "We couldn't parse the development team while parsing the following output from the file \(path.pathString): \(input)"
+        case let .developmentTeamParsingFailed(path, input):
+            return "We couldn't parse the development team while parsing the following output from the file \(path.pathString): \(input)"
+        }
+    }
+}
+
 protocol CertificateParsing {
     func parse(publicKey: AbsolutePath, privateKey: AbsolutePath) throws -> Certificate
 }
@@ -12,13 +33,15 @@ final class CertificateParser: CertificateParsing {
         let isRevoked = subject.contains("REVOKED")
         
         let nameRegex = try NSRegularExpression(pattern: "CN=([^/]+)/", options: [])
-        guard let result = nameRegex.firstMatch(in: subject, options: [], range: NSRange(location: 0, length: subject.count)) else { fatalError() }
+        guard
+            let result = nameRegex.firstMatch(in: subject, options: [], range: NSRange(location: 0, length: subject.count))
+            else { throw CertificateParserError.nameParsingFailed(publicKey, subject) }
         let name = NSString(string: subject).substring(with: result.range(at: 1)).spm_chomp()
         
         let developmentTeamRegex = try NSRegularExpression(pattern: "OU=([^/]+)/", options: [])
         guard
             let developmentTeamResult = developmentTeamRegex.firstMatch(in: subject, options: [], range: NSRange(location: 0, length: subject.count))
-        else { fatalError() }
+        else { throw CertificateParserError.developmentTeamParsingFailed(publicKey, subject) }
         let developmentTeam = NSString(string: subject).substring(with: developmentTeamResult.range(at: 1)).spm_chomp()
         
         return Certificate(publicKey: publicKey,
--- Sources/TuistSigningTesting/MockSigningInstaller.swift
+++ Sources/TuistSigningTesting/MockSigningInstaller.swift
@@ -3,10 +3,14 @@ import TSCBasic
 
 public final class MockSigningInstaller: SigningInstalling {
     public init() {}
-
-    public var installSigningStub: ((AbsolutePath) throws -> Void)?
-
-    public func installSigning(at path: AbsolutePath) throws {
-        try installSigningStub?(path)
+    
+    var installProvisioningProfileStub: ((ProvisioningProfile) throws -> Void)?
+    public func installProvisioningProfile(_ provisioningProfile: ProvisioningProfile) throws {
+        try installProvisioningProfileStub?(provisioningProfile)
+    }
+    
+    var installCertificateStub: ((Certificate, AbsolutePath) throws -> Void)?
+    public func installCertificate(_ certificate: Certificate, keychainPath: AbsolutePath) throws {
+        try installCertificateStub?(certificate, keychainPath)
     }
 }
