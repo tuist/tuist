f22ff9370287f37172b3f01bfac4a03add337171
Pedro Piñera <pedro@ppinera.es>
2020-05-09 20:39:22 +0200

    Change testimonies section

--- website/src/pages/index.jsx
+++ website/src/pages/index.jsx
@@ -419,9 +419,9 @@ const Reflection = ({ name, avatarUrl, testimony, role, company }) => {
   )
 }
 
-const Reflections = () => {
+const Testimonies = () => {
   const { theme } = useThemeUI()
-  const reflections = [
+  const testimonies = [
     {
       name: 'Kassem Wridan',
       testimony:
@@ -457,19 +457,7 @@ const Reflections = () => {
         'https://pbs.twimg.com/profile_images/999765687777148928/wSJxk3Ni_400x400.jpg',
     },
   ]
-  const [offset, setOffset] = useState(0)
-  const next = () => {
-    if (offset + 1 + 3 <= reflections.length) {
-      setOffset(offset + 1)
-    }
-  }
-  const previous = () => {
-    if (offset > 0) {
-      setOffset(offset - 1)
-    }
-  }
-  const selectedTestimonies = reflections.slice(offset, offset + 3)
-
+  const testimony = testimonies[Math.floor(Math.random() * testimonies.length)]
   return (
     <div
       sx={{
@@ -505,70 +493,134 @@ const Reflections = () => {
                 sx={{ color: 'gray' }}
               >
                 Tuist is already trusted by companies that let us do the
-                heavy-lifting and complext work
+                heavy-lifting and complex work for them.
               </p>
             </div>
-            <div
-              sx={{
-                mt: 5,
-                display: 'flex',
-                flexDirection: ['column', 'row'],
-                alignItems: ['stretch', 'stretch'],
-                justifyContent: 'space-between',
-              }}
-            >
-              <div
-                sx={{
-                  alignSelf: 'center',
-                  px: 2,
-                  cursor: 'pointer',
-                  display: ['none', 'inherit'],
-                }}
-                onClick={previous}
-              >
-                <FontAwesomeIcon
-                  sx={{
-                    mt: -1,
-                    path: { fill: theme.colors.text },
-                    '&:hover': { path: { fill: theme.colors.primary } },
-                  }}
-                  icon={faChevronLeft}
-                  size="lg"
-                />
-              </div>
-              {selectedTestimonies.map((reflection, index) => {
-                return (
-                  <Reflection
-                    key={index}
-                    name={reflection.name}
-                    testimony={reflection.testimony}
-                    role={reflection.role}
-                    company={reflection.company}
-                    avatarUrl={reflection.avatarUrl}
+
+            <section className="overflow-hidden" sx={{ bg: 'background' }}>
+              <div className="relative max-w-screen-xl mx-auto pt-20 pb-12 px-4 sm:px-6 lg:px-8 lg:py-20">
+                <svg
+                  className="absolute top-full left-0 transform translate-x-80 -translate-y-24 lg:hidden"
+                  width={784}
+                  height={404}
+                  fill="none"
+                  viewBox="0 0 784 404"
+                >
+                  <defs>
+                    <pattern
+                      id="e56e3f81-d9c1-4b83-a3ba-0d0ac8c32f32"
+                      x={0}
+                      y={0}
+                      width={20}
+                      height={20}
+                      patternUnits="userSpaceOnUse"
+                    >
+                      <rect
+                        x={0}
+                        y={0}
+                        width={4}
+                        height={4}
+                        sx={{ path: { fill: theme.colors.muted } }}
+                        className="text-gray-200"
+                        fill="currentColor"
+                      />
+                    </pattern>
+                  </defs>
+                  <rect
+                    width={784}
+                    height={404}
+                    fill="url(#e56e3f81-d9c1-4b83-a3ba-0d0ac8c32f32)"
                   />
-                )
-              })}
-              <div
-                sx={{
-                  alignSelf: 'center',
-                  ml: 2,
-                  px: 2,
-                  cursor: 'pointer',
-                  display: ['none', 'inherit'],
-                }}
-                onClick={next}
-              >
-                <FontAwesomeIcon
-                  sx={{
-                    mt: -1,
-                    path: { fill: theme.colors.text },
-                    '&:hover': { path: { fill: theme.colors.primary } },
-                  }}
-                  icon={faChevronRight}
-                  size="lg"
-                />
+                </svg>
+                <svg
+                  className="hidden lg:block absolute right-full top-1/2 transform translate-x-1/2 -translate-y-1/2"
+                  width={404}
+                  height={784}
+                  fill="none"
+                  viewBox="0 0 404 784"
+                >
+                  <defs>
+                    <pattern
+                      id="56409614-3d62-4985-9a10-7ca758a8f4f0"
+                      x={0}
+                      y={0}
+                      width={20}
+                      height={20}
+                      patternUnits="userSpaceOnUse"
+                    >
+                      <rect
+                        x={0}
+                        y={0}
+                        width={4}
+                        height={4}
+                        className="text-gray-200"
+                        fill="currentColor"
+                      />
+                    </pattern>
+                  </defs>
+                  <rect
+                    width={404}
+                    height={784}
+                    fill="url(#56409614-3d62-4985-9a10-7ca758a8f4f0)"
+                  />
+                </svg>
+                <div className="relative lg:flex lg:items-center">
+                  <div className="hidden lg:block lg:flex-shrink-0">
+                    <img
+                      className="h-64 w-64 rounded-full xl:h-80 xl:w-80"
+                      src={testimony.avatarUrl}
+                      alt=""
+                    />
+                  </div>
+                  <div className="relative lg:ml-10">
+                    <svg
+                      className="absolute top-0 left-0 transform -translate-x-8 -translate-y-24 h-36 w-36 text-indigo-200 opacity-50"
+                      stroke="currentColor"
+                      fill="none"
+                      viewBox="0 0 144 144"
+                    >
+                      <path
+                        strokeWidth={2}
+                        d="M41.485 15C17.753 31.753 1 59.208 1 89.455c0 24.664 14.891 39.09 32.109 39.09 16.287 0 28.386-13.03 28.386-28.387 0-15.356-10.703-26.524-24.663-26.524-2.792 0-6.515.465-7.446.93 2.327-15.821 17.218-34.435 32.11-43.742L41.485 15zm80.04 0c-23.268 16.753-40.02 44.208-40.02 74.455 0 24.664 14.891 39.09 32.109 39.09 15.822 0 28.386-13.03 28.386-28.387 0-15.356-11.168-26.524-25.129-26.524-2.792 0-6.049.465-6.98.93 2.327-15.821 16.753-34.435 31.644-43.742L121.525 15z"
+                      />
+                    </svg>
+                    <blockquote>
+                      <div
+                        className="text-2xl leading-9 font-medium"
+                        sx={{ color: 'text' }}
+                      >
+                        <p>{testimony.testimony}</p>
+                      </div>
+                      <footer className="mt-8">
+                        <div className="flex">
+                          <div className="flex-shrink-0 lg:hidden">
+                            <img
+                              className="h-12 w-12 rounded-full"
+                              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                              alt=""
+                            />
+                          </div>
+                          <div className="ml-4 lg:ml-0">
+                            <div
+                              className="text-base leading-6 font-medium"
+                              sx={{ color: 'text' }}
+                            >
+                              {testimony.name}
+                            </div>
+                            <div
+                              className="text-base leading-6 font-medium"
+                              sx={{ color: 'primary' }}
+                            >
+                              {testimony.role}, {testimony.company}
+                            </div>
+                          </div>
+                        </div>
+                      </footer>
+                    </blockquote>
+                  </div>
+                </div>
               </div>
-            </div>
+            </section>
           </div>
         </Main>
       </div>
@@ -838,7 +890,7 @@ const IndexPage = () => {
       <Steroids />
       <Workspaces />
       <Principles />
-      <Reflections />
+      <Testimonies />
       <Contribute />
     </Layout>
   )
