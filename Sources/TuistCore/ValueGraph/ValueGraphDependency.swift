9dba155fbac2f215e3e4cc9d455450f94f381539
Pedro Piñera Buendía <pedro@ppinera.es>
2020-05-14 13:43:41 +0200

    Value graph (#1336)
    * wip
    
    * Document models
    
    * Update CHANGELOG
    
    * Fix style issues
    
    * Upgrade swiftformat
    
    * Style fixes
    
    * Ensure attributes in ValueGraphDependency cases are consistently defined

---
+++ Sources/TuistCore/ValueGraph/ValueGraphDependency.swift
@@ -0,0 +1,53 @@
+import Foundation
+import TSCBasic
+
+enum ValueGraphDependency: Hashable {
+    /// A dependency that represents a pre-compiled .xcframework.
+    case xcframework(
+        path: AbsolutePath,
+        infoPlist: XCFrameworkInfoPlist,
+        primaryBinaryPath: AbsolutePath,
+        linking: BinaryLinking
+    )
+
+    /// A dependency that represents a pre-compiled framework.
+    case framework(
+        path: AbsolutePath,
+        dsymPath: AbsolutePath?,
+        bcsymbolmapPaths: [AbsolutePath],
+        linking: BinaryLinking,
+        architectures: [BinaryArchitecture]
+    )
+
+    /// A dependency that represents a pre-compiled library.
+    case library(
+        path: AbsolutePath,
+        publicHeaders: AbsolutePath,
+        linking: BinaryLinking,
+        architectures: [BinaryArchitecture],
+        swiftModuleMap: AbsolutePath?
+    )
+
+    /// A dependency that represents a package product.
+    case packageProduct(path: AbsolutePath, product: String)
+
+    /// A dependency that represents a target that is defined in the project at the given path.
+    case target(name: String, path: AbsolutePath)
+
+    func hash(into hasher: inout Hasher) {
+        switch self {
+        case let .xcframework(path, _, _, _):
+            hasher.combine(path)
+        case let .framework(path, _, _, _, _):
+            hasher.combine(path)
+        case let .library(path, _, _, _, _):
+            hasher.combine(path)
+        case let .packageProduct(path, product):
+            hasher.combine(path)
+            hasher.combine(product)
+        case let .target(name, path):
+            hasher.combine(name)
+            hasher.combine(path)
+        }
+    }
+}
