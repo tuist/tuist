3dda665ffb1e86484f129d1899dfa95cc9687723
David Brunow <>
2020-06-05 09:44:24 -0500

    feat: Add support for watch architectures

--- Tests/TuistCoreTests/Models/BinaryArchitectureTests.swift
+++ Tests/TuistCoreTests/Models/BinaryArchitectureTests.swift
@@ -12,5 +12,8 @@ final class BinaryArchitectureTests: TuistTestCase {
         XCTAssertEqual(BinaryArchitecture.i386.rawValue, "i386")
         XCTAssertEqual(BinaryArchitecture.armv7.rawValue, "armv7")
         XCTAssertEqual(BinaryArchitecture.armv7s.rawValue, "armv7s")
+        XCTAssertEqual(BinaryArchitecture.arm64.rawValue, "arm64")
+        XCTAssertEqual(BinaryArchitecture.armv7k.rawValue, "armv7k")
+        XCTAssertEqual(BinaryArchitecture.arm6432.rawValue, "arm64_32")
     }
 }
