c3ec0136862778e1a1404b3d7c04ebed34d21537
Steve Kalkwarf <steve.kalkwarf@aexp.com>
2020-06-05 10:15:23 -0400

    Removed environment filtering test
    Oh, the irony!

--- Tests/TuistSupportIntegrationTests/System/SystemIntegrationTests.swift
+++ Tests/TuistSupportIntegrationTests/System/SystemIntegrationTests.swift
@@ -77,13 +77,6 @@ final class SystemIntegrationTests: TuistTestCase {
         XCTAssertFalse(result.contains("DEVELOPER_DIR"))
     }
 
-    func test_do_not_pass_SECRET_VARIABLE() throws {
-        try sandbox("SECRET_VARIABLE", value: "password") {
-            let result = try subject.capture("env")
-            XCTAssertFalse(result.contains("SECRET_VARIABLE"))
-        }
-    }
-
     func sandbox(_ name: String, value: String, do block: () throws -> Void) rethrows {
         try? ProcessEnv.setVar(name, value: value)
         _ = try? block()
