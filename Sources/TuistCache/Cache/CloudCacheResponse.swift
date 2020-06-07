ec3b920fcb464011692f9970b12d0eda56c7c635
Romain Boulay <romain.bou@gmail.com>
2020-06-07 12:55:27 +0200

    [remote_cache] Improve type readability in CloudCacheResponse

--- Sources/TuistCache/Cache/CloudCacheResponse.swift
+++ Sources/TuistCache/Cache/CloudCacheResponse.swift
@@ -3,6 +3,9 @@ import TuistCore
 import TuistSupport
 
 struct CloudCacheResponse: Decodable {
+
+    typealias CloudCacheResource = HTTPResource<CloudResponse<CloudCacheResponse>, CloudResponseError>
+    
     let url: URL
     let expiresAt: TimeInterval
 
@@ -11,13 +14,13 @@ struct CloudCacheResponse: Decodable {
         self.expiresAt = expiresAt
     }
     
-    public static func fetchResource(hash: String, config: Config) throws -> HTTPResource<CloudResponse<CloudCacheResponse>, CloudResponseError> {
+    public static func fetchResource(hash: String, config: Config) throws -> CloudCacheResource {
         var request = URLRequest(url: try URL.apiCacheURL(hash: hash, config: config))
         request.httpMethod = "GET"
         return .jsonResource { request }
     }
     
-    public static func storeResource(hash: String, config: Config) throws -> HTTPResource<CloudResponse<CloudCacheResponse>, CloudResponseError> {
+    public static func storeResource(hash: String, config: Config) throws -> CloudCacheResource {
         var request = URLRequest(url: try URL.apiCacheURL(hash: hash, config: config, contentMD5: "TODO"))
         request.httpMethod = "POST"
         return .jsonResource { request }
