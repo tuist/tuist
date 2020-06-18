6b2f2cdb8c47b75e85dcfb0fa4a8a037cd555898
Pedro Piñera <pedro@ppinera.es>
2020-06-18 18:47:10 +0200

    Fix social cards

--- website/gatsby-ssr.js
+++ website/gatsby-ssr.js
@@ -1,7 +1,21 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+exports.onPreRenderHTML = function onPreRenderHTML({
+  getHeadComponents,
+  replaceHeadComponents,
+}) {
+  const headComponents = getHeadComponents()
+  headComponents.sort((a, b) => {
+    if (a.type === b.type || (a.type !== 'style' && b.type !== 'style')) {
+      return 0
+    }
 
-// You can delete this file if you're not using it
+    if (a.type === 'style') {
+      return 1
+    } else if (b.type === 'style') {
+      return -1
+    }
+
+    return 0
+  })
+
+  replaceHeadComponents(headComponents)
+}
