1ade69458777bb3cd5ca9d77347a283ff2fcc986
Romain Boulay <romain.bou@gmail.com>
2020-07-01 16:38:45 +0200

    [tuist-clean] Add command

---
+++ Sources/TuistKit/Commands/CleanCommand.swift
@@ -0,0 +1,13 @@
+import Foundation
+import ArgumentParser
+
+struct CleanCommand: ParsableCommand {
+    static var configuration: CommandConfiguration {
+        CommandConfiguration(commandName: "clean",
+                             abstract: "Clean all the artefacts stored locally")
+    }
+    
+    func run() throws {
+        try CleanService().run()
+    }
+}
