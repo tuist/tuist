1c93ebc8eb3b0fbfc6da81f3f4558fe6fc55ab82
Pedro Piñera <pedro@ppinera.es>
2020-07-03 07:48:32 +0200

    Add empty test

---
+++ Tests/TuistCloudTests/ValueGraph/ValueGraphTests.swift
@@ -0,0 +1,14 @@
+import Foundation
+import XCTest
+import TuistSupport
+@testable import TuistSupportTesting
+
+final class ValueGraphTests: TuistUnitTestCase {
+    func test_init_with_cache() {
+        XCTFail("Not implemented")
+    }
+    
+    func test_init_with_graph() {
+        XCTFail("Not implemented")
+    }
+}
