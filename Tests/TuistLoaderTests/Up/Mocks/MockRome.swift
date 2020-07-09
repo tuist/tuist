1e82f29f4dbe91210deb180b8be52a250acbe29a
Michael Miscampbell <mmiscampbell@dynamicsignal.com>
2020-07-09 23:49:30 +0100

    Implementing feedback provided by Daniel and Pedro. Very much appreciated

--- Tests/TuistLoaderTests/Up/Mocks/MockRome.swift
+++ Tests/TuistLoaderTests/Up/Mocks/MockRome.swift
@@ -7,9 +7,16 @@ import TuistCore
 final class MockRome: Romeaging {
     var downloadStub: (([Platform], String?) throws -> Void)?
     var downloadCallCount: UInt = 0
+    var missingStub: (([Platform], String?) throws -> String?)?
+    var missingCallCount: UInt = 0
 
     func download(platforms: [Platform], cachePrefix: String?) throws {
         downloadCallCount += 1
         try downloadStub?(platforms, cachePrefix)
+    }
+
+    func missing(platforms: [Platform], cachePrefix: String?) throws -> String? {
+        missingCallCount += 1
+        return try missingStub?(platforms, cachePrefix)
     }
 }
