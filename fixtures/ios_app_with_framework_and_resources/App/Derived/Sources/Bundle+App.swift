af43b4068580b75d32ef81b73ffab3b5935616c2
Pedro Piñera <pedro@ppinera.es>
2020-07-16 11:16:20 +0200

    .gitignore the Derived directory

--- fixtures/ios_app_with_framework_and_resources/App/Derived/Sources/Bundle+App.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-private class AppBundle {}
-
-public extension Bundle {
-    static var app: Bundle {
-        return Bundle(for: AppBundle.self)
-    }
-}
--- fixtures/ios_app_with_framework_and_resources/StaticFramework3/Derived/Sources/Bundle+StaticFramework3.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public extension Bundle {
-    static var staticFramework3: Bundle {
-        return Bundle(url: Bundle.main.bundleURL.appendingPathComponent("StaticFramework3Resources.bundle"))!
-    }
-}
