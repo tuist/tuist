8f0a8547e0c28327dcb43175a0c259a0dd4f6071
Kassem Wridan <kas@mxpr.net>
2020-07-16 07:46:50 +0100

    Add fixture

--- fixtures/ios_app_with_framework_and_resources/App/Project.swift
+++ fixtures/ios_app_with_framework_and_resources/App/Project.swift
@@ -22,6 +22,7 @@ let project = Project(
                 .project(target: "Framework1", path: "../Framework1"),
                 .project(target: "StaticFramework", path: "../StaticFramework"),
                 .project(target: "StaticFrameworkResources", path: "../StaticFramework"),
+                .project(target: "StaticFramework2", path: "../StaticFramework2"),
             ]
         ),
         Target(
--- fixtures/ios_app_with_framework_and_resources/App/Sources/AppDelegate.swift
+++ fixtures/ios_app_with_framework_and_resources/App/Sources/AppDelegate.swift
@@ -1,11 +1,13 @@
 import Framework1
 import UIKit
 import StaticFramework
+import StaticFramework2
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     let staticFrameworkResources = StaticFrameworkResouces()
+    let staticFramework2Resources = StaticFramework2Resources()
     
     func applicationDidFinishLaunching(_: UIApplication) {
         let framework1 = Framework1File()
@@ -14,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         print("AppDelegate -> \(framework1.hello())")
         print("Main bundle image: \(String(describing: UIImage(named: "tuist")))")
         print("StaticFrameworkResouce image: \(String(describing: staticFrameworkResources.tuist))")
+        print("StaticFramework2Resouce image: \(String(describing: staticFramework2Resources.loadImage()))")
     }
     
 
