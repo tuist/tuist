da28d7951790a460328470f341190e1c3cd4ef3a
Pedro Piñera <pedro@ppinera.es>
2020-07-18 11:28:56 +0200

    Fix command name

--- Sources/TuistKit/Commands/CacheCommand.swift
+++ Sources/TuistKit/Commands/CacheCommand.swift
@@ -4,7 +4,7 @@ import TSCBasic
 
 struct CacheCommand: ParsableCommand {
     static var configuration: CommandConfiguration {
-        CommandConfiguration(commandName: "scale",
+        CommandConfiguration(commandName: "cache",
                              abstract: "A set of utilities related to the caching of targets.", subcommands: [
                                  CacheWarmCommand.self,
                                  CachePrintHashesCommand.self,
