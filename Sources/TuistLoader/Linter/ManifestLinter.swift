dad231c7410d33c096bd82e0f1c0cbf157b58c6e
matiasvillaverde <a.matiasvillaverde@gmail.com>
2020-07-24 11:42:49 +0200

    Add linter to warn about the deprecation of core data version

--- Sources/TuistLoader/Linter/ManifestLinter.swift
+++ Sources/TuistLoader/Linter/ManifestLinter.swift
@@ -45,6 +45,8 @@ public class ManifestLinter: ManifestLinting {
             issues.append(contentsOf: lint(settings: settings, declarationLocation: target.name))
         }
 
+        issues.append(contentsOf: lint(coredataModels: target.coreDataModels, declarationLocation: target.name))
+
         return issues
     }
 
@@ -55,4 +57,14 @@ public class ManifestLinter: ManifestLinting {
             LintingIssue(reason: "The configuration '\($0)' is declared multiple times within '\(declarationLocation)' settings. The last declared configuration will be used.", severity: .warning)
         }
     }
+
+    private func lint(coredataModels: [ProjectDescription.CoreDataModel], declarationLocation: String) -> [LintingIssue] {
+
+        let currentVersions = coredataModels.compactMap(\.currentVersion)
+
+        return currentVersions.map {
+            LintingIssue(reason: "The current core data model version '\(String(describing: $0))' will be infered automatically in '\(declarationLocation)' settings. It is not need it to set the current version anymore.", severity: .warning)
+        }
+    }
+
 }
