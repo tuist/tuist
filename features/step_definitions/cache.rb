3000e2f36f23c25698bf5a658a3c63200795a991
Romain Boulay <romain.bou@gmail.com>
2020-07-28 10:33:03 +0200

    [static-microfeature] Add better logging

--- features/step_definitions/cache.rb
+++ features/step_definitions/cache.rb
@@ -7,7 +7,7 @@ end
 Then(/^([a-zA-Z]+) links the xcframework ([a-zA-Z]+)$/) do |target_name, xcframework|
   projects = Xcode.projects(@workspace_path)
   target = projects.flat_map { |p| p.targets }.detect { |t| t.name == target_name }
-  flunk("Target #{target_name} in any of the projects of the workspace") if target.nil?
+  flunk("Target #{target_name} doesn't exist in any of the projects' targets of the workspace") if target.nil?
   xcframework_deps = target.frameworks_build_phases.file_display_names.filter { |d| d.include?(".xcframework") }
   unless xcframework_deps.include?("#{xcframework}.xcframework")
     flunk("Target #{target_name} doesn't link the xcframework #{xcframework}. It links the xcframeworks: #{xcframework_deps.join(", ")}")
