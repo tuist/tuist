2d09e63e5458b7b623ebe2e279dd71d51cad64fe
Romain Boulay <romain.bou@gmail.com>
2020-07-28 21:47:18 +0200

    Add acceptance test suite that covers a staticly linked micro-architecture kind of app (#1594)
    * [static-microfeature] Add new fixture to cover a staticly linked microfeature kind of app
    
    * [static-microfeature] Use the .staticFramework product everywhere
    
    * [static-microfeature] Make fixture more explicit that it's a static app
    
    * [static-microfeature] Add better logging
    
    * [static-microfeature] Add acceptance test suite to cover a staticly linked micro-feature achitecture app
    
    * [static-microfeature] Add new rule to check that a certain type of app (fully staticly linked) does not embed any xcframeworks
    
    * [static-microfeature] Fix fixture unit test target build
    
    * [static-microfeature] Update changelog

--- features/step_definitions/cache.rb
+++ features/step_definitions/cache.rb
@@ -7,7 +7,7 @@ end
 Then(/^([a-zA-Z]+) links the xcframework ([a-zA-Z]+)$/) do |target_name, xcframework|
   projects = Xcode.projects(@workspace_path)
   target = projects.flat_map { |p| p.targets }.detect { |t| t.name == target_name }
-  flunk("Target #{target_name} in any of the projects of the workspace") if target.nil?
+  flunk("Target #{target_name} doesn't exist in any of the projects' targets of the workspace") if target.nil?
   xcframework_deps = target.frameworks_build_phases.file_display_names.filter { |d| d.include?(".xcframework") }
   unless xcframework_deps.include?("#{xcframework}.xcframework")
     flunk("Target #{target_name} doesn't link the xcframework #{xcframework}. It links the xcframeworks: #{xcframework_deps.join(", ")}")
@@ -25,5 +25,19 @@ Then(/^([a-zA-Z]+) embeds the xcframework ([a-zA-Z]+)$/) do |target_name, xcfram
     .filter { |d| d.include?(".xcframework") }
   unless xcframework_deps.include?("#{xcframework}.xcframework")
     flunk("Target #{target_name} doesn't embed the xcframework #{xcframework}. It embeds the xcframeworks: #{xcframework_deps.join(", ")}")
+  end
+end
+
+Then(/^([a-zA-Z]+) does not embed any xcframeworks$/) do |target_name|
+  projects = Xcode.projects(@workspace_path)
+  target = projects.flat_map { |p| p.targets }.detect { |t| t.name == target_name }
+  flunk("Target #{target_name} in any of the projects of the workspace") if target.nil?
+  xcframework_deps = target
+    .copy_files_build_phases
+    .filter { |b| b.symbol_dst_subfolder_spec == :frameworks }
+    .flat_map { |b| b.file_display_names }
+    .filter { |d| d.include?(".xcframework") }
+  unless xcframework_deps.empty?
+    flunk("Target #{target_name} should not embed any xcframeworks, although it does embed the following xcframeworks: #{xcframework_deps.join(", ")}")
   end
 end
