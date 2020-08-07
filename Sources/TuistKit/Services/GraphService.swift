7d80dac878833665c1d275951d641a07eb4df238
Andrea Cipriani <andreacipriani89@gmail.com>
2020-08-06 23:05:32 -0400

    Check if GraphViz is installed, and install it if it's not

--- Sources/TuistKit/Services/GraphService.swift
+++ Sources/TuistKit/Services/GraphService.swift
@@ -55,13 +55,27 @@ final class GraphService {
     }
 
     private func exportDOTRepresentation(from graphVizGraph: GraphViz.Graph, at filePath: AbsolutePath) throws {
-        let dotFile = String(DOTEncoder().encode(graphVizGraph).sorted())
+        let dotFile = DOTEncoder().encode(graphVizGraph)
         try FileHandler.shared.write(dotFile, path: filePath, atomically: true)
     }
 
     private func exportPNGRepresentation(from graphVizGraph: GraphViz.Graph, at filePath: AbsolutePath) throws {
+        if try !isGraphVizInstalled() {
+            try installGraphViz()
+        }
         let data = try graphVizGraph.render(using: .dot, to: .png)
         FileManager.default.createFile(atPath: filePath.pathString, contents: data, attributes: nil)
         try System.shared.async(["open", filePath.pathString])
+    }
+
+    private func isGraphVizInstalled() throws -> Bool {
+        try System.shared.capture(["brew", "list"]).contains("graphviz")
+    }
+
+    private func installGraphViz() throws {
+        logger.notice("Installing GraphViz...")
+        var env = System.shared.env
+        env["HOMEBREW_NO_AUTO_UPDATE"] = "1"
+        try System.shared.runAndPrint(["brew", "install", "graphviz"], verbose: false, environment: env)
     }
 }
