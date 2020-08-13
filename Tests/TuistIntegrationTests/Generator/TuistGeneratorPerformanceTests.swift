6698aab6ceca709909b07b850c300af0d4e07497
Kas <kwridan@users.noreply.github.com>
2020-08-13 07:24:31 +0100

    Add `DescriptorGenerator` performance test (#1593)
    - Adding a performance test to help us evaluate performance impact of changing graph look ups within the generator
      - The test will only run when built with the release config
      - Performance tests are general convey more realistic results when run in release mode
      - This also ensures we don't add additional overhead to the existing CI jobs running unit tests
    - To support the test, a model generator is being added to simulate the creation of large projects (in memory).
    - The same code is being leveraged within `StableStructureIntegrationTests` as that too requires the same logic of creating a large projects with several targets / schemes / dependencies etc...
    
    Test Plan:
    
    - Run `TuistGeneratorPerformanceTests`
    
    Note:
    It appears there isn't a way to store baselines measurements for Swift PM projects - the feature is currently only available in Xcode

---
+++ Tests/TuistIntegrationTests/Generator/TuistGeneratorPerformanceTests.swift
@@ -0,0 +1,64 @@
+import TSCBasic
+import TuistCore
+import TuistCoreTesting
+import TuistLoaderTesting
+import TuistSupport
+import XcodeProj
+import XCTest
+
+@testable import TuistGenerator
+@testable import TuistSupport
+@testable import TuistSupportTesting
+
+final class TuistGeneratorPerformanceTests: TuistTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+
+    // MARK: - Tests
+
+    func test_generateWorkspace_performance() throws {
+        guard !isRunningInDebug() else {
+            // Performance tests need to be run in Release Mode for more realistic results
+            // Note: When we switch to Xcode11.5+ only on CI we can use `XCTSkipIf` instead of a guard statement
+            //
+            // XCTSkipIf(isRunningInDebug, "Performance tests need to be run in Release Mode for more realistic results")
+            return
+        }
+
+        measureMetrics([.wallClockTime], automaticallyStartMeasuring: false) {
+            do {
+                // Given
+                let subject = DescriptorGenerator()
+                let config = TestModelGenerator.WorkspaceConfig(projects: 50,
+                                                                testTargets: 5,
+                                                                frameworkTargets: 5,
+                                                                schemes: 10,
+                                                                sources: 200,
+                                                                resources: 100,
+                                                                headers: 100)
+                let temporaryPath = try self.temporaryPath()
+                let modelGenerator = TestModelGenerator(rootPath: temporaryPath, config: config)
+                let (graph, workspace) = try modelGenerator.generate()
+
+                // When
+                startMeasuring()
+                _ = try subject.generateWorkspace(workspace: workspace, graph: graph)
+                stopMeasuring()
+
+            } catch {
+                XCTFail("Failed to generate workspace: \(error)")
+            }
+        }
+    }
+
+    // MARK: - Helpers
+
+    private func isRunningInDebug() -> Bool {
+        #if DEBUG
+            return true
+        #else
+            return false
+        #endif
+    }
+}
