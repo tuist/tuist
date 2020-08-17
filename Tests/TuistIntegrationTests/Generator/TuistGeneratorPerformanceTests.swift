ebb60349b6611eba4ce97ab8ef1648a61bc4e326
Kassem Wridan <kwridan@bloomberg.net>
2020-08-17 08:15:06 +0100

    Add Performance Tests Workflow
    - Testing out the addition of a github workflow for performance tests
      - All tests that have "PerformanceTests" in their name will get built and run in release mode
      - Additionally, to ensure a consistent baseline, a reference branch is checked out to record baselines within the same run
    - Tests can be configured to record baselines via setting the environment variable `TEST_RECORD_BASELINES=1`
    - Tests can be configured to fail in the event of missing baselines via setting the environment variable `TEST_FAIL_MISSING_BASELINES=1`
    - The workflow is currently configured to skip assertions in the event baselines are missing (this will occur whenever a new performance test is added that will not be present in the reference branch)

--- Tests/TuistIntegrationTests/Generator/TuistGeneratorPerformanceTests.swift
+++ Tests/TuistIntegrationTests/Generator/TuistGeneratorPerformanceTests.swift
@@ -11,6 +11,8 @@ import XCTest
 @testable import TuistSupportTesting
 
 final class TuistGeneratorPerformanceTests: TuistTestCase {
+    private let manualTimer = ManualTestTimer()
+
     override func setUp() {
         super.setUp()
     }
@@ -22,7 +24,7 @@ final class TuistGeneratorPerformanceTests: TuistTestCase {
             // Performance tests need to be run in Release Mode for more realistic results
             // Note: When we switch to Xcode11.5+ only on CI we can use `XCTSkipIf` instead of a guard statement
             //
-            // XCTSkipIf(isRunningInDebug, "Performance tests need to be run in Release Mode for more realistic results")
+            // XCTSkipIf(isRunningInDebug(), "Performance tests need to be run in Release Mode for more realistic results")
             return
         }
 
@@ -50,6 +52,22 @@ final class TuistGeneratorPerformanceTests: TuistTestCase {
                 XCTFail("Failed to generate workspace: \(error)")
             }
         }
+
+        // Currently Swift PM doesn't support setting baselines, as such we'll need to
+        // manually assert measurements are within an expected value and tolerance
+        try assertMeasurement(tolerancePercentage: 0.1)
+    }
+
+    // MARK: - Overrides
+
+    override func startMeasuring() {
+        super.startMeasuring()
+        manualTimer.start()
+    }
+
+    override func stopMeasuring() {
+        super.stopMeasuring()
+        manualTimer.stop()
     }
 
     // MARK: - Helpers
@@ -60,5 +78,107 @@ final class TuistGeneratorPerformanceTests: TuistTestCase {
         #else
             return false
         #endif
+    }
+
+    private func assertMeasurement(tolerancePercentage: Double,
+                                   file: StaticString = #file,
+                                   line: UInt = #line) throws
+    {
+        if shouldRecordBaselines() {
+            try save(baseline: Baseline(measurements: manualTimer.measurements))
+        } else {
+            guard let baseline = try loadBaseline() else {
+                if shouldFailForMissingBaselines() {
+                    XCTFail("Baseline measurements were not found.")
+                } else {
+                    print("Skipping assertion, baseline measurements were not found.")
+                }
+                return
+            }
+            let expectedDuration = baseline.measurements.average()
+            let average = manualTimer.measurements.average()
+            let accuracy = expectedDuration * tolerancePercentage
+            let percentageString = String(format: "%.2f", tolerancePercentage * 100)
+            XCTAssertEqual(average,
+                           expectedDuration,
+                           accuracy: accuracy,
+                           "The measurement \(average) wasn't within expected range ~(\(expectedDuration) ±\(percentageString)%)",
+                           file: file,
+                           line: line)
+        }
+    }
+
+    private func shouldRecordBaselines() -> Bool {
+        ProcessInfo.processInfo.environment["TEST_RECORD_BASELINES"] == "1"
+    }
+
+    private func shouldFailForMissingBaselines() -> Bool {
+        ProcessInfo.processInfo.environment["TEST_FAIL_MISSING_BASELINES"] == "1"
+    }
+
+    private func currentTestBaselineFilePath() -> AbsolutePath {
+        let currentTestIdentifier = name
+            .replacingOccurrences(of: "-", with: "")
+            .replacingOccurrences(of: "[", with: "")
+            .replacingOccurrences(of: "]", with: "")
+            .replacingOccurrences(of: " ", with: ".")
+        return AbsolutePath(#file)
+            .parentDirectory
+            .parentDirectory
+            .parentDirectory
+            .parentDirectory
+            .appending(component: "performance_tests_baselines")
+            .appending(component: currentTestIdentifier)
+    }
+
+    private func loadBaseline() throws -> Baseline? {
+        let fileHandler = FileHandler()
+        let baselineFile = currentTestBaselineFilePath()
+        guard fileHandler.exists(baselineFile) else {
+            return nil
+        }
+        let data = try fileHandler.readFile(baselineFile)
+        return try JSONDecoder().decode(Baseline.self, from: data)
+    }
+
+    private func save(baseline: Baseline) throws {
+        let fileHandler = FileHandler()
+        let baselineFile = currentTestBaselineFilePath()
+        if fileHandler.exists(baselineFile) {
+            try fileHandler.delete(baselineFile)
+        }
+        try fileHandler.createFolder(baselineFile.parentDirectory)
+        let data = try JSONEncoder().encode(baseline)
+        try data.write(to: baselineFile.asURL)
+    }
+
+    // MARK: - Helper Classes
+
+    struct Baseline: Codable {
+        var measurements: [TimeInterval]
+    }
+
+    private final class ManualTestTimer {
+        private let clock = WallClock()
+        private(set) var measurements: [TimeInterval] = []
+        private var currentTimer: ClockTimer?
+
+        func start() {
+            currentTimer = clock.startTimer()
+        }
+
+        func stop() {
+            guard let elapsedTime = currentTimer?.stop() else {
+                return
+            }
+            measurements.append(elapsedTime)
+            currentTimer = nil
+        }
+    }
+}
+
+private extension Array where Element == Double {
+    func average() -> Double {
+        reduce(0, +) / Double(count)
     }
 }
