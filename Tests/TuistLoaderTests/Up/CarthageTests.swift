6db874a9b040f165a4e367ca11d343cad578e9f9
Vitaly Chupryk <vitaly@softmaxsg.com>
2020-08-18 09:44:07 +0200

    Carthage binary dependencies (#1675)
    * Fix support of Carthage binary dependencies
    
    * Add an entry to CHANGELOG.md
    
    * Add link to the PR to CHANGELOG.md
    
    * Fix SwiftFormat issues

--- Tests/TuistLoaderTests/Up/CarthageTests.swift
+++ Tests/TuistLoaderTests/Up/CarthageTests.swift
@@ -44,6 +44,8 @@ final class CarthageTests: TuistUnitTestCase {
         github "Tuist/DependencyA" "4.8.0"
         github "Tuist/DependencyB" "4.9.0"
         github "Tuist/DependencyC" "4.10.0"
+        binary "Tuist/DependencyD" "4.11.0"
+        binary "Tuist/DependencyE.json" "4.12.0"
         """
         let cartfileResolvedPath = temporaryPath.appending(component: "Cartfile.resolved")
         try cartfileResolved.write(to: cartfileResolvedPath.url,
@@ -58,6 +60,8 @@ final class CarthageTests: TuistUnitTestCase {
         // Dependency A: Outdated
         // Dependency B: Up to date
         // Dependency C: Missing version file
+        // Dependency D: Binary without extension, missing version file
+        // Dependency E: Binary with extension, missing version file
         let dependencyAVersionData = try jsonEncoder.encode(CarthageVersionFile(commitish: "4.7.0"))
         try dependencyAVersionData.write(to: buildPath.appending(component: ".DependencyA.version").url)
 
@@ -66,6 +70,6 @@ final class CarthageTests: TuistUnitTestCase {
 
         let got = try subject.outdated(path: temporaryPath)
 
-        XCTAssertEqual(got, ["DependencyA", "DependencyC"])
+        XCTAssertEqual(got, ["DependencyA", "DependencyC", "DependencyD", "DependencyE"])
     }
 }
