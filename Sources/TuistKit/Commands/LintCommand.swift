73f3f8c6278bfd80628300319cf83fea223ba767
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2020-08-22 17:20:14 +0200

    [code-lint] remove mismatched comment

--- Sources/TuistKit/Commands/LintCommand.swift
+++ Sources/TuistKit/Commands/LintCommand.swift
@@ -2,7 +2,6 @@ import ArgumentParser
 import Foundation
 import TSCBasic
 
-/// Command that builds a target from the project in the current directory.
 struct LintCommand: ParsableCommand {
     static var configuration: CommandConfiguration {
         CommandConfiguration(commandName: "lint",
