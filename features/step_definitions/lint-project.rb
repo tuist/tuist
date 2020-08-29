6d39b836a5dc2695efbc4e56ea6dc9894079cf8f
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2020-08-29 19:50:14 +0200

    [code-lint] acceptance tests - WIP

---
+++ features/step_definitions/lint-project.rb
@@ -0,0 +1,4 @@
+Then(/tuist lints the project and fails/) do
+    _, _, status = Open3.capture3("swift", "run", "tuist", "lint", "project" "--path", @dir)
+    refute(status.success?, "Expected 'tuist lint' to fail but it didn't")
+  end
