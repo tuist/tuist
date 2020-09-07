452a4a8aecde4ff6fa833387aabb191a46fc7301
facumenzella <facumenzella@gmail.com>
2020-09-07 22:24:04 +0200

    Added generate stub for MockSwiftDocController

--- Sources/TuistDocTesting/MockSwiftDocController.swift
+++ Sources/TuistDocTesting/MockSwiftDocController.swift
@@ -1,10 +1,14 @@
 import TuistDoc
+
 @testable import TuistCore
 
-final class MockSwiftDocController: SwiftDocControlling {
-    func generate(format _: SwiftDocFormat,
-                  moduleName _: String,
-                  baseURL _: String,
-                  outputDirectory _: String,
-                  sourcesPath _: String) throws {}
+public final class MockSwiftDocController: SwiftDocControlling {
+    public init() {}
+    
+    public var generateStub: ((SwiftDocFormat, String, String, String, String) throws -> Void)?
+    public func generate(format _: SwiftDocFormat,
+                         moduleName _: String,
+                         baseURL _: String,
+                         outputDirectory _: String,
+                         sourcesPath _: String) throws {}
 }
