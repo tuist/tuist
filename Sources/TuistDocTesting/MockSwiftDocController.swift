ffb403a6e83ec837470e3661f936f20936a25891
facumenzella <facumenzella@gmail.com>
2020-09-08 12:37:50 +0200

    Swiftformat

--- Sources/TuistDocTesting/MockSwiftDocController.swift
+++ Sources/TuistDocTesting/MockSwiftDocController.swift
@@ -4,7 +4,7 @@ import TuistDoc
 
 public final class MockSwiftDocController: SwiftDocControlling {
     public init() {}
-    
+
     public var generateStub: ((SwiftDocFormat, String, String, String, String) throws -> Void)?
     public func generate(format _: SwiftDocFormat,
                          moduleName _: String,
