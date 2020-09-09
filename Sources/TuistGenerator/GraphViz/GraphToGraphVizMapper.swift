5fb8f762ecf7509e989316b4e526a76c702223e0
Natan Rolnik <natanrolnik@gmail.com>
2020-09-09 19:07:22 +0300

    Add initial support for colors and shapes  in graph
    Also, use tuist’s GraphViz fork to support node fill colors and more shapes

--- Sources/TuistGenerator/GraphViz/GraphToGraphVizMapper.swift
+++ Sources/TuistGenerator/GraphViz/GraphToGraphVizMapper.swift
@@ -30,11 +30,13 @@ final class GraphToGraphVizMapper: GraphToGraphVizMapping {
                     return
                 }
 
-                let leftNode = GraphViz.Node(target.target.name)
+                var leftNode = GraphViz.Node(target.target.name)
+                leftNode.applyAttributes(attributes: target.styleAttributes)
                 nodes.append(leftNode)
 
                 target.dependencies.forEach { dependency in
-                    let rightNode = GraphViz.Node(dependency.name)
+                    var rightNode = GraphViz.Node(dependency.name)
+                    rightNode.applyAttributes(attributes: dependency.styleAttributes)
                     nodes.append(rightNode)
                     if skipExternalDependencies, dependency.isExternal { return }
                     let edge = GraphViz.Edge(from: leftNode, to: rightNode)
@@ -48,6 +50,28 @@ final class GraphToGraphVizMapper: GraphToGraphVizMapping {
         graphVizGraph.append(contentsOf: sortedNodes)
         graphVizGraph.append(contentsOf: sortedDeps)
         return graphVizGraph
+    }
+}
+
+extension GraphViz.Node {
+    fileprivate mutating func applyAttributes(attributes: NodeStyleAttributes?) {
+        self.fillColor = attributes?.fillColor
+        self.strokeWidth = attributes?.strokeWidth
+        self.shape = attributes?.shape
+    }
+}
+
+private struct NodeStyleAttributes {
+    let fillColor: GraphViz.Color?
+    let strokeWidth: Double?
+    let shape: GraphViz.Node.Shape?
+
+    init(colorName: GraphViz.Color.Name? = nil,
+         strokeWidth: Double? = nil,
+         shape: GraphViz.Node.Shape? = nil) {
+        self.fillColor = colorName.map { GraphViz.Color.named($0) }
+        self.strokeWidth = strokeWidth
+        self.shape = shape
     }
 }
 
@@ -73,5 +97,53 @@ private extension GraphNode {
         }
 
         return false
+    }
+
+    var styleAttributes: NodeStyleAttributes? {
+        if self is SDKNode {
+            return .init(colorName: .blueviolet, shape: .rectangle)
+        }
+
+        if self is CocoaPodsNode {
+            return .init(colorName: .red2)
+        }
+
+        if self is FrameworkNode {
+            return .init(colorName: .darkgoldenrod3, shape: .trapezium)
+        }
+
+        if self is LibraryNode {
+            return .init(colorName: .lightgray, shape: .folder)
+        }
+
+        if self is PackageProductNode {
+            return .init(colorName: .tan4, shape: .tab)
+        }
+
+        if self is PrecompiledNode {
+            return .init(colorName: .skyblue, shape: .trapezium)
+        }
+
+        if let targetNode = self as? TargetNode {
+            switch targetNode.target.product {
+            case .app, .watch2App:
+                return .init(colorName: .deepskyblue, strokeWidth: 1.5, shape: .box3d)
+            case .appExtension, .watch2Extension:
+                return .init(colorName: .deepskyblue2, shape: .component)
+            case .framework:
+                return .init(colorName: .darkgoldenrod1, shape: .cylinder)
+//            case .staticLibrary:
+//                return .init(colorName: .darkgoldenrod1, shape: .cylinder)
+//            case .staticFramework:
+//                return .init(colorName: .darkgoldenrod1, shape: .cylinder)
+//            case .dynamicLibrary
+//                return .init(colorName: .darkgoldenrod1, shape: .cylinder)
+//            case .bundle: return .named()
+//            case .uiTests, .unitTests
+            default: return nil
+            }
+        }
+
+        return nil
     }
 }
