31020eecfab69269138df03d6897b0b6614e6867
facumenzella <facumenzella@gmail.com>
2020-09-09 16:19:59 +0200

    Added acceptance test

---
+++ features/step_definitions/doc.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+Then("tuist generates the documentation for {string}") do |target|
+  system("swift", "run", "tuist", "doc", "--path", @dir + "/" + target, target)
+end
