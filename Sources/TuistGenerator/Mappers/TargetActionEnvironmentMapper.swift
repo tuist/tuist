e33fe42bb982e363f0eef80b2224f7241eda6018
Steve Kalkwarf <steve.kalkwarf@aexp.com>
2020-09-23 13:42:43 -0400

    Relocated `TargetActionEnvironmentMapper` to TuistGenerator

---
+++ Sources/TuistGenerator/Mappers/TargetActionEnvironmentMapper.swift
@@ -0,0 +1,20 @@
+import TuistCore
+
+public class TargetActionEnvironmentMapper: TargetMapping {
+    let showEnvVarsInLog: Bool
+
+    public init(_ showEnvVarsInLog: Bool) {
+        self.showEnvVarsInLog = showEnvVarsInLog
+    }
+
+    public func map(target: Target) throws -> (Target, [SideEffectDescriptor]) {
+        var target = target
+        let actions: [TargetAction] = target.actions.map {
+            var action = $0
+            action.showEnvVarsInLog = showEnvVarsInLog
+            return action
+        }
+        target.actions = actions
+        return (target, [SideEffectDescriptor]())
+    }
+}
