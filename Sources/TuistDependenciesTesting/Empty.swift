44c7e3496f1cfdc648f8cc349f3d7edc126aac36
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2020-10-02 21:31:21 +0200

    [init_tuistdependencies] init empty files to silent Xcode warnings

---
+++ Sources/TuistDependenciesTesting/Empty.swift
@@ -0,0 +1,3 @@
+import Foundation
+import TuistCore
+import TuistSupport
---
+++ Tests/TuistDependenciesIntegrationTests/EmptyTests.swift
@@ -0,0 +1,16 @@
+import Foundation
+import XCTest
+
+@testable import TuistDependencies
+@testable import TuistSupportTesting
+
+final class EmptyTests: TuistUnitTestCase {
+    func test_empty() {
+        // Given
+        
+        // When
+        
+        // Then
+        XCTAssertTrue(true)
+    }
+}
