9ae700ea06c2fdf43c3cce928f4bee27f8b7842b
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2020-10-17 13:59:20 +0200

    [init_commands] add `MockDependenciesController`, `DependenciesFetchServiceTests`,  `DependenciesUpdateServiceTests`

--- Sources/TuistDependenciesTesting/MockDependenciesController.swift
+++ Sources/TuistDependenciesTesting/MockDependenciesController.swift
@@ -4,13 +4,35 @@ import TSCBasic
 public final class MockDependenciesController: DependenciesControlling {
     public init() {}
     
-    var fetchStub: ((AbsolutePath) throws -> Void)?
+    var invokedFetch = false
+    var invokedFetchCount = 0
+    var invokedFetchParameters: AbsolutePath?
+    var invokedFetchParametersList = [AbsolutePath]()
+    var stubbedFetchError: Error?
+    
     public func fetch(at path: AbsolutePath) throws {
-        try fetchStub?(path)
+        invokedFetch = true
+        invokedFetchCount += 1
+        invokedFetchParameters = path
+        invokedFetchParametersList.append(path)
+        if let error = stubbedFetchError {
+            throw error
+        }
     }
     
-    var updateStub: ((AbsolutePath) throws -> Void)?
+    var invokedUpdate = false
+    var invokedUpdateCount = 0
+    var invokedUpdateParameters: AbsolutePath?
+    var invokedUpdateParametersList = [AbsolutePath]()
+    var stubbedUpdateError: Error?
+    
     public func update(at path: AbsolutePath) throws {
-        try updateStub?(path)
+        invokedUpdate = true
+        invokedUpdateCount += 1
+        invokedUpdateParameters = path
+        invokedUpdateParametersList.append(path)
+        if let error = stubbedUpdateError {
+            throw error
+        }
     }
 }
