86e6bcc95c66b1c7c064a963d1addf28f170a7a6
Vendula Švastalová <vendula.svastalova@ackee.cz>
2020-10-22 20:38:50 +0200

    🐛 Fix unbuildable fixture

--- fixtures/app_with_organization_name_config/App/AppDelegate.swift
+++ fixtures/app_with_organization_name_config/App/AppDelegate.swift
@@ -1,10 +1,8 @@
-import Framework
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
-    var framework: FrameworkClass = FrameworkClass()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
