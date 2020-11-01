c36f46d194b81b6371c8bf39b0649eb4418bc935
Andrea Cipriani <andreacipriani89@gmail.com>
2020-11-01 17:55:35 -0500

    Better format for migration.rb

--- features/step_definitions/migration.rb
+++ features/step_definitions/migration.rb
@@ -1,11 +1,12 @@
 Then(/run tuist migration list-targets for (.+) in ios_workspace_with_microfeature_architecture matches (.+)$/) do |framework, json_file|
     fixtures_path = File.expand_path("../../fixtures", __dir__)
     fixture_path = File.join(fixtures_path, "ios_workspace_with_microfeature_architecture/Frameworks/#{framework}Framework/#{framework}.xcodeproj/")
+    resources_path = File.expand_path("../resources", __dir__)
+    expected_json = File.read("#{resources_path}/#{json_file}")
+
     assert(false, "Project #{fixture_path} not found") unless File.exist?(fixture_path)
 
     out, s = Open3.capture2("swift", "run", "tuist", "migration", "list-targets", "-p", fixture_path)
 
-    resources_path = File.expand_path("../resources", __dir__)
-    expected_json = File.read("#{resources_path}/#{json_file}")
     assert(out.include?(expected_json))
 end
