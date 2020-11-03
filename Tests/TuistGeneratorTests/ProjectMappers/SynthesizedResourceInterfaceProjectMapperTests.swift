1a17d1f1672ec1409b8660c2813e0b2b78b58d0c
Marek Fořt <marekfort@me.com>
2020-11-03 15:52:06 +0100

    Run swiftformat.

--- Tests/TuistGeneratorTests/ProjectMappers/SynthesizedResourceInterfaceProjectMapperTests.swift
+++ Tests/TuistGeneratorTests/ProjectMappers/SynthesizedResourceInterfaceProjectMapperTests.swift
@@ -139,21 +139,21 @@ final class SynthesizedResourceInterfaceProjectMapperTests: TuistUnitTestCase {
                         name: targetA.name,
                         sources: [
                             SourceFile(path: derivedSourcesPath
-                                        .appending(component: "Assets+TargetA.swift"),
-                                       compilerFlags: nil,
-                                       contentHash: try contentHasher.hash("a.xcassets".data(using: .utf8)!)),
+                                .appending(component: "Assets+TargetA.swift"),
+                                compilerFlags: nil,
+                                contentHash: try contentHasher.hash("a.xcassets".data(using: .utf8)!)),
                             SourceFile(path: derivedSourcesPath
-                                        .appending(component: "Strings+TargetA.swift"),
-                                       compilerFlags: nil,
-                                       contentHash: try contentHasher.hash("aStrings.strings, aStrings.stringsdict".data(using: .utf8)!)),
+                                .appending(component: "Strings+TargetA.swift"),
+                                compilerFlags: nil,
+                                contentHash: try contentHasher.hash("aStrings.strings, aStrings.stringsdict".data(using: .utf8)!)),
                             SourceFile(path: derivedSourcesPath
-                                        .appending(component: "Environment.swift"),
-                                       compilerFlags: nil,
-                                       contentHash: try contentHasher.hash("Environment.plist".data(using: .utf8)!)),
+                                .appending(component: "Environment.swift"),
+                                compilerFlags: nil,
+                                contentHash: try contentHasher.hash("Environment.plist".data(using: .utf8)!)),
                             SourceFile(path: derivedSourcesPath
-                                        .appending(component: "Fonts+TargetA.swift"),
-                                       compilerFlags: nil,
-                                       contentHash: try contentHasher.hash("ttfFont.ttf, otfFont.otf, ttcFont.ttc".data(using: .utf8)!)),
+                                .appending(component: "Fonts+TargetA.swift"),
+                                compilerFlags: nil,
+                                contentHash: try contentHasher.hash("ttfFont.ttf, otfFont.otf, ttcFont.ttc".data(using: .utf8)!)),
                         ],
                         resources: targetA.resources
                     ),
