5460d9a15bf901897874d5d3409ad9ec9ad6d5d3
facumenzella <facumenzella@gmail.com>
2020-11-08 13:13:36 +0100

    Adding CarthageDependency and DependencyRequirement

---
+++ Sources/TuistCore/Models/CarthageDependency.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+/// CarthageDependency contains the description of a dependency to be fetched with Carthage.
+struct CarthageDependency {
+    /// Name of the dependency
+    let name: String
+
+    /// Type of requirement for the given dependency
+    let requirement: DependencyRequirement
+}
---
+++ Sources/TuistCore/Models/DependencyRequirement.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+enum DependencyRequirement {
+    case exact(String)
+}
