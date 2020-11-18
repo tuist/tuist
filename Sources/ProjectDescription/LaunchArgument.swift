c63825823588d61f1ee771f0e0eb2b5fc95c34ea
Jakub Olejnik <olejnjak@gmail.com>
2020-11-18 16:04:11 +0100

    Add docs to `LaunchArgument`

--- Sources/ProjectDescription/LaunchArgument.swift
+++ Sources/ProjectDescription/LaunchArgument.swift
@@ -1,13 +1,20 @@
 import Foundation
 
+/// Represents launch argument that is passed by when running a scheme
 public struct LaunchArgument: Equatable, Codable {
     // MARK: - Attributes
 
+    /// Name of argument
     public let name: String
+    /// If enabled then argument is marked as active
     public let isEnabled: Bool
 
     // MARK: - Init
 
+    /// Create new launch argument
+    /// - Parameters:
+    ///     - name: Name of argument
+    ///     - isEnabled: If enabled then argument is marked as active
     public init(name: String, isEnabled: Bool) {
         self.name = name
         self.isEnabled = isEnabled
