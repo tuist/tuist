0c014c0eac0c3d3d3fb23b3702d0ab2615a31e90
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2020-11-19 17:30:12 +0100

    [carthage] add `CartfileContentBuilderError`

--- Sources/TuistDependencies/DependencyManagerInteractors/Builders/CartfileContentBuilder.swift
+++ Sources/TuistDependencies/DependencyManagerInteractors/Builders/CartfileContentBuilder.swift
@@ -2,6 +2,29 @@ import TSCBasic
 import TuistCore
 import TuistSupport
 
+// MARK: - Cartfile Content Builder Error
+
+enum CartfileContentBuilderError: FatalError, Equatable {
+    /// Thrown when `Requirement.range` has been used for `carthage`'s dependency.
+    case rangeRequirementNotSupported(dependencyName: String, fromVersion: String, toVersion: String)
+    
+    /// Error type.
+    var type: ErrorType {
+        switch self {
+        case .rangeRequirementNotSupported:
+            return .abort
+        }
+    }
+
+    /// Error description.
+    var description: String {
+        switch self {
+        case .rangeRequirementNotSupported(let dependencyName, _, _):
+            return "\(dependencyName) can not be installed. Carthage do not support versions range requirement in Cartfile."
+        }
+    }
+}
+
 // MARK: - Cartfile Content Builder
 
 #warning("TODO: Add unit test!")
@@ -19,15 +42,15 @@ final class CartfileContentBuilder {
     
     // MARK: - Build
     
-    func build() -> String {
-        dependencies
-            .map { $0.toString() }
+    func build() throws -> String {
+        try dependencies
+            .map { try $0.toString() }
             .joined(separator: "\n")
     }
 }
 
 fileprivate extension CarthageDependency {
-    func toString() -> String {
+    func toString() throws -> String {
         switch requirement {
         case .exact(let version):
             return #"github "\#(name)" == \#(version)"#
@@ -36,8 +59,7 @@ fileprivate extension CarthageDependency {
         case .upToNextMinor(let version):
             return #"github "\#(name)" ~> \#(version)"#
         case .range(let fromVersion, let toVersion):
-            #warning("Im not sure if it is possible to handle in cartfile.")
-            fatalError("How to handle it?")
+            throw CartfileContentBuilderError.rangeRequirementNotSupported(dependencyName: name, fromVersion: fromVersion, toVersion: toVersion)
         case .branch(let branch):
             return #"github "\#(name)" "\#(branch)""#
         case .revision(let revision):
