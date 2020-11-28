db3c596f31ffc2e0c166dcae482970a6bda2e1e0
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2020-11-28 22:14:05 +0100

    [carthage] add - `CarthageDependencyTests`

---
+++ Tests/TuistCoreTests/Models/CarthageDependencyTests.swift
@@ -0,0 +1,40 @@
+import Foundation
+import XCTest
+
+@testable import TuistCore
+@testable import TuistSupportTesting
+
+final class CarthageDependencyTests: TuistTestCase {
+    func test_cartfileValue_exact() throws {
+        // Given
+        let dependency = CarthageDependency(name: "Alamofire/Alamofire", requirement: .exact("1.2.3"), platforms: [.iOS])
+        let expected = #"github "Alamofire/Alamofire" == 1.2.3"#
+        
+        // When
+        let got = try dependency.cartfileValue()
+        
+        // Then
+        XCTAssertEqual(got, expected)
+    }
+    
+    func test_cartfileValue_upToNextMajor() throws {
+        // Given
+        let dependency = CarthageDependency(name: "RxSwift/RxSwift", requirement: .upToNextMajor("3.0.1"), platforms: [.macOS])
+        let expected = #"github "RxSwift/RxSwift" ~> 3.0.1"#
+        
+        // When
+        let got = try dependency.cartfileValue()
+        
+        // Then
+        XCTAssertEqual(got, expected)
+    }
+    
+    func test_cartfileValue_range() throws {
+        // Given
+        let dependency = CarthageDependency(name: "RxSwift/RxSwift", requirement: .range(from: "1.0.0", to: "2.0.0"), platforms: [.iOS])
+        let expectedError = CarthageDependencyError.rangeRequirementNotSupported(dependencyName: "RxSwift/RxSwift", fromVersion: "1.0.0", toVersion: "2.0.0")
+        
+        // When / Then
+        XCTAssertThrowsSpecific(try dependency.cartfileValue(), expectedError)
+    }
+}
