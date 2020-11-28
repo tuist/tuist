cc4bf524d5a752f59945581f96843e3b3362873d
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2020-11-28 23:09:11 +0100

    [carthage] add - `CartfileContentGenerator.validate`

--- Sources/TuistDependencies/Carthage/Generators/CartfileContentGenerator.swift
+++ Sources/TuistDependencies/Carthage/Generators/CartfileContentGenerator.swift
@@ -4,6 +4,29 @@ import TuistSupport
 
 // MARK: - Cartfile Content Generating Error
 
+enum CartfileContentGeneratorError: FatalError, Equatable {
+    /// Thrown when at least one of the dependencies is invalid.
+    case invalidDependencies(validationErrors: [String])
+    
+    /// Error type.
+    var type: ErrorType {
+        switch self {
+        case .invalidDependencies:
+            return .abort
+        }
+    }
+
+    /// Error description.
+    var description: String {
+        switch self {
+        case .invalidDependencies(let validationErrors):
+            return "Cartfile can not be generated:\n" + validationErrors.map(\.description).joined(separator: "\n")
+        }
+    }
+}
+
+// MARK: - Cartfile Content Generating
+
 public protocol CartfileContentGenerating {
     /// Generates content for `Cartfile`.
     /// - Parameter dependencies: The dependencies whose will be installed.
@@ -16,8 +39,28 @@ public final class CartfileContentGenerator: CartfileContentGenerating {
     public init() { }
     
     public func cartfileContent(for dependencies: [CarthageDependency]) throws -> String {
-        try dependencies
+        try validate(dependencies: dependencies)
+        
+        return try dependencies
             .map { try $0.cartfileValue() }
             .joined(separator: "\n")
+    }
+    
+    // MARK: - Helpers
+    
+    private func validate(dependencies: [CarthageDependency]) throws {
+        let validationErrors = dependencies
+            .reduce(into: [String]()) { result, dependency in
+                switch dependency.requirement {
+                case .range(let fromVersion, let toVersion):
+                    result.append("\(dependency.name) in version between \(fromVersion) and \(toVersion) can not be installed. Carthage do not support versions range requirement in Cartfile." )
+                default:
+                    break
+                }
+            }
+        
+        if !validationErrors.isEmpty {
+            throw CartfileContentGeneratorError.invalidDependencies(validationErrors: validationErrors)
+        }
     }
 }
--- Tests/TuistDependenciesTests/Carthage/Generators/CartfileContentGenetatorTests.swift
+++ Tests/TuistDependenciesTests/Carthage/Generators/CartfileContentGenetatorTests.swift
@@ -20,11 +20,12 @@ final class CartfileContentGenetatorTests: TuistUnitTestCase {
     
     func test_build_no_dependencies() throws {
         // Given
+        let dependencies: [CarthageDependency] = []
         let expected = """
         """
         
         // When
-        let got = try subject.cartfileContent(for: [])
+        let got = try subject.cartfileContent(for: dependencies)
         
         // Then
         XCTAssertEqual(got, expected)
@@ -32,14 +33,15 @@ final class CartfileContentGenetatorTests: TuistUnitTestCase {
     
     func test_build_single_dependency() throws {
         // Given
+        let dependencies: [CarthageDependency] = [
+            .init(name: "Dependency/Dependency", requirement: .exact("1.1.1"), platforms: [.iOS]),
+        ]
         let expected = """
         github "Dependency/Dependency" == 1.1.1
         """
         
         // When
-        let got = try subject.cartfileContent(for: [
-            .init(name: "Dependency/Dependency", requirement: .exact("1.1.1"), platforms: [.iOS]),
-        ])
+        let got = try subject.cartfileContent(for: dependencies)
         
         // Then
         XCTAssertEqual(got, expected)
@@ -47,6 +49,12 @@ final class CartfileContentGenetatorTests: TuistUnitTestCase {
     
     func test_build_multiple_dependencies() throws {
         // Given
+        let dependencies: [CarthageDependency] = [
+            .init(name: "Dependency/Dependency", requirement: .exact("2.1.1"), platforms: [.iOS]),
+            .init(name: "XYZ/Foo", requirement: .revision("revision"), platforms: [.tvOS, .macOS]),
+            .init(name: "Qwerty/bar", requirement: .branch("develop"), platforms: [.watchOS]),
+            .init(name: "XYZ/Bar", requirement: .upToNextMajor("1.1.1"), platforms: [.iOS]),
+        ]
         let expected = """
         github "Dependency/Dependency" == 2.1.1
         github "XYZ/Foo" "revision"
@@ -55,14 +63,25 @@ final class CartfileContentGenetatorTests: TuistUnitTestCase {
         """
         
         // When
-        let got = try subject.cartfileContent(for: [
-            .init(name: "Dependency/Dependency", requirement: .exact("2.1.1"), platforms: [.iOS]),
-            .init(name: "XYZ/Foo", requirement: .revision("revision"), platforms: [.tvOS, .macOS]),
-            .init(name: "Qwerty/bar", requirement: .branch("develop"), platforms: [.watchOS]),
-            .init(name: "XYZ/Bar", requirement: .upToNextMajor("1.1.1"), platforms: [.iOS]),
-        ])
+        let got = try subject.cartfileContent(for: dependencies)
         
         // Then
         XCTAssertEqual(got, expected)
+    }
+    
+    func test_build_invalid_dependencies() throws {
+        let dependencies: [CarthageDependency] = [
+            .init(name: "Dependency/Dependency", requirement: .range(from: "3.1.3", to: "4.0.0"), platforms: [.iOS]),
+            .init(name: "XYZ/Foo", requirement: .revision("revision"), platforms: [.tvOS, .macOS]),
+            .init(name: "Qwerty/bar", requirement: .range(from: "1.0.1", to: "2.3.1"), platforms: [.watchOS]),
+            .init(name: "XYZ/Bar", requirement: .upToNextMajor("1.1.1"), platforms: [.iOS]),
+        ]
+        let expectedError = CartfileContentGeneratorError.invalidDependencies(validationErrors: [
+            "Dependency/Dependency in version between 3.1.3 and 4.0.0 can not be installed. Carthage do not support versions range requirement in Cartfile.",
+            "Qwerty/bar in version between 1.0.1 and 2.3.1 can not be installed. Carthage do not support versions range requirement in Cartfile.",
+        ])
+        
+        // When / Then
+        XCTAssertThrowsSpecific(try subject.cartfileContent(for: dependencies), expectedError)
     }
 }
