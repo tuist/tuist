08da73b387fd4f7a3e360874b1bcd72017298e0b
Pedro Piñera <pepibumur@gmail.com>
2020-12-14 18:25:52 +0100

    Test snapshot

--- website/src/components/home/highlights.test.tsx
+++ website/src/components/home/highlights.test.tsx
@@ -1,5 +1,11 @@
-describe('feature', () => {
-  it('whatever', () => {
-    expect(3).toBe(3)
+import React from 'react'
+
+import Highlights from './highlights'
+import renderer from 'react-test-renderer'
+
+describe('Higlights', () => {
+  it('renders the right HTML', () => {
+    const tree = renderer.create(<Highlights />).toJSON()
+    expect(tree).toMatchSnapshot()
   })
 })
