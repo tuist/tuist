82d9446f7981630ece31d796f90b81a084e2848a
Pedro Piñera <pepibumur@gmail.com>
2020-12-15 20:08:48 +0100

    Remove purge css

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -211,17 +211,6 @@ module.exports = {
     'gatsby-plugin-meta-redirect',
     `gatsby-plugin-robots-txt`,
     {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        printRejected: true, // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
-        tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-      },
-    },
-    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: 'UA-125584790-6',
