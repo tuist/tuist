90dd5fb8fd19f080114eee23686b33a271940eb0
Pedro Piñera <pepibumur@gmail.com>
2020-12-15 19:34:14 +0100

    Fix Tailwind integration

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,13 +1,11 @@
 module.exports = {
-  purge: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      boxShadow: {
-        links:
-          '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
-      },
-    },
+    extend: {},
   },
-  variants: {},
+  variants: {
+    extend: {},
+  },
   plugins: [],
 }
