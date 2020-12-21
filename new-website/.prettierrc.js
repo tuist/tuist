228c5128354e40856bedd04316bc550358d558c3
Pedro Piñera <pepibumur@gmail.com>
2020-12-21 09:26:21 +0100

    Add new website using NextJS

---
+++ new-website/.prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 4,
+  quoteProps: 'preserve',
+}
---
+++ new-website/pages/index.test.tsx
@@ -0,0 +1,7 @@
+describe('something', () => {
+    it('tests', () => {
+        expect(1).toBe(1);
+    });
+});
+
+export default undefined;
