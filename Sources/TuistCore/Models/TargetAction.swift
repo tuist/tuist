af49492919f8018cb45123c640f3c201b3c8523e
Jared Sorge <jasorge@zulily.com>
2020-12-22 09:43:02 -0800

    Update the TuistCore version

--- Sources/TuistCore/Models/TargetAction.swift
+++ Sources/TuistCore/Models/TargetAction.swift
@@ -13,20 +13,62 @@ public struct TargetAction: Equatable {
         case post
     }
 
+    /// How to execute the target action
+    ///
+    /// - file: Executes the tool, calling the script at the path. Tuist will look up the tool on the environment's PATH
+    /// - text: Executes the entered script. This should be a short command.
+    private enum Script: Equatable {
+        case file(_ tool: String?, _ path: AbsolutePath?, _ args: [String])
+        case text(String)
+    }
+
     /// Name of the build phase when the project gets generated
     public let name: String
 
-    /// Name of the tool to execute. Tuist will look up the tool on the environment's PATH
-    public let tool: String?
+    /// The script to execute in the action
+    private let script: Script
+
+    public var isEmbeddedScript: Bool {
+        switch self.script {
+        case .file:
+            return false
+        case .text:
+            return true
+        }
+    }
+
+    /// Name of the tool to execute.
+    public var tool: String? {
+        switch script {
+        case .file(let tool, _, _):
+            return tool
+        case .text:
+            return nil
+        }
+    }
 
     /// Path to the script to execute
-    public let path: AbsolutePath?
+    public var path: AbsolutePath? {
+        switch script {
+        case .file(_, let path, _):
+            return path
+        case .text:
+            return nil
+        }
+    }
 
     /// Target action order
     public let order: Order
 
     /// Arguments that to be passed
-    public let arguments: [String]
+    public var arguments: [String] {
+        switch script {
+        case .file(_, _, let args):
+            return args
+        case .text:
+            return []
+        }
+    }
 
     /// List of input file paths
     public let inputPaths: [AbsolutePath]
@@ -74,9 +116,40 @@ public struct TargetAction: Equatable {
     {
         self.name = name
         self.order = order
-        self.tool = tool
-        self.path = path
-        self.arguments = arguments
+        self.script = .file(tool, path, arguments)
+        self.inputPaths = inputPaths
+        self.inputFileListPaths = inputFileListPaths
+        self.outputPaths = outputPaths
+        self.outputFileListPaths = outputFileListPaths
+        self.showEnvVarsInLog = showEnvVarsInLog
+        self.basedOnDependencyAnalysis = basedOnDependencyAnalysis
+    }
+
+    /// Initializes a new target action with its attributes.
+    ///
+    /// - Parameters:
+    ///   - name: Name of the build phase when the project gets generated
+    ///   - order: Target action order
+    ///   - script: The text of the script to run. This should be kept small.
+    ///   - inputPaths: List of input file paths
+    ///   - inputFileListPaths: List of input filelist paths
+    ///   - outputPaths: List of output file paths
+    ///   - outputFileListPaths: List of output filelist paths
+    ///   - showEnvVarsInLog: Show environment variables in the logs
+    ///   - basedOnDependencyAnalysis: Whether to skip running this script in incremental builds
+    public init(name: String,
+                order: Order,
+                script: String,
+                inputPaths: [AbsolutePath] = [],
+                inputFileListPaths: [AbsolutePath] = [],
+                outputPaths: [AbsolutePath] = [],
+                outputFileListPaths: [AbsolutePath] = [],
+                showEnvVarsInLog: Bool = true,
+                basedOnDependencyAnalysis: Bool? = nil)
+    {
+        self.name = name
+        self.order = order
+        self.script = .text(script)
         self.inputPaths = inputPaths
         self.inputFileListPaths = inputFileListPaths
         self.outputPaths = outputPaths
@@ -92,7 +165,9 @@ public struct TargetAction: Equatable {
     /// - Returns: Shell script that should be used in the target build phase.
     /// - Throws: An error if the tool absolute path cannot be obtained.
     public func shellScript(sourceRootPath: AbsolutePath) throws -> String {
-        if let path = path {
+        if case let Script.text(text) = script {
+            return text
+        } else if let path = path {
             return "\"${PROJECT_DIR}\"/\(path.relative(to: sourceRootPath).pathString) \(arguments.joined(separator: " "))"
         } else {
             return try "\(System.shared.which(tool!).spm_chomp().spm_chuzzle()!) \(arguments.joined(separator: " "))"
---
+++ Tests/TuistCoreTests/Models/TargetActionTests.swift
@@ -0,0 +1,20 @@
+import Foundation
+import TSCBasic
+import XCTest
+@testable import TuistCore
+
+private let script = """
+echo 'Hello World'
+wd=$(pwd)
+echo "$wd"
+"""
+
+final class TargetActionTests: XCTestCase {
+    func test_embedded_script_functions() throws {
+        let subject = TargetAction(name: "name", order: .pre, script: script)
+
+        let shellScript = try subject.shellScript(sourceRootPath: .root)
+        XCTAssertEqual(script, shellScript)
+        XCTAssertTrue(subject.isEmbeddedScript)
+    }
+}
