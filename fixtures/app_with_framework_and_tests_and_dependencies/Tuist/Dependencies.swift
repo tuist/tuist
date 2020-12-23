ca940e8e8e74b02992b5d232343725d715fe4c12
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2020-12-23 14:54:13 +0100

    [carthage] fix - `app_with_framework_and_tests_and_dependencies` fixture

--- fixtures/app_with_framework_and_tests_and_dependencies/Tuist/Dependencies.swift
+++ fixtures/app_with_framework_and_tests_and_dependencies/Tuist/Dependencies.swift
@@ -1,5 +1,5 @@
 import ProjectDescription
 
 let dependencies = Dependencies([
-    .carthage(name: "Alamofire/Alamofire", requirement: .exact("5.0.4"), platforms: [.macOS])
+    .carthage(origin: .github(path: "Alamofire/Alamofire"), requirement: .exact("5.0.4"), platforms: [.macOS])
 ])
