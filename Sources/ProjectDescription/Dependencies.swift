aac39fd6cb2c536abc424ea1123682d5f8ee41ec
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2020-12-23 14:34:25 +0100

    [carthage] add - documentation

--- Sources/ProjectDescription/Dependencies.swift
+++ Sources/ProjectDescription/Dependencies.swift
@@ -1,8 +1,12 @@
 import Foundation
 
+/// A `Dependencies` manifest allows for defining external dependencies for Tuist.
 public struct Dependencies: Codable, Equatable {
+    /// List of dependencies.
     public let dependencies: [Dependency]
-
+    
+    /// Initializes a new `Dependencies` manifest instance.
+    /// - Parameter dependencies: List of dependencies.
     public init(_ dependencies: [Dependency] = []) {
         self.dependencies = dependencies
         dumpIfNeeded(self)
