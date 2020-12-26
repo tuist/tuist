1d08cb069fd7f7fc6a66f6e58e30e1738c09d1e7
David Chavez <david@dcvz.io>
2020-12-26 22:09:37 +0100

    Add support for .s source files (#2199)

--- Sources/TuistCore/Models/Target.swift
+++ Sources/TuistCore/Models/Target.swift
@@ -18,7 +18,7 @@ public enum TargetError: FatalError, Equatable {
 public struct Target: Equatable, Hashable, Comparable {
     // MARK: - Static
 
-    public static let validSourceExtensions: [String] = ["m", "swift", "mm", "cpp", "c", "d", "intentdefinition", "xcmappingmodel", "metal"]
+    public static let validSourceExtensions: [String] = ["m", "swift", "mm", "cpp", "c", "d", "s", "intentdefinition", "xcmappingmodel", "metal"]
     public static let validFolderExtensions: [String] = ["framework", "bundle", "app", "xcassets", "appiconset", "scnassets"]
 
     // MARK: - Attributes
