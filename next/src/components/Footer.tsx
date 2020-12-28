53f993103377de72c2a26f7e23adba52d64db456
Pedro Piñera <pepibumur@gmail.com>
2020-12-28 16:18:58 +0100

    Add footer

--- next/src/components/Footer.tsx
+++ next/src/components/Footer.tsx
@@ -1,21 +1,50 @@
 import React from 'react'
 import tw from 'twin.macro'
 import useSiteLinks from '../hooks/useSiteLinks'
+import logo from '../images/logo.svg'
 
 const Footer = () => {
   const links = useSiteLinks()
-  console.log(links)
+  const css = [tw`hover:text-primary focus:text-primary focus:underline`]
   return (
-    <footer css={[tw`bg-pink-200`]}>
-      <div css={[tw`max-w-6xl mx-auto bg-green-200`]}>
-        <div>Get started</div>
-        <div>Manifest specification</div>
-        <div>Dependencies</div>
-        <div>Contributors</div>
-        <div>Slack</div>
-        <div>GitHub</div>
-        <div>Blog</div>
-        <div>Releases</div>
+    <footer css={[tw`font-tuist flex flex-col items-center py-20`]}>
+      <div
+        css={[
+          tw`mx-10 md:mx-auto md:max-w-2xl flex flex-col items-center space-y-6`,
+        ]}
+      >
+        <div css={[tw`flex flex-row space-x-2 items-center`]}>
+          <img src={logo} alt="Tuist's logo" css={[tw`w-8 h-8`]} />
+          <div css={[tw`text-2xl font-normal`]}>tuist</div>
+        </div>
+        <div
+          css={[tw`flex flex-row flex-wrap space-x-3 justify-center text-lg`]}
+        >
+          <a target="__blank" css={css}>
+            Get started
+          </a>
+          <a target="__blank" css={css}>
+            Manifest specification
+          </a>
+          <a target="__blank" css={css}>
+            Dependencies
+          </a>
+          <a target="__blank" css={css}>
+            Contributors
+          </a>
+          <a href={links.slack} target="__blank" css={css}>
+            Slack
+          </a>
+          <a href={links.githubOrganization} target="__blank" css={css}>
+            GitHub
+          </a>
+          <a target="__blank" css={css}>
+            Blog
+          </a>
+          <a href={links.releases} target="__blank" css={css}>
+            Releases
+          </a>
+        </div>
       </div>
     </footer>
   )
--- next/tailwind.config.js
+++ next/tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   theme: {
+    colors: {
+      primary: '#6236FF',
+    },
     extend: {
       colors: {},
+      fontFamily: {
+        tuist:
+          'ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      },
     },
   },
   plugins: [],
