8753af957ed89d4c25c2593dc4dd58c0c5d1422a
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2021-01-03 14:34:57 +0100

    [spm-models] add - `Dependency.spm`, `Dependency.SPMRequirement`

--- Sources/ProjectDescription/Dependency.swift
+++ Sources/ProjectDescription/Dependency.swift
@@ -12,7 +12,7 @@ public enum Dependency: Codable, Equatable {
         case binary(path: String)
     }
 
-    /// Requirement for the Carthage dependency
+    /// Requirement for the Carthage dependency.
     public enum CarthageRequirement: Codable, Equatable {
         /// Mimics `== 1.0` from `Cartfile`.
         case exact(Version)
@@ -25,9 +25,31 @@ public enum Dependency: Codable, Equatable {
         /// Mimics `"revision"` from `Cartfile`.
         case revision(String)
     }
+    
+    /// Requirement for the Swift Package Manager dependency.
+    public enum SPMRequirement: Codable, Equatable {
+        /// Mimics `.exact("1.2.3")` from `Package.swift`.
+        case exact(Version)
+        /// Mimics `.upToNextMajor(from: "1.2.3")` from `Package.swift`.
+        case upToNextMajor(Version)
+        /// Mimics `.upToNextMinor(from: "1.2.3")` from `Package.swift`.
+        case upToNextMinor(Version)
+        /// Mimics `from: "1.2.3"` from `Package.swift`.
+        case atLeast(Version)
+        /// Mimics `"1.2.3"..<"1.2.6"` from `Package.swift`.
+        case range(Range<Version>)
+        /// Mimics `1.2.3"..."1.2.6` from `Package.swift`.`
+        case closedRange(ClosedRange<Version>)
+        /// Mimics `.branch("develop")` from `Package.swift`.
+        case branch(String)
+        /// Mimics `.revision("revision")` from `Package.swift`.
+        case revision(String)
+    }
 
     /// Contains the description of dependency that can by installed using Carthage. More: https://github.com/Carthage/Carthage/blob/master/Documentation/Artifacts.md
     case carthage(origin: CarthageOrigin, requirement: CarthageRequirement, platforms: Set<Platform>)
+    /// Contains the description of dependency that can by installed using Swift Package Manager. More: https://docs.swift.org/package-manager/PackageDescription/index.html
+    case spm(url: String, requirement: SPMRequirement)
 }
 
 // MARK: - Dependency: Codable
@@ -35,13 +57,16 @@ public enum Dependency: Codable, Equatable {
 extension Dependency {
     private enum Kind: String, Codable {
         case carthage
+        case spm
     }
 
     private enum CodingKeys: String, CodingKey {
         case kind
         case origin
-        case requirement
+        case carthageRequirement
         case platforms
+        case url
+        case spmRequirement
     }
 
     public init(from decoder: Decoder) throws {
@@ -50,9 +75,13 @@ extension Dependency {
         switch kind {
         case .carthage:
             let origin = try container.decode(CarthageOrigin.self, forKey: .origin)
-            let requirement = try container.decode(CarthageRequirement.self, forKey: .requirement)
+            let requirement = try container.decode(CarthageRequirement.self, forKey: .carthageRequirement)
             let platforms = try container.decode(Set<Platform>.self, forKey: .platforms)
             self = .carthage(origin: origin, requirement: requirement, platforms: platforms)
+        case .spm:
+            let url = try container.decode(String.self, forKey: .url)
+            let requirement = try container.decode(SPMRequirement.self, forKey: .spmRequirement)
+            self = .spm(url: url, requirement: requirement)
         }
     }
 
@@ -62,8 +91,58 @@ extension Dependency {
         case let .carthage(origin, requirement, platforms):
             try container.encode(Kind.carthage, forKey: .kind)
             try container.encode(origin, forKey: .origin)
-            try container.encode(requirement, forKey: .requirement)
+            try container.encode(requirement, forKey: .carthageRequirement)
             try container.encode(platforms, forKey: .platforms)
+        case let .spm(url, requirement):
+            try container.encode(Kind.spm, forKey: .kind)
+            try container.encode(url, forKey: .url)
+            try container.encode(requirement, forKey: .spmRequirement)
+        }
+    }
+}
+
+// MARK: - Dependency.CarthageOrigin: Codable
+
+extension Dependency.CarthageOrigin {
+    private enum Kind: String, Codable {
+        case github
+        case git
+        case binary
+    }
+
+    private enum CodingKeys: String, CodingKey {
+        case kind
+        case path
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let kind = try container.decode(Kind.self, forKey: .kind)
+        switch kind {
+        case .github:
+            let path = try container.decode(String.self, forKey: .path)
+            self = .github(path: path)
+        case .git:
+            let path = try container.decode(String.self, forKey: .path)
+            self = .git(path: path)
+        case .binary:
+            let path = try container.decode(String.self, forKey: .path)
+            self = .binary(path: path)
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case let .github(path):
+            try container.encode(Kind.github, forKey: .kind)
+            try container.encode(path, forKey: .path)
+        case let .git(path):
+            try container.encode(Kind.git, forKey: .kind)
+            try container.encode(path, forKey: .path)
+        case let .binary(path):
+            try container.encode(Kind.binary, forKey: .kind)
+            try container.encode(path, forKey: .path)
         }
     }
 }
@@ -130,48 +209,87 @@ extension Dependency.CarthageRequirement {
     }
 }
 
-// MARK: - Dependency.CarthageOrigin: Codable
+// MARK: - Dependency.SPMRequirement: Codable
 
-extension Dependency.CarthageOrigin {
+extension Dependency.SPMRequirement {
     private enum Kind: String, Codable {
-        case github
-        case git
-        case binary
+        case exact
+        case upToNextMajor
+        case upToNextMinor
+        case atLeast
+        case range
+        case closedRange
+        case revision
+        case branch
     }
 
     private enum CodingKeys: String, CodingKey {
         case kind
-        case path
+        case version
+        case range
+        case closedRange
+        case branch
+        case revision
     }
-
+    
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         let kind = try container.decode(Kind.self, forKey: .kind)
         switch kind {
-        case .github:
-            let path = try container.decode(String.self, forKey: .path)
-            self = .github(path: path)
-        case .git:
-            let path = try container.decode(String.self, forKey: .path)
-            self = .git(path: path)
-        case .binary:
-            let path = try container.decode(String.self, forKey: .path)
-            self = .binary(path: path)
+        case .exact:
+            let version = try container.decode(Version.self, forKey: .version)
+            self = .exact(version)
+        case .upToNextMajor:
+            let version = try container.decode(Version.self, forKey: .version)
+            self = .upToNextMajor(version)
+        case .upToNextMinor:
+            let version = try container.decode(Version.self, forKey: .version)
+            self = .upToNextMinor(version)
+        case .atLeast:
+            let version = try container.decode(Version.self, forKey: .version)
+            self = .atLeast(version)
+        case .range:
+            let range = try container.decode(Range<Version>.self, forKey: .range)
+            self = .range(range)
+        case .closedRange:
+            let closedRange = try container.decode(ClosedRange<Version>.self, forKey: .closedRange)
+            self = .closedRange(closedRange)
+        case .revision:
+            let revision = try container.decode(String.self, forKey: .revision)
+            self = .revision(revision)
+        case .branch:
+            let branch = try container.decode(String.self, forKey: .branch)
+            self = .branch(branch)
         }
     }
-
+    
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         switch self {
-        case let .github(path):
-            try container.encode(Kind.github, forKey: .kind)
-            try container.encode(path, forKey: .path)
-        case let .git(path):
-            try container.encode(Kind.git, forKey: .kind)
-            try container.encode(path, forKey: .path)
-        case let .binary(path):
-            try container.encode(Kind.binary, forKey: .kind)
-            try container.encode(path, forKey: .path)
+        case let .exact(version):
+            try container.encode(Kind.exact, forKey: .kind)
+            try container.encode(version, forKey: .version)
+        case let .upToNextMajor(version):
+            try container.encode(Kind.upToNextMajor, forKey: .kind)
+            try container.encode(version, forKey: .version)
+        case let .upToNextMinor(version):
+            try container.encode(Kind.upToNextMinor, forKey: .kind)
+            try container.encode(version, forKey: .version)
+        case let .atLeast(version):
+            try container.encode(Kind.atLeast, forKey: .kind)
+            try container.encode(version, forKey: .version)
+        case let .range(range):
+            try container.encode(Kind.range, forKey: .kind)
+            try container.encode(range, forKey: .range)
+        case let .closedRange(closedRange):
+            try container.encode(Kind.closedRange, forKey: .kind)
+            try container.encode(closedRange, forKey: .closedRange)
+        case let .revision(revision):
+            try container.encode(Kind.revision, forKey: .kind)
+            try container.encode(revision, forKey: .revision)
+        case let .branch(branch):
+            try container.encode(Kind.branch, forKey: .kind)
+            try container.encode(branch, forKey: .branch)
         }
     }
 }
