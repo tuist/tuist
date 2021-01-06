079678ec571899d57acac62e442390c70e94d0c3
Pedro Piñera <pepibumur@gmail.com>
2021-01-06 18:28:59 +0100

    Add ModernEra fonts

--- next/tailwind.config.js
+++ next/tailwind.config.js
@@ -6,10 +6,8 @@ module.exports = {
         'primary-dark': '#0A0619',
       },
       fontFamily: {
-        tuist:
-          'ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
-        'tuist-mono':
-          'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace;',
+        tuist: 'ModernEra;',
+        'tuist-mono': 'ModernEraMono;',
       },
     },
   },
