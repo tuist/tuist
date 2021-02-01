632b509106c5c1d1d4f5512b60d52de2ccb236a5
David Harris <daharr@umich.edu>
2021-02-01 06:17:02 -0500

    Treat CoreData files as resources for static/dynamic bundle generation (#2376)
    - The auto generated resource targets for static libraries / frameworks now also handle copying across CoreData models 
        - CoreData models need to be both compiled and bundled, however for static frameworks they can't bundle resources as such they need to be bundled in the supplementary resources target (`.bundle` target)
    - CoreData models are now added as resources to target that don't support sources (e.g. `.bundle`) 
        - This ensure the `.bundle` targets don't accidentally create an executable which is invalid for iOS applications
    - A new fixture has been added to help test this use case within `fixtures/ios_app_with_coredata_in_static_framework`
    
    Test Plan:
    
    - Run `swift run tuist generate --path fixtures/ios_app_with_coredata_in_static_framework`
    - Verify the generated project compiles and runs without issues
    - Verify the CoreData models are included as sources within the static framework target
    - Verify the CoreData models are included as resources within the bundle target

---
+++ fixtures/ios_app_with_coredata_in_static_framework/Framework/Project.swift
@@ -0,0 +1,14 @@
+import ProjectDescription
+
+let project = Project(name: "Framework",
+                      targets: [
+                        Target(name: "Framework",
+                             platform: .iOS,
+                             product: .staticFramework,
+                             bundleId: "io.tuist.framework",
+                             infoPlist: .default,
+                             sources: ["Sources/**"],
+                             coreDataModels: [
+                               CoreDataModel("CoreData/Users.xcdatamodeld")
+                             ])
+                      ])
