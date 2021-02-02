530854228eb19477c1d9db0b86b599e62d486843
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2021-02-02 13:09:09 +0100

    [Chimera][SPM] TuistGraph's models (#2395)
    * [spm-core-models] add - `SwiftPackageManagerDependency`
    
    * [spm-core-models] fix - run `bundle exec rake style_correct`

--- Sources/TuistGraph/Models/Dependencies.swift
+++ Sources/TuistGraph/Models/Dependencies.swift
@@ -2,10 +2,13 @@ import Foundation
 
 public struct Dependencies: Equatable {
     public let carthageDependencies: [CarthageDependency]
+    public let swiftPackageManagerDependencies: [SwiftPackageManagerDependency]
 
     public init(
-        carthageDependencies: [CarthageDependency]
+        carthageDependencies: [CarthageDependency],
+        swiftPackageManagerDependencies: [SwiftPackageManagerDependency]
     ) {
         self.carthageDependencies = carthageDependencies
+        self.swiftPackageManagerDependencies = swiftPackageManagerDependencies
     }
 }
