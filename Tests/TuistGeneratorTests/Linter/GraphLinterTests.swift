22a599ad8d3fae0d1c37f0053bce3545570d4725
Natan Rolnik <natanrolnik@gmail.com>
2021-02-08 13:11:45 +0200

    Remove decision records, fix tests

--- Tests/TuistGeneratorTests/Linter/GraphLinterTests.swift
+++ Tests/TuistGeneratorTests/Linter/GraphLinterTests.swift
@@ -986,7 +986,7 @@ final class GraphLinterTests: TuistUnitTestCase {
         XCTAssertEqual(got, [
             LintingIssue(reason: "The bundle identifier 'com.tuist.frameworks.test' is being used by multiple targets: frameworkA, frameworkB, and frameworkF.", severity: .warning),
             LintingIssue(reason: "The bundle identifier 'com.tuist.frameworks.test2' is being used by multiple targets: frameworkC and frameworkE.", severity: .warning),
-        ]
+        ])
     }
 
     func test_lintBundleIdentifiersShouldIgnoreVariables() {
