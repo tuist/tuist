fdb341a7c4525bd7af76eeb41ca160b1151062cd
Andrea Cipriani <andreacipriani89@gmail.com>
2021-02-12 18:48:56 -0500

    Set _superCommandName in all sub-commands

--- Sources/TuistKit/Commands/Scale/CloudAuthCommand.swift
+++ Sources/TuistKit/Commands/Scale/CloudAuthCommand.swift
@@ -5,6 +5,7 @@ import TSCBasic
 struct CloudAuthCommand: ParsableCommand {
     static var configuration: CommandConfiguration {
         CommandConfiguration(commandName: "auth",
+                             _superCommandName: "cloud",
                              abstract: "Authenticates the user on the server with the URL defined in the Config.swift file")
     }
 
--- Sources/TuistKit/Commands/Scale/CloudSessionCommand.swift
+++ Sources/TuistKit/Commands/Scale/CloudSessionCommand.swift
@@ -5,6 +5,7 @@ import TSCBasic
 struct CloudSessionCommand: ParsableCommand {
     static var configuration: CommandConfiguration {
         CommandConfiguration(commandName: "session",
+                             _superCommandName: "cloud",
                              abstract: "Prints any existing session to authenticate on the server with the URL defined in the Config.swift file")
     }
 
