b50c8370897b3adbf37d6bc30cde5a8b1679cdc6
Natan Rolnik <natanrolnik@gmail.com>
2021-03-01 22:18:09 +0200

    PR changes

--- Sources/TuistCore/ContentHashing/HashingFilesFilter.swift
+++ Sources/TuistCore/ContentHashing/HashingFilesFilter.swift
@@ -1,17 +1,17 @@
 import Foundation
 import TSCBasic
 
-internal class HashingFilesFilter {
+class HashingFilesFilter {
     /// an array of filters, which should return if a path should be included in hashing calculations or not.
     private let filters: [(AbsolutePath) -> Bool]
 
-    internal init() {
+    init() {
         filters = [
             { $0.basename.uppercased() != ".DS_STORE" },
         ]
     }
 
-    internal func callAsFunction(_ path: AbsolutePath) -> Bool {
+    func callAsFunction(_ path: AbsolutePath) -> Bool {
         !filters.contains(where: { $0(path) == false })
     }
 }
