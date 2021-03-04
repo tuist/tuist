c13c2dc4e504bdf2942a6dc5ead02f07090447ff
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2021-03-04 17:50:32 +0100

    [carthage-no-use-binaries] fix - swftlint violation

--- Sources/TuistDependenciesTesting/Carthage/MockCarthageCommandGenerator.swift
+++ Sources/TuistDependenciesTesting/Carthage/MockCarthageCommandGenerator.swift
@@ -8,19 +8,30 @@ public final class MockCarthageCommandGenerator: CarthageCommandGenerating {
 
     var invokedCommand = false
     var invokedCommandCount = 0
-    var invokedCommandParameters: (path: AbsolutePath, produceXCFrameworks: Bool, noUseBinaries: Bool, platforms: Set<Platform>?)?
-    var invokedCommandParametersList = [(path: AbsolutePath, produceXCFrameworks: Bool, noUseBinaries: Bool, platforms: Set<Platform>?)]()
+    var invokedCommandParameters: CommandArgs?
+    var invokedCommandParametersList = [CommandArgs]()
     var commandStub: ((AbsolutePath, Bool, Bool, Set<Platform>?) -> [String])?
 
     public func command(path: AbsolutePath, produceXCFrameworks: Bool, noUseBinaries: Bool, platforms: Set<Platform>?) -> [String] {
         invokedCommand = true
         invokedCommandCount += 1
-        invokedCommandParameters = (path, produceXCFrameworks, noUseBinaries, platforms)
-        invokedCommandParametersList.append((path, produceXCFrameworks, noUseBinaries, platforms))
+        invokedCommandParameters = CommandArgs(path: path, produceXCFrameworks: produceXCFrameworks, noUseBinaries: noUseBinaries, platforms: platforms)
+        invokedCommandParametersList.append(CommandArgs(path: path, produceXCFrameworks: produceXCFrameworks, noUseBinaries: noUseBinaries, platforms: platforms))
         if let stub = commandStub {
             return stub(path, produceXCFrameworks, noUseBinaries, platforms)
         } else {
             return []
         }
+    }
+}
+
+// MARK: - Models
+
+extension MockCarthageCommandGenerator {
+    struct CommandArgs {
+        let path: AbsolutePath
+        let produceXCFrameworks: Bool
+        let noUseBinaries: Bool
+        let platforms: Set<Platform>?
     }
 }
