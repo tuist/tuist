01f8c8765bfe37e681cc07b093c1dd485173e098
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2021-03-04 17:21:08 +0100

    [carthage-no-use-binaries] change - update acceptance test

--- fixtures/ios_app_with_setup/Setup.swift
+++ fixtures/ios_app_with_setup/Setup.swift
@@ -2,5 +2,5 @@ import ProjectDescription
 
 let setup = Setup([
     .custom(name: "My Custom Tool", meet: ["touch", "/tmp/my_test_tool"], isMet: ["ls", "/tmp/my_test_tool"]),
-    .carthage(platforms: [.iOS], useXCFrameworks: true),
+    .carthage(platforms: [.iOS], useXCFrameworks: true, noUseBinaries: true),
 ])
