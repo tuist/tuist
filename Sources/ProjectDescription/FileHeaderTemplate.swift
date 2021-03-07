cadd801d9204dc5af91605fbb484a0d3fdcbbc4e
Jakub Olejnik <olejnjak@gmail.com>
2021-03-07 20:17:26 +0100

    Improve documentation

--- Sources/ProjectDescription/FileHeaderTemplate.swift
+++ Sources/ProjectDescription/FileHeaderTemplate.swift
@@ -1,6 +1,13 @@
 import Foundation
 
 /// Enum representing customizable file header template for Xcode built-in file templates
+///
+/// Lets you define custom file header template for built-in Xcode templates, e.g. when you create new Swift file you can automatically have your custom define file header.
+///
+/// Tuist automatically performs several template transformations for you
+///  - if your template starts with comment slashes (`//`) we remove them as they are added automatically by Xcode
+///  - if your template doesn't start with comment and whitespace or newline, we add a space - otherwise your header would be glued to implicit comment slashes which you probably do not want
+///  - if your template has trailing newline, we remove it as it is implicitly added by Xcode
 public enum FileHeaderTemplate: Codable, Equatable, ExpressibleByStringInterpolation {
     enum CodingKeys: String, CodingKey {
         case file
