df32faf7f27436b40b9d836a4de56933980a9740
fortmarek <marekfort@me.com>
2021-03-09 08:32:07 +0100

    Add grpc dependency

--- fixtures/ios_workspace_with_microfeature_architecture/Frameworks/CoreFramework/Project.swift
+++ fixtures/ios_workspace_with_microfeature_architecture/Frameworks/CoreFramework/Project.swift
@@ -1,25 +1,37 @@
 import ProjectDescription
 import BundlePlugin
 
-let project = Project(name: "Core",
-                      targets: [
-                        Target(name: "Core",
-                               platform: .iOS,
-                               product: .framework,
-                               bundleId: .bundleId(for: "Core"),
-                               infoPlist: "Info.plist",
-                               sources: ["Sources/**"],
-                               resources: [
-                                       /* Path to resources can be defined here */
-                                       // "Resources/**"
-                               ]),
-                        Target(name: "CoreTests",
-                               platform: .iOS,
-                               product: .unitTests,
-                               bundleId: .bundleId(for: "CoreTests"),
-                               infoPlist: "Tests.plist",
-                               sources: "Tests/**",
-                               dependencies: [
-                                    .target(name: "Core")
-                               ])
-                      ])
+let project = Project(
+    name: "Core",
+    packages: [
+        .package(url: "https://github.com/grpc/grpc-swift.git", .upToNextMajor(from: "1.0.0")),
+    ],
+    targets: [
+        Target(
+            name: "Core",
+            platform: .iOS,
+            product: .framework,
+            bundleId: .bundleId(for: "Core"),
+            infoPlist: "Info.plist",
+            sources: ["Sources/**"],
+            resources: [
+                /* Path to resources can be defined here */
+                // "Resources/**"
+            ],
+            dependencies: [
+                .package(product: "GRPC"),
+            ]
+        ),
+        Target(
+            name: "CoreTests",
+            platform: .iOS,
+            product: .unitTests,
+            bundleId: .bundleId(for: "CoreTests"),
+            infoPlist: "Tests.plist",
+            sources: "Tests/**",
+            dependencies: [
+                .target(name: "Core")
+            ]
+        )
+    ]
+)
--- fixtures/ios_workspace_with_microfeature_architecture/Frameworks/CoreFramework/Sources/CoreClass.swift
+++ fixtures/ios_workspace_with_microfeature_architecture/Frameworks/CoreFramework/Sources/CoreClass.swift
@@ -1,5 +1,9 @@
 import Foundation
+@_implementationOnly import GRPC
 
 public class CoreClass {
-    public init() {}
+    public init() {
+        let statusCode = GRPCStatus.Code.alreadyExists
+        print(statusCode)
+    }
 }
