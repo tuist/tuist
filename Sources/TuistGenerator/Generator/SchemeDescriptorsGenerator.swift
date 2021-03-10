cbbd7572fda813a0d61ef9dcbc24d52b7389c139
Shai Mishali <freak4pc@gmail.com>
2021-03-10 09:08:30 +0200

    Scheme location simulation (#2616)
    This PR adds a new `simulatedLocation` property on RunAction's options. 
    
    This allows setting either one of Xcode's built-in simulated locations for a scheme, or provide your own GPX file (which will automatically be included in the project).
    
    For example, this is done using:
    
    ```swift
    runAction: RunAction(config: .debug,
                         executable: "QA",
                         arguments: Arguments(
                           environment: [:],
                           launchArguments: []
                         ),
                         options: .options(
                           storeKitConfigurationPath: nil,
                           simulatedLocation: .custom(gpxFile: "Gett/Resources/Habarzel 21.gpx")
                         )
    )
    ```

--- Sources/TuistGenerator/Generator/SchemeDescriptorsGenerator.swift
+++ Sources/TuistGenerator/Generator/SchemeDescriptorsGenerator.swift
@@ -375,6 +375,7 @@ final class SchemeDescriptorsGenerator: SchemeDescriptorsGenerating {
         var commandlineArguments: XCScheme.CommandLineArguments?
         var environments: [XCScheme.EnvironmentVariable]?
         var storeKitConfigurationFileReference: XCScheme.StoreKitConfigurationFileReference?
+        var locationScenarioReference: XCScheme.LocationScenarioReference?
 
         if let arguments = scheme.runAction?.arguments {
             commandlineArguments = XCScheme.CommandLineArguments(arguments: commandlineArgruments(arguments.launchArguments))
@@ -386,13 +387,29 @@ final class SchemeDescriptorsGenerator: SchemeDescriptorsGenerating {
         let isSchemeForAppExtension = self.isSchemeForAppExtension(scheme: scheme, graphTraverser: graphTraverser)
         let launchActionConstants: Constants.LaunchAction = isSchemeForAppExtension == true ? .extension : .default
 
+        let graphTarget = graphTraverser.target(path: target.projectPath, name: target.name)
+
         if
             let storeKitFilePath = scheme.runAction?.options.storeKitConfigurationPath,
-            let graphTarget = graphTraverser.target(path: target.projectPath, name: target.name)
+            let graphTarget = graphTarget
         {
             // the identifier is the relative path between the storekit file, and the xcode project
             let fileRelativePath = storeKitFilePath.relative(to: graphTarget.project.xcodeProjPath)
             storeKitConfigurationFileReference = .init(identifier: fileRelativePath.pathString)
+        }
+
+        if let locationScenario = scheme.runAction?.options.simulatedLocation {
+            var identifier = locationScenario.identifier
+
+            if case let .gpxFile(gpxPath) = locationScenario {
+                let fileRelativePath = gpxPath.relative(to: graphTraverser.workspace.xcWorkspacePath)
+                identifier = fileRelativePath.pathString
+            }
+
+            locationScenarioReference = .init(
+                identifier: identifier,
+                referenceType: locationScenario.referenceType
+            )
         }
 
         return XCScheme.LaunchAction(
@@ -403,6 +420,7 @@ final class SchemeDescriptorsGenerator: SchemeDescriptorsGenerating {
             selectedLauncherIdentifier: launchActionConstants.launcher,
             askForAppToLaunch: launchActionConstants.askForAppToLaunch,
             pathRunnable: pathRunnable,
+            locationScenarioReference: locationScenarioReference,
             disableMainThreadChecker: disableMainThreadChecker,
             commandlineArguments: commandlineArguments,
             environmentVariables: environments,
--- Sources/TuistLoader/Models+ManifestMappers/RunActionOptions+ManifestMapper.swift
+++ Sources/TuistLoader/Models+ManifestMappers/RunActionOptions+ManifestMapper.swift
@@ -13,13 +13,26 @@ extension TuistGraph.RunActionOptions {
                      generatorPaths: GeneratorPaths) throws -> TuistGraph.RunActionOptions
     {
         var storeKitConfigurationPath: AbsolutePath?
+        var simulatedLocation: SimulatedLocation?
 
         if let path = manifest.storeKitConfigurationPath {
             storeKitConfigurationPath = try generatorPaths.resolveSchemeActionProjectPath(path)
         }
 
+        if let manifestLocation = manifest.simulatedLocation {
+            switch (manifestLocation.identifier, manifestLocation.gpxFile) {
+            case let (identifier?, .none):
+                simulatedLocation = .reference(identifier)
+            case let (.none, gpxFile?):
+                simulatedLocation = .gpxFile(try generatorPaths.resolveSchemeActionProjectPath(gpxFile))
+            default:
+                break
+            }
+        }
+
         return TuistGraph.RunActionOptions(
-            storeKitConfigurationPath: storeKitConfigurationPath
+            storeKitConfigurationPath: storeKitConfigurationPath,
+            simulatedLocation: simulatedLocation
         )
     }
 }
