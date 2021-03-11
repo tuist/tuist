71030f2e5415e93847b8470b531e93d865e0cf42
Pedro Piñera <pepibumur@gmail.com>
2021-03-11 18:25:46 +0100

    Add missing tests

---
+++ tools/fourier/test/fourier/services/build/tuist/all_test.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+require "test_helper"
+
+module Fourier
+  module Services
+    module Build
+      module Tuist
+        class AllTest < TestCase
+          def test_call
+            # Given
+            Utilities::System
+              .expects(:tuist)
+              .with("build")
+
+            # Then
+            Services::Build::Tuist::All.call
+          end
+        end
+      end
+    end
+  end
+end
---
+++ tools/fourier/test/fourier/services/edit/tuist_test.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+require "test_helper"
+
+module Fourier
+  module Services
+    module Edit
+      class TuistTest < TestCase
+        def test_call
+          # Given
+          Utilities::System
+            .expects(:tuist)
+            .with("edit", "--only-current-directory")
+
+          # Then
+          Services::Edit::Tuist.call
+        end
+      end
+    end
+  end
+end
