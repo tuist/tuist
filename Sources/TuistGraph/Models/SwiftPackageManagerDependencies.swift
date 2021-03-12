36128f15301502573bc9630b436a77df9321b39e
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2021-03-12 18:50:50 +0100

    [spm-fetch-dependencies] add - generate `Package.swift` content based on `TuistGraph.SwiftPackageManagerDependencies`

--- Sources/TuistGraph/Models/SwiftPackageManagerDependencies.swift
+++ Sources/TuistGraph/Models/SwiftPackageManagerDependencies.swift
@@ -10,4 +10,56 @@ public struct SwiftPackageManagerDependencies: Equatable {
     public init(_ packages: [Package]) {
         self.packages = packages
     }
+    
+    /// Returns `Package.swift` representation.
+    public func stringValue() -> String {
+        """
+        // swift-tools-version:5.3
+
+        import PackageDescription
+
+        let package = Package(
+            name: "PackageName",
+            dependencies: [
+                \(packages.map { $0.stringValue + "," }.joined(separator: "\n        "))
+            ]
+        )
+        """
+    }
+}
+
+// MARK: - Package.stringValue()
+
+private extension Package {
+    /// Returns `Package.swift` representation.
+    var stringValue: String {
+        switch self {
+        case let .local(path):
+            return #".package(path: "\#(path)")"#
+        case let .remote(url, requirement):
+            return #".package(url: "\#(url)", \#(requirement.stringValue))"#
+        }
+    }
+}
+
+// MARK: - Requirement.stringValue()
+
+private extension Requirement {
+    /// Returns `Package.swift` representation.
+    var stringValue: String {
+        switch self {
+        case let .exact(version):
+            return #".exact("\#(version)")"#
+        case let .upToNextMajor(version):
+            return #".upToNextMajor(from: "\#(version)")"#
+        case let .upToNextMinor(version):
+            return #".upToNextMinor(from: "\#(version)")"#
+        case let .branch(branch):
+            return #".branch("\#(branch)")"#
+        case let .revision(revision):
+            return #".revision("\#(revision)")"#
+        case let .range(from, to):
+            return #""\#(from)"..<"\#(to)""#
+        }
+    }
 }
