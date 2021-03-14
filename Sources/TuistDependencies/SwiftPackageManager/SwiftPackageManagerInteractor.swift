0bf061b57d6cf21d3f87707a5a955dd4410041b1
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2021-03-14 21:49:00 +0100

    [spm-fetch-dependencies] add - fetch depedencies

--- Sources/TuistDependencies/SwiftPackageManager/SwiftPackageManagerInteractor.swift
+++ Sources/TuistDependencies/SwiftPackageManager/SwiftPackageManagerInteractor.swift
@@ -1,12 +1,32 @@
 import TSCBasic
-import TuistSupport
 import TuistGraph
+import TuistSupport
 
-//// MARK: - Swift Package Manager Interactor Error
-//
-//public enum SwiftPackageManagerInteractorError: FatalError {
-//
-//}
+enum SwiftPackageManagerInteractorError: FatalError, Equatable {
+    /// Thrown when `Package.resolved` cannont be found in temporary directory after `Swift Package Manager` installation.
+    case packageResolvedNotFound
+    /// Thrown when `.build` directory cannont be found in temporary directory after `Swift Package Manager` installation.
+    case buildDirectoryNotFound
+    
+    /// Error type.
+    var type: ErrorType {
+        switch self {
+        case .packageResolvedNotFound,
+             .buildDirectoryNotFound:
+            return .abort
+        }
+    }
+    
+    /// Error description.
+    var description: String {
+        switch self {
+        case .packageResolvedNotFound:
+            return "Package.resolved file was not found after Carthage installation."
+        case .buildDirectoryNotFound:
+            return ".build directory was not found after Carthage installation."
+        }
+    }
+}
 
 // MARK: - Swift Package Manager Interacting
 
@@ -20,9 +40,151 @@ public protocol SwiftPackageManagerInteracting {
 // MARK: - Swift Package Manager Interactor
 
 public final class SwiftPackageManagerInteractor: SwiftPackageManagerInteracting {
-    public init() {}
+    private let fileHandler: FileHandling
+
+    public init(
+        fileHandler: FileHandling = FileHandler.shared
+    ) {
+        self.fileHandler = fileHandler
+    }
 
     public func fetch(dependenciesDirectory: AbsolutePath, dependencies: SwiftPackageManagerDependencies) throws {
-        #warning("IMPLEMENT ME")
+        logger.info("We are starting to fetch the Swift Package Manager dependencies.", metadata: .section)
+
+        try fileHandler.inTemporaryDirectory { temporaryDirectoryPath in
+            // prepare paths
+            let pathsProvider = SwiftPackageManagerPathsProvider(
+                dependenciesDirectory: dependenciesDirectory,
+                temporaryDirectoryPath: temporaryDirectoryPath
+            )
+            
+            // prepare for installation
+            try prepareForInstallation(pathsProvider: pathsProvider, dependencies: dependencies)
+            
+            // build command
+            let command = buildCommand(packagePath: temporaryDirectoryPath)
+            
+            // run `Swift Package Manager`
+            logger.info("SwiftPackageManager:", metadata: .subsection)
+            try System.shared.runAndPrint(command)
+            
+            // post installation
+            try postInstallationActions(pathsProvider: pathsProvider)
+        }
+        
+        logger.info("Swift Package Manage dependencies were fetched successfully.", metadata: .subsection)
+    }
+    
+    // MARK: - Installation
+    
+    private func prepareForInstallation(pathsProvider: SwiftPackageManagerPathsProvider, dependencies: SwiftPackageManagerDependencies) throws {
+        // copy `.build` directory from previous run if exist
+        if fileHandler.exists(pathsProvider.destinationSwiftPackageManagerBuildDirectory) {
+            try copyDirectory(
+                from: pathsProvider.destinationSwiftPackageManagerBuildDirectory,
+                to: pathsProvider.temporarySwiftPackageManagerBuildDirectory
+            )
+        }
+        
+        // copy `Package.resolved` directory from previous run if exist
+        if fileHandler.exists(pathsProvider.destinationPackageResolvedPath) {
+            try copyDirectory(
+                from: pathsProvider.destinationPackageResolvedPath,
+                to: pathsProvider.temporaryPackageResolvedPath
+            )
+        }
+        
+        // create `Package.swift`
+        let packageManifestContent = dependencies.stringValue()
+        let packageManifestPath = pathsProvider.temporaryDirectoryPath.appending(component: "Package.swift")
+        try fileHandler.write(packageManifestContent, path: packageManifestPath, atomically: true)
+        
+        // log
+        logger.info("Package.swift:", metadata: .subsection)
+        logger.info("\(packageManifestContent)")
+    }
+    
+    private func buildCommand(packagePath: AbsolutePath) -> [String] {
+        let command = ["swift", "package", "--package-path", "\(packagePath.pathString)", "resolve"]
+        
+        // log
+        logger.info("Command:", metadata: .subsection)
+        logger.info("\(command.joined(separator: " "))")
+        
+        return command
+    }
+    
+    private func postInstallationActions(pathsProvider: SwiftPackageManagerPathsProvider) throws {
+        // validation
+        guard fileHandler.exists(pathsProvider.temporaryPackageResolvedPath) else {
+            throw SwiftPackageManagerInteractorError.packageResolvedNotFound
+        }
+        guard fileHandler.exists(pathsProvider.temporarySwiftPackageManagerBuildDirectory) else {
+            throw SwiftPackageManagerInteractorError.buildDirectoryNotFound
+        }
+        
+        // save `Package.resolved`
+        try copyFile(
+            from: pathsProvider.temporaryPackageResolvedPath,
+            to: pathsProvider.destinationPackageResolvedPath
+        )
+        
+        // save `.build` direcotry
+        try copyDirectory(
+            from: pathsProvider.temporarySwiftPackageManagerBuildDirectory,
+            to: pathsProvider.destinationSwiftPackageManagerBuildDirectory
+        )
+    }
+    
+    // MARK: - Helpers
+
+    private func copyFile(from fromPath: AbsolutePath, to toPath: AbsolutePath) throws {
+        try fileHandler.createFolder(toPath.removingLastComponent())
+
+        if fileHandler.exists(toPath) {
+            try fileHandler.replace(toPath, with: fromPath)
+        } else {
+            try fileHandler.copy(from: fromPath, to: toPath)
+        }
+    }
+
+    private func copyDirectory(from fromPath: AbsolutePath, to toPath: AbsolutePath) throws {
+        try fileHandler.createFolder(toPath.removingLastComponent())
+
+        if fileHandler.exists(toPath) {
+            try fileHandler.delete(toPath)
+        }
+
+        try fileHandler.copy(from: fromPath, to: toPath)
+    }
+}
+
+// MARK: - Models
+
+private struct SwiftPackageManagerPathsProvider {
+    let dependenciesDirectory: AbsolutePath
+    let temporaryDirectoryPath: AbsolutePath
+    
+    let destinationPackageResolvedPath: AbsolutePath
+    let destinationSwiftPackageManagerBuildDirectory: AbsolutePath
+    
+    let temporaryPackageResolvedPath: AbsolutePath
+    let temporarySwiftPackageManagerBuildDirectory: AbsolutePath
+    
+    init(dependenciesDirectory: AbsolutePath, temporaryDirectoryPath: AbsolutePath) {
+        self.dependenciesDirectory = dependenciesDirectory
+        self.temporaryDirectoryPath = temporaryDirectoryPath
+        
+        destinationPackageResolvedPath = dependenciesDirectory
+            .appending(component: Constants.DependenciesDirectory.lockfilesDirectoryName)
+            .appending(component: Constants.DependenciesDirectory.packageResolvedName)
+        destinationSwiftPackageManagerBuildDirectory = dependenciesDirectory
+            .appending(component: Constants.DependenciesDirectory.swiftPackageManagerDirectoryName)
+            .appending(component: ".build")
+        
+        temporaryPackageResolvedPath = temporaryDirectoryPath
+            .appending(component: Constants.DependenciesDirectory.packageResolvedName)
+        temporarySwiftPackageManagerBuildDirectory = temporaryDirectoryPath
+            .appending(component: ".build")
     }
 }
