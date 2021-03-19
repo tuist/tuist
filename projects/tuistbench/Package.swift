2ad45ad290d0d7e6b2f67392518d02b002c26872
Pedro Piñera Buendía <pepibumur@gmail.com>
2021-03-19 08:15:50 +0100

    Update dependencies (#2679)
    * Update project dependencies
    
    * Update xcbeautify and swiftlint
    
    * Update the changelog
    
    * Fix dependencies

--- projects/tuistbench/Package.swift
+++ projects/tuistbench/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-tools-support-core", .upToNextMinor(from: "0.1.12")),
+        .package(url: "https://github.com/apple/swift-tools-support-core", .upToNextMinor(from: "0.2.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
