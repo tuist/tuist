85663daf808fb556c131f3502f8746dd5e1a0477
Jared Sorge <jared@jsorge.net>
2021-03-22 13:04:16 -0700

    Add a codable test

--- Tests/ProjectDescriptionTests/ConfigTests.swift
+++ Tests/ProjectDescriptionTests/ConfigTests.swift
@@ -14,6 +14,7 @@ final class ConfigTests: XCTestCase {
                 .disableSynthesizedResourceAccessors,
                 .disableShowEnvironmentVarsInScriptPhases,
                 .enableCodeCoverage,
+                .disablePackageVersionLocking,
             ]
         )
 
