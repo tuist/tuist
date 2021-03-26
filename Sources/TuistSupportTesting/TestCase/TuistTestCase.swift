464cdd24224b98762ac8044087bc4aee5bad8645
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2021-03-26 20:54:05 +0100

    [build-frameworks] add - `TuistTestCase.XCTAssertDirectoryContentEqual`

--- Sources/TuistSupportTesting/TestCase/TuistTestCase.swift
+++ Sources/TuistSupportTesting/TestCase/TuistTestCase.swift
@@ -161,6 +161,39 @@ open class TuistTestCase: XCTestCase {
 
         XCTAssertFalse(output.contains(notExpected), message, file: file, line: line)
     }
+    
+    public func XCTAssertDirectoryContentEqual(
+        _ directory: AbsolutePath,
+        _ expected: [String],
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        do {
+            let directoryContent = try fileHandler
+                .contentsOfDirectory(directory)
+                .map { $0.pathString }
+                .sorted()
+            
+            let expectedContent = expected
+                .map { directory.appending(RelativePath($0)) }
+                .map { $0.pathString }
+                .sorted()
+            
+            let message = """
+            The directory content:
+            ===========
+            \(directoryContent.isEmpty ? "<Empty>" : directoryContent.joined(separator: "\n"))
+
+            Doesn't equal to expected:
+            ===========
+            \(expectedContent.isEmpty ? "<Empty>" : expectedContent.joined(separator: "\n"))
+            """
+            
+            XCTAssertEqual(directoryContent, expectedContent, message, file: file, line: line)
+        } catch {
+            XCTFail(error.localizedDescription, file: file, line: line)
+        }
+    }
 
     public func temporaryFixture(_ pathString: String) throws -> AbsolutePath {
         let path = RelativePath(pathString)
