7d50a120ef841c763637d732fdce9397c89b26c4
Pedro Piñera <pepibumur@gmail.com>
2021-03-29 18:58:11 +0200

    Fix swiftlint issue

--- projects/fourier/test/fourier/services/update/swiftlint_test.rb
+++ projects/fourier/test/fourier/services/update/swiftlint_test.rb
@@ -7,26 +7,32 @@ module Fourier
     module Update
       class SwiftlintTest < TestCase
         include TestHelpers::TemporaryDirectory
+        include TestHelpers::SupressOutput
 
         def test_call
-          # Given
-          Dir
-            .expects(:mktmpdir)
-            .yields(@tmp_dir)
-          zip_path = File.join(@tmp_dir, "swiftlint.zip")
-          content_path = File.join(@tmp_dir, "bin")
-          Down
-            .expects(:download)
-            .with(Swiftlint::PORTABLE_BINARY_URL, destination: zip_path)
-          Utilities::Zip
-            .expects(:extract)
-            .with(zip: zip_path, into: content_path)
-          FileUtils
-            .expects(:copy_entry)
-            .with(content_path, Swiftlint::OUTPUT_DIRECTORY, false, false, true)
+          supressing_output do
+            # Given
+            Dir
+              .expects(:mktmpdir)
+              .yields(@tmp_dir)
+            zip_path = File.join(@tmp_dir, "swiftlint.zip")
+            content_path = File.join(@tmp_dir, "bin")
+            Down
+              .expects(:download)
+              .with(Swiftlint::PORTABLE_BINARY_URL, destination: zip_path)
+            Utilities::Zip
+              .expects(:extract)
+              .with(zip: zip_path, into: content_path)
+            FileUtils
+              .expects(:chmod)
+              .with("u=rwx", File.join(content_path, "swiftlint"))
+            FileUtils
+              .expects(:copy_entry)
+              .with(content_path, Swiftlint::OUTPUT_DIRECTORY, false, false, true)
 
-          # When
-          Services::Update::Swiftlint.call
+            # When
+            Services::Update::Swiftlint.call
+          end
         end
       end
     end
