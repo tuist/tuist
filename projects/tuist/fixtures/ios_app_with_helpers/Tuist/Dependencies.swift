7571de7bb22f7e619ba6acce57ac73c7cafa66e8
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2021-04-08 12:33:36 +0200

    [spm] fix - acceptance test

--- projects/tuist/fixtures/ios_app_with_helpers/Tuist/Dependencies.swift
+++ projects/tuist/fixtures/ios_app_with_helpers/Tuist/Dependencies.swift
@@ -5,7 +5,13 @@ let dependencies = Dependencies(
         [
             .git(path: "https://github.com/Alamofire/Alamofire", requirement: .exact("5.0.4")),
             .git(path: "https://github.com/Swinject/Swinject", requirement: .exact("2.7.1"))
-        ],
-        platforms: [.macOS]
-    )
+        ]
+    ),
+    swiftPackageManager: .swiftPackageManager(
+        [
+            .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
+            .package(url: "https://github.com/ReactiveX/RxSwift.git", .exact("6.1.0"))
+        ]
+    ),
+    platforms: [.iOS]
 )
