366a860e0a44379754de2814132c78e4fdc97660
fortmarek <marekfort@me.com>
2021-04-11 11:17:30 +0200

    Add Project documentation

--- Sources/ProjectDescription/Project.swift
+++ Sources/ProjectDescription/Project.swift
@@ -2,15 +2,25 @@ import Foundation
 
 // MARK: - Project
 
+/// Project is similar to what you specify in `.xcodeproj`
 public struct Project: Codable, Equatable {
+    /// Name of the project
     public let name: String
+    /// Organization name to be used for the project
     public let organizationName: String?
+    /// Project Swift packages
     public let packages: [Package]
+    /// Project targets
     public let targets: [Target]
+    /// Project schemes
     public let schemes: [Scheme]
+    /// Project settings
     public let settings: Settings?
+    /// Customizable file header template for Xcode built-in file templates
     public let fileHeaderTemplate: FileHeaderTemplate?
+    /// The additional files to include in the project (won't be included in a build phase)
     public let additionalFiles: [FileElement]
+    /// Resource synthesizers create type-safe accessors for your resources
     public let resourceSynthesizers: [ResourceSynthesizer]
 
     public init(
