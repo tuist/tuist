45b25b064cd9804375232dca09b53406998c0a59
Kas <kwridan@users.noreply.github.com>
2021-04-12 15:40:08 +0100

    Remove unused `GeneratorModelLoading` protocol (#2798)

--- Sources/TuistLoader/Loaders/ManifestModelConverter.swift
+++ Sources/TuistLoader/Loaders/ManifestModelConverter.swift
@@ -10,3 +10,39 @@ public protocol ManifestModelConverting {
     func convert(manifest: ProjectDescription.Workspace, path: AbsolutePath) throws -> TuistGraph.Workspace
     func convert(manifest: ProjectDescription.Project, path: AbsolutePath) throws -> TuistGraph.Project
 }
+
+public final class ManifestModelConverter: ManifestModelConverting {
+    private let manifestLoader: ManifestLoading
+
+    public convenience init() {
+        self.init(
+            manifestLoader: ManifestLoader()
+        )
+    }
+
+    public init(manifestLoader: ManifestLoading) {
+        self.manifestLoader = manifestLoader
+    }
+
+    public func convert(
+        manifest: ProjectDescription.Project,
+        path: AbsolutePath
+    ) throws -> TuistGraph.Project {
+        let generatorPaths = GeneratorPaths(manifestDirectory: path)
+        return try TuistGraph.Project.from(manifest: manifest, generatorPaths: generatorPaths)
+    }
+
+    public func convert(
+        manifest: ProjectDescription.Workspace,
+        path: AbsolutePath
+    ) throws -> TuistGraph.Workspace {
+        let generatorPaths = GeneratorPaths(manifestDirectory: path)
+        let workspace = try TuistGraph.Workspace.from(
+            manifest: manifest,
+            path: path,
+            generatorPaths: generatorPaths,
+            manifestLoader: manifestLoader
+        )
+        return workspace
+    }
+}
