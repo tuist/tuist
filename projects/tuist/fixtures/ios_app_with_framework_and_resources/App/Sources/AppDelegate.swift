0f106bbaec828d33b76ec544b70c76005c278fe4
Giovanni Filaferro <gif@bendingspoons.com>
2021-05-06 12:08:31 +0200

    fix: ios fixture

--- projects/tuist/fixtures/ios_app_with_framework_and_resources/App/Sources/AppDelegate.swift
+++ projects/tuist/fixtures/ios_app_with_framework_and_resources/App/Sources/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let framework1 = Framework1File()
 
         print(hello())
-        print("Custom plist: \(Environment.myKey)")
+        print("Custom plist: \(App.myKey)")
         print("AppDelegate -> \(framework1.hello())")
         print("Main bundle image: \(String(describing: UIImage(named: "tuist")))")
         print("Asset catalogue image: \(String(describing: AppAsset.assetCatalogLogo.image))")
