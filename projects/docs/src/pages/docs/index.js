666336326a1e89e36b2ba92bb91d66c907df80e5
Pedro Piñera <pedro@ppinera.es>
2021-05-12 20:23:56 +0200

    Fix documentation website

--- projects/docs/src/pages/docs/index.js
+++ projects/docs/src/pages/docs/index.js
@@ -2,13 +2,11 @@ import React from 'react'
 import { Redirect, useLocation } from '@docusaurus/router'
 import config from '../../../docusaurus.config'
 
-const { defaultDocsLandingPage } = config.customFields
-
 function Home() {
   const location = useLocation()
   const defaultUrl = [
     location.pathname.replace(/\/$/, ''),
-    defaultDocsLandingPage,
+    'tutorial/get-started',
   ].join('/')
 
   return <Redirect to={defaultUrl} />
