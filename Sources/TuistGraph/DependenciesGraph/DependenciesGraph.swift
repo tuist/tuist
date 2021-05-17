322b02b7bff59c4fafc4ba950989b0a0157e4e90
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2021-05-17 20:13:38 +0200

    [carthage-graph] add - init `DependenciesGraph`

---
+++ Sources/TuistGraph/DependenciesGraph/DependenciesGraph.swift
@@ -0,0 +1,15 @@
+import Foundation
+import TSCBasic
+
+/// A directed acyclic graph (DAG) that Tuist uses to represent the third party dependency tree.
+public struct DependenciesGraph: Equatable, Codable {
+//    /// A dictionary that contains the one-to-many dependencies that represent the graph.
+//    public var dependencies: [DependenciesGraphDependency: Set<DependenciesGraphDependency>]
+//    
+    /// Create an instance of `DependenciesGraph` model.
+    public init(
+//        dependencies: [DependenciesGraphDependency: Set<DependenciesGraphDependency>]
+    ) {
+//        self.dependencies = dependencies
+    }
+}
---
+++ Sources/TuistGraph/DependenciesGraph/DependenciesGraphDependency.swift
@@ -0,0 +1,40 @@
+import Foundation
+import TSCBasic
+
+// A node of the third party dependency graph.
+public enum DependenciesGraphDependency: Hashable, Equatable, Codable {
+    /// A dependency that represents a pre-compiled .xcframework.
+    case xcframework(path: AbsolutePath)
+}
+
+// MARK: - Codable
+
+extension DependenciesGraphDependency {
+    private enum Kind: String, Codable {
+        case xcframework
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case kind
+        case path
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let kind = try container.decode(Kind.self, forKey: .kind)
+        switch kind {
+        case .xcframework:
+            let path = try container.decode(AbsolutePath.self, forKey: .path)
+            self = .xcframework(path: path)
+        }
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        switch self {
+        case let .xcframework(path):
+            try container.encode(Kind.xcframework, forKey: .kind)
+            try container.encode(path, forKey: .path)
+        }
+    }
+}
