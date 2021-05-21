17fcccb10cd2b202ea39f8ad5ab71135f6a85aa6
Santiago Delgado <sdelga2@ext.uber.com>
2021-05-21 11:29:13 -0500

    Adding acceptance testing

---
+++ projects/tuist/fixtures/ios_app_with_templates/Tuist/Templates/custom_using_copy_folder/custom_using_copy_folder.swift
@@ -0,0 +1,21 @@
+import ProjectDescription
+
+let nameAttributeFour: Template.Attribute = .required("name")
+let platformAttributeFour: Template.Attribute = .optional("platform", default: "ios")
+
+let testContentsFour = """
+// this is test \(nameAttributeFour) content
+"""
+
+let templateFour = Template(
+    description: "Custom template",
+    attributes: [
+        nameAttributeFour,
+        platformAttributeFour
+    ],
+    files: [
+        .string(path: "\(nameAttributeFour)/custom.swift", contents: testContentsFour),
+        .file(path: "\(nameAttributeFour)/generated.swift", templatePath: "platform_four.stencil"),
+        .directory(path: "\(nameAttributeFour)",sourcePath: "sourceFolder")
+    ]
+)
