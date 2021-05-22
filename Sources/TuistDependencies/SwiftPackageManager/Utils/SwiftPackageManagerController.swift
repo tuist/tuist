90f4092c568d7855b8cce8b2029fe505ee005a27
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2021-05-22 15:08:13 +0200

    [swift-tools-version] add - `SwiftPackageManagerControlling.setToolsVersion`

--- Sources/TuistDependencies/SwiftPackageManager/Utils/SwiftPackageManagerController.swift
+++ Sources/TuistDependencies/SwiftPackageManager/Utils/SwiftPackageManagerController.swift
@@ -9,35 +9,53 @@ public protocol SwiftPackageManagerControlling {
     /// - Parameter path: Directory where the `Package.swift` is defined.
     func resolve(at path: AbsolutePath) throws
 
-    /// Update package dependencies
+    /// Updates package dependencies.
     /// - Parameter path: Directory where the `Package.swift` is defined.
     func update(at path: AbsolutePath) throws
+    
+    /// Sets tools version of package to the given value.
+    /// - Parameter path: Directory where the `Package.swift` is defined.
+    /// - Parameter version: Version of tools. When `nil` then the environment’s version will be set.
+    func setToolsVersion(at path: AbsolutePath, to version: String?) throws
 }
 
 public final class SwiftPackageManagerController: SwiftPackageManagerControlling {
     public init() {}
 
     public func resolve(at path: AbsolutePath) throws {
-        let command = buildSwiftPackageCommand(packagePath: path, subcommand: "resolve")
+        let command = buildSwiftPackageCommand(packagePath: path, subcommand: ["resolve"])
 
         try System.shared.run(command)
     }
 
     public func update(at path: AbsolutePath) throws {
-        let command = buildSwiftPackageCommand(packagePath: path, subcommand: "update")
+        let command = buildSwiftPackageCommand(packagePath: path, subcommand: ["update"])
 
+        try System.shared.run(command)
+    }
+    
+    public func setToolsVersion(at path: AbsolutePath, to version: String?) throws {
+        let subcommand: [String]
+        if let version = version {
+            subcommand = ["tools-version", "--set", version]
+        } else {
+            subcommand = ["tools-version", "--set-current"]
+        }
+        
+        let command = buildSwiftPackageCommand(packagePath: path, subcommand: subcommand)
+        
         try System.shared.run(command)
     }
 
     // MARK: - Helpers
 
-    private func buildSwiftPackageCommand(packagePath: AbsolutePath, subcommand: String) -> [String] {
+    private func buildSwiftPackageCommand(packagePath: AbsolutePath, subcommand: [String]) -> [String] {
         [
             "swift",
             "package",
             "--package-path",
             packagePath.pathString,
-            subcommand,
         ]
+        + subcommand
     }
 }
