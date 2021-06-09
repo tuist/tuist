450b5a320251f967cca88524b9409c16ff77f3ce
Kamil Harasimowicz <kamil.harasimowicz@gmail.com>
2021-06-09 17:39:21 +0200

    [carthage-graph] change - `compactMapValues` instead of `reduce`

--- Sources/TuistDependencies/Carthage/Utils/CarthageGraphGenerator.swift
+++ Sources/TuistDependencies/Carthage/Utils/CarthageGraphGenerator.swift
@@ -25,11 +25,11 @@ public final class CarthageGraphGenerator: CarthageGraphGenerating {
             .map { try FileHandler.shared.readFile($0) }
             .map { try jsonDecoder.decode(CarthageVersionFile.self, from: $0) }
             .flatMap { $0.allProducts }
-
-        let thirdPartyDependencies = Dictionary(grouping: products, by: { $0.name })
-            .reduce(into: [String: ThirdPartyDependency]()) { result, product in
-                guard let frameworkName = product.value.first?.container else { return }
-
+        
+        let thirdPartyDependencies: [String: ThirdPartyDependency] = Dictionary(grouping: products, by: \.name)
+            .compactMapValues { product in
+                guard let frameworkName = product.first?.container else { return nil }
+                
                 let path = AbsolutePath("/")
                     .appending(components: [
                         Constants.tuistDirectoryName,
@@ -37,9 +37,9 @@ public final class CarthageGraphGenerator: CarthageGraphGenerating {
                         Constants.DependenciesDirectory.carthageDirectoryName,
                         frameworkName,
                     ])
-
-                let architectures: Set<BinaryArchitecture> = Set(product.value.flatMap { $0.architectures })
-                result[product.key] = .xcframework(path: path, architectures: architectures)
+                
+                let architectures: Set<BinaryArchitecture> = Set(product.flatMap { $0.architectures })
+                return .xcframework(path: path, architectures: architectures)
             }
 
         return DependenciesGraph(thirdPartyDependencies: thirdPartyDependencies)
