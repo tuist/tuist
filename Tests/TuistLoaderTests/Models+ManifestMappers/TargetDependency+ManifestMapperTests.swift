bbb922c86980f2c6125b0508636b46e097bdf98f
Daniele Formichelli <df@bendingspoons.com>
2021-06-28 16:52:59 +0200

    test: add DependencyManifestMapperTests external tests

--- Tests/TuistLoaderTests/Models+ManifestMappers/TargetDependency+ManifestMapperTests.swift
+++ Tests/TuistLoaderTests/Models+ManifestMappers/TargetDependency+ManifestMapperTests.swift
@@ -43,4 +43,84 @@ final class DependencyManifestMapperTests: TuistUnitTestCase {
         }
         XCTAssertEqual(product, "library")
     }
+
+    func test_from_when_external_xcframework() throws {
+        // Given
+        let dependency = ProjectDescription.TargetDependency.external(name: "library")
+        let generatorPaths = GeneratorPaths(manifestDirectory: AbsolutePath("/"))
+
+        // When
+        let got = try TuistGraph.TargetDependency.from(
+            manifest: dependency,
+            generatorPaths: generatorPaths,
+            externalDependencies: [
+                "library": [.xcframework(path: "/path.xcframework")]
+            ]
+        )
+
+        // Then
+        XCTAssertEqual(got.count, 1)
+        guard case let .xcframework(path) = got[0] else {
+            XCTFail("Dependency should be xcframework")
+            return
+        }
+        XCTAssertEqual(path, "/path.xcframework")
+    }
+
+    func test_from_when_external_project() throws {
+        // Given
+        let dependency = ProjectDescription.TargetDependency.external(name: "library")
+        let generatorPaths = GeneratorPaths(manifestDirectory: AbsolutePath("/"))
+
+        // When
+        let got = try TuistGraph.TargetDependency.from(
+            manifest: dependency,
+            generatorPaths: generatorPaths,
+            externalDependencies: [
+                "library": [.project(target: "Target", path: "/Project")]
+            ]
+        )
+
+        // Then
+        XCTAssertEqual(got.count, 1)
+        guard case let .project(target, path) = got[0] else {
+            XCTFail("Dependency should be project")
+            return
+        }
+        XCTAssertEqual(target, "Target")
+        XCTAssertEqual(path, "/Project")
+    }
+
+    func test_from_when_external_multiple() throws {
+        // Given
+        let dependency = ProjectDescription.TargetDependency.external(name: "library")
+        let generatorPaths = GeneratorPaths(manifestDirectory: AbsolutePath("/"))
+
+        // When
+        let got = try TuistGraph.TargetDependency.from(
+            manifest: dependency,
+            generatorPaths: generatorPaths,
+            externalDependencies: [
+                "library": [
+                    .xcframework(path: "/path.xcframework"),
+                    .project(target: "Target", path: "/Project")
+                ]
+            ]
+        )
+
+        // Then
+        XCTAssertEqual(got.count, 2)
+        guard case let .xcframework(path) = got[0] else {
+            XCTFail("First dependency should be xcframework")
+            return
+        }
+        XCTAssertEqual(path, "/path.xcframework")
+
+        guard case let .project(target, path) = got[1] else {
+            XCTFail("Dependency should be project")
+            return
+        }
+        XCTAssertEqual(target, "Target")
+        XCTAssertEqual(path, "/Project")
+    }
 }
