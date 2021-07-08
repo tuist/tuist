fc9285c0691ca604b97a7139d97d50a601d86ced
Daniele Formichelli <df@bendingspoons.com>
2021-07-08 17:23:05 +0200

    feat: add path, sources, and resources test

--- Tests/TuistDependenciesTests/SwiftPackageManager/Utils/PackageInfoMapperTest.swift
+++ Tests/TuistDependenciesTests/SwiftPackageManager/Utils/PackageInfoMapperTest.swift
@@ -95,6 +95,102 @@ final class PackageInfoMapperTests: TuistUnitTestCase {
             )
         )
     }
+
+    func testMap_whenCustomSources() throws {
+        let project = try subject.map(
+            packageInfo: .init(
+                products: [
+                    .init(name: "Product1", type: .library(.automatic), targets: ["Target1"]),
+                ],
+                targets: [
+                    .test(name: "Target1", sources: ["Subfolder", "Another/Subfolder/file.swift"]),
+                ],
+                platforms: []
+            )
+        )
+        XCTAssertEqual(
+            project,
+            .test(
+                name: "Package",
+                targets: [
+                    .test(
+                        name: "Target1",
+                        customSources: [
+                            "/Package/Path/Sources/Target1/Subfolder/**",
+                            "/Package/Path/Sources/Target1/Another/Subfolder/file.swift",
+                        ]
+                    )
+                ]
+            )
+        )
+    }
+
+    func testMap_whenHasResources() throws {
+        let project = try subject.map(
+            packageInfo: .init(
+                products: [
+                    .init(name: "Product1", type: .library(.automatic), targets: ["Target1"]),
+                ],
+                targets: [
+                    .test(
+                        name: "Target1",
+                        resources: [
+                            .init(rule: .copy, path: "Resource/Folder"),
+                            .init(rule: .process, path: "Another/Resource/Folder"),
+                        ]
+                    ),
+                ],
+                platforms: []
+            )
+        )
+        XCTAssertEqual(
+            project,
+            .test(
+                name: "Package",
+                targets: [
+                    .test(
+                        name: "Target1",
+                        customResources: [
+                            "/Package/Path/Sources/Target1/Resource/Folder/**",
+                            "/Package/Path/Sources/Target1/Another/Resource/Folder/**",
+                        ]
+                    )
+                ]
+            )
+        )
+    }
+
+    func testMap_whenCustomPath() throws {
+        let project = try subject.map(
+            packageInfo: .init(
+                products: [
+                    .init(name: "Product1", type: .library(.automatic), targets: ["Target1"]),
+                ],
+                targets: [
+                    .test(
+                        name: "Target1",
+                        path: "Custom/Path",
+                        sources: ["Sources/Folder"],
+                        resources: [.init(rule: .copy, path: "Resource/Folder")]
+                    ),
+                ],
+                platforms: []
+            )
+        )
+        XCTAssertEqual(
+            project,
+            .test(
+                name: "Package",
+                targets: [
+                    .test(
+                        name: "Target1",
+                        customSources: "/Package/Path/Custom/Path/Sources/Folder/**",
+                        customResources: "/Package/Path/Custom/Path/Resource/Folder/**"
+                    )
+                ]
+            )
+        )
+    }
 }
 
 extension PackageInfoMapping {
@@ -117,14 +213,17 @@ extension PackageInfoMapping {
 extension PackageInfo.Target {
     fileprivate static func test(
         name: String,
-        type: PackageInfo.Target.TargetType = .regular
+        type: PackageInfo.Target.TargetType = .regular,
+        path: String? = nil,
+        sources: [String]? = nil,
+        resources: [PackageInfo.Target.Resource] = []
     ) -> Self {
         return .init(
             name: name,
-            path: nil,
+            path: path,
             url: nil,
-            sources: nil,
-            resources: [],
+            sources: sources,
+            resources: resources,
             exclude: [],
             dependencies: [],
             publicHeadersPath: nil,
@@ -146,14 +245,19 @@ extension ProjectDescription.Project {
 }
 
 extension ProjectDescription.Target {
-    fileprivate static func test(name: String) -> Self {
+    fileprivate static func test(
+        name: String,
+        customSources: SourceFilesList? = nil,
+        customResources: ResourceFileElements? = nil
+    ) -> Self {
         return .init(
             name: name,
             platform: .iOS,
             product: .staticFramework,
             bundleId: name,
             infoPlist: .default,
-            sources: "/Package/Path/Sources/\(name)/**",
+            sources: customSources ?? "/Package/Path/Sources/\(name)/**",
+            resources: customResources,
             settings: DependenciesGraph.spmSettings()
         )
     }
