bc10ecca468e7562001a0f9e413a36419025fbe2
Daniele Formichelli <df@bendingspoons.com>
2021-07-26 20:05:46 +0200

    feat: add fixture to test SPM dependencies (#3228)

---
+++ projects/tuist/fixtures/app_with_spm_dependencies/Project.swift
@@ -0,0 +1,33 @@
+import ProjectDescription
+
+let project = Project(
+    name: "App",
+    targets: [
+        Target(
+            name: "App",
+            platform: .iOS,
+            product: .app,
+            bundleId: "io.tuist.app",
+            infoPlist: .default,
+            sources: "App/**",
+            dependencies: [
+                .external(name: "Adjust"),
+                .external(name: "Alamofire"),
+                .external(name: "ComposableArchitecture"),
+            ]
+        ),
+        Target(
+            name: "AppTests",
+            platform: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.appTests",
+            infoPlist: .default,
+            sources: "AppTests/**",
+            dependencies: [
+                .target(name: "App"),
+                .external(name: "Quick"),
+                .external(name: "Nimble"),
+            ]
+        ),
+    ]
+)
