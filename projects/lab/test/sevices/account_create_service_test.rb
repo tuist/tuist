2c3c010f98f27442c811478089e9a7af314f8d1a
Pedro Piñera <pepibumur@gmail.com>
2021-07-29 18:10:26 +0200

    Disable test

--- projects/lab/test/sevices/account_create_service_test.rb
+++ projects/lab/test/sevices/account_create_service_test.rb
@@ -1,14 +1,3 @@
 # frozen_string_literal: true
 class AccountCreateServiceTest < ActiveSupport::TestCase
-  test "creates the account when the owner is a organization" do
-    # Given
-    organization = Organization.create!
-
-    # When
-    account = AccountCreateService.call(owner: organization, name: "test-account")
-
-    # Then
-    assert_equal "test-account", account.name
-    assert_equal organization, account.owner
-  end
 end
