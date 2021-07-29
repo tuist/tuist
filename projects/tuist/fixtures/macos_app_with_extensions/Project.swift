2e966d8c400dca5c262c5e50b79417e8959504d8
Pedro Piñera <pedro@ppinera.es>
2021-07-29 20:20:28 +0200

    Add a fixture app for a macOS app with extensions

---
+++ projects/tuist/fixtures/macos_app_with_extensions/Project.swift
@@ -0,0 +1,44 @@
+import ProjectDescription
+
+/**
+These are necessary for the compiler to find the Workflow's SDK and link against it.
+**/
+let workflowExtensionSettings: SettingsDictionary = [
+    "ADDITIONAL_SDKS": "/Library/Developer/SDKs/WorkflowExtensionSDK.sdk $(inherited)",
+    "OTHER_LDFLAGS": "-fapplication-extension -e_ProExtensionMain -lProExtension",
+    "FRAMEWORK_SEARCH_PATHS": "/Library/Frameworks $(inherited)",
+    "LIBRARY_SEARCH_PATHS": "/usr/lib $(inherited)",
+    "SWIFT_OBJC_BRIDGING_HEADER": "$SRCROOT/Workflow/Workflow-Bridging-Header.h",
+    "HEADER_SEARCH_PATHS": "/usr/include $(inherited)"
+];
+
+let project = Project(
+    name: "App",
+    targets: [
+        Target(
+            name: "App",
+            platform: .macOS,
+            product: .app,
+            bundleId: "io.tuist.app",
+            infoPlist: .default,
+            sources: "App/**",
+            dependencies: [.target(name: "Workflow")]
+        ),
+        Target(
+            name: "Workflow",
+            platform: .macOS,
+            product: .appExtension,
+            bundleId: "io.tuist.app.workflow",
+            infoPlist: .extendingDefault(with: [
+                "NSExtensionPointIdentifier": .string("com.apple.FinalCut.WorkflowExtension"),
+                "ProExtensionPrincipalViewControllerClass": .string("$(PRODUCT_MODULE_NAME).WorkflowViewController")
+            ]),
+            sources: "Workflow/Sources/**",
+            resources: "Workflow/Resources/**",
+            settings: Settings(configurations: [
+                .debug(name: "Debug", settings:  workflowExtensionSettings, xcconfig: nil),
+                .release(name: "Release", settings: workflowExtensionSettings, xcconfig: nil),
+            ])
+        )
+    ]
+)
---
+++ projects/tuist/fixtures/macos_app_with_extensions/Workflow/Workflow-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Workflow-Bridging-Header.h
+//  Workflow
+//
+//  Created by Pedro Piñera Buendía on 29.07.21.
+//
+
+#import <ProExtension/ProExtension.h>
