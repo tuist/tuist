4c3b066a9966e65ed4685e3180795904994810af
Sam Watts <samuel.watts@gmail.com>
2021-08-02 20:29:07 +0100

    Added function on Environment to fetch any value (without TUIST_ prefix)

--- Sources/ProjectDescription/Environment.swift
+++ Sources/ProjectDescription/Environment.swift
@@ -29,7 +29,15 @@ public struct Environment {
 
     public static subscript(dynamicMember member: String) -> Value? {
         let snakeCaseMember = "TUIST_\(member.camelCaseToSnakeCase().uppercased())"
-        guard let value = ProcessInfo.processInfo.environment[snakeCaseMember] else { return nil }
+        return value(for: snakeCaseMember)
+    }
+
+    public static func value(for key: String) -> Value? {
+        value(for: key, environment: ProcessInfo.processInfo.environment)
+    }
+
+    static func value(for key: String, environment: [String: String]) -> Value? {
+        guard let value = environment[key] else { return nil }
         let trueValues = ["1", "true", "TRUE", "yes", "YES"]
         let falseValues = ["0", "false", "FALSE", "no", "NO"]
         if trueValues.contains(value) {
@@ -42,12 +50,12 @@ public struct Environment {
     }
 }
 
-extension Optional where Wrapped == Environment.Value {
+public extension Optional where Wrapped == Environment.Value {
     /// Retrieve the Environment value as a string or return the specified default string value
     /// - Parameters:
     ///   - default: default String value to be returned
     /// - Returns: String
-    public func getString(default defaultString: String) -> String {
+    func getString(default defaultString: String) -> String {
         if case let .string(value) = self { return value }
         return defaultString
     }
@@ -56,7 +64,7 @@ extension Optional where Wrapped == Environment.Value {
     /// - Parameters:
     ///   - default: default Boolean value to be returned
     /// - Returns: Bool
-    public func getBoolean(default defaultBoolean: Bool) -> Bool {
+    func getBoolean(default defaultBoolean: Bool) -> Bool {
         if case let .boolean(value) = self { return value }
         return defaultBoolean
     }
