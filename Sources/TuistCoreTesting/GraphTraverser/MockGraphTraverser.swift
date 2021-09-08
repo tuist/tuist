625795a121fb8c45a9c6a5641a83b18c383bfeef
Alfredo Delli Bovi <alfredo.dellibovi@gmail.com>
2021-09-08 21:32:20 +0200

    Improve tests of LinkGeneratorTests (#3409)
    * Refactor LinkGeneratorTests to mock graphTraverser
    
    * Fix format

--- Sources/TuistCoreTesting/GraphTraverser/MockGraphTraverser.swift
+++ Sources/TuistCoreTesting/GraphTraverser/MockGraphTraverser.swift
@@ -308,8 +308,22 @@ final class MockGraphTraverser: GraphTraversing {
         return stubbedLinkableDependenciesResult
     }
 
-    func searchablePathDependencies(path _: AbsolutePath, name _: String) throws -> Set<GraphDependencyReference> {
-        fatalError()
+    var invokedSearchablePathDependencies = false
+    var invokedSearchablePathDependenciesCount = 0
+    var invokedSearchablePathDependenciesParameters: (path: AbsolutePath, name: String)?
+    var invokedSearchablePathDependenciesParametersList = [(path: AbsolutePath, name: String)]() // swiftlint:disable:this identifier_name
+    var stubbedSearchablePathDependenciesError: Error?
+    var stubbedSearchablePathDependenciesResult: Set<GraphDependencyReference>! = []
+
+    func searchablePathDependencies(path: AbsolutePath, name: String) throws -> Set<GraphDependencyReference> {
+        invokedSearchablePathDependencies = true
+        invokedSearchablePathDependenciesCount += 1
+        invokedSearchablePathDependenciesParameters = (path, name)
+        invokedSearchablePathDependenciesParametersList.append((path, name))
+        if let error = stubbedSearchablePathDependenciesError {
+            throw error
+        }
+        return stubbedSearchablePathDependenciesResult
     }
 
     var invokedCopyProductDependencies = false
