438d3645e8354b203aeb9624dbaf215145b01438
Daniele Formichelli <df@bendingspoons.com>
2021-09-24 17:05:58 +0200

    chore: bump alamofire in carthage fixture

--- projects/tuist/fixtures/app_with_carthage_dependencies/Tuist/Dependencies.swift
+++ projects/tuist/fixtures/app_with_carthage_dependencies/Tuist/Dependencies.swift
@@ -2,7 +2,7 @@ import ProjectDescription
 
 let dependencies = Dependencies(
     carthage: [
-        .github(path: "Alamofire/Alamofire", requirement: .exact("5.4.3")),
+        .github(path: "Alamofire/Alamofire", requirement: .exact("5.4.4")),
         .github(path: "Quick/Nimble", requirement: .exact("9.2.0")),
     ],
     platforms: [.iOS]
