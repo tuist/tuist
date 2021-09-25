1cd9138d58781baad179afdd0081e4f2ff4b7f0a
Daniele Formichelli <df@bendingspoons.com>
2021-09-25 16:55:23 +0200

    chore: bump nimble in carthage fixture

--- projects/tuist/fixtures/app_with_carthage_dependencies/Tuist/Dependencies.swift
+++ projects/tuist/fixtures/app_with_carthage_dependencies/Tuist/Dependencies.swift
@@ -3,7 +3,7 @@ import ProjectDescription
 let dependencies = Dependencies(
     carthage: [
         .github(path: "Alamofire/Alamofire", requirement: .exact("5.4.4")),
-        .github(path: "Quick/Nimble", requirement: .exact("9.2.0")),
+        .github(path: "Quick/Nimble", requirement: .exact("9.2.1")),
     ],
     platforms: [.iOS]
 )
