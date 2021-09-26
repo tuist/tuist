cef016f7aac77f78099bd6286cc0eda2819c1d4a
Marek Fořt <marekfort@me.com>
2021-09-26 14:01:39 +0200

    Tuist executable (#3494)
    * Search for executable in path with tuist prefix if not tuist command
    
    * Add acceptance test for new task
    
    * Update documentation
    
    * Check if executable exists
    
    * Update projects/docs/docs/commands/task.md
    
    Co-authored-by: Daniele Formichelli <df@bendingspoons.com>
    
    * Update Sources/TuistKit/Commands/TuistCommand.swift
    
    Co-authored-by: Daniele Formichelli <df@bendingspoons.com>
    
    * Fix finding command
    
    Co-authored-by: Daniele Formichelli <df@bendingspoons.com>

--- projects/tuist/features/step_definitions/tasks.rb
+++ projects/tuist/features/step_definitions/tasks.rb
@@ -8,6 +8,14 @@ Then(/^tuist runs a task (.+) with attribute (.+) as (.+)$/) do |name, attribute
   system(@tuist, "exec", name, "--#{attribute}", attribute_value, "--path", @dir)
 end
 
-Then(/^content of a file named ([a-zA-Z\-_]+) should be equal to (.+)$/) do |file, content|
+Then(/^content of a file named ([a-zA-Z\-_\.]+) should be equal to (.+)$/) do |file, content|
   assert_equal File.read(File.join(@dir, file)), content
+end
+
+Then(/^tuist runs ([a-zA-Z\-_]+)$/) do |command|
+  system(@tuist, command, @dir)
+end
+
+Then(/^(.+) is added to PATH$/) do |executable_name|
+  ENV["PATH"] = ENV["PATH"] + ":#{@dir}"
 end
