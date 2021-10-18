7c59046664299cae32c21143ae518771f0805b3a
Daniele Formichelli <df@bendingspoons.com>
2021-10-18 16:07:06 +0200

    feat: add back bootstrap of TuistAnalytics in main.swift

--- Sources/tuist/main.swift
+++ Sources/tuist/main.swift
@@ -1,11 +1,20 @@
 import Foundation
-import enum TSCBasic.ProcessEnv
+import TSCBasic
 import TuistAnalytics
+import TuistLoader
 import TuistSupport
 
 if CommandLine.arguments.contains("--verbose") { try? ProcessEnv.setVar(Constants.EnvironmentVariables.verbose, value: "true") }
 
 TuistSupport.LogOutput.bootstrap()
+
+let path: AbsolutePath
+if let argumentIndex = CommandLine.arguments.firstIndex(of: "--path") {
+    path = AbsolutePath(CommandLine.arguments[argumentIndex + 1], relativeTo: .current)
+} else {
+    path = .current
+}
+try TuistAnalytics.bootstrap(config: ConfigLoader.shared.loadConfig(path: path))
 
 import TuistKit
 
