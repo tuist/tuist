1e7e2c9d9ace5fcb53c83d27552a5688c50e8edc
Pedro Piñera <pedro@ppinera.es>
2021-11-04 08:37:30 -0400

    Some fixes

--- projects/tuist/features/support/system.rb
+++ projects/tuist/features/support/system.rb
@@ -5,8 +5,19 @@ require "open3"
 module System
   def system(*args)
     log(args.join(" "))
-    _, err, status = Open3.capture3(args.join(" "))
-    assert(status.success?, err)
+    if ARGV.include?("--verbose")
+      status = Open3.popen2e(*args) do |stdin, stdout_stderr, wait_thread|
+        Thread.new do
+          stdout_stderr.each {|l| puts l }
+        end
+        stdin.close
+        wait_thread.value
+      end
+      assert(status.success?)
+    else
+      _, err, status = Open3.capture3(*args)
+      assert(status.success?, err)
+    end
   end
 
   def xcodebuild(*args)
