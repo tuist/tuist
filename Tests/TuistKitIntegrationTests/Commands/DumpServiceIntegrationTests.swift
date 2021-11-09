2ac80ef5526575705c69dc69a8a2d6127e019b03
Daniele Formichelli <df@bendingspoons.com>
2021-11-09 12:19:38 +0100

    test: fix test_prints_the_manifest_when_config_manifest

--- Tests/TuistKitIntegrationTests/Commands/DumpServiceIntegrationTests.swift
+++ Tests/TuistKitIntegrationTests/Commands/DumpServiceIntegrationTests.swift
@@ -126,6 +126,7 @@ final class DumpServiceTests: TuistTestCase {
             generationOptions: []
         )
         """
+        try fileHandler.createFolder(tmpDir.appending(component: "Tuist"))
         try config.write(
             toFile: tmpDir.appending(components: "Tuist", "Config.swift").pathString,
             atomically: true,
