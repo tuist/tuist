9d590c505851de616e5ebaafb9130502bf325db3
Adrian Silva <adri4silva@gmail.com>
2021-11-19 20:26:24 +0100

    Create XML file with foundation

--- Sources/TuistCache/Mappers/Graph/PlayGraphMapper.swift
+++ Sources/TuistCache/Mappers/Graph/PlayGraphMapper.swift
@@ -31,23 +31,52 @@ public class PlayGraphMapper: GraphMapping {
         let contentXCPlaygroundPath = playgroundPath.appending(component: "contents.xcplayground")
         let contentsSwiftPath = playgroundPath.appending(component: "Contents.swift")
 
-        let xcplaygroundContent = """
-        <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-        <playground version='5.0' target-platform='\(targetPlatform)' buildActiveScheme='true' executeOnSourceChanges='false' importAppTypes='true'>
-            <timeline fileName='timeline.xctimeline'/>
-        </playground>
+        guard let document = makePlaygroundDocument(using: targetPlatform) else {
+            return (graph, [])
+        }
+        let documentContent = makePlaygroundContent()
+
+        var sideEffects: [SideEffectDescriptor] = []
+        sideEffects.append(.directory(.init(path: playgroundPath, state: .present)))
+        sideEffects.append(.file(.init(path: contentXCPlaygroundPath, contents: document.xmlData, state: .present)))
+        sideEffects.append(.file(.init(path: contentsSwiftPath, contents: documentContent.data(using: .utf8), state: .present)))
+
+        return (graph, sideEffects)
+    }
+}
+
+private extension PlayGraphMapper {
+    func makePlaygroundDocument(using platform: String) -> XMLDocument? {
+        let playground = XMLElement(name: "playground")
+        guard let versionAttribute = XMLNode.attribute(withName: "version", stringValue: "5.0") as? XMLNode,
+            let platformAttribute = XMLNode.attribute(withName: "target-platform", stringValue: "\(platform)") as? XMLNode,
+            let activeSchemeAttribute = XMLNode.attribute(withName: "buildActiveScheme", stringValue: "true") as? XMLNode,
+            let executeAttribute = XMLNode.attribute(withName: "executeOnSourceChanges", stringValue: "false") as? XMLNode,
+            let appTypesAttribute = XMLNode.attribute(withName: "importAppTypes", stringValue: "true") as? XMLNode,
+            let filenameAttribute = XMLNode.attribute(withName: "fileName", stringValue: "timeline.xctimeline") as? XMLNode
+        else { return nil }
+
+        playground.addAttribute(versionAttribute)
+        playground.addAttribute(platformAttribute)
+        playground.addAttribute(activeSchemeAttribute)
+        playground.addAttribute(executeAttribute)
+        playground.addAttribute(appTypesAttribute)
+        let timeline = XMLElement(name: "timeline")
+        timeline.addAttribute(filenameAttribute)
+        playground.addChild(timeline)
+        let document = XMLDocument(rootElement: playground)
+        document.characterEncoding = "UTF-8"
+        document.isStandalone = true
+        document.version = "1.0"
+        return document
+    }
+
+    func makePlaygroundContent() -> String {
         """
-        let contentsSwiftContent = """
         import Foundation
         import \(targetName)
 
         print("Hello \(targetName)")
         """
-        var sideEffects: [SideEffectDescriptor] = []
-        sideEffects.append(.directory(.init(path: playgroundPath, state: .present)))
-        sideEffects.append(.file(.init(path: contentXCPlaygroundPath, contents: xcplaygroundContent.data(using: .utf8), state: .present)))
-        sideEffects.append(.file(.init(path: contentsSwiftPath, contents: contentsSwiftContent.data(using: .utf8), state: .present)))
-
-        return (graph, sideEffects)
     }
 }
