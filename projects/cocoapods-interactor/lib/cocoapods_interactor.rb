f4998a219228d8e7ea92af8d86ac35ce0bf142b2
Pedro Piñera Buendía <pedro@ppinera.es>
2021-11-26 09:45:14 +0100

    Set up CocoaPods interactor project (#3725)
    * Add CocoaPods interactor project and CI pipeline
    
    * Remove environment variable
    
    * don't lock the Ruby version

---
+++ projects/cocoapods-interactor/lib/cocoapods_interactor.rb
@@ -0,0 +1,3 @@
+module CocoaPodsInteractor
+
+end
---
+++ projects/cocoapods-interactor/test/test_helper.rb
@@ -0,0 +1,11 @@
+$LOAD_PATH.unshift(File.expand_path("../../lib", __FILE__))
+
+ENV["TEST"] = "true"
+
+require "cocoapods_interactor"
+
+require "minitest/autorun"
+require "mocha/minitest"
+require "byebug"
+require "minitest/reporters"
+Minitest::Reporters.use!(Minitest::Reporters::SpecReporter.new)
