fb65aa4237a67b7aacf71dbc5b209cb6c26a97a8
Marek Fořt <marekfort@me.com>
2021-11-28 18:10:02 +0100

    Create new organization (#3745)
    * Create new organization
    
    * Organization settings (#3751)
    
    * Update routing
    
    * Fetch owner and display organization tab
    
    * Show list of users for the project's organization
    
    * Show users' roles

--- projects/cloud/app/models/organization.rb
+++ projects/cloud/app/models/organization.rb
@@ -5,6 +5,9 @@ class Organization < ApplicationRecord
 
   # Associations
   has_one :account, as: :owner, class_name: "Account", dependent: :destroy
+  # Inspired from: https://github.com/RolifyCommunity/rolify/wiki/Usage#finding-roles-through-associations
+  has_many :users, -> { where(:roles => {name: :user}).where.not(:roles => {name: :admin}) }, through: :roles, class_name: 'User', source: :users
+  has_many :admins, -> { where(:roles => {name: :admin}) }, through: :roles, class_name: 'User', source: :users
 
   def name
     account.name
