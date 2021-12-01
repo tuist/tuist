aab7fb55d3218ce5b51119010e2bc40b35f9fcc6
Pedro Piñera <pepibumur@gmail.com>
2021-12-01 17:17:19 +0100

    Fix the content of the digest

--- projects/backbone/app/jobs/weekly_digest_job.rb
+++ projects/backbone/app/jobs/weekly_digest_job.rb
@@ -40,15 +40,15 @@ class WeeklyDigestJob < ApplicationJob
       b.section do |s|
         body = <<~BODY
         *Project generation*
-        · Generated projects: #{generated_projects_past_week} _(#{generated_projects_diff}% compared to the week before, #{generated_projects_week_before_past})_
+        · Generated projects: #{generated_projects_past_week} _(#{more_or_less_percentage_in_words(generated_projects_diff)} the week before, #{generated_projects_week_before_past})_
 
         *Caching*
-        · Focused projects: #{focused_projects_past_week} _(#{focused_projects_diff}% compared to the week before, #{focused_projects_week_before_past})_
-        · Cache-warmed projects: #{warmed_projects_past_week} _(#{warmed_projects_diff}% compared to the week before, #{warmed_projects_week_before_past})_
+        · Focused projects: #{focused_projects_past_week} _(#{more_or_less_percentage_in_words(focused_projects_diff)} the week before, #{focused_projects_week_before_past})_
+        · Cache-warmed projects: #{warmed_projects_past_week} _(#{more_or_less_percentage_in_words(warmed_projects_diff)} the week before, #{warmed_projects_week_before_past})_
 
         *Dependencies*
-        · Fetched dependencies: #{fetched_dependencies_past_week} _(#{fetched_dependencies_diff}% compared to the week before, #{fetched_dependencies_week_before_past})_
-        · Updated dependencies: #{updated_dependencies_past_week} _(#{updated_dependencies_diff}% compared to the week before, #{updated_dependencies_week_before_past})_
+        · Fetched dependencies: #{fetched_dependencies_past_week} _(#{more_or_less_percentage_in_words(fetched_dependencies_diff)} than the week before, #{fetched_dependencies_week_before_past})_
+        · Updated dependencies: #{updated_dependencies_past_week} _(#{more_or_less_percentage_in_words(updated_dependencies_diff)} than the week before, #{updated_dependencies_week_before_past})_
 
         BODY
         s.mrkdwn(text: body)
@@ -56,5 +56,13 @@ class WeeklyDigestJob < ApplicationJob
     end
 
     client.chat_postMessage(channel: channel, blocks: blocks.to_json, as_user: true)
+  end
+
+  def more_or_less_percentage_in_words(percentage)
+    if percentage < 0
+      "#{percentage}% less than"
+    else
+      "#{percentage}% more than"
+    end
   end
 end
