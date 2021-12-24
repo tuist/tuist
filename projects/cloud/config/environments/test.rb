a091ad5522206e5b0fd1c73e4bb7b66af4f9b63c
Pedro Piñera Buendía <pedro@craftweg.com>
2021-12-24 08:30:17 +0100

    Update Tuist Cloud to Rails 7 (#3888)
    * Update the dependency to Rails 7
    
    * Update the project to Rails 7
    
    * Some fixes
    
    Co-authored-by: Pedro Piñera <pedro@ppinera.es>

--- projects/cloud/config/environments/test.rb
+++ projects/cloud/config/environments/test.rb
@@ -8,21 +8,15 @@ require "active_support/core_ext/integer/time"
 # and recreated between test runs. Don't rely on the data there!
 
 Rails.application.configure do
-  config.after_initialize do
-    Bullet.enable        = true
-    Bullet.bullet_logger = true
-    Bullet.raise         = true # raise an error if n+1 query occurs
-  end
-
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.cache_classes = false
-  config.action_view.cache_template_loading = true
+  # Turn false under Spring and add config.action_view.cache_template_loading = true
+  config.cache_classes = true
 
-  # Do not eager load code on boot. This avoids loading your whole application
-  # just for the purpose of running a single test. If you are using a tool that
-  # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = false
+  # Eager loading loads your whole application. When running a single test locally,
+  # this probably isn't necessary. It's a good idea to do in a continuous integration
+  # system, or in some way before deploying your code.
+  config.eager_load = ENV["CI"].present?
 
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
@@ -65,11 +59,9 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-
-  # Bullet
   config.after_initialize do
-    Bullet.enable = true
+    Bullet.enable        = true
     Bullet.bullet_logger = true
-    Bullet.raise = true # raise an error if n+1 query occurs
+    Bullet.raise         = true # raise an error if n+1 query occurs
   end
 end
