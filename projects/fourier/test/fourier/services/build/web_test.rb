ce27fa59a7be8f9f9b6cbfb591063677f6da14e1
ezraberch <49635435+ezraberch@users.noreply.github.com>
2021-12-26 14:09:04 -0500

    Fix depfu on `website` (#3878)
    * Remove obsolete dependency
    
    * Remove old lockfile
    
    * Update all npm dependencies (2021-12-23)
    
    * Add `--legacy-peer-deps` flag
    
    * Add `underscore`
    
    * Change `fourier` to build with `npm`
    
    * Revert `tailwindcss` to 2.2.19
    
    * Add `package-lock.json`
    
    * Turn off lockfile upload
    
    Co-authored-by: depfu[bot] <23717796+depfu[bot]@users.noreply.github.com>

--- projects/fourier/test/fourier/services/build/web_test.rb
+++ projects/fourier/test/fourier/services/build/web_test.rb
@@ -8,7 +8,7 @@ module Fourier
       class WebTest < TestCase
         def test_runs_the_website
           # Given
-          Utilities::System.expects(:system).with("yarn", "build")
+          Utilities::System.expects(:system).with("npm", "run", "build")
 
           # Then
           Services::Build::Web.call
