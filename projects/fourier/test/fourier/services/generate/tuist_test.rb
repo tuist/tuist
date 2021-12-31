4651ab0e086b6c0bf7a19decf1d7288d55dc33e3
Daniele Formichelli <df@bendingspoons.com>
2021-12-31 16:43:00 +0100

    docs: update documentation

--- projects/fourier/test/fourier/services/generate/tuist_test.rb
+++ projects/fourier/test/fourier/services/generate/tuist_test.rb
@@ -9,23 +9,22 @@ module Fourier
         def test_calls_tuist_with_the_right_arguments
           # Given
           Utilities::System.expects(:tuist).with("dependencies", "fetch")
-          Utilities::System.expects(:tuist).with("cache", "warm", "--dependencies-only", "Target")
-          Utilities::System.expects(:tuist).with("generate", "Target")
-          Utilities::System.expects(:tuist).with("generate")
+          Utilities::System.expects(:tuist).with("cache", "warm", "--dependencies-only", "Target1", "Target2")
+          Utilities::System.expects(:tuist).with("generate", "Target1", "Target2")
 
           # When/Then
-          Fourier::Services::Generate::Tuist.call(open: false, targets: "Target")
+          Fourier::Services::Generate::Tuist.call(open: false, targets: ["Target1", "Target2"])
         end
 
 
         def test_calls_tuist_with_the_right_arguments_when_open_is_true
           # Given
           Utilities::System.expects(:tuist).with("dependencies", "fetch")
-          Utilities::System.expects(:tuist).with("cache", "warm", "--dependencies-only", "Target")
-          Utilities::System.expects(:tuist).with("generate", "--open", "Targets")
+          Utilities::System.expects(:tuist).with("cache", "warm", "--dependencies-only", "Target1", "Target2")
+          Utilities::System.expects(:tuist).with("generate", "Target1", "Target2", "--open")
 
           # When/Then
-          Fourier::Services::Generate::Tuist.call(open: true, targets: "Target")
+          Fourier::Services::Generate::Tuist.call(open: true, targets: ["Target1", "Target2"])
         end
       end
     end
