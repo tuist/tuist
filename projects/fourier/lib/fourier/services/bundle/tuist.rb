0ba27d5186cae9c5d36e4de9eed2c9e19fefe84a
Alfredo Delli Bovi <alfredo.dellibovi@gmail.com>
2022-01-26 19:49:13 +0100

    Embed libswift_Concurrency.dylib for release bundle

--- projects/fourier/lib/fourier/services/bundle/tuist.rb
+++ projects/fourier/lib/fourier/services/bundle/tuist.rb
@@ -60,6 +60,8 @@ module Fourier
                 File.expand_path("Templates", Constants::ROOT_DIRECTORY),
                 File.expand_path("Templates", build_directory)
               )
+              Utilities::System.system("swift", "stdlib-tool", "--copy", "--scan-executable",
+                File.expand_path("tuist", build_directory), "--platform", "macosx", "--destination", build_directory)
 
               Dir.chdir(build_directory) do
                 output_zip_path = File.expand_path("tuist.zip", output_directory)
@@ -69,6 +71,7 @@ module Fourier
                   "zip", "-q", "-r", "--symlinks",
                   output_zip_path,
                   "tuist",
+                  "libswift_Concurrency.dylib",
                   "ProjectDescription.framework",
                   "ProjectDescription.framework.dSYM",
                   "ProjectAutomation.framework",
