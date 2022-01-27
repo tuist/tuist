057024e8cd0424d8d530844b0fb449ea516dc27c
Daniele Formichelli <df@bendingspoons.com>
2022-01-27 23:49:22 +0100

    fix: fix app_with_spm_dependencies fixturre

--- projects/tuist/fixtures/app_with_spm_dependencies/Tuist/Dependencies.swift
+++ projects/tuist/fixtures/app_with_spm_dependencies/Tuist/Dependencies.swift
@@ -8,11 +8,5 @@ let dependencies = Dependencies(
         .package(url: "https://github.com/firebase/firebase-ios-sdk", .upToNextMajor(from: "8.0.0")),
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture", .upToNextMinor(from: "0.22.0")),
     ],
-    targetSettings: [
-        "CrashReporter": [
-            "ALWAYS_SEARCH_USER_PATHS": "NO",
-            "USE_HEADERMAP": "YES",
-        ],
-    ]
     platforms: [.iOS]
 )
