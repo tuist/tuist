d6b378967be3cdd80638dafd2d01b027df0591eb
Alfredo Delli Bovi <alfredo.dellibovi@gmail.com>
2022-01-27 22:05:27 +0100

    Update step definition for `tuist fetch`

--- projects/tuist/features/step_definitions/dependencies.rb
+++ projects/tuist/features/step_definitions/dependencies.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Then(/tuist fetches dependencies/) do
-  system(@tuist, "fetch", "dependencies", "--path", @dir)
+  system(@tuist, "fetch", "--path", @dir)
 end
 
 Then(/tuist updates dependencies/) do
-  system(@tuist, "fetch", "dependencies", "--update", "--path", @dir)
+  system(@tuist, "fetch", "--update", "--path", @dir)
 end
