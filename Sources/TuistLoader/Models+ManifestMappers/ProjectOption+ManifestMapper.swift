bf0c059c958dae60a17a7b29e52bdf1f05444b19
Daniele Formichelli <df@bendingspoons.com>
2022-02-01 14:51:39 +0100

    feat: add per project scheme generation options (#4090)
    * feat: rename `-Project` scheme to `-Workspace`
    
    * docs: update changelog
    
    * feat: rename BuildGraphInspecting.projectSchemes
    
    * feat: add support for configuring code coverage and testing options at the project level
    
    * chore: formatting
    
    * docs: fix changelog

--- Sources/TuistLoader/Models+ManifestMappers/ProjectOption+ManifestMapper.swift
+++ Sources/TuistLoader/Models+ManifestMappers/ProjectOption+ManifestMapper.swift
@@ -5,8 +5,17 @@ extension TuistGraph.ProjectOption {
     /// Maps a ProjectDescription.ProjectOption instance into a TuistGraph.ProjectOption instance.
     /// - Parameters:
     ///   - manifest: Manifest representation of project options.
-    static func from(manifest: ProjectDescription.ProjectOption) -> TuistGraph.ProjectOption {
+    static func from(manifest: ProjectDescription.ProjectOption) -> Self {
         switch manifest {
+        case let .automaticSchemesOptions(options):
+            switch options {
+            case let .enabled(codeCoverageEnabled, testingOptions):
+                return .automaticSchemesOptions(.enabled(
+                    codeCoverageEnabled: codeCoverageEnabled, testingOptions: .from(manifest: testingOptions)
+                ))
+            case .disabled:
+                return .automaticSchemesOptions(.disabled)
+            }
         case .disableBundleAccessors:
             return .disableBundleAccessors
         case .disableSynthesizedResourceAccessors:
