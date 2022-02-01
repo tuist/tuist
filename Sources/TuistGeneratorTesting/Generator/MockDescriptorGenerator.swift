1c8efea479ccabf51890c672ec687a36d1ef84c2
Kassem Wridan <kwridan@bloomberg.net>
2022-02-01 14:24:39 +0000

    Update mock

--- Sources/TuistGeneratorTesting/Generator/MockDescriptorGenerator.swift
+++ Sources/TuistGeneratorTesting/Generator/MockDescriptorGenerator.swift
@@ -11,6 +11,15 @@ final class MockDescriptorGenerator: DescriptorGenerating {
         case stubNotImplemented
     }
 
+    var generateProjectSub: ((Project, GraphTraversing) throws -> ProjectDescriptor)?
+    func generateProject(project: Project, graphTraverser: GraphTraversing) throws -> ProjectDescriptor {
+        guard let generateProjectSub = generateProjectSub else {
+            throw MockError.stubNotImplemented
+        }
+
+        return try generateProjectSub(project, graphTraverser)
+    }
+
     var generateWorkspaceStub: ((GraphTraversing) throws -> WorkspaceDescriptor)?
     func generateWorkspace(graphTraverser: GraphTraversing) throws -> WorkspaceDescriptor {
         guard let generateWorkspaceStub = generateWorkspaceStub else {
