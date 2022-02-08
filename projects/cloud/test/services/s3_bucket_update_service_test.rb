4a80bd0cb698505ec43b7643860c319b8585933c
fortmarek <marekfort@me.com>
2022-02-08 08:49:03 +0100

    Add test for case when secret_access_key has not changed

--- projects/cloud/test/services/s3_bucket_update_service_test.rb
+++ projects/cloud/test/services/s3_bucket_update_service_test.rb
@@ -30,6 +30,33 @@ class S3BucketCreateServiceTest < ActiveSupport::TestCase
     assert_equal user.account.id, got.account_id
   end
 
+  test "updates an S3 bucket when secret access key has not changed" do
+    # Given
+    user = User.create!(email: "test@cloud.tuist.io", password: Devise.friendly_token.first(16))
+    s3_bucket = user.account.s3_buckets.create!(
+      access_key_id: "1",
+      name: "s3 bucket",
+      secret_access_key: "secret",
+      iv: "iv"
+    )
+
+    # When
+    got = S3BucketUpdateService.call(
+      id: s3_bucket.id,
+      name: "new name",
+      access_key_id: "new access key",
+      secret_access_key: s3_bucket.secret_access_key,
+      user: user
+    )
+
+    # Then
+    assert_equal "new name", got.name
+    assert_equal "new access key", got.access_key_id
+    assert_equal s3_bucket.secret_access_key, got.secret_access_key
+    assert_equal s3_bucket.iv, got.iv
+    assert_equal user.account.id, got.account_id
+  end
+
   test "updating an S3 bucket fails when the bucket with a given id does not exist" do
     # Given
     user = User.create!(email: "test@cloud.tuist.io", password: Devise.friendly_token.first(16))
