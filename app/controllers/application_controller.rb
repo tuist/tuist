c00e48a70be7bf0a46e24fae97597d41d802e011
Marek Fořt <marekfort@me.com>
2022-02-12 11:49:33 +0100

    AWS interactions (#4138)
    * Upload item to bucket
    
    * Check if S3 object exists
    
    * Upload and fetch artifacts from remote cache
    
    * Add test for object_exists
    
    * Add test for remaining cache methods
    
    * Lint cloud code
    
    * Add TODO for expires_at
    
    * Project ObjectNotFound error
    
    * Uncomment code in a fixture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   include Pundit
 
   before_action :authenticate_user!
+  protect_from_forgery with: :null_session
 
   def app
     render(layout: "app")
