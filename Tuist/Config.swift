8801b85a96d2c5e2698377366103f00757281036
fortmarek <marekfort@me.com>
2022-03-13 20:08:51 +0100

    Temporarily remove cloud setup

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -1,5 +1,5 @@
 import ProjectDescription
 
 let config = Config(
-    cloud: .cloud(projectId: "tuist/tuist", url: "https://cloud.tuist.io")
+    // cloud: .cloud(projectId: "tuist/tuist", url: "https://cloud.tuist.io")
 )
