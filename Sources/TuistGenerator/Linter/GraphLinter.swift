ca1f100644627a56b6768f0795d614917842fa6f
Daniele Formichelli <df@bendingspoons.com>
2022-03-14 17:21:14 +0100

    feat: allow bundle as extension dependency

--- Sources/TuistGenerator/Linter/GraphLinter.swift
+++ Sources/TuistGenerator/Linter/GraphLinter.swift
@@ -367,6 +367,7 @@ public class GraphLinter: GraphLinting {
             LintableTarget(platform: .iOS, product: .dynamicLibrary),
             LintableTarget(platform: .iOS, product: .staticFramework),
             LintableTarget(platform: .iOS, product: .framework),
+            LintableTarget(platform: .iOS, product: .bundle),
         ],
         LintableTarget(platform: .iOS, product: .appClip): [
             LintableTarget(platform: .iOS, product: .staticLibrary),
