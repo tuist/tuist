312e1761c9b6a066cf77a7851606688921fb4cf7
fortmarek <marekfort@me.com>
2022-03-21 09:02:13 +0100

    Allow other hosts

--- projects/cloud/app/controllers/auth_controller.rb
+++ projects/cloud/app/controllers/auth_controller.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
-require "net/http"
-
 class AuthController < ApplicationController
   def authenticate
-    redirect_to("http://127.0.0.1:4545/auth?token=#{current_user.token}&account=#{current_user.account.name}")
+    redirect_to(
+      "http://127.0.0.1:4545/auth?token=#{current_user.token}&account=#{current_user.account.name}",
+      allow_other_host: true
+    )
   end
 end
