a07750c5994ae8289f636d74c7c3599b08bcb4b6
Marek Fořt <marekfort@me.com>
2022-03-24 08:41:49 +0100

    Resend invite (#4274)
    * Set up resending invite
    
    * Resend invitation
    
    * Set up resending invite in frontend
    
    * Destroy invitation along with organization
    
    * Fix lint issues
    
    * Remove accepted flag
    
    * Automatically add pending invitation in OrganizationStore

--- test/services/organization_invite_service_test.rb
+++ test/services/organization_invite_service_test.rb
@@ -12,7 +12,7 @@ class OrganizationInviteServiceTest < ActiveSupport::TestCase
     invitee_email = "test1@cloud.tuist.io"
 
     # When
-    got = OrganizationInviteService.call(
+    got = OrganizationInviteService.new.invite(
       inviter: inviter,
       invitee_email: invitee_email,
       organization_id: organization.id
@@ -23,6 +23,34 @@ class OrganizationInviteServiceTest < ActiveSupport::TestCase
     assert_equal got.organization, organization
   end
 
+  test "invite a user to two organizations" do
+    # Given
+    inviter = User.create!(email: "test@cloud.tuist.io", password: Devise.friendly_token.first(16))
+    organization_one = Organization.create!
+    organization_two = Organization.create!
+    Account.create!(owner: organization_one, name: "tuist_one")
+    inviter.add_role(:admin, organization_one)
+    Account.create!(owner: organization_two, name: "tuist_two")
+    inviter.add_role(:admin, organization_two)
+    invitee_email = "test1@cloud.tuist.io"
+
+    # When
+    OrganizationInviteService.new.invite(
+      inviter: inviter,
+      invitee_email: invitee_email,
+      organization_id: organization_one.id
+    )
+    got = OrganizationInviteService.new.invite(
+      inviter: inviter,
+      invitee_email: invitee_email,
+      organization_id: organization_two.id
+    )
+    # Then
+    assert_equal got.inviter, inviter
+    assert_equal got.invitee_email, invitee_email
+    assert_equal got.organization, organization_two
+  end
+
   test "inviting a user to the organization if user is not admin fails" do
     # Given
     inviter = User.create!(email: "test@cloud.tuist.io", password: Devise.friendly_token.first(16))
@@ -31,7 +59,7 @@ class OrganizationInviteServiceTest < ActiveSupport::TestCase
 
     # When / Then
     assert_raises(OrganizationInviteService::Error::Unauthorized) do
-      OrganizationInviteService.call(
+      OrganizationInviteService.new.invite(
         inviter: inviter,
         invitee_email: "test1@cloud.tuist.io",
         organization_id: organization.id
@@ -45,10 +73,73 @@ class OrganizationInviteServiceTest < ActiveSupport::TestCase
 
     # When / Then
     assert_raises(OrganizationInviteService::Error::OrganizationNotFound) do
-      OrganizationInviteService.call(
+      OrganizationInviteService.new.invite(
         inviter: inviter,
         invitee_email: "test1@cloud.tuist.io",
         organization_id: "1"
+      )
+    end
+  end
+
+  test "inviting user to the same organization twice fails" do
+    # Given
+    inviter = User.create!(email: "test@cloud.tuist.io", password: Devise.friendly_token.first(16))
+    organization = Organization.create!
+    Account.create!(owner: organization, name: "tuist")
+    inviter.add_role(:admin, organization)
+    invitee_email = "test1@cloud.tuist.io"
+
+    # When / Then
+    OrganizationInviteService.new.invite(
+      inviter: inviter,
+      invitee_email: invitee_email,
+      organization_id: organization.id
+    )
+    assert_raises(OrganizationInviteService::Error::DuplicateInvitation) do
+      OrganizationInviteService.new.invite(
+        inviter: inviter,
+        invitee_email: invitee_email,
+        organization_id: organization.id
+      )
+    end
+  end
+
+  test "resend invitation" do
+    # Given
+    invitee_email = "test1@cloud.tuist.io"
+    organization = Organization.create!
+    Account.create!(owner: organization, name: "tuist")
+    inviter = User.create!(email: "test@cloud.tuist.io", password: Devise.friendly_token.first(16))
+    inviter.add_role(:admin, organization)
+    invitation = inviter.invitations.create!(
+      invitee_email: invitee_email,
+      organization_id: organization.id,
+      token: "token"
+    )
+    InvitationMailer.any_instance.expects(:invitation_mail).once
+
+    # When
+    got = OrganizationInviteService.new.resend_invite(
+      invitation_id: invitation.id,
+    )
+
+    # Then
+    assert_equal got.inviter, inviter
+    assert_equal got.invitee_email, invitee_email
+    assert_equal got.organization, organization
+  end
+
+  test "resend invitation fails when not found" do
+    # Given
+    organization = Organization.create!
+    Account.create!(owner: organization, name: "tuist")
+    inviter = User.create!(email: "test@cloud.tuist.io", password: Devise.friendly_token.first(16))
+    inviter.add_role(:admin, organization)
+
+    # When / Then
+    assert_raises(OrganizationInviteService::Error::InvitationNotFound) do
+      OrganizationInviteService.new.resend_invite(
+        invitation_id: 0
       )
     end
   end
