361b6f5f9f3579883d2dcf89e0be3cade6da38bf
Aris Sarris <78614622+a-sarris@users.noreply.github.com>
2022-04-01 20:51:24 +0300

    Fix for duplicated settings (for example, `-Xcc`) incorrectly removed (#4325)

--- Sources/TuistGenerator/Utils/SettingsHelper.swift
+++ Sources/TuistGenerator/Utils/SettingsHelper.swift
@@ -75,33 +75,57 @@ final class SettingsHelper {
         // it will need to be merged with the oldValue, otherwise the oldValue will be discarded
         // and the newValue returned without merging.
         //
-        // The .uniqued() method ensures the result of merging does not contain duplicates
-        // and all the elements are sorted, i.e. merging the following values:
+        // The .sortAndTrim() method ensures the result of merging does not contain duplicate "$(inherited)" and that "$(inherited)" is the first element
+        // i.e. merging the following values:
         // oldValue = ["$(inherited)", "VALUE_1"]
         // newValue = ["$(inherited)", "VALUE_2"]
-        // would result in ["$(inherited)", "$(inherited)", "VALUE_1", "VALUE_2"] if .uniqued() was not used.
+        // would result in ["$(inherited)", "$(inherited)", "VALUE_1", "VALUE_2"] if .sortAndTrim() was not used.
+        let inherited = "$(inherited)"
         switch (oldValue, newValue) {
-        case let (.string(old), .string(new)) where new.contains("$(inherited)"):
+        case let (.string(old), .string(new)) where new.contains(inherited):
             // Example: ("OLD", "$(inherited) NEW") -> ["$(inherited) NEW", "OLD"]
             // This case shouldn't happen as all default multi-value settings are defined as NSArray<NSString>
-            return .array([old, new].uniqued())
+            return .array(Self.sortAndTrim(array: [old, new], element: inherited))
 
-        case let (.string(old), .array(new)) where new.contains("$(inherited)"):
+        case let (.string(old), .array(new)) where new.contains(inherited):
             // Example: ("OLD", ["$(inherited)", "NEW"]) -> ["$(inherited)", "NEW", "OLD"]
-            return .array(([old] + new).uniqued())
+            return .array(Self.sortAndTrim(array: [old] + new, element: inherited))
 
-        case let (.array(old), .string(new)) where new.contains("$(inherited)"):
+        case let (.array(old), .string(new)) where new.contains(inherited):
             // Example: (["OLD", "OLD_2"], "$(inherited) NEW") -> ["$(inherited) NEW", "OLD", "OLD_2"]
             // This case shouldn't happen as all default multi-value settings are defined as NSArray<NSString>
-            return .array((old + [new]).uniqued())
+            return .array(Self.sortAndTrim(array: old + [new], element: inherited))
 
-        case let (.array(old), .array(new)) where new.contains("$(inherited)"):
+        case let (.array(old), .array(new)) where new.contains(inherited):
             // Example: (["OLD", "OLD_2"], ["$(inherited)", "NEW"]) -> ["$(inherited)", "NEW", "OLD", OLD_2"]
-            return .array((old + new).uniqued())
+            return .array(Self.sortAndTrim(array: old + new, element: inherited))
 
         default:
             // The newValue does not contain $(inherited) so the oldValue should be omitted
             return newValue
         }
+    }
+
+    private static func sortAndTrim(array: [String], element: String) -> [String] {
+        guard array.contains(where: { $0.starts(with: element) }) else { return array }
+        // Move items that contain `element` to the top of the array
+        return array
+            .sorted { first, _ in first.contains(element) }
+            .enumerated()
+            .compactMap { index, item in
+                // Remove duplicate `element`
+                if index > 0,
+                   item == element
+                {
+                    return nil
+                } else if index > 0, item.contains(element) {
+                    // "$(inherited) flag" -> "flag"
+                    return item
+                        .replacingOccurrences(of: element, with: "")
+                        .trimmingCharacters(in: .whitespacesAndNewlines)
+                } else {
+                    return item
+                }
+            }
     }
 }
