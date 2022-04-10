271279165dd994a12a08f5a7cb91d6fb619e4fe9
fortmarek <marekfort@me.com>
2022-04-10 10:26:18 +0200

    Turn on cloud config

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -1,5 +1,5 @@
 import ProjectDescription
 
 let config = Config(
-    // cloud: .cloud(projectId: "tuist/tuist", url: "https://cloud.tuist.io", options: [.optional])
+    cloud: .cloud(projectId: "tuist/tuist", url: "https://cloud.tuist.io", options: [.optional])
 )
