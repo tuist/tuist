277dd834287a702b8918504a00a5a75d64b24cac
Nicholas Kim <nicholaskim94@gmail.com>
2022-05-03 02:15:46 +0900

    Defer the display of warnings untill after project generation (#4387)
    Co-authored-by: Nicholas Kim <nicholas.kim@kakaobank.com>

--- Tests/TuistKitTests/Mocks/MockManifestGraphLoader.swift
+++ Tests/TuistKitTests/Mocks/MockManifestGraphLoader.swift
@@ -7,7 +7,7 @@ import TuistGraphTesting
 
 final class MockManifestGraphLoader: ManifestGraphLoading {
     var stubLoadGraph: Graph?
-    func load(path _: AbsolutePath) async throws -> (Graph, [SideEffectDescriptor]) {
-        (stubLoadGraph ?? .test(), [])
+    func load(path _: AbsolutePath) async throws -> (Graph, [SideEffectDescriptor], [LintingIssue]) {
+        (stubLoadGraph ?? .test(), [], [])
     }
 }
