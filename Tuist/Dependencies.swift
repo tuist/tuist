ff1441937a9795a465a29544d5871c1b932f7bb1
Marek Fořt <marekfort@me.com>
2022-05-06 09:03:59 +0200

    Fix zipping and unzipping cached frameworks with symlinks (#4355)
    * Turn on cloud config
    
    * Dirty cache
    
    * Use zip command to include symlinks
    
    * Fix storing target
    
    * Add analytics to Config.swift
    
    * Use ZIPFoundation instead of ZIP
    
    * Revert "Dirty cache"
    
    This reverts commit c2b0b195f8920ce0c12e80f1f37b68b825553efb.
    
    * Move to fork of ZIPFoundation
    
    * Use xcframeworks
    
    * Dirty cache
    
    * Fix TuistGeneratorTests
    
    * Revert "Dirty cache"
    
    This reverts commit 690374b4eb91a2dfb8cd8fdc68b5882519addcff.
    
    * Fix fourier tests
    
    * Lint and fix fourier
    
    * Rename pathsPath to pathsDirectoryPath

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -9,7 +9,10 @@ let dependencies = Dependencies(
             .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.4.2")),
             .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", .upToNextMajor(from: "4.2.2")),
             .package(url: "https://github.com/httpswift/swifter.git", .revision("1e4f51c92d7ca486242d8bf0722b99de2c3531aa")),
-            .package(url: "https://github.com/marmelroy/Zip.git", .upToNextMajor(from: "2.1.1")),
+            .package(
+                url: "https://github.com/fortmarek/ZIPFoundation.git",
+                .revision("9dbe729b90202c19d0fe1010f1430fa75a576cd3")
+            ),
             .package(url: "https://github.com/rnine/Checksum.git", .upToNextMajor(from: "1.0.2")),
             .package(url: "https://github.com/stencilproject/Stencil.git", .upToNextMajor(from: "0.14.1")),
             .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", .upToNextMajor(from: "2.8.0")),
--- projects/fourier/test/fourier/services/generate/tuist_test.rb
+++ projects/fourier/test/fourier/services/generate/tuist_test.rb
@@ -9,8 +9,9 @@ module Fourier
         def test_calls_tuist_with_the_right_arguments
           # Given
           Utilities::System.expects(:tuist).with("fetch")
-          Utilities::System.expects(:tuist).with("cache", "warm", "--dependencies-only", "Target1", "Target2")
-          Utilities::System.expects(:tuist).with("generate", "Target1", "Target2")
+          Utilities::System.expects(:tuist).with("cache", "warm", "--dependencies-only", "--xcframeworks", "Target1",
+            "Target2")
+          Utilities::System.expects(:tuist).with("generate", "--xcframeworks", "Target1", "Target2")
 
           # When/Then
           Fourier::Services::Generate::Tuist.call(no_open: false, targets: ["Target1", "Target2"])
@@ -20,8 +21,9 @@ module Fourier
         def test_calls_tuist_with_the_right_arguments_when_no_open_is_true
           # Given
           Utilities::System.expects(:tuist).with("fetch")
-          Utilities::System.expects(:tuist).with("cache", "warm", "--dependencies-only", "Target1", "Target2")
-          Utilities::System.expects(:tuist).with("generate", "Target1", "Target2", "--no-open")
+          Utilities::System.expects(:tuist).with("cache", "warm", "--dependencies-only", "--xcframeworks", "Target1",
+            "Target2")
+          Utilities::System.expects(:tuist).with("generate", "--xcframeworks", "Target1", "Target2", "--no-open")
 
           # When/Then
           Fourier::Services::Generate::Tuist.call(no_open: true, targets: ["Target1", "Target2"])
