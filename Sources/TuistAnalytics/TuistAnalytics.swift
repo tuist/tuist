5f2e61aead229f38f25db9ad38bea997923ee74d
Daniele Formichelli <df@bendingspoons.com>
2022-06-25 16:23:27 +0200

    fix: fix couldn't find dispatcher error (#4565)

--- Sources/TuistAnalytics/TuistAnalytics.swift
+++ Sources/TuistAnalytics/TuistAnalytics.swift
@@ -7,6 +7,8 @@ import TuistLoader
 public enum TuistAnalytics {
     public static func bootstrap(config: Config) throws {
         AsyncQueue.sharedInstance.register(dispatcher: TuistAnalyticsDispatcher(cloud: config.cloud))
-        AsyncQueue.sharedInstance.start() // Re-try to send all events that got persisted and haven't been sent yet
+        Task.detached(priority: .background) {
+            AsyncQueue.sharedInstance.start() // Re-try to send all events that got persisted and haven't been sent yet
+        }
     }
 }
