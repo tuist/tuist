fd6094a3cd104c118a91a9c773909378f7b3f298
Alexander Weiß <weiss_alex@gmx.net>
2022-06-29 19:22:47 +0200

    fix: Lint complaint

--- Sources/TuistSupportTesting/SwiftPackageManager/PlatformTestInfo.swift
+++ Sources/TuistSupportTesting/SwiftPackageManager/PlatformTestInfo.swift
@@ -2,6 +2,7 @@ import Foundation
 import TuistGraph
 
 /// Global configuration of which platform versions used during mock generation
+// swiftlint:disable identifier_name
 public var PLATFORM_TEST_VERSION: [Platform: String] = [
     .iOS: "10.0",
     .macOS: "10.15",
