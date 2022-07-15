af4410f4a9fa8c5d61feed6e7234bf6198a2ba22
woohyunjin06 <woohyunjin06@gmail.com>
2022-07-15 18:47:36 +0900

    Change example plugin remote url to use tuist repository

--- projects/tuist/fixtures/app_with_plugins/Tuist/Config.swift
+++ projects/tuist/fixtures/app_with_plugins/Tuist/Config.swift
@@ -3,6 +3,6 @@ import ProjectDescription
 let config = Config(
     plugins: [
         .local(path: .relativeToManifest("../../LocalPlugin")),
-        .git(url: "https://github.com/woohyunjin06/ExampleTuistPlugin", tag: "3.1.0"),
+        .git(url: "https://github.com/Tuist/ExampleTuistPlugin", tag: "3.0.0"),
     ]
 )
