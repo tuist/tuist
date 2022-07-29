755a95790e7e887dde7d571b8d55f5b2e633bbf6
Daniele Formichelli <df@bendingspoons.com>
2022-07-29 18:25:47 +0200

    fix: fix clean service tests

--- Tests/TuistKitTests/Services/CleanServiceTests.swift
+++ Tests/TuistKitTests/Services/CleanServiceTests.swift
@@ -61,11 +61,28 @@ final class CleanServiceTests: TuistUnitTestCase {
         }
         cacheDirectoriesProvider.cacheDirectoryStub = cachePath
         let projectPath = try temporaryPath()
-        let dependenciesPath = projectPath.appending(
-            components: Constants.tuistDirectoryName,
+        let dependenciesPath = projectPath.appending(components:
+            Constants.tuistDirectoryName,
             Constants.DependenciesDirectory.name
         )
+        let lockfilesPath = projectPath.appending(components:
+            Constants.tuistDirectoryName,
+            Constants.DependenciesDirectory.lockfilesDirectoryName
+        )
+        let carthageDependenciesPath = projectPath.appending(
+            components: Constants.tuistDirectoryName,
+            Constants.DependenciesDirectory.name,
+            Constants.DependenciesDirectory.carthageDirectoryName
+        )
+        let spmDependenciesPath = projectPath.appending(
+            components: Constants.tuistDirectoryName,
+            Constants.DependenciesDirectory.name,
+            Constants.DependenciesDirectory.carthageDirectoryName
+        )
         try fileHandler.createFolder(dependenciesPath)
+        try fileHandler.createFolder(lockfilesPath)
+        try fileHandler.createFolder(carthageDependenciesPath)
+        try fileHandler.createFolder(spmDependenciesPath)
 
         // When
         try subject.run(categories: CleanCategory.allCases, path: nil)
@@ -77,34 +94,17 @@ final class CleanServiceTests: TuistUnitTestCase {
         XCTAssertFalse(manifestsExists, "Cache folder at path \(cachePaths[2].pathString) should have been deleted by the test.")
         let testsExists = FileManager.default.fileExists(atPath: cachePaths[3].pathString)
         XCTAssertFalse(testsExists, "Cache folder at path \(cachePaths[3].pathString) should not have been deleted by the test.")
-        XCTAssertFalse(
-            FileManager.default.fileExists(atPath: dependenciesPath.pathString),
-            "Cache folder at path \(dependenciesPath) should have been deleted by the test."
+        XCTAssertTrue(
+            FileManager.default.fileExists(atPath: lockfilesPath.pathString),
+            "Cache folder at path \(lockfilesPath) should not have been deleted by the test."
         )
-    }
-
-    func test_run_with_dependencies_category() throws {
-        // Given
-        let cachePaths = try createFolders([
-          "Tuist/Dependencies",
-          "Tuist/Dependencies/Lockfiles",
-          "Tuist/Dependencies/Carthage",
-          "Tuist/Dependencies/SwiftPackageManager"
-        ])
-        for path in cachePaths {
-            let correctlyCreated = FileManager.default.fileExists(atPath: path.pathString)
-            XCTAssertTrue(correctlyCreated, "Test setup is not properly done. Folder \(path.pathString) should exist")
-        }
-
-        // When
-        try subject.run(categories: [.dependencies], path: try temporaryPath().pathString)
-
-        // Then
-        let lockfilesExists = FileManager.default.fileExists(atPath: cachePaths[1].pathString)
-        XCTAssertTrue(lockfilesExists, "Lockflies folder at path \(cachePaths[1]) should not have been deleted by the test.")
-        let carthageExists = FileManager.default.fileExists(atPath: cachePaths[2].pathString)
-        XCTAssertFalse(carthageExists, "Carthage folder at path \(cachePaths[2]) should have been deleted by the test.")
-        let spmExists = FileManager.default.fileExists(atPath: cachePaths[2].pathString)
-        XCTAssertFalse(spmExists, "SwiftPackageManager folder at path \(cachePaths[2]) should have been deleted by the test.")
+        XCTAssertFalse(
+            FileManager.default.fileExists(atPath: carthageDependenciesPath.pathString),
+            "Cache folder at path \(carthageDependenciesPath) should not have been deleted by the test."
+        )
+        XCTAssertFalse(
+            FileManager.default.fileExists(atPath: spmDependenciesPath.pathString),
+            "Cache folder at path \(spmDependenciesPath) should not have been deleted by the test."
+        )
     }
 }
