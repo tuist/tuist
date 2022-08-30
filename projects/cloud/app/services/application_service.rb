9f846e734060013eea13e5a36f3b0d3d3848ed88
fortmarek <marekfort@me.com>
2022-08-30 21:32:07 +0200

    Fix cloud lint issues

--- projects/cloud/app/services/application_service.rb
+++ projects/cloud/app/services/application_service.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationService
-  def self.call(*args, **kwargs, &block)
-    new(*args, **kwargs, &block).call
+  class << self
+    def call(*args, **kwargs, &block)
+      new(*args, **kwargs, &block).call
+    end
   end
 
   def call
