975b609899ffab4a8906af8eb1006f95f9218262
Marek Fořt <marekfort@me.com>
2022-10-20 22:27:52 +0200

    Migrate NewProject to the mobx arch (#4830)

---
+++ app/frontend/components/pages/new-project/NewProjectPage.tsx
@@ -0,0 +1,93 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import {
+  Card,
+  FormLayout,
+  TextField,
+  Layout,
+  Page,
+  Select,
+  Button,
+  Stack,
+} from '@shopify/polaris';
+
+import { useNavigate } from 'react-router-dom';
+import { observer, useLocalObservable } from 'mobx-react-lite';
+import { NewProjectPageStore } from './NewProjectPageStore';
+import { useApolloClient } from '@apollo/client';
+
+export const NewProjectPage = observer(() => {
+  const client = useApolloClient();
+  const newProjectPageStore = useLocalObservable(() => {
+    const newProjectPageStore = new NewProjectPageStore(client);
+    newProjectPageStore.load();
+    return newProjectPageStore;
+  });
+
+  const navigate = useNavigate();
+  const createProjectButtonTapped = () => {
+    newProjectPageStore.createNewProject((projectSlug) =>
+      navigate(`/${projectSlug}`),
+    );
+  };
+
+  return (
+    <Page title="New Project">
+      <Layout sectioned>
+        <Card sectioned>
+          <FormLayout>
+            <Select
+              label="Owner"
+              options={newProjectPageStore.options}
+              onChange={(value) =>
+                newProjectPageStore.selectedProjectOwnerChanged(value)
+              }
+              value={
+                newProjectPageStore.selectedProjectOwner ?? undefined
+              }
+            />
+            {newProjectPageStore.isCreatingOrganization && (
+              <TextField
+                type="text"
+                label="Organization name"
+                value={newProjectPageStore.organizationName}
+                onChange={(value) =>
+                  newProjectPageStore.organizationNameChanged(value)
+                }
+                autoComplete="off"
+              />
+            )}
+            {/* TODO: Only allow kebab-case names */}
+            <TextField
+              type="text"
+              label="Project name"
+              value={newProjectPageStore.newProjectName}
+              onChange={(value) => {
+                newProjectPageStore.projectNameChanged(value);
+              }}
+              autoComplete="off"
+            />
+            <Stack>
+              <Button
+                primary
+                disabled={
+                  newProjectPageStore.isCreateProjectButtonDisabled
+                }
+                onClick={createProjectButtonTapped}
+              >
+                Create project
+              </Button>
+              <Button
+                destructive
+                onClick={() => {
+                  navigate(-1);
+                }}
+              >
+                Cancel
+              </Button>
+            </Stack>
+          </FormLayout>
+        </Card>
+      </Layout>
+    </Page>
+  );
+});
