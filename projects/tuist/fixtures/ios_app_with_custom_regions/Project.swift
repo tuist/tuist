b3ecb4d343301ffe2b82c3ad949c3a9f9c4747a7
Stéphane G <101190846+spqw@users.noreply.github.com>
2022-11-29 17:11:22 +0100

    Add fixture for ios app with custom regions (#4877)
    * Add fixture for ios app with custom regions
    
    * chore: remove custom plist
    
    Co-authored-by: Daniele Formichelli <df@bendingspoons.com>

---
+++ projects/tuist/fixtures/ios_app_with_custom_regions/Project.swift
@@ -0,0 +1,19 @@
+import ProjectDescription
+
+let project = Project(
+    name: "App",
+    options: .options(
+        defaultKnownRegions: ["en-GB", "Base"],
+        developmentRegion: "en-GB"
+    ),
+    targets: [
+        Target(
+            name: "App",
+            platform: .iOS,
+            product: .app,
+            bundleId: "io.tuist.app",
+            infoPlist: .default,
+            sources: ["App/Sources/**"]
+        ),
+    ]
+)
