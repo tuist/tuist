9ea832c768dedf83b7267b22a4b43a692ec863b3
Benjamin Prieur <benjamin+github@prieur.org>
2022-12-15 08:35:19 +0100

    Add rcproject to supported source files (#4925)
    Resolves https://github.com/tuist/tuist/issues/202#issuecomment-1350953469
    
    - Add support for Reality Composer files as project soruces
    
    Test Plan:
    
    - Add a Reality Composer Project file to an existing project under sources
    - Regenerate the project via `tuist generate`
    - Verify the Reality Composer Project file appears in the generated project

--- Sources/TuistGraph/Models/Target.swift
+++ Sources/TuistGraph/Models/Target.swift
@@ -8,7 +8,7 @@ public struct Target: Equatable, Hashable, Comparable, Codable {
     //       in order to compile the documentation archive (including Tutorials, Articles, etc.)
     public static let validSourceExtensions: [String] = [
         "m", "swift", "mm", "cpp", "cc", "c", "d", "s", "intentdefinition", "xcmappingmodel", "metal", "mlmodel", "docc",
-        "playground",
+        "playground", "rcproject",
     ]
     public static let validFolderExtensions: [String] = [
         "framework", "bundle", "app", "xcassets", "appiconset", "scnassets",
