d8a409e6ff416e8224c273b9769d1a63e4c392a5
David Truong <truongvd@gmail.com>
2022-12-22 15:32:39 -0800

    Sets sending of analytics to cloud as default behavior

--- Sources/ProjectDescription/Cloud.swift
+++ Sources/ProjectDescription/Cloud.swift
@@ -4,13 +4,15 @@ import Foundation
 public struct Cloud: Codable, Equatable {
     /// Options for cloud configuration.
     public enum Option: String, Codable, Equatable {
+        /// Allows sending analytics to cloud dashboard.
         case analytics
+        
         /// Marks whether cloud connection is optional.
         /// If not present, tuist commands will fail regardless of whether an authentication token is available locally from `tuist cloud auth` or not.
         case optional
     }
 
-    /// The base URL that points to the Cloud server
+    /// The base URL that points to the Cloud server.
     public let url: String
 
     /// The project unique identifier.
@@ -25,7 +27,7 @@ public struct Cloud: Codable, Equatable {
     ///   - url: Base URL to the Cloud server.
     ///   - options: Cloud options.
     /// - Returns: A Cloud instance.
-    public static func cloud(projectId: String, url: String, options: [Option] = []) -> Cloud {
+    public static func cloud(projectId: String, url: String, options: [Option] = [.analytics]) -> Cloud {
         Cloud(url: url, projectId: projectId, options: options)
     }
 }
