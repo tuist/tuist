2f1d3ffd24dd41a74deb2eb91c16c479fdd5ceca
fortmarek <marekfort@me.com>
2022-12-23 14:39:51 +0100

    Use SVG instead of serving image from remote

--- Sources/TuistSupport/Utils/HTTPRedirectListener.swift
+++ Sources/TuistSupport/Utils/HTTPRedirectListener.swift
@@ -103,10 +103,12 @@ public final class HTTPRedirectListener: HTTPRedirectListening {
           <body
             class="flex h-screen w-screen flex-col justify-center items-center bg-gray-200"
           >
-            <img
+            <div
               class="w-40 h-40 mb-10"
-              src="\(logoURL.absoluteString)"
-            />
+            >
+              <svg viewBox="0 0 160 160" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="80" cy="80" r="80" fill="url(#paint0_linear)"></circle><path d="M39.6667 115.61C39.6667 115.61 56.5 129 80.5 129C104.5 129 121.333 115.61 121.333 115.61M35 92.878C35 92.878 64.1667 128 80.5 128M80.5 128C96.8333 128 126 92.878 126 92.878M80.5 128C64.1667 128 35 64.7805 35 64.7805M80.5 128C96.8333 128 126 64.7805 126 64.7805M80.5 128C72.0001 128 53.6667 40.1951 53.6667 40.1951M80.5 128C88.9999 128 107.333 40.1951 107.333 40.1951M80.8333 128L80.8333 32" stroke="white" stroke-width="4"></path><defs><linearGradient id="paint0_linear" x1="80" y1="0" x2="80" y2="160" gradientUnits="userSpaceOnUse"><stop stop-color="#6236FF"></stop><stop offset="1" stop-color="#3000DA"></stop></linearGradient></defs>
+              </svg>
+            </div>
             <div class="bg-white shadow sm:rounded-lg">
               <div class="px-4 py-5 sm:p-6">
                 <h3 class="text-lg leading-6 font-medium text-gray-900">
