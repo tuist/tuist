d7d619de71f4d299aca08c6233023d5a4fffd0b9
Serhii Butenko <sereja.butenko@gmail.com>
2023-01-12 16:18:17 +0200

    chore: Add fixtures

---
+++ projects/tuist/fixtures/macos_app_with_app_inside/Project.swift
@@ -0,0 +1,26 @@
+import ProjectDescription
+
+let project = Project(
+    name: "Embedded App",
+    targets: [
+        Target(
+            name: "MainApp",
+            platform: .macOS,
+            product: .app,
+            bundleId: "io.tuist.MainApp",
+            infoPlist: "MainApp/Info.plist",
+            sources: ["MainApp/Sources/**"],
+            dependencies: [
+                .target(name: "InnerApp")
+            ]
+        ),
+        Target(
+            name: "InnerApp",
+            platform: .macOS,
+            product: .app,
+            bundleId: "io.tuist.InnerApp",
+            infoPlist: "InnerApp/Info.plist",
+            sources: ["InnerApp/Sources/**"]
+        )
+    ]
+)
