b472440d47978295d77cfa47f2c7b861d28fde80
fortmarek <marekfort@me.com>
2023-01-16 22:27:45 +0100

    Fix lint issues

--- Sources/TuistCloudTesting/MockCreateProjectService.swift
+++ Sources/TuistCloudTesting/MockCreateProjectService.swift
@@ -4,6 +4,7 @@ import TuistCloud
 public final class MockCreateProjectService: CreateProjectServicing {
     public init() {}
 
+    // swiftlint:disable:next large_tuple
     public var createProjectStub: ((String, String, URL) async throws -> Void)?
     public func createProject(name: String, organizationName: String, serverURL: URL) async throws {
         try await createProjectStub?(name, organizationName, serverURL)
