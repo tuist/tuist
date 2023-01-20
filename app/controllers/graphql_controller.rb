1290cf046edee7dd88043135eb53ee9c712858ca
Marek Fořt <marekfort@me.com>
2023-01-20 12:03:19 +0100

    Fix lint issues (#4991)

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -56,7 +56,8 @@ class GraphqlController < ApplicationController
       logger.error(e.message)
       logger.error(e.backtrace.join("\n"))
 
-      render(json: { errors: [{ message: e.message, backtrace: e.backtrace }], data: {} },
+      render(
+        json: { errors: [{ message: e.message, backtrace: e.backtrace }], data: {} },
         status: :internal_server_error)
     end
 end
