9b48a67e6e1ce66edc7df2d9f8592c869cd9cb0a
Clemens Beck <clemens.beck@qnips.io>
2023-01-20 15:19:03 +0100

    Allow period character only for app targets

--- Sources/TuistGenerator/Linter/TargetLinter.swift
+++ Sources/TuistGenerator/Linter/TargetLinter.swift
@@ -71,8 +71,12 @@ class TargetLinter: TargetLinting {
     }
 
     private func lintProductName(target: Target) -> [LintingIssue] {
-        let allowed = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_.")
+        var allowed = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_")
 
+        if target.product == .app {
+            allowed.formUnion(CharacterSet(charactersIn: "."))
+        }
+        
         if target.productName.unicodeScalars.allSatisfy(allowed.contains) == false {
             let reason =
                 "Invalid product name '\(target.productName)'. This string must contain only alphanumeric (A-Z,a-z,0-9), period (.), and underscore (_) characters."
