cdddef3ec2a8e02af3b6c021e53bb4dadb852545
Daniele Formichelli <df@bendingspoons.com>
2023-02-16 12:32:05 +0100

    fix tools version

--- projects/tuistbench/Package.swift
+++ projects/tuistbench/Package.swift
@@ -1,5 +1,4 @@
-// swift-tools-version:5.2.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.7.0
 
 import PackageDescription
 
