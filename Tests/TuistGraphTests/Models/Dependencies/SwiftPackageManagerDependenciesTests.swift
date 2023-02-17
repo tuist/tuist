57cf1abca08077db4d2fe17e258afc8964013938
Daniele Formichelli <df@bendingspoons.com>
2023-02-17 12:34:10 +0100

    lint

--- Tests/TuistGraphTests/Models/Dependencies/SwiftPackageManagerDependenciesTests.swift
+++ Tests/TuistGraphTests/Models/Dependencies/SwiftPackageManagerDependenciesTests.swift
@@ -143,7 +143,7 @@ final class SwiftPackageManagerDependenciesTests: TuistUnitTestCase {
                 .package(path: "path/path"),
             ]
         )
-        
+
         """
         XCTAssertEqual(got, expected)
     }
