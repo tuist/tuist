e9ecd3df179af7921256ee8cbe09c34bdcacdc24
Daniele Formichelli <df@bendingspoons.com>
2023-02-18 14:26:26 +0100

    fix lint

--- Sources/TuistScaffold/TemplatesDirectoryLocator.swift
+++ Sources/TuistScaffold/TemplatesDirectoryLocator.swift
@@ -33,14 +33,14 @@ public final class TemplatesDirectoryLocator: TemplatesDirectoryLocating {
         #if DEBUG
             // Used only for debug purposes to find templates in your tuist working directory
             // `bundlePath` points to tuist/Templates
-            // swiftlint:disable:next force_try
-            let bundlePath = try! AbsolutePath(validating: #file.replacingOccurrences(of: "file://", with: ""))
+            let maybeBundlePath = try? AbsolutePath(validating: #file.replacingOccurrences(of: "file://", with: ""))
                 .removingLastComponent()
                 .removingLastComponent()
                 .removingLastComponent()
         #else
-            let bundlePath = try! AbsolutePath(validating: Bundle(for: TemplatesDirectoryLocator.self).bundleURL.path)
+            let bundlePath = try? AbsolutePath(validating: Bundle(for: TemplatesDirectoryLocator.self).bundleURL.path)
         #endif
+        guard let bundlePath = maybeBundlePath else { return nil }
         let paths = [
             bundlePath,
             bundlePath.parentDirectory,
