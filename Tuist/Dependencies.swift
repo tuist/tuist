dcf0e6851263199f4c11618be68feb79a73fa93d
Daniele Formichelli <df@bendingspoons.com>
2023-02-19 17:58:57 +0100

    fix unzipping

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -10,6 +10,7 @@ let dependencies = Dependencies(
             .package(url: "https://github.com/CombineCommunity/CombineExt.git", .upToNextMajor(from: "1.8.0")),
             .package(url: "https://github.com/FabrizioBrancati/Queuer.git", .upToNextMajor(from: "2.1.1")),
             .package(url: "https://github.com/Flight-School/AnyCodable", from: "0.6.7"),
+            .package(url: "https://github.com/fortmarek/ZIPFoundation.git", from: "9dbe729b90202c19d0fe1010f1430fa75a576cd3"),
             .package(url: "https://github.com/httpswift/swifter.git", .revision("1e4f51c92d7ca486242d8bf0722b99de2c3531aa")),
             .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", .upToNextMajor(from: "4.2.2")),
             .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.6.0")),
@@ -19,7 +20,6 @@ let dependencies = Dependencies(
             .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", .exact("2.9.0")),
             .package(url: "https://github.com/SwiftGen/SwiftGen", .exact("6.5.0")),
             .package(url: "https://github.com/tuist/XcodeProj.git", .upToNextMajor(from: "8.9.0")),
-            .package(url: "https://github.com/weichsel/ZIPFoundation.git", from: "0.9.16"),
         ]
     ),
     platforms: [.macOS]
