b847e651d9c17a32cafdf2e79b67a46c8b4f1b75
JCSooHwanCho <chh0107@gmail.com>
2023-04-16 17:11:52 +0900

    bump up Xcodeproj to 8.9.0

--- Package.swift
+++ Package.swift
@@ -62,7 +62,7 @@ let package = Package(
         .package(url: "https://github.com/SwiftDocOrg/GraphViz.git", exact: "0.2.0"),
         .package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", exact: "2.9.0"),
         .package(url: "https://github.com/SwiftGen/SwiftGen", exact: "6.5.1"),
-        .package(url: "https://github.com/tuist/XcodeProj.git", exact: "8.8.0"),
+        .package(url: "https://github.com/tuist/XcodeProj.git", exact: "8.9.0"),
     ],
     targets: [
         .target(
