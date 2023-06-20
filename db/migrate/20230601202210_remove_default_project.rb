3e097d255c2e78e8922883f2a00c03597cfc2671
fortmarek <marekfort@me.com>
2023-06-20 10:08:58 +0200

    Fix default bucket name

--- db/migrate/20230601202210_remove_default_project.rb
+++ db/migrate/20230601202210_remove_default_project.rb
@@ -9,17 +9,5 @@ class RemoveDefaultProject < ActiveRecord::Migration[7.0]
     S3Bucket.where(is_default: true).delete_all
     remove_reference(:s3_buckets, :default_project, index: true, foreign_key: false)
     remove_column(:s3_buckets, :is_default)
-
-    # s3_bucket = S3BucketCreateService.call(
-    #   name: "tuist",
-    #   access_key_id: Rails.application.credentials.aws[:access_key_id],
-    #   secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-    #   region: "eu-west-1",
-    #   account_id: nil,
-    # )
-    # project.update(remote_cache_storage: s3_bucket)
-
-    # TODO: We should add instructions on how to create a bucket manually for self-hosted sollutions
-    # s3_client.create_bucket(bucket: )
   end
 end
