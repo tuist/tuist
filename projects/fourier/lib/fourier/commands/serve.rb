ddb31e0121bb96331483cb6b61ef21db53338d3f
Pedro Piñera <pedro@pepicrft.me>
2023-06-23 10:54:29 +0200

    Delete website

--- projects/fourier/lib/fourier/commands/serve.rb
+++ projects/fourier/lib/fourier/commands/serve.rb
@@ -3,11 +3,6 @@
 module Fourier
   module Commands
     class Serve < Base
-      desc "web", "Serve the website"
-      def web
-        Services::Serve::Web.call
-      end
-
       desc "docs", "Serve the documentation website"
       def docs
         Services::Serve::Docs.call
