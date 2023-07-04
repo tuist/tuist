a9cc88094f5d0bff4252282e7ee175baa52e011b
Yen-Chia Lin <buju77@gmail.com>
2023-07-04 18:45:10 +0200

    Fixed a bug where turning on and off the rendering of markdown files in Workspace config wouldn't turn off rendering properly and would stay in read-only mode (fixes #5196) (#5261)
    * Fixed a bug where turning on and off the rendering of markdown files in Workspace config wouldn't turn off rendering properly and would stay in read-only mode (fixes #5196)
    
    * chore: remove bugfix comment
    
    * remove trailing whitespace
    
    ---------
    
    Co-authored-by: Daniele Formichelli <df@bendingspoons.com>

--- Sources/TuistKit/Mappers/Workspace/TuistWorkspaceRenderMarkdownMapper.swift
+++ Sources/TuistKit/Mappers/Workspace/TuistWorkspaceRenderMarkdownMapper.swift
@@ -5,13 +5,9 @@ import TuistSupport
 /// Tuist Workspace Markdown render Mapper.
 ///
 /// A mapper that includes a .xcodesample.plist file within the generated xcworkspace directory.
-/// This is used to render markdown inside the de workspace.
+/// This is used to render markdown inside the workspace.
 final class TuistWorkspaceRenderMarkdownReadmeMapper: WorkspaceMapping {
     func map(workspace: WorkspaceWithProjects) throws -> (WorkspaceWithProjects, [SideEffectDescriptor]) {
-        guard workspace.workspace.generationOptions.renderMarkdownReadme else {
-            return (workspace, [])
-        }
-
         let tuistGeneratedFileDescriptor = FileDescriptor(
             path: workspace
                 .workspace
@@ -19,7 +15,8 @@ final class TuistWorkspaceRenderMarkdownReadmeMapper: WorkspaceMapping {
                 .appending(
                     component: ".xcodesamplecode.plist"
                 ),
-            contents: try PropertyListEncoder().encode([String]())
+            contents: try PropertyListEncoder().encode([String]()),
+            state: workspace.workspace.generationOptions.renderMarkdownReadme ? .present : .absent
         )
 
         return (workspace, [
