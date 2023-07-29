a1b4874c234f459ee56f81e93f8d5fdb5e4d601f
fortmarek <marekfort@me.com>
2023-07-29 15:52:48 +0200

    Add projects index route

--- app/services/user_projects_fetch_service.rb
+++ app/services/user_projects_fetch_service.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Service for fetching projects that a user can access
 class UserProjectsFetchService < ApplicationService
   attr_reader :user
 
@@ -10,7 +11,7 @@ class UserProjectsFetchService < ApplicationService
 
   def call
     user_organization_ids = UserOrganizationsFetchService.call(user: user).pluck(:id)
-    account_ids = Account.where(owner_id: user_organization_ids, owner_type: "Organization").pluck(:id)
+    account_ids = Account.where(owner_id: user_organization_ids, owner_type: 'Organization').pluck(:id)
     account_ids.push(Account.find_by(owner: user).id)
     Project.where(account_id: account_ids)
   end
