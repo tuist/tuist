e6584df7f5cbef078370a0f4b6fdaa89624fb47e
fortmarek <marekfort@me.com>
2023-08-04 09:11:01 +0200

    Add endpoint to delete an organization

--- app/services/organization_fetch_service.rb
+++ app/services/organization_fetch_service.rb
@@ -1,14 +1,56 @@
 # frozen_string_literal: true
 
 class OrganizationFetchService < ApplicationService
-  attr_reader :name
+  module Error
+    class Unauthorized < CloudError
+      attr_reader :name
 
-  def initialize(name:)
+      def initialize(name)
+        @name = name
+      end
+
+      def message
+        "You do not have a permission to view the #{name} organization."
+      end
+
+      def status_code
+        :unauthorized
+      end
+    end
+
+    class OrganizationNotFound < CloudError
+      attr_reader :name
+
+      def initialize(name)
+        @name = name
+      end
+
+      def status_code
+        :not_found
+      end
+
+      def message
+        "Organization #{name} was not found."
+      end
+    end
+  end
+
+  attr_reader :name, :user
+
+  def initialize(name:, user:)
     super()
     @name = name
+    @user = user
   end
 
   def call
-    Account.find_by(name: name).owner
+    begin
+      organization = Account.find_by!(name: name).owner
+      raise Error::OrganizationNotFound.new(name) unless organization.is_a?(Organization)
+    rescue ActiveRecord::RecordNotFound
+      raise Error::OrganizationNotFound.new(name)
+    end
+    raise Error::Unauthorized.new(name) unless OrganizationPolicy.new(user, organization).show?
+    organization
   end
 end
--- test/services/organization_fetch_service_test.rb
+++ test/services/organization_fetch_service_test.rb
@@ -5,13 +5,37 @@ require "test_helper"
 class OrganizationFetchServiceTest < ActiveSupport::TestCase
   test "fetches an organization with a given name" do
     # Given
+    user = User.create!(email: "test@cloud.tuist.io", password: Devise.friendly_token.first(16))
     organization = Organization.create!
     account = Account.create!(owner: organization, name: "tuist")
+    user.add_role(:user, organization)
 
     # When
-    got = OrganizationFetchService.call(name: account.name)
+    got = OrganizationFetchService.call(name: account.name, user: user)
 
     # Then
     assert_equal organization, got
+  end
+
+  test "fails with organization not found if the organization does not exist" do
+    # Given
+    user = User.create!(email: "test@cloud.tuist.io", password: Devise.friendly_token.first(16))
+
+    # When / Then
+    assert_raises(OrganizationFetchService::Error::OrganizationNotFound) do
+      OrganizationFetchService.call(name: "non-existent-name", user: user)
+    end
+  end
+
+  test "fails with unauthorized error if the user does not have the rights to access the organization" do
+    # Given
+    user = User.create!(email: "test@cloud.tuist.io", password: Devise.friendly_token.first(16))
+    organization = Organization.create!
+    account = Account.create!(owner: organization, name: "tuist")
+
+    # When / Then
+    assert_raises(OrganizationFetchService::Error::Unauthorized) do
+      OrganizationFetchService.call(name: account.name, user: user)
+    end
   end
 end
