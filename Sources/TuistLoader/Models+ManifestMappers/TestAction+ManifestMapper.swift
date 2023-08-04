094b171c880192c248a45d205e5b14921199ca4e
Stéphane Copin <7003579+stephanecopin@users.noreply.github.com>
2023-08-04 13:48:55 -0400

    Refactor TestPlan manifest init into its own file

--- Sources/TuistLoader/Models+ManifestMappers/TestAction+ManifestMapper.swift
+++ Sources/TuistLoader/Models+ManifestMappers/TestAction+ManifestMapper.swift
@@ -89,26 +89,3 @@ extension TuistGraph.TestAction {
         )
     }
 }
-
-extension TestPlan {
-    init(path: AbsolutePath, isDefault: Bool, generatorPaths: GeneratorPaths) throws {
-        let jsonDecoder = JSONDecoder()
-        let testPlanData = try Data(contentsOf: path.asURL)
-        let xcTestPlan = try jsonDecoder.decode(XCTestPlan.self, from: testPlanData)
-
-        try self.init(
-            path: path,
-            testTargets: xcTestPlan.testTargets.map { testTarget in
-                try TestTarget(
-                    target: TargetReference(
-                        projectPath: generatorPaths.resolve(path: .relativeToRoot(testTarget.target.projectPath))
-                            .removingLastComponent(),
-                        name: testTarget.target.name
-                    ),
-                    isEnabled: testTarget.enabled
-                )
-            },
-            isDefault: isDefault
-        )
-    }
-}
---
+++ Sources/TuistLoader/Models+ManifestMappers/TestPlan+ManifestMapper.swift
@@ -0,0 +1,27 @@
+import Foundation
+import TSCBasic
+import TuistGraph
+
+extension TestPlan {
+    init(path: AbsolutePath, isDefault: Bool, generatorPaths: GeneratorPaths) throws {
+        let jsonDecoder = JSONDecoder()
+        let testPlanData = try Data(contentsOf: path.asURL)
+        let xcTestPlan = try jsonDecoder.decode(XCTestPlan.self, from: testPlanData)
+
+        try self.init(
+            path: path,
+            testTargets: xcTestPlan.testTargets.map { testTarget in
+                try TestTarget(
+                    target: TargetReference(
+                        projectPath: generatorPaths.resolve(path: .relativeToRoot(testTarget.target.projectPath))
+                            .removingLastComponent(),
+                        name: testTarget.target.name
+                    ),
+                    isEnabled: testTarget.enabled
+                )
+            },
+            isDefault: isDefault
+        )
+    }
+}
+ 
