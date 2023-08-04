5517b361dc2269d9db366aace5ae115b4c29280a
fortmarek <marekfort@me.com>
2023-08-04 16:35:13 +0200

    Add route to get organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -24,6 +24,34 @@ class Organization < ApplicationRecord
   end
 
   def as_json(options = {})
-    super(options.merge(only: [:id])).merge({ name: name })
+    members = []
+    members
+    .concat(
+      admins.map { |admin| OrganizationMember.new(id: admin.id, name: admin.account.name, email: admin.email, role: :admin) }
+    )
+    .concat(
+      users.map { |user| OrganizationMember.new(id: user.id, name: user.account.name, email: user.email, role: :user) }
+    )
+    super(options.merge(only: [:id])).merge({ name: name, members: members })
+  end
+end
+
+class OrganizationMember
+  attr_reader :id, :name, :email, :role
+
+  def initialize(id:, name:, email:, role:)
+    @id = id
+    @name = name
+    @email = email
+    @role = role
+  end
+
+  def as_json()
+    {
+      id: id,
+      name: name,
+      email: email,
+      role: role
+    }
   end
 end
