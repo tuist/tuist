ef8d06d97a9602d074b4ad1f50ca2c3a79222530
fortmarek <marekfort@me.com>
2023-08-15 23:39:34 +0200

    Add svg to precompilation

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w[stylesheets/application.tailwind.css]
+Rails.application.config.assets.precompile += ['*.svg']
