4929f7c5541def33a5a8add06756ea48e373e5d7
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2023-08-16 22:56:15 +0300

    format

--- Sources/TuistSupport/Extensions/String+Extras.swift
+++ Sources/TuistSupport/Extensions/String+Extras.swift
@@ -123,20 +123,20 @@ extension String {
 
         return ([first] + rest).joined(separator: "")
     }
-    
+
     /// Make the string a valid Swift identifier (class name)
     public func toValidSwiftIdentifier() -> String {
         // Step 1: Start with a capital letter
         let capitalized = camelized.uppercasingFirst
-        
+
         // Step 2: Remove invalid characters with underscores
         let sanitized = capitalized.replacingOccurrences(of: "[^A-Za-z0-9_]", with: "", options: .regularExpression)
-        
+
         // Step 3: Add underscore prefix if the string starts with a number
         if sanitized.first?.isNumber == true {
             return "_" + sanitized
         }
-        
+
         return sanitized
     }
 
