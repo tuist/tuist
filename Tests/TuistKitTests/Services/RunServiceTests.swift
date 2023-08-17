1fcb4fe940b43ff47b6fc26bbad5c3c89a7c3160
Mike Simons <msimons@etsy.com>
2023-08-17 13:57:43 -0400

    Fix warnings around waiting on expectations

--- Tests/TuistKitTests/Services/RunServiceTests.swift
+++ Tests/TuistKitTests/Services/RunServiceTests.swift
@@ -83,7 +83,7 @@ final class RunServiceTests: TuistUnitTestCase {
         buildGraphInspector.runnableTargetStub = { _, _ in .test() }
 
         try await subject.run(generate: true)
-        await waitForExpectations(timeout: 1)
+        await fulfillment(of: [expectation])
     }
 
     func test_run_generates_when_workspaceNotFound() async throws {
@@ -102,7 +102,7 @@ final class RunServiceTests: TuistUnitTestCase {
 
         // When
         try await subject.run()
-        await waitForExpectations(timeout: 1)
+        await fulfillment(of: [expectation])
     }
 
     func test_run_buildsTarget() async throws {
@@ -132,7 +132,7 @@ final class RunServiceTests: TuistUnitTestCase {
             clean: clean,
             configuration: configuration
         )
-        await waitForExpectations(timeout: 1)
+        await fulfillment(of: [expectation])
     }
 
     func test_run_runsTarget() async throws {
@@ -171,7 +171,8 @@ final class RunServiceTests: TuistUnitTestCase {
             version: version.description,
             arguments: arguments
         )
-        await waitForExpectations(timeout: 1)
+
+        await fulfillment(of: [expectation])
     }
 
     func test_run_throws_beforeBuilding_if_cantRunTarget() async throws {
@@ -192,7 +193,7 @@ final class RunServiceTests: TuistUnitTestCase {
             try await subject.run(),
             TestError()
         )
-        await waitForExpectations(timeout: 1)
+        await fulfillment(of: [expectation])
     }
 }
 
