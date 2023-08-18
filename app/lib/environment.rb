f27893c33c2591535efbe01b5ca2f8605209bd0e
Pedro <pedro@pepicrft.me>
2023-08-18 10:39:48 +0200

    Add Environment.self_hosted? interface to check if the app is running in a self-hosted setup

--- app/lib/environment.rb
+++ app/lib/environment.rb
@@ -4,6 +4,10 @@ module Environment
   TRUTHY_VALUES = ["1", "true", "TRUE", "yes", "YES"]
 
   class << self
+    def self_hosted?(env: ENV)
+      truthy?(env["TUIST_CLOUD_SELF_HOSTED"])
+    end
+
     def use_env_variables?(env: ENV)
       truthy?(env["TUIST_CLOUD_ENV_VARIABLES"])
     end
