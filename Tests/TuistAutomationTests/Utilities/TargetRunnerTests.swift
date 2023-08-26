60839a3745b696409901364b31e1fa37b3570154
Mike Simons <waltflanagan@users.noreply.github.com>
2023-08-26 05:15:39 -0400

    Cleanup warnings introduced in Xcode 14.3.1 (#5362)
    * Adding missing imports to clean up Xcode 14 warnings
    
    * Fix warnings about heterogeneous collections
    
    * Fix warnings around waiting on expectations
    
    * Add timeout to await

--- Tests/TuistAutomationTests/Utilities/TargetRunnerTests.swift
+++ Tests/TuistAutomationTests/Utilities/TargetRunnerTests.swift
@@ -107,7 +107,7 @@ final class TargetRunnerTests: TuistUnitTestCase {
             arguments: []
         )
 
-        await waitForExpectations(timeout: 1.0)
+        await fulfillment(of: [expectation], timeout: 1)
     }
 
     func test_runsExecutable_when_platform_is_macOS_and_product_is_commandLineTool() async throws {
