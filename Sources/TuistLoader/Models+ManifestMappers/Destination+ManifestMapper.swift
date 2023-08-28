2ef1ab92193f250da6e7f6516818d0df4311ae90
Mike Simons <simonsm2@gmail.com>
2023-08-28 10:42:49 -0400

    Initial addition of multiplatform support in ProjectDescription

--- Sources/TuistLoader/Models+ManifestMappers/Destination+ManifestMapper.swift
+++ Sources/TuistLoader/Models+ManifestMappers/Destination+ManifestMapper.swift
@@ -8,6 +8,39 @@ extension TuistGraph.Destination {
     ///   - manifest: Manifest representation of Package.
     ///   - generatorPaths: Generator paths.
     static func from(
+        destinations: ProjectDescription.Destinations
+    ) throws -> TuistGraph.Destinations {
+        let mappedDestinations: [TuistGraph.Destination] = destinations.map { destination in
+            switch destination {
+            case .iPhone:
+                return .iPhone
+            case .iPad:
+                return .iPad
+            case .mac:
+                return .mac
+            case .macWithiPadDesign:
+                return .macWithiPadDesign
+            case .macCatalyst:
+                return .macCatalyst
+            case .appleWatch:
+                return .appleWatch
+            case .appleTv:
+                return .appleTv
+            case .appleVision:
+                return .appleVision
+            case .appleVisionWithiPadDesign:
+                return .appleVisionWithiPadDesign
+            }
+        }
+
+        return Set(mappedDestinations)
+    }
+
+    /// Maps a ProjectDescription.Package instance into a TuistGraph.Package model.
+    /// - Parameters:
+    ///   - manifest: Manifest representation of Package.
+    ///   - generatorPaths: Generator paths.
+    static func from(
         platform: ProjectDescription.Platform,
         deploymentTarget: ProjectDescription.DeploymentTarget?
     ) throws -> TuistGraph.Destinations {
