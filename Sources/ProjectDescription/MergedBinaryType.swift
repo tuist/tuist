fa98544d8f13d5f78ef4d2e25314b11cd06a79e3
Alberto Garcia <45308839+AlbGarciam@users.noreply.github.com>
2023-09-06 14:58:45 +0200

    Update Sources/ProjectDescription/MergedBinaryType.swift
    Co-authored-by: Daniele Formichelli <df@bendingspoons.com>

--- Sources/ProjectDescription/MergedBinaryType.swift
+++ Sources/ProjectDescription/MergedBinaryType.swift
@@ -2,7 +2,7 @@
 public enum MergedBinaryType: Equatable, Codable {
     /// Target is never going to merge available dependencies
     case disabled
-    /// Target is going to merge direct target dependencies (just the ones declared as part of it's project)
+    /// Target is going to merge direct target dependencies (just the ones declared as part of its project)
     case automatic
     /// Target is going to merge direct and specified dependencies that are not part of the project.
     case manual(mergeableDependencies: Set<String>)
