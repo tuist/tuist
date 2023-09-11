940a80ef9b4c7a47dbe23debf614fc64eaeff2b6
Pedro <pedro@pepicrft.me>
2023-09-11 18:47:25 +0200

    Fix linting issues

--- projects/tuist/fixtures/app_with_spm_dependencies/StringifyMacro/Sources/StringifyMacros/StringifyMacro.swift
+++ projects/tuist/fixtures/app_with_spm_dependencies/StringifyMacro/Sources/StringifyMacros/StringifyMacro.swift
@@ -15,7 +15,7 @@ import SwiftSyntaxMacros
 public struct StringifyMacro: ExpressionMacro {
     public static func expansion(
         of node: some FreestandingMacroExpansionSyntax,
-        in context: some MacroExpansionContext
+        in _: some MacroExpansionContext
     ) -> ExprSyntax {
         guard let argument = node.argumentList.first?.expression else {
             fatalError("compiler bug: the macro does not have any arguments")
