4e968ab4f419abdb919ce0d0b503eae786c2289a
Pedro Piñera Buendía <663605+pepicrft@users.noreply.github.com>
2023-09-12 07:24:56 +0200

    Fix warm caching failing due to a wrong assumption around Mac Catalyst (#5399)
    * Not compile for Mac Catalyst when warming the cache if the graph doesn't support it
    
    * Update Sources/TuistCore/Graph/GraphTraverser.swift
    
    Co-authored-by: Alex Ozun <alex.ozun@gmail.com>
    
    * Apply recommendations
    
    * Fix test
    
    * Fix linting issues
    
    * Fix tests
    
    * Fix linting issues
    
    ---------
    
    Co-authored-by: Alex Ozun <alex.ozun@gmail.com>
    Co-authored-by: Daniele Formichelli <df@bendingspoons.com>

--- Sources/TuistCore/Graph/GraphTraverser.swift
+++ Sources/TuistCore/Graph/GraphTraverser.swift
@@ -189,6 +189,21 @@ public class GraphTraverser: GraphTraversing {
             .first { $0.target.product == .appClip }
     }
 
+    public func buildsForMacCatalyst(path: AbsolutePath, name: String) -> Bool {
+        guard target(path: path, name: name)?.target.supportsCatalyst ?? false else {
+            return false
+        }
+        return allDependenciesSatisfy(from: .target(name: name, path: path)) { dependency in
+            if let target = self.target(from: dependency) {
+                return target.target.supportsCatalyst
+            } else {
+                // TODO: - Obtain this information from pre-compiled binaries
+                // lipo -info should include "macabi" in the list of architectures
+                return false
+            }
+        }
+    }
+
     public func directStaticDependencies(path: AbsolutePath, name: String) -> Set<GraphDependencyReference> {
         Set(
             graph.dependencies[.target(name: name, path: path)]?
@@ -571,6 +586,17 @@ public class GraphTraverser: GraphTraversing {
         }
 
         return references
+    }
+
+    func allDependenciesSatisfy(from rootDependency: GraphDependency, meets: (GraphDependency) -> Bool) -> Bool {
+        var allSatisfy = true
+        _ = filterDependencies(from: rootDependency, test: { dependency in
+            if !meets(dependency) {
+                allSatisfy = false
+            }
+            return true
+        })
+        return allSatisfy
     }
 
     func transitiveStaticDependencies(from dependency: GraphDependency) -> Set<GraphDependency> {
