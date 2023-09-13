004767d81994f8d16784ac6a0c9d0a8a4fa284ba
Pedro <pedro@pepicrft.me>
2023-09-13 11:54:22 +0200

    Add a unique index to accounts#customer_id

--- db/migrate/20230829211134_stripe_plan.rb
+++ db/migrate/20230829211134_stripe_plan.rb
@@ -2,5 +2,6 @@ class StripePlan < ActiveRecord::Migration[7.0]
   def change
     add_column(:accounts, :customer_id, :string, default: nil)
     add_column(:accounts, :plan, :integer, default: nil)
+    add_index(:accounts, :customer_id, unique: true)
   end
 end
