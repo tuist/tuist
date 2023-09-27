ead22b15b349a1e196e626022de2b370e6ebec40
Pedro <pedro@pepicrft.me>
2023-09-27 16:07:45 +0200

    Delete app_with_carthage_dependencies acceptance test

--- projects/tuist/fixtures/app_with_carthage_dependencies/Tuist/Dependencies.swift
+++
@@ -1,9 +0,0 @@
-import ProjectDescription
-
-let dependencies = Dependencies(
-    carthage: [
-        .github(path: "Alamofire/Alamofire", requirement: .exact("5.8.0")),
-        .github(path: "Quick/Nimble", requirement: .exact("12.0.0")),
-    ],
-    platforms: [.iOS]
-)
