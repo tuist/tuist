0219e7d9a1b062a29f859f6c9ea59d855e702fbd
Daniele Formichelli <df@bendingspoons.com>
2023-10-03 11:05:28 +0200

    chore: vendor swiftlint using SPM (#5465)

--- make/tasks/lint.sh
+++ make/tasks/lint.sh
@@ -6,4 +6,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR=$($SCRIPT_DIR/../utilities/root_dir.sh)
 
 $ROOT_DIR/projects/fourier/vendor/swiftformat/swiftformat $ROOT_DIR --lint
-$ROOT_DIR/projects/fourier/vendor/swiftlint/swiftlint --quiet --config $ROOT_DIR/.swiftlint.yml $ROOT_DIR/Sources
+swift run --package-path $ROOT_DIR/projects/tuist/vendor swiftlint lint --quiet --config $ROOT_DIR/.swiftlint.yml $ROOT_DIR/Sources
---
+++ projects/tuist/vendor/Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "tuistvendor",
+    platforms: [.macOS(.v12)],
+    products: [],
+    dependencies: [
+        .package(url: "https://github.com/realm/SwiftLint", from: "0.53.0"),
+    ],
+    targets: []
+)
