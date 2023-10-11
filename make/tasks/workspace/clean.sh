31e9401bac5a572abdbc5c6d926557c679565c3a
Pedro <pedro@pepicrft.me>
2023-10-11 17:55:41 +0200

    Fix clean script

--- make/tasks/workspace/clean.sh
+++ make/tasks/workspace/clean.sh
@@ -7,14 +7,14 @@ ROOT_DIR=$($SCRIPT_DIR/../../utilities/root_dir.sh)
 DERIVED_DATA_PATH=$($ROOT_DIR/make/utilities/derived_data_path.sh)
 
 rm -rf $ROOT_DIR/.build
-for dir in "$DERIVED_DATA_PATH"/tuist-*; do
+for dir in "$DERIVED_DATA_PATH"tuist-*; do
     # Check if it is a directory before deleting
     if [[ -d "$dir" ]]; then
         echo "Deleting directory: $dir"
         rm -rf "$dir"
     fi
 done
-for dir in "$DERIVED_DATA_PATH"/Tuist-*; do
+for dir in "$DERIVED_DATA_PATH"Tuist-*; do
     # Check if it is a directory before deleting
     if [[ -d "$dir" ]]; then
         echo "Deleting directory: $dir"
