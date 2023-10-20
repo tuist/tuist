43252e9ba6d12854bb2a8262013c6a03e2b0c24d
Daniele Formichelli <df@bendingspoons.com>
2023-10-20 08:12:09 +0200

    chore: simplify Makefile

--- make/tasks/workspace/lint-fix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ROOT_DIR=$($SCRIPT_DIR/../../utilities/root_dir.sh)
-
-swift run --package-path $ROOT_DIR/projects/tuist/vendor swiftformat $ROOT_DIR
-swift run --package-path $ROOT_DIR/projects/tuist/vendor swiftlint lint --fix --quiet --config $ROOT_DIR/.swiftlint.yml $ROOT_DIR/Sources
