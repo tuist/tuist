95baef2bd689d402821aab59295a9828fdda36b0
Elon Park <dev.sungwoon@gmail.com>
2023-10-22 22:20:02 +0900

    test: add ios_app_with_custom_resource_parser_options fixture

---
+++ projects/tuist/fixtures/ios_app_with_custom_resource_parser_options/Project.swift
@@ -0,0 +1,24 @@
+import ProjectDescription
+
+let project = Project(
+    name: "App",
+    options: .options(
+        developmentRegion: "fr"
+    ),
+    targets: [
+        Target(
+            name: "App",
+            platform: .iOS,
+            product: .app,
+            bundleId: "io.tuist.app",
+            infoPlist: .default,
+            sources: ["App/Sources/**"],
+            resources: [
+                "App/Resources/**/*.strings",
+            ]
+        ),
+    ],
+    resourceSynthesizers: [
+        .strings(parserOptions: ["separator": .stringValue("/")]),
+    ]
+)
