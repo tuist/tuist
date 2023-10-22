4dabb9293f549c5296ff650124e5c7b926d3bcbc
Elon Park <dev.sungwoon@gmail.com>
2023-10-22 22:17:58 +0900

    feat: add ResourceSynthesizer parserOptions in ProjectDescription

--- Tests/ProjectDescriptionTests/ResourceSynthesizerTests.swift
+++ Tests/ProjectDescriptionTests/ResourceSynthesizerTests.swift
@@ -16,11 +16,18 @@ final class ResourceSynthesizerTests: XCTestCase {
         )
     }
 
+    func test_codable_when_parserOptions() {
+        XCTAssertCodable(
+            ResourceSynthesizer.strings(parserOptions: ["separator": .stringValue("/")])
+        )
+    }
+
     func test_codable_when_custom() {
         XCTAssertCodable(
             ResourceSynthesizer.custom(
                 name: "Custom",
                 parser: .json,
+                parserOptions: ["key": .stringValue("value")],
                 extensions: ["lottie"]
             )
         )
