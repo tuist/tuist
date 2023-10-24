e66d3dd0735fec009a7155711668d2f4ddae7806
Pedro <pedro@pepicrft.me>
2023-10-24 08:08:28 +0200

    Use pass instead

--- lib/warden_token_strategies.rb
+++ lib/warden_token_strategies.rb
@@ -14,7 +14,7 @@ Warden::Strategies.add(:project_token_authenticatable) do
     if project
       success!(project)
     else
-      skip!
+      pass()
     end
   end
 
@@ -35,7 +35,7 @@ Warden::Strategies.add(:user_token_authenticatable) do
     if user
       success!(user)
     else
-      skip!
+      pass()
     end
   end
 
