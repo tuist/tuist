6766041ab20193814106c80120b4a9cd6bc5c4ef
baegteun <baegteun@gmail.com>
2023-10-25 17:00:09 +0900

    Update missing the launchStyle mapping in RunAction+ManifestMapper

--- Sources/TuistGraph/Models/LaunchStyle.swift
+++ Sources/TuistGraph/Models/LaunchStyle.swift
@@ -1,6 +1,16 @@
 import Foundation
+import ProjectDescription
 
 public enum LaunchStyle: Codable {
     case automatically
     case waitForExecutableToBeLaunched
+
+    public static func from(launchStyle: ProjectDescription.LaunchStyle) -> TuistGraph.LaunchStyle {
+        switch launchStyle {
+        case .automatically:
+            return .automatically
+        case .waitForExecutableToBeLaunched:
+            return .waitForExecutableToBeLaunched
+        }
+    }
 }
