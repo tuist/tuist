798f3500ad9f0f800a9fc46f776c3b78cb7cb9e8
fortmarek <marekfort@me.com>
2023-10-26 15:54:06 +0200

    Run rubocop

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -2,7 +2,7 @@
 
 if Environment.tuist_hosted? && Environment.production_like_env?
   # Use AppSignal's logger and a STDOUT logger
-  console_logger = ActiveSupport::Logger.new(STDOUT)
+  console_logger = ActiveSupport::Logger.new($stdout)
   appsignal_logger = ActiveSupport::TaggedLogging.new(Appsignal::Logger.new("rails"))
   Rails.application.config.log_tags = [:request_id]
   Rails.logger = ActiveSupport::BroadcastLogger.new(console_logger, appsignal_logger)
