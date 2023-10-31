1ce5be3256ab5717c2b1c5a37554394599507688
Pedro Piñera Buendía <663605+pepicrft@users.noreply.github.com>
2023-10-31 20:35:24 +0100

    Delete router constraints (#331)
    I believe the `devise_for` inside constraints at the router is the
    culprit of the authentication issues some users are seeing. My guess (I
    couldn't reproduce it), is that `devise_for` is setting global state in
    the `Warden` global configuration, and since `devise_for` now executes
    every time a request reaches the server, the behavior is now dependent
    on the order of the requests reaching the server.
    To my surprise, I tried to reproduce the 401 issue that we were
    receiving when using a project token to authenticate and I was not able
    to reproduce it in canary.

--- config/routes.rb
+++ config/routes.rb
@@ -1,63 +1,50 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # API Constraint
-  constraints(lambda { |req| req.path.starts_with?('/api/') }) do
-    namespace :api do
-      get "/cache", to: "cache#show"
-      get "/cache/exists", to: "cache#exists"
-      post "/cache", to: "cache#upload_cache_artifact"
-      post "/cache/verify_upload", to: "cache#verify_upload"
-      put "/projects/:account_name/:project_name/cache/clean", to: "cache#clean"
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks",
+    sessions: "users/sessions",
+    registrations: "users/registrations",
+  }
+  root to: "application#app"
 
-      post "/analytics", to: "analytics#analytics"
-
-      resources :projects, :projects, only: [:create, :index, :destroy]
-      resources :organizations, :organizations, only: [:create, :index, :destroy, :show]
-      get '/projects/:account_name/:project_name', to: 'projects#show'
-      resources :invitations, path: '/organizations/:organization_name/invitations', only: [:create]
-      delete '/organizations/:organization_name/invitations', to: 'invitations#destroy'
-      delete '/organizations/:organization_name/members/:username', to: 'members#destroy'
-      put '/organizations/:organization_name/members/:username', to: 'members#update'
-    end
+  namespace :api do
+    delete '/organizations/:organization_name/invitations', to: 'invitations#destroy'
+    delete '/organizations/:organization_name/members/:username', to: 'members#destroy'
+    get '/projects/:account_name/:project_name', to: 'projects#show'
+    get "/cache", to: "cache#show"
+    get "/cache/exists", to: "cache#exists"
+    post "/analytics", to: "analytics#analytics"
+    post "/cache", to: "cache#upload_cache_artifact"
+    post "/cache/verify_upload", to: "cache#verify_upload"
+    put '/organizations/:organization_name/members/:username', to: 'members#update'
+    put "/projects/:account_name/:project_name/cache/clean", to: "cache#clean"
+    resources :invitations, path: '/organizations/:organization_name/invitations', only: [:create]
+    resources :organizations, :organizations, only: [:create, :index, :destroy, :show]
+    resources :projects, :projects, only: [:create, :index, :destroy]
   end
 
-  constraints(lambda { |req| !req.path.starts_with?('/api/') }) do
-    # GraphQL
-    devise_for :users, controllers: {
-      omniauth_callbacks: "users/omniauth_callbacks",
-      sessions: "users/sessions",
-      registrations: "users/registrations",
-    }
-
-    # GraphiQL
-    if Rails.env.development?
-      mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-    end
-    post "/graphql", to: "graphql#execute"
-
-    root to: "application#app"
-
-    get "/get-started", to: "application#get_started"
-    post "/create-customer-portal-session/:account_id", to: "organizations#billing_plan"
-
-    resources :organizations, param: :name, only: [:index] do
-      get 'billing/plan', to: "organizations#billing_plan"
-    end
-
-    resources :projects, path: '/:account_name', param: :project_name, only: [:show] do
-    end
-
-    resources :projects, path: '/:account_name', param: :name, only: [] do
-      get 'analytics', to: 'analytics#analytics'
-      get 'analytics/targets', to: 'analytics#analytics_targets'
-    end
-
-    get "/auth/invitations/:token", to: "auth#accept_invitation"
-    get "/auth/cli/success", to: "auth#cli_success"
-    get "/auth", to: "auth#authenticate"
-    post '/webhooks/stripe', to: 'webhooks#stripe'
-
-    get "/(*all)", to: "application#app"
+  resources :organizations, param: :name, only: [:index] do
+    get 'billing/plan', to: "organizations#billing_plan"
   end
+
+  resources :projects, path: '/:account_name', param: :name, only: [] do
+    get 'analytics', to: 'analytics#analytics'
+    get 'analytics/targets', to: 'analytics#analytics_targets'
+  end
+
+  get "/auth/invitations/:token", to: "auth#accept_invitation"
+  get "/auth/cli/success", to: "auth#cli_success"
+  get "/auth", to: "auth#authenticate"
+  post '/webhooks/stripe', to: 'webhooks#stripe'
+  resources :projects, path: '/:account_name', param: :project_name, only: [:show]
+  get "/get-started", to: "application#get_started"
+  post "/create-customer-portal-session/:account_id", to: "organizations#billing_plan"
+  post "/graphql", to: "graphql#execute"
+
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
+  end
+
+  get "/(*all)", to: "application#app"
 end
