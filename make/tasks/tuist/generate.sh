f438fd6e4417a77395ea0cd34e54ca869b113e05
Daniele Formichelli <df@bendingspoons.com>
2023-11-01 17:20:38 +0100

    Simplify Makefile (#5514)
    * chore: simplify Makefile
    
    * chore: add back `make workspace/lint-fix`

--- make/tasks/tuist/generate.sh
+++ make/tasks/tuist/generate.sh
@@ -6,4 +6,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR=$($SCRIPT_DIR/../../utilities/root_dir.sh)
 
 swift build --package-path $ROOT_DIR
+$ROOT_DIR/.build/debug/tuist fetch --path $ROOT_DIR
+if [ -f TuistCloud ]; then
+    $ROOT_DIR/.build/debug/tuist cache warm --path $ROOT_DIR --xcframeworks $@
+fi
 $ROOT_DIR/.build/debug/tuist generate --path $ROOT_DIR --xcframeworks $@
--- make/tasks/workspace/up.sh
+++ make/tasks/workspace/up.sh
@@ -6,18 +6,19 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR=$($SCRIPT_DIR/../../utilities/root_dir.sh)
 REPO_URL="git@github.com:tuist/TuistCloud.git"
 CLONE_DIR=$ROOT_DIR/TuistCloud
-COMMIT_SHA=$(cat $ROOT_DIR/.tuist-cloud-version) 
+COMMIT_SHA=$(cat $ROOT_DIR/.tuist-cloud-version)
 
 # Assigns a default value
 : "${TUIST_INCLUDE_TUIST_CLOUD:=0}"
 
-if [[ "${TUIST_INCLUDE_TUIST_CLOUD}" == "1" ]]; then    
+if [[ "${TUIST_INCLUDE_TUIST_CLOUD}" == "1" ]]; then
     # Check if 'cloud' directory exists
     if [ ! -d "$CLONE_DIR" ]; then
         echo "Directory $CLONE_DIR does not exist. Cloning the repository..."
         git clone "$REPO_URL" "$CLONE_DIR"
     else
-        echo "Directory $CLONE_DIR already exists. Skipping clone."
+        echo "Directory $CLONE_DIR already exists. Fetching the repository clone."
+        git --git-dir="$CLONE_DIR/.git" fetch
     fi
 
     # Discard all local changes and checkout the specific commit SHA
