49fa44ca94db8990bee8fbba08426dde6e90c645
Pedro <pedro@pepicrft.me>
2023-11-01 16:27:01 +0100

    Add dependencies

--- projects/tuist/fixtures/framework_with_native_swift_macro/Tuist/Package.swift
+++ projects/tuist/fixtures/framework_with_native_swift_macro/Tuist/Package.swift
@@ -4,6 +4,6 @@ import PackageDescription
 let package = Package(
     name: "FrameworkWithNativeSwiftMacro",
     dependencies: [
-        .remote(url: "https://github.com/alschmut/StructBuilderMacro.git", requirement: .exact("0.2.0"))
+        .package(url: "https://github.com/alschmut/StructBuilderMacro.git", .exact("0.2.0"))
     ]
 )
