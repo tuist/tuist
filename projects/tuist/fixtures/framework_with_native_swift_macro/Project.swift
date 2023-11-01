11dd435bfc2fd4d58896ce6eb6e3bd46fbb90287
Pedro <pedro@pepicrft.me>
2023-11-01 16:39:33 +0100

    Get project generation working

--- projects/tuist/fixtures/framework_with_native_swift_macro/Project.swift
+++ projects/tuist/fixtures/framework_with_native_swift_macro/Project.swift
@@ -2,9 +2,6 @@ import ProjectDescription
 
 let project = Project(
     name: "FrameworkWithSwiftMacro",
-    packages: [
-        .remote(url: "https://github.com/alschmut/StructBuilderMacro.git", requirement: .exact("0.2.0")),
-    ],
     targets: [
         Target(
             name: "Framework",
@@ -13,7 +10,7 @@ let project = Project(
             bundleId: "io.tuist.FrameworkWithSwiftMacro",
             sources: ["Sources/**/*"],
             dependencies: [
-                .packageMacro(product: "StructBuilder"),
+                .external(name: "StructBuilder")
             ]
         ),
     ]
