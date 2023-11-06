d7b44efee440e448feb198c842de246b4674cc14
Pedro <pedro@pepicrft.me>
2023-11-06 19:10:28 +0100

    Add missing vendor directory

---
+++ vendor/Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "tuistvendor",
+    platforms: [.macOS(.v12)],
+    products: [],
+    dependencies: [
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.52.6"),
+        .package(url: "https://github.com/realm/SwiftLint", from: "0.53.0"),
+        .package(url: "https://github.com/tuist/xcbeautify", from: "1.0.0"),
+    ],
+    targets: []
+)
