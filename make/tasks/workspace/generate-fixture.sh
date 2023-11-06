fef119a9686584eb20dab75a66b0937de4ff990e
Pedro <pedro@pepicrft.me>
2023-11-06 14:58:35 +0100

    Add a script to generate a fixture

---
+++ make/tasks/workspace/generate-fixture.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR=$($SCRIPT_DIR/../../utilities/root_dir.sh)
+
+path=$(pwd)
+projects=""
+targets=""
+sources=""
+
+# Function to display usage
+usage() {
+  echo "Usage: $0 --path <path> --projects <projects> --targets <targets> --sources <sources>"
+  exit 1
+}
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    --path) path="$2"; shift ;;
+    --projects) projects="$2"; shift ;;
+    --targets) targets="$2"; shift ;;
+    --sources) sources="$2"; shift ;;
+    *) echo "Unknown parameter passed: $1"; usage ;;
+  esac
+  shift
+done
+
+if [ -z "$projects" ] || [ -z "$targets" ] || [ -z "$sources" ]; then
+    usage
+fi
+
+echo "$(format_section "Generating fixture")"
+
+echo "Path: $path"
+echo "Projects: $projects"
+echo "Targets: $targets"
+echo "Sources: $sources"
+
+swift run --package-path $ROOT_DIR tuistfixturegenerator generate --path $path --projects $projects --targets $targets --sources $sources
--- projects/fourier/lib/fourier.rb
+++ projects/fourier/lib/fourier.rb
@@ -16,49 +16,6 @@ module Fourier
   class CLI < Thor
     class_option :verbose, type: :boolean
 
-    desc "test", "Run tests"
-    subcommand "test", Commands::Test
-
-    desc "fixture", "Generate a fixture"
-    option(
-      :path,
-      desc: "The path to the directory where the fixture will be generated",
-      type: :string,
-      required: false,
-      aliases: :p,
-      default: "Fixture",
-    )
-    option(
-      :projects,
-      desc: "The number of projects to generate",
-      type: :numeric,
-      required: true,
-      aliases: :P,
-    )
-    option(
-      :targets,
-      desc: "The number of targets to generate",
-      type: :numeric,
-      required: true,
-      aliases: :t,
-    )
-    option(
-      :sources,
-      desc: "The number of sources to generate",
-      type: :numeric,
-      required: true,
-      aliases: :s,
-    )
-    def fixture
-      path = File.expand_path(options[:path], Dir.pwd)
-      Services::Fixture.call(
-        path: path,
-        projects: options[:projects],
-        targets: options[:targets],
-        sources: options[:sources],
-      )
-    end
-
     desc "benchmark", "Benchmark Tuist"
     def benchmark
       Services::Benchmark.call
