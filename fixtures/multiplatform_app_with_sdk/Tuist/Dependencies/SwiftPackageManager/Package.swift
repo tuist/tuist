7b3a04fdf802dc71b616281832e723617151c527
Mike Simons <simonsm2@gmail.com>
2023-11-10 17:09:51 -0500

    Remove things that should be ignored

--- fixtures/multiplatform_app_with_sdk/Tuist/Dependencies/SwiftPackageManager/Package.swift
+++
@@ -1,9 +0,0 @@
-// swift-tools-version: 5.9
-import PackageDescription
-
-let package = Package(
-    name: "PackageName",
-    dependencies: [
-        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "10.15.0"),
-    ]
-)
