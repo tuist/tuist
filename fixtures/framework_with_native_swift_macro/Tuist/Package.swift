a419fd088b854c566a49a445dd8369c66ba9ca71
Pedro <pedro@pepicrft.me>
2023-11-16 11:11:17 +0100

    Configure CI to skip the Swift Macro fingerprint validation and adjust the fixture that uses Xcode integration to include the TCA macros too

--- fixtures/framework_with_native_swift_macro/Tuist/Package.swift
+++ fixtures/framework_with_native_swift_macro/Tuist/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "FrameworkWithNativeSwiftMacro",
     dependencies: [
-        .package(url: "https://github.com/alschmut/StructBuilderMacro.git", .exact("0.2.0")),
+        .package(url: "https://github.com/alschmut/StructBuilderMacro", .exact("0.2.0")),
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture", .exact("1.4.0"))
     ]
 )
--- fixtures/framework_with_swift_macro/Project.swift
+++ fixtures/framework_with_swift_macro/Project.swift
@@ -3,7 +3,8 @@ import ProjectDescription
 let project = Project(
     name: "FrameworkWithSwiftMacro",
     packages: [
-        .remote(url: "https://github.com/alschmut/StructBuilderMacro.git", requirement: .exact("0.2.0")),
+        .remote(url: "https://github.com/alschmut/StructBuilderMacro", requirement: .exact("0.2.0")),
+        .remote(url: "https://github.com/pointfreeco/swift-composable-architecture", requirement: .exact("1.4.0")),
     ],
     targets: [
         Target(
@@ -13,6 +14,8 @@ let project = Project(
             bundleId: "io.tuist.FrameworkWithSwiftMacro",
             sources: ["Sources/**/*"],
             dependencies: [
+                .package(product: "ComposableArchitecture", type: .macro),
+                .package(product: "CasePaths", type: .macro),
                 .package(product: "StructBuilder", type: .macro),
             ]
         ),
