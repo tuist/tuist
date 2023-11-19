026a235219c3fda5c353f8dbc30353831307d144
Daniele Formichelli <df@bendingspoons.com>
2023-11-19 17:07:33 +0100

    chore: restore Package.swift

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -1,1 +1,25 @@
-../Package.swift
+// swift-tools-version: 5.8
+import PackageDescription
+
+let package = Package(
+    name: "PackageName",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
+        .package(url: "https://github.com/apple/swift-log", from: "1.5.3"),
+        .package(url: "https://github.com/apple/swift-tools-support-core", from: "0.6.1"),
+        .package(url: "https://github.com/CombineCommunity/CombineExt", from: "1.8.1"),
+        .package(url: "https://github.com/FabrizioBrancati/Queuer", from: "2.1.1"),
+        .package(url: "https://github.com/Flight-School/AnyCodable", from: "0.6.7"),
+        .package(url: "https://github.com/weichsel/ZIPFoundation", from: "0.9.17"),
+        .package(url: "https://github.com/httpswift/swifter", revision: "1e4f51c92d7ca486242d8bf0722b99de2c3531aa"),
+        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess", from: "4.2.2"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "1.8.0"),
+        .package(url: "https://github.com/rnine/Checksum", from: "1.0.2"),
+        .package(url: "https://github.com/stencilproject/Stencil", from: "0.15.1"),
+        .package(url: "https://github.com/SwiftDocOrg/GraphViz", exact: "0.2.0"),
+        .package(url: "https://github.com/SwiftGen/StencilSwiftKit", from: "2.10.1"),
+        .package(url: "https://github.com/SwiftGen/SwiftGen", from: "6.6.2"),
+        .package(url: "https://github.com/tuist/XcodeProj", from: "8.15.0"),
+    ]
+)
