fba92b12025bcb49c1e38a4bf720fa32427e291b
Marek Fořt <marekfort@me.com>
2023-11-21 18:51:59 +0100

    Use asdf for linting and tests (#5598)
    * Use asdf for linting and tests
    
    * Remove vendor directory
    
    * Temporarily disable cache for tuist test

--- make/tasks/workspace/lint-fix.sh
+++ make/tasks/workspace/lint-fix.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR=$($SCRIPT_DIR/../../utilities/root_dir.sh)
 
-swift run --package-path $ROOT_DIR/vendor swiftformat $ROOT_DIR
-swift run --package-path $ROOT_DIR/vendor swiftlint lint --fix --quiet --config $ROOT_DIR/.swiftlint.yml $ROOT_DIR/Sources
+swiftformat $ROOT_DIR
+swiftlint lint --fix --quiet --config $ROOT_DIR/.swiftlint.yml $ROOT_DIR/Sources
