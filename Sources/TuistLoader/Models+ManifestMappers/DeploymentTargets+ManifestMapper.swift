8454c3699b12e15adb30fd1dcd84e414821a5d7f
Mike Simons <simonsm2@gmail.com>
2023-11-20 19:25:05 -0500

    Remove unused code

--- Sources/TuistLoader/Models+ManifestMappers/DeploymentTargets+ManifestMapper.swift
+++ Sources/TuistLoader/Models+ManifestMappers/DeploymentTargets+ManifestMapper.swift
@@ -16,26 +16,4 @@ extension TuistGraph.DeploymentTargets {
             visionOS: manifest.visionOS
         )
     }
-
-    /// Maps a ProjectDescription.DeploymentTarget instance into a TuistGraph.DeploymentTarget instance.
-    /// - Parameters:
-    ///   - manifest: Manifest representation of deployment target model.
-    static func from(manifest: ProjectDescription.DeploymentTarget?) -> TuistGraph.DeploymentTargets {
-        if let manifest {
-            switch manifest {
-            case let .iOS(version, _, _):
-                return .iOS(version)
-            case let .macOS(version):
-                return .macOS(version)
-            case let .watchOS(version):
-                return .watchOS(version)
-            case let .tvOS(version):
-                return .tvOS(version)
-            case let .visionOS(version):
-                return .visionOS(version)
-            }
-        } else {
-            return .empty()
-        }
-    }
 }
