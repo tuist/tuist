a9bf18a4c5c7ea9a4a229860c0a939567d8191d6
Daniel Bachar <danielbac@zillowgroup.com>
2023-11-27 11:31:40 -0700

    Adding tests

--- Sources/TuistSupport/Extensions/URL+Extras.swift
+++ Sources/TuistSupport/Extensions/URL+Extras.swift
@@ -5,10 +5,14 @@ extension URL {
         /**
          Xcode 15 introduced breaking changes in the URL constructor that we need to account for.
          */
+        if #available(macOS 14.0, *) {
         #if swift(>=5.9)
             return URL(string: url, encodingInvalidCharacters: false) != nil
         #else
             return URL(string: url) != nil
         #endif
+        } else {
+            return URL(string: url) != nil
+        }
     }
 }
