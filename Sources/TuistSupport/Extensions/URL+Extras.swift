3ba0814da7d5cee0662ae335881367f2e8005c76
Daniel Bachar <danielbac@zillowgroup.com>
2023-11-27 10:27:03 -0700

    The API check should actually be by the swift version and not macOS

--- Sources/TuistSupport/Extensions/URL+Extras.swift
+++ Sources/TuistSupport/Extensions/URL+Extras.swift
@@ -2,15 +2,13 @@ import Foundation
 
 extension URL {
     public static func isValid(_ url: String) -> Bool {
-        var valid = false
         /**
          Xcode 15 introduced breaking changes in the URL constructor that we need to account for.
          */
-        if #available(macOS 14.0, *) {
-            valid = URL(string: url, encodingInvalidCharacters: false) != nil
-        } else {
-            valid = URL(string: url) != nil
-        }
-        return valid
+        #if swift(>=5.9)
+            return URL(string: url, encodingInvalidCharacters: false) != nil
+        #else
+            return URL(string: url) != nil
+        #endif
     }
 }
