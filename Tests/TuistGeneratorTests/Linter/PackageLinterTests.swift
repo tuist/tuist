8e9b3454dc46f99b71153a2355134cde725a49b7
Daniel Bachar <danielbac@zillowgroup.com>
2023-11-27 13:56:59 -0700

    Remove unneeded validation for linter

--- Tests/TuistGeneratorTests/Linter/PackageLinterTests.swift
+++ Tests/TuistGeneratorTests/Linter/PackageLinterTests.swift
@@ -31,18 +31,6 @@ final class PackageLinterTests: TuistUnitTestCase {
         XCTContainsLintingIssue(got, LintingIssue(reason: "Package with local path (\(path)) does not exist.", severity: .error))
     }
 
-    func test_lint_when_a_remote_url_is_not_valid() {
-        let url = "\\NotValidUrl"
-        let package = Package.remote(url: url, requirement: Requirement.exact(""))
-
-        let got = subject.lint(package)
-
-        XCTContainsLintingIssue(
-            got,
-            LintingIssue(reason: "Package with remote URL (\(url)) does not have a valid URL.", severity: .error)
-        )
-    }
-
     func test_lint_when_a_local_path_exists() {
         let path = try! AbsolutePath(validating: "/")
         let package = Package.local(path: path)
