05c529300cf234a4e0b769bedb4e56ed5bc30a20
Pedro <pedro@pepicrft.me>
2023-12-09 12:44:17 +0100

    Use an #else clause

--- Sources/TuistLoader/ProjectDescriptionHelpers/ProjectDescriptionHelpersHasher.swift
+++ Sources/TuistLoader/ProjectDescriptionHelpers/ProjectDescriptionHelpersHasher.swift
@@ -32,9 +32,10 @@ public final class ProjectDescriptionHelpersHasher: ProjectDescriptionHelpersHas
             .compactMap { $0.compactMap { byte in String(format: "%02x", byte) }.joined() }
         let tuistEnvVariables = Environment.shared.manifestLoadingVariables.map { "\($0.key)=\($0.value)" }.sorted()
         let swiftVersion = try System.shared.swiftVersion()
-        var debug = false
         #if DEBUG
-            debug = true
+            let debug = true
+        #else
+            let debug = false
         #endif
 
         let identifiers = [swiftVersion, tuistVersion] + fileHashes + tuistEnvVariables + ["\(debug)"]
