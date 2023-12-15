c99c83184fdd5ffc19205bc21b855ae27f31bae0
Pedro Piñera Buendía <663605+pepicrft@users.noreply.github.com>
2023-12-15 16:26:09 +0100

    Update make/tasks/docs/preview.sh
    Co-authored-by: Daniele Formichelli <df@bendingspoons.com>

--- make/tasks/docs/preview.sh
+++ make/tasks/docs/preview.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR=$($SCRIPT_DIR/../../utilities/root_dir.sh)
 
-swift package --package-path $ROOT_DIR --disable-sandbox preview-documentation --target tuist  --hosting-base-path /
+swift package --package-path $ROOT_DIR --disable-sandbox preview-documentation --target tuist --hosting-base-path /
