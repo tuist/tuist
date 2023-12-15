cf60cd253596aaf9197064e2b9326ae78bff8aba
Pedro <pedro@pepicrft.me>
2023-12-15 16:54:06 +0100

    Add a warning to tell users to update their installation method

--- Sources/tuistenv/main.swift
+++ Sources/tuistenv/main.swift
@@ -5,4 +5,13 @@ import TuistSupport
 try TuistSupport.Environment.shared.bootstrap()
 LogOutput.bootstrap()
 
+WarningController.shared.append(warning: """
+The method used to install this version of Tuist is deprecated and will be deleted soon.
+Please, uninstall it by running:
+
+    curl -Ls https://uninstall.tuist.io | bash
+
+And follow the new installation instructions at https://github.com/tuist/tuist#recommended-rtx
+""")
+
 TuistCommand.main()
