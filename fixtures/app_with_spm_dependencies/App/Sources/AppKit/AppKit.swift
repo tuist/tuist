9671063a2190d1c95bbc7b82d87b23453a9f7ffa
Daniele Formichelli <df@bendingspoons.com>
2023-12-30 16:52:44 +0100

    chore: format

--- fixtures/app_with_spm_dependencies/App/Sources/AppKit/AppKit.swift
+++ fixtures/app_with_spm_dependencies/App/Sources/AppKit/AppKit.swift
@@ -5,7 +5,7 @@ public enum AppKit {
     public static func start() {
         // Use Alamofire to make sure it links fine
         _ = AF.download("http://www.tuist.io")
-      
+
         // Use ComposableArchitecture to make sure it links fine
         _ = EmptyReducer<Never, Never>()
     }
