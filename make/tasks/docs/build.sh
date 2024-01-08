76f84e128b482ca3e3665f06edda712244d1da48
Pedro <pedro@pepicrft.me>
2024-01-08 09:44:36 +0100

    Use netlify.toml instead

--- make/tasks/docs/build.sh
+++ make/tasks/docs/build.sh
@@ -6,9 +6,17 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR=$($SCRIPT_DIR/../../utilities/root_dir.sh)
 
 swift package --package-path $ROOT_DIR --allow-writing-to-directory .build/documentation generate-documentation --target tuist --disable-indexing --output-path .build/documentation --transform-for-static-hosting
-echo "/index.html	/documentation/tuist" > "$ROOT_DIR/.build/documentation/_redirects"
 cp $ROOT_DIR/assets/favicon.ico $ROOT_DIR/.build/documentation/favicon.ico
 cp $ROOT_DIR/assets/favicon.svg $ROOT_DIR/.build/documentation/favicon.svg
 
-REDIRECT_LINE="/                /documentation/tuist"
-echo "$REDIRECT_LINE" > "$ROOT_DIR/.build/documentation/_redirects"
+cat <<EOL > $ROOT_DIR/.build/documentation/netlify.toml
+[[redirects]]
+  from = "/index.html"
+  to = "/documentation/tuist"
+  status = 301
+
+[[redirects]]
+  from = "/"
+  to = "/documentation/tuist"
+  status = 301
+EOL
