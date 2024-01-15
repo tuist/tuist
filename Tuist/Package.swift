053667052405e93a820760691bd5221949af00ac
Charles Pisciotta <cpisciottadeveloping@gmail.com>
2024-01-15 11:11:29 -0500

    Bump to xcbeautify 2.0

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .package(url: "https://github.com/SwiftGen/StencilSwiftKit", from: "2.10.1"),
         .package(url: "https://github.com/SwiftGen/SwiftGen", from: "6.6.2"),
         .package(url: "https://github.com/tuist/XcodeProj", from: "8.15.0"),
-        .package(url: "https://github.com/cpisciotta/xcbeautify", from: "1.4.0"),
+        .package(url: "https://github.com/cpisciotta/xcbeautify", branch: "charles/Swift-5.9"),
         .package(url: "https://github.com/krzysztofzablocki/Difference.git", from: "1.0.2"),
     ]
 )
