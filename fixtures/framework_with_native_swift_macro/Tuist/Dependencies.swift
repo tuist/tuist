893e76a6ad3d2e874f62e39eaece8ed20db6197c
Shahzad Majeed <smajeed@draftkings.com>
2024-01-22 21:59:06 -0500

    misc

--- fixtures/framework_with_native_swift_macro/Tuist/Dependencies.swift
+++ fixtures/framework_with_native_swift_macro/Tuist/Dependencies.swift
@@ -1,7 +1,24 @@
 import ProjectDescription
 
+let frameworkType = Product.framework
 let dependencies = Dependencies(
     swiftPackageManager: .init(
+//        productTypes: [
+//            "CasePaths": frameworkType,
+//            "SwiftBasicFormat": frameworkType,
+//            "SwiftCompilerPlugin": frameworkType,
+//            "SwiftCompilerPluginMessageHandling": frameworkType,
+//            "SwiftDiagnostics": frameworkType,
+//            "SwiftOperators": frameworkType,
+//            "SwiftParser": frameworkType,
+//            "SwiftParserDiagnostic": frameworkType,
+//            "SwiftSyntax": frameworkType,
+//            "SwiftSyntax509": frameworkType,
+//            "SwiftSyntaxBuilder": frameworkType,
+//            "SwiftSyntaxMacroExpansion": frameworkType,
+//            "SwiftSyntaxMacros": frameworkType,
+//            "XCTestDynamicOverlay": frameworkType,
+//        ],
         targetSettings: [:]
     ),
     platforms: [.macOS, .iOS]
