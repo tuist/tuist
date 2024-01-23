810cfd02a0fe4f04834a3fc30aeb71953b750e52
fortmarek <marekfort@me.com>
2024-01-23 09:19:41 +0100

    Run lint:fix

--- docs/Package.swift
+++ docs/Package.swift
@@ -1,10 +1,4 @@
 // swift-tools-version:5.7
-/*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-Package manifest for SlothCreator.
-*/
 
 import PackageDescription
 
@@ -14,7 +8,7 @@ let package = Package(
         .executable(
             name: "tuist",
             targets: ["tuist"]
-        )
+        ),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
@@ -22,6 +16,6 @@ let package = Package(
     targets: [
         .target(
             name: "tuist"
-        )
+        ),
     ]
 )
