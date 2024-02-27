ac12c8c2e1951502f7f9d8d159a96687d2586272
Marek Fořt <marekfort@me.com>
2024-02-27 17:23:21 +0100

    Update docs for optional Cloud.Option (#6002)

--- Sources/ProjectDescription/Cloud.swift
+++ Sources/ProjectDescription/Cloud.swift
@@ -4,9 +4,8 @@ import Foundation
 public struct Cloud: Codable, Equatable {
     /// Options for cloud configuration.
     public enum Option: String, Codable, Equatable {
-        /// Marks whether cloud connection is optional.
-        /// If not present, tuist commands will fail regardless of whether an authentication token is available locally from
-        /// `tuist cloud auth` or not.
+        /// Marks whether Tuist Cloud authentication is optional.
+        /// If present, the interaction with Tuist Cloud will be skipped (instead of failing) if a user is not authenticated.
         case optional
     }
 
