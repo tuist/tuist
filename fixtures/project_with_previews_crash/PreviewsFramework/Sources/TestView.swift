b95dcd2a2efee606d990d0225a22f81e22a5bb8a
Antti Laitala <antti.laitala@ouraring.com>
2024-03-01 09:06:49 +0200

    Add fixture

---
+++ fixtures/project_with_previews_crash/PreviewsFramework/Sources/TestView.swift
@@ -0,0 +1,19 @@
+import ResourcesFramework
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        VStack {
+            Button("Click to read file from bundle") {
+                text = readFileFromBundle()
+            }
+            Text(text)
+        }
+    }
+    
+    @State var text = "-"
+}
+
+#Preview {
+    TestView()
+}
---
+++ fixtures/project_with_previews_crash/Tuist/Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "project_with_previews_crash",
+    dependencies: [
+        .package(path: "../ResourcesFramework")
+    ]
+)
