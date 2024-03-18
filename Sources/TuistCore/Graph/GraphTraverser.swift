5a7a72bc5450c3c920b47833eed873bf588bc53c
Alexander Weiß <weiss_alex@gmx.net>
2024-03-18 21:07:30 +0100

    feat: Update code according to PR suggestions

--- Sources/TuistCore/Graph/GraphTraverser.swift
+++ Sources/TuistCore/Graph/GraphTraverser.swift
@@ -573,54 +573,59 @@ public class GraphTraverser: GraphTraversing {
     }
 
     public func needsEnableTestingSearchPaths(path: AbsolutePath, name: String) -> Bool {
-        var cache = NSCache<GraphTargetCacheKey, NSNumber>()
+        var cache: [GraphTarget: Bool] = [:]
 
         func _needsEnableTestingSearchPaths(
             path: AbsolutePath,
-            name: String,
-            cache: NSCache<GraphTargetCacheKey, NSNumber>
+            name: String
         ) -> Bool {
-            if let target = target(path: path, name: name),
-               let gotNeeds = cache.object(forKey: .init(target))
-            {
-                return gotNeeds.boolValue
+            // Target could not be created, something must be wrong
+            guard let target = target(path: path, name: name) else {
+                return false
             }
 
-            if dependsOnXCTest(path: path, name: name) {
-                return true
+            // If a cache value is already present use it
+            if let cacheValue = cache[target] {
+                return cacheValue
             }
 
+            // Find all target dependencies
             let allTargetDependencies = allTargetDependencies(
                 path: path,
                 name: name
             )
 
+            // Check whether the current target depends on XCTest
+            let currentTargetDependsOnXCTest = dependsOnXCTest(path: path, name: name)
+
+            // If there are no further dependencies cache the value for the current target and return the value of it
             guard !allTargetDependencies.isEmpty else {
-                return false
+                cache[target] = currentTargetDependsOnXCTest
+                return currentTargetDependsOnXCTest
             }
 
-            var enable = false
+            // If there are dependencies found, we need to traverse deeper down the graph
+            var enable: Bool? // placeholder when we find a dependency that needs to enable testing paths
             for dependency in allTargetDependencies {
-                let key = GraphTargetCacheKey(dependency)
-
-                if let cachedNeeds = cache.object(forKey: key)?.boolValue {
-                    return cachedNeeds
-                }
-
                 let needs = needsEnableTestingSearchPaths(path: dependency.path, name: dependency.target.name)
 
                 if needs {
-                    cache.setObject(1, forKey: .init(dependency))
+                    cache[dependency] = true
                     enable = true
                     break
                 } else {
-                    cache.setObject(0, forKey: .init(dependency))
+                    cache[dependency] = false
                 }
             }
-            return enable
+
+            // Either found a value or we use the one from the current target
+            let result = enable ?? currentTargetDependsOnXCTest
+
+            cache[target] = result
+            return result
         }
 
-        return _needsEnableTestingSearchPaths(path: path, name: name, cache: cache)
+        return _needsEnableTestingSearchPaths(path: path, name: name)
     }
 
     public func dependsOnXCTest(path: AbsolutePath, name: String) -> Bool {
