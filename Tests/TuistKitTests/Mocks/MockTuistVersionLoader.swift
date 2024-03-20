b75d625c5c7ed985454150ca353c010b4365571c
dxmvsh <dimash.live@gmail.com>
2024-03-20 15:21:08 +0500

    fix(MockTuistVersionLoader): add import of TuistKit

--- Tests/TuistKitTests/Mocks/MockTuistVersionLoader.swift
+++ Tests/TuistKitTests/Mocks/MockTuistVersionLoader.swift
@@ -1,9 +1,10 @@
 import Foundation
+@testable import TuistKit
 
 public final class MockTuistVersionLoader: TuistVersionLoading {
     var getVersionStub: String = "4.0.1"
     private(set) var getVersionCalls = 0
-    func getVersion() throws -> String {
+    public func getVersion() throws -> String {
         getVersionStub
     }
 }
