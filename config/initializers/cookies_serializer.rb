12dddb9f4e1aa1e480c66e4dee35aa36e5555205
Pedro <pedro@pepicrft.me>
2024-03-30 14:44:40 +0800

    Fix log in

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -6,7 +6,7 @@
 # Valid options are :json, :marshal, and :hybrid.
 Rails.application.config.action_dispatch.cookies_serializer = :json
 
-Rails.application.config.session_store(:cookie_store, key: '_tuist_cloud_session', domain: ".127.0.0.1")
+Rails.application.config.session_store(:cookie_store, key: '_tuist_cloud_session')
 Rails.application.config.action_dispatch.cookies_serializer = :json
 
 # These salts are optional, but it doesn't hurt to explicitly configure them the same between the two apps.
--- phx/config/stag.exs
+++ phx/config/stag.exs
@@ -16,8 +16,3 @@ config :swoosh, local: false
 
 # Do not print debug messages in production
 config :logger, level: :info
-
-# Runtime production configuration, including reading
-# of environment variables, is done on config/runtime.exs.
-
-config :appsignal, :config, active: true
