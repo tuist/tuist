94241c70c589aa8b4a76f572e684925ab61a94e0
Pedro <pedro@pepicrft.me>
2024-03-31 09:19:15 +0800

    Remove logger change

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -13,7 +13,4 @@ if Environment.production_like_env?
     Rails.application.config.log_tags = [:request_id]
     Rails.logger = ActiveSupport::BroadcastLogger.new(console_logger)
   end
-else
-  Rails.logger = Logger.new($stdout)
-  Rails.logger.level = Logger::DEBUG
 end
