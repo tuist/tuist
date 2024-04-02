15e2181b4bd718dc48fcb5d9eb32f43a0e8cd6f0
Pedro <pedro@pepicrft.me>
2024-04-02 21:15:49 +0200

    Generate documentation for manifest types

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,74 +1,74 @@
-import { defineConfig } from 'vitepress'
-import cliDataLoader from "../docs/reference/cli/commands.data"
-import projectDescriptionTypesDataLoader from "../docs/reference/project-description/types.data"
-import examplesDataLoader from "../docs/reference/examples/examples.data"
+import { defineConfig } from "vitepress";
+import cliDataLoader from "../docs/reference/cli/commands.data";
+import projectDescriptionTypesDataLoader from "../docs/reference/project-description/types.data";
+import examplesDataLoader from "../docs/reference/examples/examples.data";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Tuist",
-  titleTemplate: ':title | Tuist',
+  titleTemplate: ":title | Tuist",
   description: "Scale your Xcode app development",
   srcDir: "docs",
-  lastUpdated: true,  
+  lastUpdated: true,
   locales: {
     root: {
       label: "English",
-      lange: "en"
-    }
+      lange: "en",
+    },
   },
   themeConfig: {
     logo: "/logo.png",
     search: {
-      provider: 'local'
+      provider: "local",
     },
     nav: [
-      { text: "Tuist Cloud", link: "/cloud/what-is-cloud"},
-      { text: 'Guide', link: '/guide/introduction/what-is-tuist' },
-      { text: 'Reference', link: '/reference/cli/generate' },
-      { text: 'Changelog', link: 'https://github.com/tuist/tuist/releases' }
+      { text: "Tuist Cloud", link: "/cloud/what-is-cloud" },
+      { text: "Guide", link: "/guide/introduction/what-is-tuist" },
+      { text: "Reference", link: "/reference/cli/generate" },
+      { text: "Changelog", link: "https://github.com/tuist/tuist/releases" },
     ],
     editLink: {
-      pattern: 'https://github.com/tuist/docs/edit/main/docs/:path'
+      pattern: "https://github.com/tuist/docs/edit/main/docs/:path",
     },
     sidebar: {
-      '/cloud': [
+      "/cloud": [
         {
           text: "Tuist Cloud",
           items: [
             {
               text: "What is Tuist Cloud?",
-              link: "/cloud/what-is-cloud"
+              link: "/cloud/what-is-cloud",
             },
             {
               text: "Get started",
-              link: "/cloud/get-started"
+              link: "/cloud/get-started",
             },
             {
               text: "Binary caching",
-              link: "/cloud/binary-caching"
+              link: "/cloud/binary-caching",
             },
             {
               text: "Selective testing",
-              link: "/cloud/selective-testing"
+              link: "/cloud/selective-testing",
             },
             {
               text: "On-premise",
-              link: "/cloud/on-premise"
-            }
-          ]
-        }
+              link: "/cloud/on-premise",
+            },
+          ],
+        },
       ],
-      '/guide/': [
+      "/guide/": [
         {
           text: "Introduction",
           items: [
             {
               text: "What is Tuist?",
-              link: "/guide/introduction/what-is-tuist"
+              link: "/guide/introduction/what-is-tuist",
             },
             {
               text: "The cost of convenience",
-              link: "/guide/introduction/cost-of-convenience"
+              link: "/guide/introduction/cost-of-convenience",
             },
             {
               text: "From v3 to v4",
@@ -84,40 +84,46 @@ export default defineConfig({
               items: [
                 {
                   text: "Create a project",
-                  link: "/guide/introduction/adopting-tuist/new-project"
+                  link: "/guide/introduction/adopting-tuist/new-project",
                 },
                 {
                   text: "Use it with a Swift Package",
-                  link: "/guide/introduction/adopting-tuist/swift-package"
+                  link: "/guide/introduction/adopting-tuist/swift-package",
                 },
                 {
                   text: "Migrate from .xcodeproj",
-                  link: "/guide/introduction/adopting-tuist/swift-package"
+                  link: "/guide/introduction/adopting-tuist/swift-package",
                 },
                 {
-                  text: "Migrate local Swift Packages"
+                  text: "Migrate local Swift Packages",
                 },
                 {
                   text: "Migrate from XcodeGen",
-                  link: "/guide/introduction/adopting-tuist/migrate-from-xcodegen"
+                  link: "/guide/introduction/adopting-tuist/migrate-from-xcodegen",
                 },
                 {
                   text: "Migrate from Bazel",
-                  link: "/guide/introduction/adopting-tuist/migrate-from-xcodegen"
-                }
-              ]
-            }
-          ]
+                  link: "/guide/introduction/adopting-tuist/migrate-from-xcodegen",
+                },
+              ],
+            },
+          ],
         },
         {
           text: "Project",
           items: [
-            {text: "Directory structure", link: '/guide/project/directory-structure'},
-            {text: "Editing", link: "/guide/project/editing"},
-            {text: "Dependencies", link: "/guide/project/dependencies"},
-            {text: "Code sharing", link: "/guide/project/code-sharing"},
-            {text: "Synthesized files", link: "/guide/project/synthesized-files"}
-          ]
+            {
+              text: "Directory structure",
+              link: "/guide/project/directory-structure",
+            },
+            { text: "Editing", link: "/guide/project/editing" },
+            { text: "Dependencies", link: "/guide/project/dependencies" },
+            { text: "Code sharing", link: "/guide/project/code-sharing" },
+            {
+              text: "Synthesized files",
+              link: "/guide/project/synthesized-files",
+            },
+          ],
         },
         {
           text: "Automation",
@@ -126,26 +132,26 @@ export default defineConfig({
             { text: "Build" },
             { text: "Test" },
             { text: "Run" },
-            {text: "Graph", link: "/guide/automation/graph"},
-            { text: "Clean" }
-          ]
+            { text: "Graph", link: "/guide/automation/graph" },
+            { text: "Clean" },
+          ],
         },
         {
           text: "Scale",
           items: [
             {
               text: "Xcode",
-              link: "/guide/scale/xcode"
+              link: "/guide/scale/xcode",
             },
             {
               text: "µFeatures architecture",
-              link: "/guide/scale/ufeatures-architecture"
+              link: "/guide/scale/ufeatures-architecture",
             },
             {
               text: "Tuist Cloud",
-              link: "/cloud/what-is-cloud"
-            }
-          ]
+              link: "/cloud/what-is-cloud",
+            },
+          ],
         },
         {
           text: "Extensions",
@@ -154,18 +160,18 @@ export default defineConfig({
               text: "Types",
               collapsed: true,
               items: [
-                { text: "Tasks"},
+                { text: "Tasks" },
                 { text: "Templates" },
-                { text: "Resource synthesizers" }
-              ]
+                { text: "Resource synthesizers" },
+              ],
             },
             {
-              text: "Sharing (plugins)"
-            }
-          ]
-        }
+              text: "Sharing (plugins)",
+            },
+          ],
+        },
       ],
-      '/reference/': [
+      "/reference/": [
         {
           text: "Reference",
           items: [
@@ -174,41 +180,44 @@ export default defineConfig({
               items: cliDataLoader.load().map((item) => {
                 return {
                   text: item.title,
-                  link: `/reference/cli/${item.command}`
-                }
-              })
+                  link: `/reference/cli/${item.command}`,
+                };
+              }),
             },
             {
               text: "Project Description",
               items: projectDescriptionTypesDataLoader.load().map((item) => {
                 return {
                   text: item.title,
-                  link: `/reference/project-description/${item.name}`
-                }
-              })
+                  link: `/reference/project-description/${item.name}`,
+                };
+              }),
             },
             {
               text: "Examples",
               items: examplesDataLoader.load().map((item) => {
                 return {
                   text: item.title,
-                  link: `/reference/examples/${item.name}`
-                }
-              })
-            }
-          ]
-        }
-      ]
+                  link: `/reference/examples/${item.name}`,
+                };
+              }),
+            },
+          ],
+        },
+      ],
     },
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/tuist/tuist' },
-      { icon: 'x', link: 'https://x.com/tuistio' },
-      { icon: 'mastodon', link: 'https://fosstodon.org/@tuist' },
-      { icon: 'slack', link: 'https://join.slack.com/t/tuistapp/shared_invite/zt-1y667mjbk-s2LTRX1YByb9EIITjdLcLw' }
+      { icon: "github", link: "https://github.com/tuist/tuist" },
+      { icon: "x", link: "https://x.com/tuistio" },
+      { icon: "mastodon", link: "https://fosstodon.org/@tuist" },
+      {
+        icon: "slack",
+        link: "https://join.slack.com/t/tuistapp/shared_invite/zt-1y667mjbk-s2LTRX1YByb9EIITjdLcLw",
+      },
     ],
     footer: {
-      message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2024-present Tuist GmbH'
-    }
-  }
-})
+      message: "Released under the MIT License.",
+      copyright: "Copyright © 2024-present Tuist GmbH",
+    },
+  },
+});
--- docs/docs/reference/project-description/types.data.js
+++ docs/docs/reference/project-description/types.data.js
@@ -1,13 +1,27 @@
+import * as path from "node:path";
+import fg from "fast-glob";
+import fs from "node:fs";
+
 export default {
-    load() {
-      return [
-          {
-              title: "Project",
-              name: "project",
-              description: "",
-              content: "# Project\nThis is just a test"
-          }
-      ];
-    },
-  };
-  
+  load() {
+    const generatedDirectory = path.join(
+      import.meta.dirname,
+      "../../../generated"
+    );
+    const files = fg
+      .sync("**/*.md", {
+        cwd: generatedDirectory,
+        absolute: true,
+        ignore: ["**/README.md"],
+      })
+      .sort();
+    return files.map((file) => {
+      return {
+        title: path.basename(file).replace(".md", ""),
+        name: path.basename(file).replace(".md", "").toLowerCase(),
+        description: "",
+        content: fs.readFileSync(file, "utf-8"),
+      };
+    });
+  },
+};
