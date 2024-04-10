fc5e667898973836c1dfb1ae03b3cb665bdf8ed2
Clemens Beck <clemensbeck@icloud.com>
2024-04-10 13:04:19 +0200

    Add fixture for privacyManifest

---
+++ fixtures/ios_app_with_privacy_manifest/MyApp/Tests/MyAppTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class MyAppTests: XCTestCase {
+    func test_twoPlusTwo_isFour() {
+        XCTAssertEqual(2+2, 4)
+    }
+}
---
+++ fixtures/ios_app_with_privacy_manifest/Project.swift
@@ -0,0 +1,47 @@
+import ProjectDescription
+
+let project = Project(
+    name: "MyApp",
+    targets: [
+        .target(
+            name: "MyApp",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.MyApp",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchStoryboardName": "LaunchScreen.storyboard",
+                ]
+            ),
+            sources: ["MyApp/Sources/**"],
+            resources: .resources(
+                ["MyApp/Resources/**"],
+                privacyManifest: .privacyManifest(
+                    tracking: false,
+                    trackingDomains: [],
+                    collectedDataTypes: [
+                    ],
+                    accessedApiTypes: [
+                        [
+                            "NSPrivacyAccessedAPIType": "NSPrivacyAccessedAPICategoryUserDefaults",
+                            "NSPrivacyAccessedAPITypeReasons": [
+                                "CA92.1",
+                            ],
+                        ],
+                    ]
+                )
+            ),
+            dependencies: []
+        ),
+        .target(
+            name: "MyAppTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "io.tuist.MyAppTests",
+            infoPlist: .default,
+            sources: ["MyApp/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "MyApp")]
+        ),
+    ]
+)
