6bb1f674d733913ba88b4e9f2e863714ea45bd31
Marek Fořt <marekfort@me.com>
2024-05-06 16:13:12 +0200

    Fix db:seed (#682)

--- phx/priv/repo/seeds.exs
+++ phx/priv/repo/seeds.exs
@@ -14,20 +14,40 @@ _tuist_project =
   Projects.get_project_by_slug("tuist/tuist") ||
     Projects.create_project(%{name: "tuist", account: %{id: account.id}}, token: "tuist")
 
-{:ok, tuist_cloud_acceptance_tests_project} =
-  Projects.get_project_by_slug("tuist/tuist-cloud-acceptance-tests") ||
-    Projects.create_project(%{name: "tuist-cloud-acceptance-tests", account: %{id: account.id}},
-      token: "tuist-cloud-acceptance-tests"
-    )
+_tuist_project =
+  with {:ok, project} <- Projects.get_project_by_slug("tuist/tuist") do
+    project
+  else
+    {:error, _} ->
+      Projects.create_project(%{name: "tuist", account: %{id: account.id}},
+        token: "tuist"
+      )
+  end
+
+tuist_cloud_acceptance_tests_project =
+  with {:ok, project} <- Projects.get_project_by_slug("tuist/tuist-cloud-acceptance-tests") do
+    project
+  else
+    {:error, _} ->
+      Projects.create_project(%{name: "tuist-cloud-acceptance-tests", account: %{id: account.id}},
+        token: "tuist-cloud-acceptance-tests"
+      )
+  end
 
 CommandEvents.create_command_event(%{
   name: "build",
   duration: 20000,
   tuist_version: "4.1.0",
-  project: %{id: tuist_cloud_acceptance_tests_project.id},
-  cacheable_targets: "A;B",
-  local_cache_target_hits: "",
-  remote_cache_target_hits: "B",
+  project_id: tuist_cloud_acceptance_tests_project.id,
+  cacheable_targets: ["A", "B"],
+  local_cache_target_hits: [""],
+  remote_cache_target_hits: ["B"],
+  swift_version: "5.2",
+  macos_version: "10.15",
+  subcommand: "",
+  command_arguments: [],
+  is_ci: false,
+  client_id: "client-id",
   created_at: Time.utc_now()
 })
 
@@ -35,10 +55,16 @@ CommandEvents.create_command_event(%{
   name: "build",
   duration: 34000,
   tuist_version: "4.1.0",
-  project: %{id: tuist_cloud_acceptance_tests_project.id},
-  cacheable_targets: "A;B;C",
-  local_cache_target_hits: "",
-  remote_cache_target_hits: "B",
+  project_id: tuist_cloud_acceptance_tests_project.id,
+  cacheable_targets: ["A", "B", "C"],
+  local_cache_target_hits: [""],
+  remote_cache_target_hits: ["B"],
+  swift_version: "5.2",
+  macos_version: "10.15",
+  subcommand: "",
+  command_arguments: [],
+  is_ci: false,
+  client_id: "client-id",
   created_at: NaiveDateTime.new!(Date.add(Time.utc_now(), -1), ~T[14:00:00.000])
 })
 
@@ -46,10 +72,16 @@ CommandEvents.create_command_event(%{
   name: "build",
   duration: 41000,
   tuist_version: "4.1.0",
-  project: %{id: tuist_cloud_acceptance_tests_project.id},
-  cacheable_targets: "A;B;C",
-  local_cache_target_hits: "",
-  remote_cache_target_hits: "B",
+  project_id: tuist_cloud_acceptance_tests_project.id,
+  cacheable_targets: ["A", "B", "C"],
+  local_cache_target_hits: [""],
+  remote_cache_target_hits: ["B"],
+  swift_version: "5.2",
+  macos_version: "10.15",
+  subcommand: "",
+  command_arguments: [],
+  is_ci: false,
+  client_id: "client-id",
   created_at: NaiveDateTime.new!(Date.add(Time.utc_now(), -6), ~T[14:00:00.000])
 })
 
@@ -57,9 +89,15 @@ CommandEvents.create_command_event(%{
   name: "build",
   duration: 78000,
   tuist_version: "4.1.0",
-  project: %{id: tuist_cloud_acceptance_tests_project.id},
-  cacheable_targets: "A;B;C",
-  local_cache_target_hits: "",
-  remote_cache_target_hits: "B",
+  project_id: tuist_cloud_acceptance_tests_project.id,
+  cacheable_targets: ["A", "B", "C"],
+  local_cache_target_hits: [""],
+  remote_cache_target_hits: ["B"],
+  swift_version: "5.2",
+  macos_version: "10.15",
+  subcommand: "",
+  command_arguments: [],
+  is_ci: false,
+  client_id: "client-id",
   created_at: NaiveDateTime.new!(Date.add(Time.utc_now(), -32), ~T[14:00:00.000])
 })
