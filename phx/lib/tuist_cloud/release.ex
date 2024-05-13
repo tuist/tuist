34e76d4efd16cfe0740ee363e38065097ce27342
Marek Fořt <marekfort@me.com>
2024-05-13 17:52:14 +0200

    Remove backfill script (#722)
    The backfill of customer id migration went through well, so we can
    delete it. I don't really see a point of keeping it around anymore.

--- phx/lib/tuist_cloud/release.ex
+++ phx/lib/tuist_cloud/release.ex
@@ -25,37 +25,4 @@ defmodule TuistCloud.Release do
   defp load_app do
     Application.load(@app)
   end
-
-  def backfill_customer_id do
-    Application.ensure_all_started(@app)
-
-    TuistCloud.Repo.all(TuistCloud.Accounts.Account)
-    |> Enum.filter(fn account -> account.customer_id == nil end)
-    |> Enum.each(&backfill_account_customer_id/1)
-  end
-
-  defp backfill_account_customer_id(account) do
-    user =
-      if account.owner_type == "Organization" do
-        organization = TuistCloud.Accounts.get_organization_by_id(account.owner_id)
-
-        members = TuistCloud.Accounts.get_organization_members(organization, :admin)
-
-        if Enum.empty?(members) do
-          nil
-        else
-          hd(members)
-        end
-      else
-        TuistCloud.Accounts.get_user!(account.owner_id)
-      end
-
-    if !is_nil(user) do
-      customer_id = TuistCloud.Billing.create_customer(%{name: account.name, email: user.email})
-
-      account
-      |> Ecto.Changeset.change(customer_id: customer_id)
-      |> TuistCloud.Repo.update!()
-    end
-  end
 end
