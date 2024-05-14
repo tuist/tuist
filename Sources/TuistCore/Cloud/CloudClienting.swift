c3c63db4e82cd5f140db35f1b3647216af31804c
fortmarek <marekfort@me.com>
2024-05-14 12:27:37 +0200

    Remove HTTPResource

--- Sources/TuistCore/Cloud/CloudClienting.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-import TuistSupport
-
-public protocol CloudClienting {
-    func request<T, E>(_ resource: HTTPResource<T, E>) async throws -> (object: T, response: HTTPURLResponse)
-}
