299abeef872b4f2f55970fbbea431df42edf5fdf
Mike Simons <msimons@etsy.com>
2024-05-22 21:30:13 -0400

    Remove global for string colorization to support Swift Concurrency

--- Sources/TuistSupport/Utils/ColorizeSwift.swift
+++ Sources/TuistSupport/Utils/ColorizeSwift.swift
@@ -57,8 +57,6 @@ public enum TerminalStyle {
 }
 
 extension String {
-    /// Enable/disable colorization
-    public static var isColorizationEnabled = true
 
     public func bold() -> String {
         applyStyle(TerminalStyle.bold)
@@ -93,7 +91,6 @@ extension String {
     }
 
     public func reset() -> String {
-        guard String.isColorizationEnabled else { return self }
         return "\u{001B}[0m" + self
     }
 
@@ -110,7 +107,6 @@ extension String {
     }
 
     private func applyStyle(_ codeStyle: TerminalStyleCode) -> String {
-        guard String.isColorizationEnabled else { return self }
         let str = replacingOccurrences(of: TerminalStyle.reset.open, with: TerminalStyle.reset.open + codeStyle.open)
 
         return codeStyle.open + str + TerminalStyle.reset.open
