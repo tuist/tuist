0e77e0260ab69876777a5d62874f197712a3c5c0
Pedro <pedro@pepicrft.me>
2024-05-27 18:15:59 +0200

    Move the last tasks and delete the 'make' directory

--- .mise/utilities/derived_data_path.sh
+++ .mise/utilities/derived_data_path.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ROOT_DIR="$(dirname "$(dirname "$SCRIPT_DIR")")"
-
 DERIVED_DATA_PATH=$(defaults read com.apple.dt.Xcode IDECustomDerivedDataLocation 2>/dev/null)
 
 # Check if the path is set
--- make/tasks/workspace/build-with-spm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ROOT_DIR=$($SCRIPT_DIR/../../utilities/root_dir.sh)
-DERIVED_DATA_PATH=$($ROOT_DIR/make/utilities/derived_data_path.sh)
-
-swift build --package-path $ROOT_DIR --build-path $ROOT_DIR/.build $@
