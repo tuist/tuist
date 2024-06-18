acad9c493dba0fe7b29a19368333af519fb12efa
Pedro <pedro@pepicrft.me>
2024-06-18 10:34:51 +0200

    Update release date

--- Sources/TuistServer/Client/CloudClientCLIMetadataHeadersMiddleware.swift
+++ Sources/TuistServer/Client/CloudClientCLIMetadataHeadersMiddleware.swift
@@ -5,7 +5,7 @@ import TuistSupport
 /// This middleware includes the release date of the CLI in the headers so that we can show
 /// warnings if the on-premise installation is too old.
 struct CloudClientCLIMetadataHeadersMiddleware: ClientMiddleware {
-    let releaseDate = "2024.06.10"
+    let releaseDate = "2024.06.18"
 
     func intercept(
         _ request: Request,
