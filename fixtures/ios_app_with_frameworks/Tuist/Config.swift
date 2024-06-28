f7133f8b2c94c8bc8bac204dc197b660077d64fc
fortmarek <marekfort@me.com>
2024-06-28 10:11:01 +0200

    Revert Tuist fixture URL to canary

--- fixtures/ios_app_with_frameworks/Tuist/Config.swift
+++ fixtures/ios_app_with_frameworks/Tuist/Config.swift
@@ -3,7 +3,7 @@ import ProjectDescription
 let config = Config(
     cloud: .cloud(
         projectId: "tuist/tuist-cloud-acceptance-tests",
-        url: "http://127.0.0.1:8080",
+        url: "https://cloud-canary.tuist.io",
         options: [.optional]
     )
 )
