93a98d8a0c2bfc5990aa94bb3ef7ccc43dafb802
Simon Bergström <simon.endby@gmail.com>
2024-06-28 09:27:19 +0200

    fix(Quick): added Quick's QuickObjcRuntime target to the list of testing targets to automatically add 'ENABLE_TESTING_SEARCH_PATHS=YES' (#6445)

--- Sources/TuistLoader/SwiftPackageManager/PackageInfoMapper.swift
+++ Sources/TuistLoader/SwiftPackageManager/PackageInfoMapper.swift
@@ -282,6 +282,7 @@ public final class PackageInfoMapper: PackageInfoMapping {
                     "Nimble", // https://github.com/Quick/Nimble
                     "NimbleObjectiveC", // https://github.com/Quick/Nimble
                     "Quick", // https://github.com/Quick/Quick
+                    "QuickObjCRuntime", // https://github.com/Quick/Quick
                     "RxTest", // https://github.com/ReactiveX/RxSwift
                     "RxTest-Dynamic", // https://github.com/ReactiveX/RxSwift
                     "SnapshotTesting", // https://github.com/pointfreeco/swift-snapshot-testing
