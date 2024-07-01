4a9ea6bdae323b151bd618e0fd39f429e18a3535
Marek Fořt <marekfort@me.com>
2024-07-01 12:25:06 +0200

    Use subscription instead of account's plan column for determining the plan in the billing plug (#837)
    We will be soon moving away from the `plan` column. I'm preparing us for
    that move by rewriting the `BillingPlug` to use the `Subscription`
    instead of the `plan` column of `Account`.

--- lib/tuist_cloud_web/plugs/api/authorization/billing_plug.ex
+++ lib/tuist_cloud_web/plugs/api/authorization/billing_plug.ex
@@ -5,6 +5,7 @@ defmodule TuistCloudWeb.API.Authorization.BillingPlug do
   use TuistCloudWeb, :controller
   use TuistCloudWeb, :verified_routes
 
+  alias TuistCloud.Billing
   alias TuistCloud.Accounts
   alias TuistCloudWeb.API.EnsureProjectPresencePlug
 
@@ -21,24 +22,27 @@ defmodule TuistCloudWeb.API.Authorization.BillingPlug do
   end
 
   def call(conn, _, true) do
-    %{account: %{plan: plan, name: account_handle} = account} =
+    %{account: %{name: account_handle} = account} =
       EnsureProjectPresencePlug.get_project(conn)
 
-    case {plan, Accounts.get_current_month_remote_cache_hits_count(account)} do
-      {plan, _} when plan in [:pro, :enterprise, :air] ->
-        conn
+    subscription = Billing.get_current_active_subscription(account)
 
-      {:none, hits} when plan in [:none] and hits < @remote_cache_hits_threshold ->
+    case {subscription, Accounts.get_current_month_remote_cache_hits_count(account)} do
+      {nil, hits} when hits > @remote_cache_hits_threshold ->
         conn
         |> put_status(:payment_required)
         |> json(%{
-          # credo:disable-for-next-line
-          # TODO: Add a link to the billing page
           message: ~s"""
-          The account '#{account_handle}' has reached the limit of remote cache hits #{@remote_cache_hits_threshold} of the 'Tuist Air' plan and requires payment. Manage your billing at #{url(~p"/#{account_handle}/billing")}.
+          The account '#{account_handle}' has reached the limit of remote cache hits #{@remote_cache_hits_threshold} of the 'Tuist Air' plan and requires payment. Manage your billing at #{url(~p"/#{account_handle}/settings/billing")}.
           """
         })
         |> halt()
+
+      {nil, hits} when hits <= @remote_cache_hits_threshold ->
+        conn
+
+      {_, _} ->
+        conn
     end
   end
 
