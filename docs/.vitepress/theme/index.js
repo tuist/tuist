83e7186ae0a37dfbd94dc43b34ef0a7340b5ab63
Pedro <pedro@pepicrft.me>
2024-07-17 11:17:18 +0200

    Improve the log in button and add a link to the API documentation

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,10 +1,7 @@
 import DefaultTheme from "vitepress/theme";
 import "./custom.css";
-import Button from "./components/Button.vue";
 /** @type {import('vitepress').Theme} */
 export default {
   extends: DefaultTheme,
-  enhanceApp({ app }) {
-    app.component("Button", Button);
-  },
+  enhanceApp({ app }) {},
 };
