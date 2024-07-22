8f146bd839c2d0c25f4165bcc40b4d0fc9ea8dd0
fortmarek <marekfort@me.com>
2024-07-22 16:58:13 +0200

    Revert "Speed up the query that fetches the current month cache hits count (#891)"
    This reverts commit 2cfeaca0e95209034b89a6b89a6c2088d358ee70.

--- lib/tuist/command_events/event.ex
+++ lib/tuist/command_events/event.ex
@@ -88,20 +88,26 @@ defmodule Tuist.CommandEvents.Event do
     |> validate_inclusion(:status, [:success, :failure])
   end
 
-  def get_current_month_remote_cache_hits_count_query(%Account{id: account_id}) do
-    today = Tuist.Time.utc_now()
-    beginning_of_month = Timex.beginning_of_month(today)
+  defmacro this_month_fragment() do
+    quote do
+      fragment(
+        "date_trunc('month', ?::timestamptz) = date_trunc('month', ?::timestamptz)",
+        c.created_at,
+        ^Tuist.Time.utc_now()
+      )
+    end
+  end
 
+  def get_current_month_remote_cache_hits_count_query(%Account{id: account_id}) do
     from c in __MODULE__,
       join: p in Project,
-      on: p.id == c.project_id and p.account_id == ^account_id,
-      where:
-        c.created_at >= ^beginning_of_month and
-          c.created_at < ^today,
+      on: p.id == c.project_id,
+      where: p.account_id == ^account_id,
+      where: this_month_fragment(),
       select:
         count(
           fragment(
-            "CASE WHEN COALESCE(? <> '{}', false) OR COALESCE(? <> '{}', false) THEN 1 END",
+            "CASE WHEN COALESCE(array_length(?, 1), 0) > 0 OR COALESCE(array_length(?, 1), 0) > 0 THEN 1 ELSE NULL END",
             c.remote_cache_target_hits,
             c.remote_test_target_hits
           )
