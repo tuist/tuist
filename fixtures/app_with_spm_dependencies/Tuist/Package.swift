0195b9f6894b81cf3af074639a5e7449b73c700a
Kai Oelfke <k.oelfke@me.com>
2024-07-26 11:06:48 +0900

    Update sentry to 8.32.0 to fix Xcode 16 issue

--- fixtures/app_with_spm_dependencies/Tuist/Package.swift
+++ fixtures/app_with_spm_dependencies/Tuist/Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         .package(url: "https://github.com/ZipArchive/ZipArchive", .upToNextMajor(from: "2.5.5")),
         .package(url: "https://github.com/jpsim/Yams", .upToNextMajor(from: "5.0.6")),
         .package(url: "https://github.com/google/GoogleSignIn-iOS", .upToNextMajor(from: "7.0.0")),
-        .package(url: "https://github.com/getsentry/sentry-cocoa", .upToNextMajor(from: "8.20.0")),
+        .package(url: "https://github.com/getsentry/sentry-cocoa", .upToNextMajor(from: "8.32.0")),
         .package(url: "https://github.com/realm/realm-swift", .upToNextMajor(from: "10.46.0")),
         .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack", .upToNextMajor(from: "3.8.4")),
         .package(url: "https://github.com/facebook/zstd", exact: "1.5.5"),
