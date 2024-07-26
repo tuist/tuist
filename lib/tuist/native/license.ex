d94117000e0a7773759ea6c7df507cbed72bd135
Pedro Piñera Buendía <663605+pepicrft@users.noreply.github.com>
2024-07-26 18:07:50 +0200

    Preparation for making the registry a public repository (#903)
    In preparation for making the registry repository public, I require the
    `TUIST_LICENSE` to be present for us, too, but in this case, with
    `expiration_date` set to `none.` This prevents anyone from running the
    service as if it were Tuist-hosted (changes have been validated in
    staging).
    
    I also created another Dockerfile for on-premise builds that don't copy
    the encrypted secrets. I tried to use a single Dockerfile and
    conditionally run a `COPY` step when an `ARG` has a certain value but
    that's not supported. Since we don't change the `Dockerfile` frequently,
    I think the risk of inconsistencies is quite low.
    
    ---------
    
    Co-authored-by: Marek Fořt <marekfort@me.com>

--- lib/tuist/native/license.ex
+++ lib/tuist/native/license.ex
@@ -5,7 +5,11 @@ defmodule Tuist.Native.License do
   defstruct [:id, :features, :expiration_date]
 
   def expired?(license) do
-    {:ok, expiration_date} = Date.from_iso8601(license.expiration_date)
-    Date.after?(Tuist.Date.utc_today(), expiration_date)
+    if license.expiration_date == "none" do
+      false
+    else
+      {:ok, expiration_date} = Date.from_iso8601(license.expiration_date)
+      Date.after?(Tuist.Date.utc_today(), expiration_date)
+    end
   end
 end
--- test/tuist/native/license_test.exs
+++ test/tuist/native/license_test.exs
@@ -3,6 +3,13 @@ defmodule Tuist.Native.LicenseTest do
   use Tuist.DataCase
   use Mimic
 
+  test "license has none as expiration_date" do
+    license = %License{expiration_date: "none"}
+
+    # When / Then
+    refute License.expired?(license)
+  end
+
   test "license is not expired" do
     # Given
     Tuist.Date
