9daaa0105e5cadbd8215df164bc71e929752df8d
Gorbenko Roman <45801227+rofle100lvl@users.noreply.github.com>
2024-08-11 18:22:21 +0200

    Fix tests

--- Tests/TuistKitAcceptanceTests/LintAcceptanceTests.swift
+++ Tests/TuistKitAcceptanceTests/LintAcceptanceTests.swift
@@ -3,10 +3,10 @@ import TuistAcceptanceTesting
 @testable import TuistKit
 
 final class LintAcceptanceTests: TuistAcceptanceTestCase {
-    func test_ios_app_with_frameworks() async throws {
+    func test_ios_app_with_headers() async throws {
         try await setUpFixture(.iosAppWithHeaders)
         try await run(LintImplicitImportsCommand.self)
-        XCTAssertStandardOutput(pattern: "Implicit dependencies were not found.")
+        XCTAssertStandardOutput(pattern: "We did not find any implicit dependencies in your project.")
     }
 
     func test_ios_app_with_implicit_dependencies() async throws {
