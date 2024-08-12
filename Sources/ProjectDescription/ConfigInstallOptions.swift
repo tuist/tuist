87981c09496ccf60e245bc74b9d27d1c00cd5bc5
Dariusz Rybicki <dariusz@darrarski.pl>
2024-08-12 19:11:41 +0200

    Update documentation comments
    Co-authored-by: Marek Fořt <marekfort@me.com>

--- Sources/ProjectDescription/ConfigInstallOptions.swift
+++ Sources/ProjectDescription/ConfigInstallOptions.swift
@@ -1,7 +1,7 @@
 extension Config {
     /// Options for install.
     public struct InstallOptions: Codable, Equatable, Sendable {
-        /// Arguments passed to Swift Package Manager.
+        /// Arguments passed to the Swift Package Manager's `swift package` command when running `swift package resolve`.
         public var passthroughSwiftPackageManagerArguments: [String]
 
         public static func options(
