b0c9182e1ab48a7c1dcc1f9bfcdfbeaff31b55f0
Pedro Piñera Buendía <663605+pepicrft@users.noreply.github.com>
2024-08-21 13:03:07 +0200

    Highlights and time saved (#965)
    With the rewrite of the history, [this
    PR](https://github.com/tuist/server/pull/961) got automatically closed.
    I'm reopening it on top of the new history.

--- lib/tuist_web/controllers/marketing_html.ex
+++ lib/tuist_web/controllers/marketing_html.ex
@@ -5,6 +5,40 @@ defmodule TuistWeb.MarketingHTML do
 
   embed_templates "marketing_html/*"
 
+  attr :title, :string, required: true
+  attr :subtitle, :string, required: true
+  attr :cta_variant, :string, required: false, default: "light"
+  attr :cta_href, :string, required: false
+  attr :cta_text, :string, required: false
+
+  def home_highlight_card(assigns) do
+    ~H"""
+    <div class="marketing__home__section__highlights__item">
+      <div class="marketing__home__section__highlights__item__main">
+        <h3 class="font-xxl-strong marketing__home__section__highlights__item__main__title">
+          <%= @title %>
+        </h3>
+        <p class="font-m marketing__home__section__highlights__item__main__subtitle">
+          <%= @subtitle %>
+        </p>
+      </div>
+      <div
+        :if={not is_nil(Map.get(assigns, :cta_text))}
+        class="marketing__home__section__highlights__item__footer"
+      >
+        <.secondary_button
+          :if={not is_nil(Map.get(assigns, :cta_text))}
+          size="medium"
+          variant={@cta_variant}
+          href={@cta_href}
+        >
+          <%= @cta_text %>
+        </.secondary_button>
+      </div>
+    </div>
+    """
+  end
+
   attr :category, :string, required: true
   attr :title, :string, required: true
   attr :description, :string, required: true
