3e6393c8e1407731f435a4fdc0dbb0b638b4d6a7
Mike Simons <msimons@etsy.com>
2024-08-21 10:00:26 -0400

    Add app extension and link against shared framework to exhibit bug.

---
+++ fixtures/app_with_framework_and_tests/AppExtension/ExtensionBundle.swift
@@ -0,0 +1,16 @@
+//
+//  ExtensionBundle.swift
+//  Extension
+//
+//  Created by msimons on 8/21/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ExtensionBundle: WidgetBundle {
+    var body: some Widget {
+        Extension()
+    }
+}
