40a7f38b04807d0a1e3dae6c467054f6e612331b
Gorbenko Roman <45801227+rofle100lvl@users.noreply.github.com>
2024-08-25 09:10:11 +0200

    Resolved conflicts in sidebars.mjs

--- docs/.vitepress/sidebars.mjs
+++ docs/.vitepress/sidebars.mjs
@@ -290,7 +290,6 @@ export const guidesSidebar = [
         ],
       },
       {
-<<<<<<< HEAD
         text: `<span style="display: flex; flex-direction: row; align-items: center; gap: 7px;">Verification</span>`,
         link: "guides/develop/verification",
         collapsed: true,
@@ -302,9 +301,6 @@ export const guidesSidebar = [
         ],
       },
       {
-        text: `<span style="display: flex; flex-direction: row; align-items: center; gap: 7px;">Workflows ${cloudBlank02Icon()} ${comingSoonBadge()}</span>`,
-        link: "guides/develop/workflows",
-=======
         text: `<span style="display: flex; flex-direction: row; align-items: center; gap: 7px;">Automate ${cloudBlank02Icon()}</span>`,
         collapsed: true,
         items: [
@@ -317,7 +313,6 @@ export const guidesSidebar = [
             link: "guides/develop/automate/workflows",
           },
         ],
->>>>>>> main
       },
     ],
   },
