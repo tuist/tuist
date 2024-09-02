9ad34280f86af4b6f14f51e00b5a8b4ccaa6d69c
Patryk Górski <ocznik@gmail.com>
2024-09-02 15:37:07 +0200

    Fix lint issue

--- fixtures/ios_app_with_spm_dependencies_forced_resolved_versions/Tuist/Config.swift
+++ fixtures/ios_app_with_spm_dependencies_forced_resolved_versions/Tuist/Config.swift
@@ -1,7 +1,5 @@
 import ProjectDescription
 
 let config = Config(
-  installOptions: .options(
-    passthroughSwiftPackageManagerArguments: ["--force-resolved-versions"]
-  )
+    installOptions: .options(passthroughSwiftPackageManagerArguments: ["--force-resolved-versions"])
 )
