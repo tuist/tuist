586dd239f7caa5c2067e244143bb54b0b83b25f3
Gorbenko Roman <45801227+rofle100lvl@users.noreply.github.com>
2024-09-09 08:36:13 +0200

    Remove bundle targets

--- Sources/TuistKit/Services/Inspect/InspecImplicitImportsService.swift
+++ Sources/TuistKit/Services/Inspect/InspecImplicitImportsService.swift
@@ -95,10 +95,24 @@ final class InspectImportsService {
         )
     }
 
+<<<<<<< HEAD
     private func lint(graphTraverser: GraphTraverser) async throws -> [InspectImplicitImportsServiceErrorIssue] {
         let allInternalTargets = graphTraverser
+=======
+    private func lint(
+        graphTraverser: GraphTraverser,
+        inspectType: InspectType
+    ) async throws -> [InspectImportsServiceErrorIssue] {
+        var allTargets = graphTraverser
+>>>>>>> 86e8cbb62 (Remove bundle targets)
             .allInternalTargets()
         let allTargets = allInternalTargets.union(graphTraverser.allExternalTargets())
+
+        if inspectType == .redundant {
+            allTargets = allTargets.filter {
+                $0.target.product != .bundle
+            }
+        }
 
         let allTargetNames = Set(allTargets.map(\.target.productName))
 
