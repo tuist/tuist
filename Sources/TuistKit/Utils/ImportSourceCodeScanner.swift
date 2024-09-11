ffa5c80e65e2beac29304b62d2eecd8cdf3e4ea8
Gorbenko Roman <45801227+rofle100lvl@users.noreply.github.com>
2024-09-11 10:24:02 +0200

    Changed approach to regex

--- Sources/TuistKit/Utils/ImportSourceCodeScanner.swift
+++ Sources/TuistKit/Utils/ImportSourceCodeScanner.swift
@@ -5,13 +5,6 @@ enum ProgrammingLanguage {
     case objc
 }
 
-enum Comment {
-    case singleLineComment(from: Int)
-    case partialOneLineComment(from: Int, to: Int)
-    case multilineCommentOpen(from: Int)
-    case multilineCommentClose(from: Int)
-}
-
 struct Match {
     let module: String
     let range: Range<String.Index>
@@ -35,25 +28,17 @@ final class ImportSourceCodeScanner {
             "@import\\s+([A-Za-z_0-9]+)|#(?:import|include)\\s+<([A-Za-z_0-9-]+)/"
         }
 
+        var codeWithoutComments = CommentsRemover.removeComments(from: code)
+
         var result: [String] = []
 
         let regex = try NSRegularExpression(pattern: pattern, options: [])
 
-        let lines = code.components(separatedBy: .newlines)
-
-        var multilineCommentStarted = false
+        let lines = codeWithoutComments.components(separatedBy: .newlines)
 
         for line in lines {
-            let comments = findComments(line: line)
             let range = NSRange(location: 0, length: line.utf16.count)
             let matches = regex.matches(in: line, options: [], range: range)
-
-            if multilineCommentStarted, !comments.contains(where: { comment in
-                if case .multilineCommentClose = comment { return true }
-                return false
-            }) {
-                continue
-            }
 
             for match in matches {
                 let match = switch language {
@@ -63,37 +48,9 @@ final class ImportSourceCodeScanner {
                     processMatchObjc(match: match, line: line)
                 }
                 if let match {
-                    let importStart = line.distance(from: line.startIndex, to: match.range.lowerBound)
-                    let importEnd = line.distance(from: line.startIndex, to: match.range.upperBound)
-
-                    var skip = multilineCommentStarted
-                    for comment in comments {
-                        switch comment {
-                        case let .singleLineComment(from: from):
-                            skip = importStart > from
-                        case let .partialOneLineComment(from: from, to: to):
-                            skip = importStart > from && importStart < importEnd && importEnd < to
-                        case let .multilineCommentOpen(from: from):
-                            skip = importStart > from
-                        case let .multilineCommentClose(from: from):
-                            skip = importStart < from
-                        }
-                    }
-
-                    if skip { continue }
-
                     if !result.contains(where: { $0 == match.module }) {
                         result.append(match.module)
                     }
-                }
-            }
-            for comment in comments {
-                switch comment {
-                case .singleLineComment, .partialOneLineComment: break
-                case .multilineCommentOpen:
-                    multilineCommentStarted = true
-                case .multilineCommentClose:
-                    multilineCommentStarted = false
                 }
             }
         }
@@ -127,63 +84,5 @@ final class ImportSourceCodeScanner {
             )
         }
         return result
-    }
-
-    private func findComments(line: String) -> [Comment] {
-        let singleLineCommentIndex = line.indicesOf(string: "//").first
-        var openMultilineCommentIndices = line.indicesOf(string: "/*").sorted().filter {
-            if let singleLineCommentIndex {
-                return singleLineCommentIndex > $0
-            }
-            return true
-        }
-        var closeMultilineCommentIndices = line.indicesOf(string: "*/").sorted().filter {
-            if let singleLineCommentIndex {
-                return singleLineCommentIndex > $0
-            }
-            return true
-        }
-
-        var comments = [Comment]()
-
-        while let closeComment = closeMultilineCommentIndices.first,
-              let openComment = openMultilineCommentIndices.first
-        {
-            comments.append(Comment.partialOneLineComment(from: openComment, to: closeComment))
-            openMultilineCommentIndices.removeFirst()
-            closeMultilineCommentIndices.removeFirst()
-        }
-
-        if let openComment = openMultilineCommentIndices.first {
-            comments.append(.multilineCommentOpen(from: openComment))
-        }
-
-        if let closeComment = closeMultilineCommentIndices.first {
-            comments.append(.multilineCommentClose(from: closeComment))
-        }
-
-        if let terminatedComment = singleLineCommentIndex {
-            comments.append(.singleLineComment(from: terminatedComment))
-        }
-
-        return comments
-    }
-}
-
-extension String {
-    fileprivate func indicesOf(string: String) -> [Int] {
-        var indices = [Int]()
-        var searchStartIndex = startIndex
-
-        while searchStartIndex < endIndex,
-              let range = range(of: string, range: searchStartIndex ..< endIndex),
-              !range.isEmpty
-        {
-            let index = distance(from: startIndex, to: range.lowerBound)
-            indices.append(index)
-            searchStartIndex = range.upperBound
-        }
-
-        return indices
     }
 }
