f33507bbae44e226043dd0e2674fcbd7586126ae
Pedro <pedro@pepicrft.me>
2024-09-19 01:17:48 +0200

    Fix error tracking warning

--- lib/tuist/application.ex
+++ lib/tuist/application.ex
@@ -3,7 +3,6 @@ defmodule Tuist.Application do
 
   use Application
   alias Tuist.Environment
-  import Environment, only: [run_if_error_tracking_enabled: 1]
 
   @impl true
   def start(_type, _args) do
