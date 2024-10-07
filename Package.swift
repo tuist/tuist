01012dc22a4b183418212f9121b2fc6734dea847
Marek Fořt <marekfort@me.com>
2024-10-07 16:00:00 +0200

    Fix error when resolving a symlink for a directory during tuist test (#6821)
    * Fix error when resolving a symlink for a directory during tuist test
    
    * Update FileSystem
    
    * Update Package.resolved

--- Package.swift
+++ Package.swift
@@ -475,7 +475,7 @@ let package = Package(
         ),
         .package(url: "https://github.com/tuist/Path", .upToNextMajor(from: "0.3.0")),
         .package(url: "https://github.com/tuist/XcodeGraph.git", exact: "0.12.1"),
-        .package(url: "https://github.com/tuist/FileSystem.git", .upToNextMajor(from: "0.2.0")),
+        .package(url: "https://github.com/tuist/FileSystem.git", .upToNextMajor(from: "0.3.1")),
         .package(url: "https://github.com/tuist/Command.git", exact: "0.8.0"),
     ],
     targets: targets
