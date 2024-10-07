d822812c34413c3b70ba9dbed1edb0c67c43bdd6
Annalise Mariottini <annalise.mariottini@columbia.edu>
2024-10-07 10:49:22 -0400

    remove boilderplate from Tuist directory

--- fixtures/app_with_local_spm_module_with_remote_dependencies/Tuist/Package.swift
+++ fixtures/app_with_local_spm_module_with_remote_dependencies/Tuist/Package.swift
@@ -5,18 +5,11 @@ import PackageDescription
     import ProjectDescription
 
     let packageSettings = PackageSettings(
-        // Customize the product types for specific package product
-        // Default is .staticFramework
-        // productTypes: ["Alamofire": .framework,] 
         productTypes: [:]
     )
 #endif
 
 let package = Package(
     name: "App",
-    dependencies: [
-        // Add your own dependencies here:
-        // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
-        // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
-    ]
+    dependencies: []
 )
