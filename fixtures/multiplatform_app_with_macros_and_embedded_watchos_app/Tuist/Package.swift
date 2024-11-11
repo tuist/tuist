bfa18e165c5469e21cb6bf2ea3b3aa57d6e3850c
Pierre Rodgers <prodgers@etsy.com>
2024-11-11 11:23:21 -0500

    update fixture to enable user script sandboxing

--- fixtures/multiplatform_app_with_macros_and_embedded_watchos_app/Tuist/Package.swift
+++ fixtures/multiplatform_app_with_macros_and_embedded_watchos_app/Tuist/Package.swift
@@ -1,6 +1,13 @@
 // swift-tools-version: 5.9
 import PackageDescription
 
+#if TUIST
+import ProjectDescription
+
+let packageSettings = PackageSettings(baseSettings: .settings(base: ["ENABLE_USER_SCRIPT_SANDBOXING": true]))
+
+#endif
+
 let package = Package(
     name: "Dependencies",
     dependencies: [
