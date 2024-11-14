aea4bbe4680deb285660f6dfa3f226c6dff92d35
Mike Simons <msimons@etsy.com>
2024-11-14 13:42:47 -0500

    Linting

--- fixtures/multiplatform_app_with_macros_and_embedded_watchos_app/Modules/ModuleA/Macros/Tests/MacrosTests.swift
+++ fixtures/multiplatform_app_with_macros_and_embedded_watchos_app/Modules/ModuleA/Macros/Tests/MacrosTests.swift
@@ -1,23 +1,22 @@
+import ModuleA
 import SwiftSyntaxMacros
 import SwiftSyntaxMacrosTestSupport
 import XCTest
-import ModuleA
 
 final class APIv3ModelMacroTests: XCTestCase {
     let testMacros: [String: Macro.Type] = [
         "Stringify": StringifyMacro.self,
     ]
-    
+
     func testStringifyStruct() throws {
         assertMacroExpansion(
-                 """
-                 #stringify(1+1)
-                 """,
-                 expandedSource: """
-                 (1+1, "1+1")
-                 """,
-                 macros: testMacros
+            """
+            #stringify(1+1)
+            """,
+            expandedSource: """
+            (1+1, "1+1")
+            """,
+            macros: testMacros
         )
     }
-    
 }
