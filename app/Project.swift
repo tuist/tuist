da2d9aacdb0801c51c72c78688684108aec07c63
fortmarek <fortmarek@users.noreply.github.com>
2024-11-15 09:26:47 +0000

    [Release] [skip ci] Tuist App app@0.5.2

--- app/Project.swift
+++ app/Project.swift
@@ -47,8 +47,8 @@ let project = Project(
                     "SUPublicEDKey": "ObyvL/hvYnFyAypkWwYaoeqE/iqB0LK6ioI3SA/Y1+k=",
                     "SUFeedURL":
                         "https://raw.githubusercontent.com/tuist/tuist/main/app/appcast.xml",
-                    "CFBundleShortVersionString": "0.5.1",
-                    "CFBundleVersion": "0.5.1",
+                    "CFBundleShortVersionString": "0.5.2",
+                    "CFBundleVersion": "0.5.2",
                 ]
             ),
             sources: ["TuistApp/Sources/**"],
