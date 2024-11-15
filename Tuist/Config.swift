9845c8ba6f5494a17c79f0fdf1be39ba2258adcf
fortmarek <marekfort@me.com>
2024-11-15 12:56:19 +0100

    Revert "Update tuist in .mise.toml to 4.34.1"
    This reverts commit 23ddb20895ecd83ca0335450c48ef994ba2caf5f.

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -2,6 +2,7 @@ import ProjectDescription
 
 let config = Config(
     fullHandle: "tuist/tuist",
+    url: "https://cloud.tuist.io",
     swiftVersion: .init("5.10"),
     generationOptions: .options(
         optionalAuthentication: true
