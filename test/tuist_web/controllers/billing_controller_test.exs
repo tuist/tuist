4021881a5e611456b3c6701ba23aea8da76d25d2
Pedro Piñera Buendía <663605+pepicrft@users.noreply.github.com>
2024-11-19 17:29:10 +0100

    Introduce pricing changes (#1125)
    This PR introduces the necessary changes to eliminate the $249 base
    price from Tuist Pro. Since we can only roll this out with notifying the
    users. The access to the changes is gatewayed through
    `Tuist.Billing.new?`.
    
    In a follow-up PR, I'll add the logic to deliver the email to notify
    accounts about the changes. Reached the day, we'll have to archive the
    $249 price item so that it cannot longer be used, and migrate the
    existing subscriptions to remove that item.
    
    > [!NOTE]
    > I've taken the opportunity to re-use the pricing plans across the
    marketing and the dashboard routes

--- test/tuist_web/controllers/billing_controller_test.exs
+++ test/tuist_web/controllers/billing_controller_test.exs
@@ -6,30 +6,69 @@ defmodule TuistWeb.BillingControllerTest do
   alias Tuist.Accounts
   use Mimic
 
-  test "redirects to Stripe when user has permission", %{conn: conn} do
-    %{account: account} =
-      user = AccountsFixtures.user_fixture(email: "tuist@tuist.io", preload: [:account])
+  describe "upgrade" do
+    test "redirects to Stripe when user has permission", %{conn: conn} do
+      %{account: account} =
+        user = AccountsFixtures.user_fixture(email: "tuist@tuist.io", preload: [:account])
 
-    Billing
-    |> expect(:create_session, fn _ -> %{url: "https://stripe.com"} end)
+      success_url = url(~p"/#{account.name}/billing") <> "?new_plan=pro"
 
-    conn =
-      conn
-      |> log_in_user(user)
-      |> get("/#{account.name}/billing/manage")
+      Billing
+      |> expect(:update_plan, fn %{
+                                   plan: :pro,
+                                   account: ^account,
+                                   success_url: ^success_url
+                                 } ->
+        "https://stripe.com"
+      end)
 
-    assert redirected_to(conn) == "https://stripe.com"
+      conn =
+        conn
+        |> log_in_user(user)
+        |> get("/#{account.name}/billing/upgrade")
+
+      assert redirected_to(conn) == "https://stripe.com"
+    end
+
+    test "raises UnauthorizedError when user does not have permission", %{conn: conn} do
+      organization = AccountsFixtures.organization_fixture()
+      organization_account = Accounts.get_account_from_organization(organization)
+      user = AccountsFixtures.user_fixture(email: "tuist@tuist.io")
+
+      assert_raise TuistWeb.Errors.UnauthorizedError, fn ->
+        conn
+        |> log_in_user(user)
+        |> get("/#{organization_account.name}/billing/upgrade")
+      end
+    end
   end
 
-  test "raises UnauthorizedError when user does not have permission", %{conn: conn} do
-    organization = AccountsFixtures.organization_fixture()
-    organization_account = Accounts.get_account_from_organization(organization)
-    user = AccountsFixtures.user_fixture(email: "tuist@tuist.io")
+  describe "manage" do
+    test "redirects to Stripe when user has permission", %{conn: conn} do
+      %{account: account} =
+        user = AccountsFixtures.user_fixture(email: "tuist@tuist.io", preload: [:account])
 
-    assert_raise TuistWeb.Errors.UnauthorizedError, fn ->
-      conn
-      |> log_in_user(user)
-      |> get("/#{organization_account.name}/billing/manage")
+      Billing
+      |> expect(:create_session, fn _ -> %{url: "https://stripe.com"} end)
+
+      conn =
+        conn
+        |> log_in_user(user)
+        |> get("/#{account.name}/billing/manage")
+
+      assert redirected_to(conn) == "https://stripe.com"
+    end
+
+    test "raises UnauthorizedError when user does not have permission", %{conn: conn} do
+      organization = AccountsFixtures.organization_fixture()
+      organization_account = Accounts.get_account_from_organization(organization)
+      user = AccountsFixtures.user_fixture(email: "tuist@tuist.io")
+
+      assert_raise TuistWeb.Errors.UnauthorizedError, fn ->
+        conn
+        |> log_in_user(user)
+        |> get("/#{organization_account.name}/billing/manage")
+      end
     end
   end
 end
--- test/tuist_web/plugs/api/authorization/billing_plug_test.exs
+++ test/tuist_web/plugs/api/authorization/billing_plug_test.exs
@@ -1,5 +1,4 @@
 defmodule TuistWeb.API.Authorization.BillingPlugTest do
-  alias Tuist.Environment
   alias Tuist.Billing
   alias Tuist.Billing.Subscription
   alias TuistWeb.API.Authorization.BillingPlug
@@ -29,64 +28,9 @@ defmodule TuistWeb.API.Authorization.BillingPlugTest do
     }
   end
 
-  test "returns the same connection if the plan is enterprise", %{
-    conn: conn,
-    project: project
-  } do
-    # Given
-    account = project.account
-
-    Billing
-    |> stub(:get_current_active_subscription, fn ^account ->
-      %Subscription{plan: :enterprise}
-    end)
-
-    project = project |> Repo.reload() |> Repo.preload(:account)
-
-    plug_opts = BillingPlug.init([])
-
-    conn =
-      %{conn | query_params: Map.put(conn.query_params, "cache_category", "builds")}
-      |> EnsureProjectPresencePlug.put_project(project)
-
-    # When
-    got = conn |> BillingPlug.call(plug_opts)
-
-    # Then
-    assert got == conn
-  end
-
-  test "returns the same connection if the plan is pro", %{
-    conn: conn,
-    project: project
-  } do
-    # Given
-    account = project.account
-
-    Billing
-    |> stub(:get_current_active_subscription, fn ^account ->
-      %Subscription{plan: :pro}
-    end)
-
-    project = project |> Repo.reload() |> Repo.preload(:account)
-
-    plug_opts = BillingPlug.init([])
-
-    conn =
-      %{conn | query_params: Map.put(conn.query_params, "cache_category", "builds")}
-      |> EnsureProjectPresencePlug.put_project(project)
-
-    # When
-    got = conn |> BillingPlug.call(plug_opts)
-
-    # Then
-    assert got == conn
-  end
-
-  test "returns the same connection if the plan is air", %{
-    conn: conn,
-    project: project
-  } do
+  @tag current_month_remote_cache_hits_count: 50
+  test "returns the same connection when it's the new billing, the plan is air, and the usage is below the threshold",
+       %{conn: conn, project: project} do
     # Given
     account = project.account
 
@@ -95,8 +39,6 @@ defmodule TuistWeb.API.Authorization.BillingPlugTest do
       %Subscription{plan: :air}
     end)
 
-    project = project |> Repo.reload() |> Repo.preload(:account)
-
     plug_opts = BillingPlug.init([])
 
     conn =
@@ -110,120 +52,16 @@ defmodule TuistWeb.API.Authorization.BillingPlugTest do
     assert got == conn
   end
 
-  test "returns the same connection if the plan is air and the trial ends in more than 3 days",
-       %{
-         conn: conn,
-         project: project
-       } do
+  @tag current_month_remote_cache_hits_count: 250
+  test "returns an error when it's the new billing, the plan is air, and the usage is above the threshold",
+       %{conn: conn, project: project} do
     # Given
     account = project.account
 
     Billing
     |> stub(:get_current_active_subscription, fn ^account ->
-      %Subscription{plan: :air, trial_end: ~U[2021-01-01 00:00:00Z]}
+      %Subscription{plan: :air}
     end)
-
-    Tuist.Time
-    |> stub(:utc_now, fn -> ~U[2020-12-20 00:00:00Z] end)
-
-    project = project |> Repo.reload() |> Repo.preload(:account)
-
-    plug_opts = BillingPlug.init([])
-
-    conn =
-      %{conn | query_params: Map.put(conn.query_params, "cache_category", "builds")}
-      |> EnsureProjectPresencePlug.put_project(project)
-
-    # When
-    got = conn |> BillingPlug.call(plug_opts)
-
-    # Then
-    assert got == conn
-  end
-
-  test "returns connection with a warning if the plan is air and the trial ends in less than 3 days",
-       %{
-         conn: conn,
-         project: project
-       } do
-    # Given
-    account = project.account
-
-    Billing
-    |> stub(:get_current_active_subscription, fn ^account ->
-      %Subscription{plan: :air, trial_end: ~U[2021-01-01 00:00:00Z]}
-    end)
-
-    Tuist.Time
-    |> stub(:utc_now, fn -> ~U[2020-12-30 00:00:00Z] end)
-
-    project = project |> Repo.reload() |> Repo.preload(:account)
-
-    plug_opts = BillingPlug.init([])
-
-    conn =
-      %{conn | query_params: Map.put(conn.query_params, "cache_category", "builds")}
-      |> EnsureProjectPresencePlug.put_project(project)
-
-    # When
-    got = conn |> BillingPlug.call(plug_opts)
-
-    # Then
-    assert TuistWeb.WarningsHeaderPlug.get_warnings(got) ==
-             [
-               "Your trial period ends in 2 days. Please update your billing information to avoid service interruption: #{url(~p"/#{project.account.name}/billing")}"
-             ]
-  end
-
-  test "returns connection with a warning if the plan is air and the trial ends today",
-       %{
-         conn: conn,
-         project: project
-       } do
-    # Given
-    account = project.account
-
-    Billing
-    |> stub(:get_current_active_subscription, fn ^account ->
-      %Subscription{plan: :air, trial_end: ~U[2021-01-01 01:00:00Z]}
-    end)
-
-    Tuist.Time
-    |> stub(:utc_now, fn -> ~U[2021-01-01 00:00:00Z] end)
-
-    project = project |> Repo.reload() |> Repo.preload(:account)
-
-    plug_opts = BillingPlug.init([])
-
-    conn =
-      %{conn | query_params: Map.put(conn.query_params, "cache_category", "builds")}
-      |> EnsureProjectPresencePlug.put_project(project)
-
-    # When
-    got = conn |> BillingPlug.call(plug_opts)
-
-    # Then
-    assert TuistWeb.WarningsHeaderPlug.get_warnings(got) ==
-             [
-               "Your trial period ends today. Please update your billing information to avoid service interruption: #{url(~p"/#{project.account.name}/billing")}"
-             ]
-  end
-
-  @tag current_month_remote_cache_hits_count: 201
-  test "returns an error if the account has no active subscription and the current month remote cache hits count is over the threshold",
-       %{
-         conn: conn,
-         project: project
-       } do
-    # Given
-    account = project.account
-
-    Billing
-    |> stub(:get_current_active_subscription, fn ^account ->
-      nil
-    end)
-
-    project = project |> Repo.reload() |> Repo.preload(:account)
 
     plug_opts = BillingPlug.init([])
 
@@ -237,51 +75,21 @@ defmodule TuistWeb.API.Authorization.BillingPlugTest do
     # Then
     assert json_response(got, :payment_required) == %{
              "message" => ~s"""
-             The account '#{project.account.name}' has reached the limit of remote cache hits #{BillingPlug.remote_cache_hits_threshold()} of the 'Tuist Air' plan and requires payment. Manage your billing at #{url(~p"/#{project.account.name}/billing")}.
+             The account '#{account.name}' has reached the limits of the plan 'Tuist Air' and requires upgrading to the plan 'Tuist Pro'. You can upgrade your plan at #{url(~p"/#{account.name}/billing/upgrade")}.
              """
            }
   end
 
-  @tag current_month_remote_cache_hits_count: 201
-  test "returns the same connection if the on premise license is not expired",
-       %{
-         conn: conn,
-         project: project
-       } do
+  @tag current_month_remote_cache_hits_count: 1000
+  test "returns an error when it's the new billing, the plan is pro, and the subscription is not active",
+       %{conn: conn, project: project} do
     # Given
-    Environment
-    |> stub(:on_premise?, fn -> true end)
+    account = project.account
 
-    Tuist.License |> stub(:get_license, fn -> {:ok, %{valid: true}} end)
-
-    project = project |> Repo.reload() |> Repo.preload(:account)
-
-    plug_opts = BillingPlug.init([])
-
-    conn =
-      %{conn | query_params: Map.put(conn.query_params, "cache_category", "builds")}
-      |> EnsureProjectPresencePlug.put_project(project)
-
-    # When
-    got = conn |> BillingPlug.call(plug_opts)
-
-    # Then
-    assert got == conn
-  end
-
-  @tag current_month_remote_cache_hits_count: 201
-  test "returns an error if the on premise license is expired",
-       %{
-         conn: conn,
-         project: project
-       } do
-    # Given
-    Environment
-    |> stub(:on_premise?, fn -> true end)
-
-    Tuist.License |> stub(:get_license, fn -> {:ok, %{valid: false}} end)
-
-    project = project |> Repo.reload() |> Repo.preload(:account)
+    Billing
+    |> stub(:get_current_active_subscription, fn ^account ->
+      %Subscription{plan: :pro, status: "canceled"}
+    end)
 
     plug_opts = BillingPlug.init([])
 
@@ -295,26 +103,21 @@ defmodule TuistWeb.API.Authorization.BillingPlugTest do
     # Then
     assert json_response(got, :payment_required) == %{
              "message" => ~s"""
-             The current license is expired. Please update your license to continue using the service. Contact your administrator for more information.
+             The account '#{account.name}' 'Tuist Pro' plan is not active. You can manage your billing at #{url(~p"/#{account.name}/billing/manage")}.
              """
            }
   end
 
-  @tag current_month_remote_cache_hits_count: 199
-  test "returns the same connection if the account has no active subscription and the current month remote cache hits count is below the threshold",
-       %{
-         conn: conn,
-         project: project
-       } do
+  @tag current_month_remote_cache_hits_count: 1000
+  test "returns the same connection it's the new billing, the plan is pro, and the subscription is active",
+       %{conn: conn, project: project} do
     # Given
     account = project.account
 
     Billing
     |> stub(:get_current_active_subscription, fn ^account ->
-      nil
+      %Subscription{plan: :pro, status: "active"}
     end)
-
-    project = project |> Repo.reload() |> Repo.preload(:account)
 
     plug_opts = BillingPlug.init([])
 
@@ -327,5 +130,57 @@ defmodule TuistWeb.API.Authorization.BillingPlugTest do
 
     # Then
     assert got == conn
+  end
+
+  @tag current_month_remote_cache_hits_count: 1000
+  test "returns the same connection it's the new billing, the plan is open_source, and the subscription is active",
+       %{conn: conn, project: project} do
+    # Given
+    account = project.account
+
+    Billing
+    |> stub(:get_current_active_subscription, fn ^account ->
+      %Subscription{plan: :open_source, status: "active"}
+    end)
+
+    plug_opts = BillingPlug.init([])
+
+    conn =
+      %{conn | query_params: Map.put(conn.query_params, "cache_category", "builds")}
+      |> EnsureProjectPresencePlug.put_project(project)
+
+    # When
+    got = conn |> BillingPlug.call(plug_opts)
+
+    # Then
+    assert got == conn
+  end
+
+  @tag current_month_remote_cache_hits_count: 1000
+  test "returns the an error if it's the new billing, the plan is open_source, and the subscription is canceled",
+       %{conn: conn, project: project} do
+    # Given
+    account = project.account
+
+    Billing
+    |> stub(:get_current_active_subscription, fn ^account ->
+      %Subscription{plan: :open_source, status: "canceled"}
+    end)
+
+    plug_opts = BillingPlug.init([])
+
+    conn =
+      %{conn | query_params: Map.put(conn.query_params, "cache_category", "builds")}
+      |> EnsureProjectPresencePlug.put_project(project)
+
+    # When
+    got = conn |> BillingPlug.call(plug_opts)
+
+    # Then
+    assert json_response(got, :payment_required) == %{
+             "message" => ~s"""
+             The account '#{account.name}' 'Tuist Open Source' plan is not active. You can contact Tuist at contact@tuist.io to renovate it, or upgrade to 'Tuist Pro' at #{url(~p"/#{account.name}/billing/upgrade")}.
+             """
+           }
   end
 end
