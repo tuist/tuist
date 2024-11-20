914d603ce390bce4b8672dd5a37395733e79ac43
Yunseo Kang <65678579+yungu0010@users.noreply.github.com>
2024-11-20 21:50:39 +0900

    Align ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES with the Xcode default (#7038)
    * fix type of http_timeout in .mise.toml from str to u64
    
    * remove: ALWAYS_EMBED_SWIFT_LIBRARIES from essentialTargetSettings
    
    * change target init to fit tuist4
    
    * fit to swiftlint
    
    * fit to swiftformat

--- fixtures/workspace_with_test_locale/Project.swift
+++ fixtures/workspace_with_test_locale/Project.swift
@@ -11,7 +11,7 @@ let project = Project(
         )
     ),
     targets: [
-        .init(
+        Target.target(
             name: "App",
             destinations: .iOS,
             product: .app,
