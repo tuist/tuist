f3d84734fd3aab95bf94fe5585c2fb898c1ac6f0
fortmarek <marekfort@me.com>
2024-11-21 13:35:16 +0100

    Fix failing unit tests

--- Tests/TuistGeneratorTests/Generator/ConfigGeneratorTests.swift
+++ Tests/TuistGeneratorTests/Generator/ConfigGeneratorTests.swift
@@ -689,7 +689,8 @@ final class ConfigGeneratorTests: TuistUnitTestCase {
             "TARGETED_DEVICE_FAMILY": "1,2",
             "SUPPORTED_PLATFORMS": "iphoneos iphonesimulator macosx",
             "LD_RUNPATH_SEARCH_PATHS[sdk=macosx*]": ["$(inherited)", "@executable_path/../Frameworks"],
-            "LD_RUNPATH_SEARCH_PATHS": ["$(inherited)", "@executable_path/Frameworks"],
+            "LD_RUNPATH_SEARCH_PATHS[sdk=iphoneos*]": ["$(inherited)", "@executable_path/Frameworks"],
+            "LD_RUNPATH_SEARCH_PATHS[sdk=iphonesimulator*]": ["$(inherited)", "@executable_path/Frameworks"],
         ]
 
         assert(config: debugConfig, contains: expectedSettings)
