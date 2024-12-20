cd7f1904e6f73dcf66d278e798add562eac19a0b
Nathan SALAUN <nathan.salaun@sofia.dev>
2024-12-20 16:29:21 +0100

    Add env vars for all cache args

--- Sources/TuistKit/Commands/CacheCommand.swift
+++ Sources/TuistKit/Commands/CacheCommand.swift
@@ -24,13 +24,15 @@ public struct CacheCommand: AsyncParsableCommand, HasTrackableParameters {
     @Option(
         name: .shortAndLong,
         help: "The path to the directory that contains the project whose targets will be cached.",
-        completion: .directory
+        completion: .directory,
+        envKey: .cachePath
     )
     var path: String?
 
     @Option(
         name: .shortAndLong,
-        help: "Configuration to use for binary caching."
+        help: "Configuration to use for binary caching.",
+        envKey: .cacheConfiguration
     )
     var configuration: String?
 
@@ -38,24 +40,28 @@ public struct CacheCommand: AsyncParsableCommand, HasTrackableParameters {
     A list of targets to cache. \
     Those and their dependant targets will be cached. \
     If no target is specified, all the project targets (excluding the external ones) and their dependencies will be cached.
-    """)
+    """,
+      envKey: .cacheTargets
+    )
     var targets: [String] = []
 
     @Flag(
         help: "If passed, the command doesn't cache the targets passed in the `--targets` argument, but only their dependencies",
-        envKey: .externalOnly
+        envKey: .cacheExternalOnly
     )
     var externalOnly: Bool = false
 
     @Flag(
         name: .long,
-        help: "When passed, it generates the project and skips warming the cache. This is useful for debugging purposes."
+        help: "When passed, it generates the project and skips warming the cache. This is useful for debugging purposes.",
+        envKey: .cacheGenerateOnly
     )
     var generateOnly: Bool = false
 
     @Flag(
         name: .long,
-        help: "When passed, the hashes of the cacheable frameworks in the given project are printed."
+        help: "When passed, the hashes of the cacheable frameworks in the given project are printed.",
+        envKey: .cachePrintHashes
     )
     var printHashes: Bool = false
 
