62d58d9f673bf1e52d1129987ba65b3f786a07e6
Christoph Schmatzler <christoph@tuist.dev>
2025-01-02 14:54:16 +0100

    Refactor names
    Signed-off-by: Christoph Schmatzler <christoph@tuist.dev>

--- Sources/TuistKit/Services/AccountUpdateUsernameService.swift
+++ Sources/TuistKit/Services/AccountUpdateUsernameService.swift
@@ -5,26 +5,26 @@ import TuistLoader
 import TuistServer
 import TuistSupport
 
-protocol ChangeUsernameServicing: AnyObject {
+protocol AccountUpdateUsernameServicing: AnyObject {
     func run(
         name: String,
         directory: String?
     ) async throws
 }
 
-final class ChangeUsernameService: ChangeUsernameServicing {
-    private let updateUsernameService: UpdateUsernameServicing
+final class AccountUpdateUsernameService: AccountUpdateUsernameServicing {
+    private let updateAccountUsernameService: UpdateAccountUsernameServicing
     private let serverURLService: ServerURLServicing
     private let configLoader: ConfigLoading
 
     // MARK: - Init
 
     init(
-        updateUsernameService: UpdateUsernameServicing = UpdateUsernameService(),
+        updateAccountUsernameService: UpdateAccountUsernameServicing = UpdateAccountUsernameService(),
         serverURLService: ServerURLServicing = ServerURLService(),
         configLoader: ConfigLoading = ConfigLoader(warningController: WarningController.shared)
     ) {
-        self.updateUsernameService = updateUsernameService
+        self.updateAccountUsernameService = updateAccountUsernameService
         self.serverURLService = serverURLService
         self.configLoader = configLoader
     }
@@ -43,7 +43,7 @@ final class ChangeUsernameService: ChangeUsernameServicing {
         let config = try await configLoader.loadConfig(path: directoryPath)
         let serverURL = try serverURLService.url(configServerURL: config.url)
 
-        if let name = try await updateUsernameService.updateUsername(serverURL: serverURL, name: name) {
+        if let name = try await updateAccountUsernameService.updateAccountUsername(serverURL: serverURL, name: name) {
             logger.notice("Successfully updated username to \(name).", metadata: .success)
         }
     }
