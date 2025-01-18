22a5d7e9cb03a719dd0640c1101389d25e3c678b
sanghyeok-kim <slsnsi1212@gmail.com>
2025-01-18 12:15:10 +0900

    Apply indentation fixes using lint

--- app/TuistApp/Sources/Extensions/SimulatorDeviceAndRuntime+Extras.swift
+++ app/TuistApp/Sources/Extensions/SimulatorDeviceAndRuntime+Extras.swift
@@ -2,17 +2,17 @@ import Foundation
 import TuistCore
 
 #if swift(>=6)
-extension SimulatorDeviceAndRuntime: @retroactive Comparable {
-    public static func < (lhs: SimulatorDeviceAndRuntime, rhs: SimulatorDeviceAndRuntime) -> Bool {
-        if lhs.device.name == rhs.device.name { return lhs.runtime.name < rhs.runtime.name }
-        else { return lhs.device.name < rhs.device.name }
+    extension SimulatorDeviceAndRuntime: @retroactive Comparable {
+        public static func < (lhs: SimulatorDeviceAndRuntime, rhs: SimulatorDeviceAndRuntime) -> Bool {
+            if lhs.device.name == rhs.device.name { return lhs.runtime.name < rhs.runtime.name }
+            else { return lhs.device.name < rhs.device.name }
+        }
     }
-}
 #else
-extension SimulatorDeviceAndRuntime: Comparable {
-    public static func < (lhs: SimulatorDeviceAndRuntime, rhs: SimulatorDeviceAndRuntime) -> Bool {
-        if lhs.device.name == rhs.device.name { return lhs.runtime.name < rhs.runtime.name }
-        else { return lhs.device.name < rhs.device.name }
+    extension SimulatorDeviceAndRuntime: Comparable {
+        public static func < (lhs: SimulatorDeviceAndRuntime, rhs: SimulatorDeviceAndRuntime) -> Bool {
+            if lhs.device.name == rhs.device.name { return lhs.runtime.name < rhs.runtime.name }
+            else { return lhs.device.name < rhs.device.name }
+        }
     }
-}
 #endif
