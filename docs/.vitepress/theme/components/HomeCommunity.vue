7e794496671ad6ce63d22d039ff4ffdc0b914c74
Pedro Piñera Buendía <663605+pepicrft@users.noreply.github.com>
2025-01-23 15:22:28 +0100

    Fix documentation home layout (#7271)

---
+++ docs/.vitepress/theme/components/HomeCommunity.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+    <div class="community">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+.community {
+    display: grid;
+    position: relative;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 2rem;
+
+    @media (max-width: 960px) {
+        grid-template-columns: repeat(1, 1fr);
+    }
+}
+</style>
