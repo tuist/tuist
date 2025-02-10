f4be0434e6e52a0232d4e89fcc1e4255976d5ddd
Christoph Schmatzler <christoph@tuist.dev>
2025-02-10 14:50:18 +0100

    Add .icon component (#1369)
    Previously, we only generated components as functions with the icon name
    equalling the function name. This should be the preferred usage when
    building pages and passing icons inside slots, but is limiting when we
    need to pass an icon _inside_ a slot. In particular, LiveView allows
    
    ```
    slot :head do
      attr :icon, :string
    end
    ```
    
    but does _not_ allow
    
    ```
    slot :head do
      slot :icon
    end
    ```
    
    To allow us to use icons as strings, this PR adds a helper component
    that can be used as `<.icon name="user" />`.
    
    Signed-off-by: Christoph Schmatzler <christoph@tuist.dev>

--- lib/tuist_web/components/noora/icon.ex
+++ lib/tuist_web/components/noora/icon.ex
@@ -1,5 +1,23 @@
 defmodule TuistWeb.Noora.Icon do
-  @moduledoc false
+  @moduledoc """
+  Icons in use across the Noora design system.
+
+  ## Usage
+
+  The module exposes all icons in two ways:
+  1. As a function of the shape `<.{icon_name} />`
+  2. As an `icon` component that takes a `name` attribute.
+
+  ```elixir
+  <.mail />
+  <.icon name="mail" />
+  ```
+
+  ## Adding an icon
+
+  Icons are automatically read and generated from the `icons` folder. To add an icon, simply add a SVG file to the folder. File names are
+  required to be alphanumeric. Hyphens in the file name are automatically substituted to underscores.
+  """
   use Phoenix.Component
 
   @icons_path Path.join(File.cwd!(), "lib/tuist_web/components/noora/icons")
@@ -16,6 +34,14 @@ defmodule TuistWeb.Noora.Icon do
     content = File.read!(file_path)
 
     def unquote(String.to_atom(icon_name))(assigns) do
+      assigns = assign(assigns, :content, unquote(content))
+
+      ~H"""
+      {Phoenix.HTML.raw(@content)}
+      """
+    end
+
+    def icon(%{name: unquote(icon_name)} = assigns) do
       assigns = assign(assigns, :content, unquote(content))
 
       ~H"""
