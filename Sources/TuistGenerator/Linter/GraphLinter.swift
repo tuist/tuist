4ab940a9cb023fc70a9b7d9a05cdc7e22de7ef40
Lukas Würzburger <lukas.w@me.com>
2025-02-14 11:24:42 +0100

    add comma

--- Sources/TuistGenerator/Linter/GraphLinter.swift
+++ Sources/TuistGenerator/Linter/GraphLinter.swift
@@ -559,7 +559,7 @@ public class GraphLinter: GraphLinting {
             LintableTarget(platform: .iOS, product: .dynamicLibrary),
             LintableTarget(platform: .iOS, product: .staticFramework),
             LintableTarget(platform: .iOS, product: .framework),
-            LintableTarget(platform: .macOS, product: .framework)
+            LintableTarget(platform: .macOS, product: .framework),
         ],
         LintableTarget(platform: .macOS, product: .staticLibrary): [
             LintableTarget(platform: .macOS, product: .staticLibrary),
