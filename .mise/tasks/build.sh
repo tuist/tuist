3892de57673a209bd76496ae5a2fcd69f1f02beb
Pedro <pedro@pepicrft.me>
2025-02-26 09:49:41 +0100

    Start compiling dependencies

--- .mise/tasks/build.sh
+++ .mise/tasks/build.sh
@@ -1,25 +1,31 @@
 #!/usr/bin/env bash
 #MISE description="Build the project"
 
-#USAGE flag "-s --spm" help="Build through SPM"
 #USAGE flag "-v --linux-vm" help="Build virtualizing Linux"
 
-if [ "$usage_spm" = "true" ]; then
+if command -v podman &> /dev/null; then
+    CONTAINER_ENGINE="podman"
+else
+    CONTAINER_ENGINE="docker"
+fi
+dependencies=("AEXML")
+
+# Not all the grah modules compile for Linux, so let's
+# figure out one by one until we can ensure the whole graph compiles.
+
+if [ "$usage_linux_vm" = "false" ]; then
+    swift package resolve
+fi
+
+for dependency in "${dependencies[@]}"; do
     if [ "$usage_linux_vm" = "true" ]; then
-        if command -v podman &> /dev/null; then
-            CONTAINER_ENGINE="podman"
-        else
-            CONTAINER_ENGINE="docker"
-        fi
         $CONTAINER_ENGINE run --rm \
                 --volume "$MISE_PROJECT_ROOT:/package" \
                 --workdir "/package" \
                 swiftlang/swift:nightly-6.0-focal \
                 /bin/bash -c \
-                "swift build --build-path ./.build/linux"
+                "swift package resolve && swift build --package-path .build/checkouts/$dependency"
     else
-        swift build
+        swift build --package-path .build/checkouts/$dependency
     fi
-else
-    tuist build
-fi
+done
