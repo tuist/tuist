7b88bab18a991fde87a15f06838c923aab8d9773
Mike Simons <msimons@etsy.com>
2025-03-03 08:09:38 -0500

    Update tests for strongly typed build settings

--- Tests/TuistGeneratorTests/Generator/ConfigGeneratorTests.swift
+++ Tests/TuistGeneratorTests/Generator/ConfigGeneratorTests.swift
@@ -42,8 +42,8 @@ final class ConfigGeneratorTests: TuistUnitTestCase {
 
         let debugConfig: XCBuildConfiguration = configurationList.buildConfigurations[2]
         XCTAssertEqual(debugConfig.name, "Debug")
-        XCTAssertEqual(debugConfig.buildSettings["Debug"] as? String, "Debug")
-        XCTAssertEqual(debugConfig.buildSettings["Base"] as? String, "Base")
+        XCTAssertEqual(debugConfig.buildSettings["Debug"], .string("Debug"))
+        XCTAssertEqual(debugConfig.buildSettings["Base"], .string("Base"))
     }
 
     func test_generateProjectConfig_whenRelease() async throws {
@@ -54,14 +54,14 @@ final class ConfigGeneratorTests: TuistUnitTestCase {
 
         let releaseConfig: XCBuildConfiguration = configurationList.buildConfigurations[3]
         XCTAssertEqual(releaseConfig.name, "Release")
-        XCTAssertEqual(releaseConfig.buildSettings["Release"] as? String, "Release")
-        XCTAssertEqual(releaseConfig.buildSettings["Base"] as? String, "Base")
-        XCTAssertEqual(releaseConfig.buildSettings["MTL_ENABLE_DEBUG_INFO"] as? String, "NO")
+        XCTAssertEqual(releaseConfig.buildSettings["Release"], .string("Release"))
+        XCTAssertEqual(releaseConfig.buildSettings["Base"], .string("Base"))
+        XCTAssertEqual(releaseConfig.buildSettings["MTL_ENABLE_DEBUG_INFO"], .string("NO"))
 
         let customReleaseConfig: XCBuildConfiguration = configurationList.buildConfigurations[1]
         XCTAssertEqual(customReleaseConfig.name, "CustomRelease")
-        XCTAssertEqual(customReleaseConfig.buildSettings["Base"] as? String, "Base")
-        XCTAssertEqual(customReleaseConfig.buildSettings["MTL_ENABLE_DEBUG_INFO"] as? String, "NO")
+        XCTAssertEqual(customReleaseConfig.buildSettings["Base"], .string("Base"))
+        XCTAssertEqual(customReleaseConfig.buildSettings["MTL_ENABLE_DEBUG_INFO"], .string("NO"))
     }
 
     func test_generateTargetConfig() async throws {
@@ -1353,9 +1353,9 @@ final class ConfigGeneratorTests: TuistUnitTestCase {
         line: UInt = #line
     ) {
         let matches = settings.filter {
-            if let stringValue = config?.buildSettings[$0.key] as? String {
+            if let stringValue = config?.buildSettings[$0.key]?.stringValue {
                 return $0.value == .string(stringValue)
-            } else if let arrayValue = config?.buildSettings[$0.key] as? [String] {
+            } else if let arrayValue = config?.buildSettings[$0.key]?.arrayValue {
                 return $0.value == .array(arrayValue)
             } else {
                 return false
