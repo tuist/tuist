30cd36b1f0903e1f20eab71ddc746a45d975e189
Shahzad Majeed <shahzy57@gmail.com>
2025-03-05 20:22:53 -0600

    Externalize graph format parameter so that consumers can choose any format

--- Sources/ProjectAutomation/Tuist.swift
+++ Sources/ProjectAutomation/Tuist.swift
@@ -26,7 +26,7 @@ public enum Tuist {
 
     /// Loads and returns the graph at the given path.
     /// - parameter path: the path which graph should be loaded. If nil, the current path is used.
-    public static func graph(at path: String? = nil) throws -> Graph {
+    public static func graph(at path: String? = nil, format: String = "json") throws -> Graph {
         let temporaryDirectory = try createTemporaryDirectory()
 
         do {
@@ -34,7 +34,7 @@ public enum Tuist {
             var arguments = [
                 "tuist",
                 "graph",
-                "--format", "json",
+                "--format", format,
                 "--output-path", temporaryDirectory.path,
             ]
             if let path {
