4e9a26e884e35676edd353894f95bd9afe0ffed9
Christoph Schmatzler <christoph@tuist.dev>
2025-03-19 10:09:35 +0100

    feat(noora/card): Add card component (#1519)
    Resolves: https://github.com/tuist/server/issues/1487
    
    Adds the card component required for most, if not all, of our prototype
    pages.
    
    ![CleanShot 2025-03-17 at 09 45
    54@2x](https://github.com/user-attachments/assets/5d11471a-bb82-433e-b052-bdc17f8aed05)
    
    The wrapper component is pretty straightforward since it's consistent
    between uses. The section component I kept super generic, since in the
    existing prototypes alone we have
    - tables
    - graphs
    - multiple sections side-by-side as sort of overview
    
    So this is gonna be pretty use-case and page specific. That's why I kept
    the styling minimal (background, border, and a default padding).
    
    ---------
    
    Signed-off-by: Christoph Schmatzler <christoph@tuist.dev>
    Co-authored-by: Marek Fořt <marekfort@me.com>

---
+++ lib/tuist_web/storybook/card.story.exs
@@ -0,0 +1,32 @@
+defmodule TuistWeb.Storybook.Card do
+  @moduledoc false
+  use PhoenixStorybook.Story, :component
+
+  def function, do: &TuistWeb.Noora.Card.card/1
+
+  def imports,
+    do: [{TuistWeb.Noora.Card, [card_section: 1]}, {TuistWeb.Noora.Button, [button: 1]}]
+
+  def variations do
+    [
+      %Variation{
+        id: :card,
+        attributes: %{
+          style: "width: 350px",
+          icon: "dashboard",
+          title: "Recent test runs"
+        },
+        slots: [
+          """
+          <:actions>
+            <.button variant="secondary" label="View more" size="medium" />
+          </:actions>
+          <.card_section style="width: 100%;">
+            This is where the content of the card will be displayed.
+          </.card_section>
+          """
+        ]
+      }
+    ]
+  end
+end
