4f454afcba716966322cee1370e80b33409ae792
Pedro Piñera Buendía <663605+pepicrft@users.noreply.github.com>
2025-03-24 15:50:00 +0100

    Some SEO fixes (#1555)
    I noticed a couple of issues with the SEO-related headers that we were
    returning:
    - There was a typo. We used `X-Robots-Tags` instead of `X-Robots-Tag`.
    - We enabled SEO even for on-premise instances, which we shouldn't.

--- lib/tuist_web/router.ex
+++ lib/tuist_web/router.ex
@@ -539,12 +539,16 @@ defmodule TuistWeb.Router do
   end
 
   def disable_robot_indexing(conn, _params) do
-    conn |> put_resp_header("x-robots-tags", "noindex, nofollow")
+    conn |> put_resp_header("x-robots-tag", "noindex, nofollow")
   end
 
-  defp enable_robot_indexing(conn, _params) do
-    # Once we iterate on the open-graph tags of the dashboard pages for public projects
-    # we should iterate on this to enable indexing for public projects
-    conn |> put_resp_header("x-robots-tags", "index, follow")
+  defp enable_robot_indexing(conn, params) do
+    if Tuist.Environment.prod?() and not Tuist.Environment.on_premise?() do
+      # Once we iterate on the open-graph tags of the dashboard pages for public projects
+      # we should iterate on this to enable indexing for public projects
+      conn |> put_resp_header("x-robots-tag", "index, follow")
+    else
+      conn |> disable_robot_indexing(params)
+    end
   end
 end
--- test/tuist_web/router_test.exs
+++ test/tuist_web/router_test.exs
@@ -2,11 +2,34 @@ defmodule TuistWeb.RouterTest do
   use TuistTestSupport.Cases.ConnCase, async: true
   use TuistTestSupport.Cases.StubCase, billing: true
   alias TuistTestSupport.Fixtures.AccountsFixtures
+  use Mimic
 
-  test "marketing pages are indexable and followable",
+  test "marketing pages are non-indexable and non-followable when a production and on-premise environment",
        %{conn: conn} do
+    Tuist.Environment |> stub(:on_premise?, fn -> true end)
+    Tuist.Environment |> stub(:prod?, fn -> true end)
+
     for route <- [~p"/blog", ~p"/about", ~p"/pricing", ~p"/terms", ~p"/blog"] do
-      assert conn |> get(route) |> get_resp_header("x-robots-tags") == ["index, follow"]
+      assert conn |> get(route) |> get_resp_header("x-robots-tag") == ["noindex, nofollow"]
+    end
+  end
+
+  test "marketing pages are non-indexable and non-followable when a non-production environment",
+       %{conn: conn} do
+    Tuist.Environment |> stub(:prod?, fn -> false end)
+
+    for route <- [~p"/blog", ~p"/about", ~p"/pricing", ~p"/terms", ~p"/blog"] do
+      assert conn |> get(route) |> get_resp_header("x-robots-tag") == ["noindex, nofollow"]
+    end
+  end
+
+  test "marketing pages are indexable and followable when a production and non on-premise environment",
+       %{conn: conn} do
+    Tuist.Environment |> stub(:on_premise?, fn -> false end)
+    Tuist.Environment |> stub(:prod?, fn -> true end)
+
+    for route <- [~p"/blog", ~p"/about", ~p"/pricing", ~p"/terms", ~p"/blog"] do
+      assert conn |> get(route) |> get_resp_header("x-robots-tag") == ["index, follow"]
     end
   end
 
@@ -29,7 +52,7 @@ defmodule TuistWeb.RouterTest do
     # Then
     assert conn
            |> get(~p"/#{organization_account.name}/projects")
-           |> get_resp_header("x-robots-tags") == [
+           |> get_resp_header("x-robots-tag") == [
              "noindex, nofollow"
            ]
   end
