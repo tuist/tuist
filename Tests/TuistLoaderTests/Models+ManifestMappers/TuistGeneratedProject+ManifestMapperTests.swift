56c151881306e2106c7ac368efee04a6ac37b514
fortmarek <marekfort@me.com>
2025-03-27 10:10:53 +0100

    Disable build insights unless fullHandle is defined in Tuist.swift

---
+++ Tests/TuistLoaderTests/Models+ManifestMappers/TuistGeneratedProject+ManifestMapperTests.swift
@@ -0,0 +1,55 @@
+import FileSystem
+import Foundation
+import ProjectDescription
+import Testing
+import TuistCore
+
+@testable import TuistLoader
+
+struct TuistGeneratedProjectManifestMapperTests {
+    private let fileSystem = FileSystem()
+
+    @Test func buildInsightsDisabled_when_fullHandle_is_nil() async throws {
+        try await fileSystem.runInTemporaryDirectory(prefix: UUID().uuidString) { temporaryDirectory in
+            // When
+            let got = try TuistCore.TuistGeneratedProjectOptions.GenerationOptions.from(
+                manifest: .options(),
+                generatorPaths: GeneratorPaths(manifestDirectory: temporaryDirectory, rootDirectory: temporaryDirectory),
+                fullHandle: nil
+            )
+
+            // Then
+            #expect(got.buildInsightsDisabled == true)
+        }
+    }
+
+    @Test func buildInsightsDisabled_when_fullHandle_is_defined() async throws {
+        try await fileSystem.runInTemporaryDirectory(prefix: UUID().uuidString) { temporaryDirectory in
+            // When
+            let got = try TuistCore.TuistGeneratedProjectOptions.GenerationOptions.from(
+                manifest: .options(),
+                generatorPaths: GeneratorPaths(manifestDirectory: temporaryDirectory, rootDirectory: temporaryDirectory),
+                fullHandle: "tuist/tuist"
+            )
+
+            // Then
+            #expect(got.buildInsightsDisabled == false)
+        }
+    }
+
+    @Test func buildInsightsDisabled_when_fullHandle_is_defined_and_insights_disabled_in_generation_options() async throws {
+        try await fileSystem.runInTemporaryDirectory(prefix: UUID().uuidString) { temporaryDirectory in
+            // When
+            let got = try TuistCore.TuistGeneratedProjectOptions.GenerationOptions.from(
+                manifest: .options(
+                    buildInsightsDisabled: true
+                ),
+                generatorPaths: GeneratorPaths(manifestDirectory: temporaryDirectory, rootDirectory: temporaryDirectory),
+                fullHandle: "tuist/tuist"
+            )
+
+            // Then
+            #expect(got.buildInsightsDisabled == true)
+        }
+    }
+}
