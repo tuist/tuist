71c07b2b7f10da4f279b507e4cf7d4e4fcf36a4f
Pedro <pedro@pepicrft.me>
2025-04-01 15:55:12 +0200

    Document Target.shouldCoredataModelsBeSources

--- Sources/TuistGenerator/Generator/BuildPhaseGenerator.swift
+++ Sources/TuistGenerator/Generator/BuildPhaseGenerator.swift
@@ -367,18 +367,6 @@ final class BuildPhaseGenerator: BuildPhaseGenerating {
         ))
 
         if !target.shouldCoredataModelsBeSources {
-            // CoreData models are typically added to the sources build phase
-            // and Xcode automatically bundles the models.
-            // For static libraries / frameworks however, they don't support resources,
-            // the models could be bundled in a stand alone `.bundle`
-            // as resources.
-            //
-            // e.g.
-            // MyStaticFramework (.staticFramework) -> Includes CoreData models as sources
-            // MyStaticFrameworkResources (.bundle) -> Includes CoreData models as resources
-            //
-            // - Note: Technically, CoreData models can be added a sources build phase in a `.bundle`
-            // but that will result in the `.bundle` having an executable, which is not valid on iOS.
             pbxBuildFiles.append(contentsOf: generateCoreDataModels(
                 coreDataModels: target.coreDataModels,
                 fileElements: fileElements,
