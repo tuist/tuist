b0a4621635aa735b75ec01b310c64975f3d13f92
Christoph Schmatzler <christoph@tuist.dev>
2025-04-01 16:42:36 +0200

    feat(ui): user confirmation (#1599)
    ![CleanShot 2025-04-01 at 13 26
    49@2x](https://github.com/user-attachments/assets/a4d5ef00-cd7d-4c9f-9f38-9a3d6b17c4c6)
    ![CleanShot 2025-04-01 at 13 27
    07@2x](https://github.com/user-attachments/assets/1d6f7e0b-ae74-4efa-a3a4-7838475f9e49)
    
    
    - Adds automatic confirmation on page load without button press
    - Redirects after 5 seconds if success
    - New design
    
    ---------
    
    Signed-off-by: Christoph Schmatzler <christoph@tuist.dev>
    Co-authored-by: Marek Fořt <marekfort@me.com>

--- lib/tuist_web/live/user_confirmation_live.ex
+++ lib/tuist_web/live/user_confirmation_live.ex
@@ -1,9 +1,75 @@
 defmodule TuistWeb.UserConfirmationLive do
   use TuistWeb, :live_view
+  use TuistWeb.Noora
 
   alias Tuist.Accounts
 
-  def render(%{live_action: :edit} = assigns) do
+  def render(assigns) do
+    ~H"""
+    <%= if FunWithFlags.enabled?(:noora) do %>
+      <.noora_confirmation {assigns} />
+    <% else %>
+      <.legacy_confirmation {assigns} />
+    <% end %>
+    """
+  end
+
+  def noora_confirmation(assigns) do
+    ~H"""
+    <div id="confirmation">
+      <div data-part="wrapper">
+        <div data-part="frame">
+          <div data-part="content">
+            <img src="/images/tuist_logo_32x32@2x.png" alt={gettext("Tuist Logo")} data-part="logo" />
+            <div data-part="dots">
+              <.dots_light />
+              <.dots_dark />
+            </div>
+            <%= if @success do %>
+              <div data-part="header">
+                <h1 data-part="title">{gettext("Account confirmed!")}</h1>
+                <span data-part="subtitle">
+                  {gettext("Your account has been confirmed.")}
+                </span>
+              </div>
+              <.alert
+                id="confirmation-success"
+                type="secondary"
+                status="success"
+                size="small"
+                title={
+                  gettext("Your account has been confirmed. You will be redirected shortly...")
+                }
+              />
+            <% else %>
+              <div data-part="header">
+                <h1 data-part="title">{gettext("Confirmation failed")}</h1>
+                <span data-part="subtitle">
+                  {gettext("Your account could not be confirmed.")}
+                </span>
+              </div>
+              <.alert
+                id="confirmation-failure"
+                type="secondary"
+                status="error"
+                size="small"
+                title={gettext("User confirmation link is invalid or it has expired.")}
+              />
+            <% end %>
+          </div>
+        </div>
+      </div>
+
+      <div data-part="background">
+        <div data-part="top-right-gradient"></div>
+        <div data-part="bottom-left-gradient"></div>
+        <div data-part="shell"><.shell /></div>
+      </div>
+    </div>
+    """
+  end
+
+  def legacy_confirmation(%{live_action: :edit} = assigns) do
     ~H"""
     <.stack class="auth-page" gap="4xl">
       <.auth_header
@@ -33,9 +99,25 @@ defmodule TuistWeb.UserConfirmationLive do
     """
   end
 
-  def mount(%{"token" => token}, _session, socket) do
+  def handle_params(%{"token" => token}, _session, socket) do
     form = to_form(%{"token" => token}, as: "user")
-    {:ok, assign(socket, form: form), temporary_assigns: [form: nil]}
+
+    if FunWithFlags.enabled?(:noora) do
+      case Accounts.confirm_user(token) do
+        {:ok, _} ->
+          Process.send_after(self(), :redirect, 5000)
+          {:noreply, assign(socket, success: true)}
+
+        :error ->
+          {:noreply, assign(socket, success: false)}
+      end
+    else
+      {:noreply, assign(socket, form: form, temporary_assigns: [form: nil])}
+    end
+  end
+
+  def handle_info(:redirect, socket) do
+    {:noreply, redirect(socket, to: ~p"/users/log_in")}
   end
 
   # Do not log in the user after confirmation to avoid a
