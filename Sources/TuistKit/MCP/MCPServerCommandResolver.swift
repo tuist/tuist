2aa1ade79103a5a63a09e26c0b79654db01a4ca2
Pedro <pedro@pepicrft.me>
2025-04-03 14:58:27 +0200

    Fix getting the executable path

--- Sources/TuistKit/MCP/MCPServerCommandResolver.swift
+++ Sources/TuistKit/MCP/MCPServerCommandResolver.swift
@@ -1,3 +1,4 @@
+import Darwin
 import Foundation
 import Mockable
 
@@ -9,11 +10,21 @@ protocol MCPServerCommandResolving {
 struct MCPServerCommandResolver: MCPServerCommandResolving {
     private let executablePath: String
 
-    init(executablePath: String = CommandLine.arguments[0]) {
+    init(executablePath: String = Self.getExecutablePath()) {
         self.executablePath = executablePath
     }
 
     func resolve() -> (String, [String]) {
+        print(executablePath)
         return (executablePath, ["mcp", "start"])
+    }
+
+    private static func getExecutablePath() -> String! {
+        var buffer = [CChar](repeating: 0, count: Int(PATH_MAX))
+        var pathLength = UInt32(buffer.count)
+        if _NSGetExecutablePath(&buffer, &pathLength) == 0 {
+            return String(cString: buffer)
+        }
+        return nil
     }
 }
