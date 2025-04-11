82747745bdd1c10105bbdb48f73f9b41bd07b8f0
Pedro <pedro@pepicrft.me>
2025-04-11 12:52:30 +0200

    Create fixture

---
+++ fixtures/generated_ios_static_library_with_string_resources/Project.swift
@@ -0,0 +1,16 @@
+import ProjectDescription
+
+let project = Project(
+    name: "GeneratediOSStaticLibraryWithStringResources",
+    targets: [
+        .target(
+            name: "GeneratediOSStaticLibraryWithStringResources",
+            destinations: .iOS,
+            product: .staticLibrary,
+            bundleId: "io.tuist.GeneratediOSStaticLibraryWithStringResources",
+            sources: ["GeneratediOSStaticLibraryWithStringResources/Sources/**"],
+            resources: ["GeneratediOSStaticLibraryWithStringResources/Resources/**"],
+            dependencies: []
+        )
+    ]
+)
