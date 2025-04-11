e1381a47885269c1a9d4a8dec597b6e10b59b9f5
Pedro <pedro@pepicrft.me>
2025-04-11 12:18:30 +0200

    Add schemeIdentifierSchemeName attribute to ActivityLog

--- Sources/TuistKit/Services/Inspect/Models/ActivityLog.swift
+++ Sources/TuistKit/Services/Inspect/Models/ActivityLog.swift
@@ -4,4 +4,12 @@ struct ActivityLog: Codable {
     let fileName: String
     let timeStartedRecording: Double
     let timeStoppedRecording: Double
+    let schemeIdentifierSchemeName: String
+
+    enum CodingKeys: String, CodingKey {
+        case fileName
+        case timeStartedRecording
+        case timeStoppedRecording
+        case schemeIdentifierSchemeName = "schemeIdentifier-schemeName"
+    }
 }
