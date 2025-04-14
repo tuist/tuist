a582efc63621f6e5d91f2d36a90c08999468a0e9
Simon Cook <simon.cook@trademe.co.nz>
2025-04-14 21:35:23 +1200

    Fix issue where targets with no sources generated an empty compile sources phase, leading to issues in watchOS apps (#7493)
    * Fix issue where targets with no sources generated an empty compile sources phase, leading to issues in watchOS apps
    
    * Implement a better fix to only add a sources phase if there are sources
    
    * Add unit test
    
    * Fix lint issues

--- Sources/TuistGenerator/Generator/BuildPhaseGenerator.swift
+++ Sources/TuistGenerator/Generator/BuildPhaseGenerator.swift
@@ -254,8 +254,6 @@ final class BuildPhaseGenerator: BuildPhaseGenerating {
         pbxproj: PBXProj
     ) throws {
         let sourcesBuildPhase = PBXSourcesBuildPhase()
-        pbxproj.add(object: sourcesBuildPhase)
-        pbxTarget.buildPhases.append(sourcesBuildPhase)
 
         var buildFilesCache = Set<AbsolutePath>()
         let sortedFiles = files
@@ -310,6 +308,11 @@ final class BuildPhaseGenerator: BuildPhaseGenerating {
 
         pbxBuildFiles.forEach { pbxproj.add(object: $0) }
         sourcesBuildPhase.files = pbxBuildFiles
+
+        if !pbxBuildFiles.isEmpty {
+            pbxproj.add(object: sourcesBuildPhase)
+            pbxTarget.buildPhases.append(sourcesBuildPhase)
+        }
     }
 
     func generateHeadersBuildPhase(
