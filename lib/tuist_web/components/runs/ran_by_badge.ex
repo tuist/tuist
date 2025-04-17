03c8efbe0227aff23a5ece17188fd654ae1bcec7
Christoph Schmatzler <christoph@tuist.dev>
2025-04-17 10:53:53 +0200

    add conditions to ran_by badge (#1736)
    fixes
    https://appsignal.com/tuist-cloud/sites/6607b64d83eb6789e61c8ba7/exceptions/incidents/782

--- lib/tuist_web/components/runs/ran_by_badge.ex
+++ lib/tuist_web/components/runs/ran_by_badge.ex
@@ -2,18 +2,27 @@ defmodule TuistWeb.Runs.RanByBadge do
   @moduledoc """
   A component used to render a badge indicating who executed the run.
   """
-  use Phoenix.Component
+  use TuistWeb, :html
   use TuistWeb.Noora
 
   attr :run, :map, required: true
 
   def run_ran_by_badge_cell(assigns) do
     ~H"""
-    <%= if @run.is_ci do %>
-      <.badge_cell label="CI" icon="settings" color="information" style="light-fill" />
-    <% else %>
-      <.badge_cell label={@run.user.account.name} icon="user" color="primary" style="light-fill" />
-    <% end %>
+    <.badge_cell :if={@run.is_ci} label="CI" icon="settings" color="information" style="light-fill" />
+    <.badge_cell
+      :if={Map.has_key?(@run, :user)}
+      label={@run.user.account.name}
+      icon="user"
+      color="primary"
+      style="light-fill"
+    />
+    <.badge_cell
+      :if={!@run.is_ci and !Map.has_key?(@run, :user)}
+      label={gettext("Unknown")}
+      color="neutral"
+      style="light-fill"
+    />
     """
   end
 end
