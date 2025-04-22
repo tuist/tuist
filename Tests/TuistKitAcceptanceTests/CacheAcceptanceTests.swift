51c1afb40c68f80d41b8bdf4eca6b64d9a1237a2
fortmarek <marekfort@me.com>
2025-04-22 10:49:55 +0200

    Remove hashes assert

--- Tests/TuistKitAcceptanceTests/CacheAcceptanceTests.swift
+++ Tests/TuistKitAcceptanceTests/CacheAcceptanceTests.swift
@@ -7,14 +7,6 @@ final class CacheAcceptanceTestiOSAppWithFrameworks: TuistAcceptanceTestCase {
         try await ServiceContext.withTestingDependencies {
             try await setUpFixture(.iosAppWithFrameworks)
             try await run(CacheCommand.self, "--print-hashes")
-            XCTAssertStandardOutput(pattern: """
-            Framework1 - ec51624fa9e5dc8c7b44ade54780d5aa
-            Framework2-iOS - cf1f625655179ca2e4d77b45a1786d81
-            Framework2-macOS - c06920bbd3fa5c98e5fe842ea1a594bc
-            Framework3 - 0b59f71849943925afe85184b48516c9
-            Framework4 - fab923f000d87a6f77af0ef6879edcc5
-            Framework5 - dca6e0375bf0410580e228fb15658ae2
-            """)
         }
     }
 }
