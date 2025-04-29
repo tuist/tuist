8389627f82888593adb16284a411fc93fda4b887
Marek Fořt <marekfort@me.com>
2025-04-29 09:33:09 +0200

    Fix irreversible migrations (#1805)
    When removing columns, we _have_ to specify the type of the column or
    add explicit `up` and `down` methods. Otherwise, the rollback doesn't
    work.
    
    We should run `mix ecto.rollback` for _every_ new migration to make sure
    it's reversible. Otherwise, this leads to issues for our enterprise
    customers when they need to rollback to some previous version for
    whatever reason.

--- priv/repo/migrations/20250422091839_remove_cache_columns_from_accounts.exs
+++ priv/repo/migrations/20250422091839_remove_cache_columns_from_accounts.exs
@@ -4,9 +4,9 @@ defmodule Tuist.Repo.Migrations.RemoveCacheColumnsFromAccounts do
   def change do
     alter table(:accounts) do
       # excellent_migrations:safety-assured-for-next-line column_removed
-      remove :cache_download_event_count
+      remove :cache_download_event_count, :integer, default: 0
       # excellent_migrations:safety-assured-for-next-line column_removed
-      remove :cache_upload_event_count
+      remove :cache_upload_event_count, :integer, default: 0
     end
   end
 end
