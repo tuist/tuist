c2cb32c6d30c7f2eaba4b0fec966fa657b422b6a
Hilton Campbell <hcampbell@acorns.com>
2025-05-27 15:37:50 -0700

    Map sandbox errors

--- Sources/TuistLoader/Loaders/ManifestLoader.swift
+++ Sources/TuistLoader/Loaders/ManifestLoader.swift
@@ -355,6 +355,7 @@ public class ManifestLoader: ManifestLoading {
             let manifest = string[startTokenRange.upperBound ..< endTokenRange.lowerBound]
             return manifest.data(using: .utf8)!
         } catch {
+            try mapErrorIfNeeded(in: path, error: error)
             logUnexpectedImportErrorIfNeeded(in: path, error: error, manifest: manifest)
             logPluginHelperBuildErrorIfNeeded(in: path, error: error, manifest: manifest)
             throw error
@@ -458,7 +459,7 @@ public class ManifestLoader: ManifestLoading {
                         try? await rootDirectoryLocator.locate(from: path),
                     ].compactMap { $0 }
                 )
-                return ["/usr/bin/sandbox-exec", "-p", profile] + arguments
+                return ["sandbox-exec", "-p", profile] + arguments
             #else
                 return arguments
             #endif
@@ -498,6 +499,27 @@ public class ManifestLoader: ManifestLoading {
         ; Allow reading from specified paths
         \(Set(readOnlyPaths).map { "(allow file-read* (subpath \"\(try $0.realPath)\"))" }.joined(separator: "\n"))
         """
+    }
+
+    private func mapErrorIfNeeded(in path: AbsolutePath, error: Error) throws {
+        guard case let TuistSupport.SystemError.signalled(command, code, standardError) = error,
+              command == "sandbox-exec",
+              code == 5,
+              let errorMessage = String(data: standardError, encoding: .utf8) else { return }
+
+        throw ManifestLoaderError.manifestLoadingFailed(
+            path: path,
+            data: Data(),
+            context: """
+            Caught sandbox policy violation while loading manifest. This typically happens when the manifest attempts to access file system or network resources. To resolve this, you can:
+            1. Modify your manifest to avoid accessing file system or network resources, or
+            2. Disable the sandbox by setting `disableSandbox: true` in your Config.swift:
+               ```swift
+               let config = Config(project: .tuist(generationOptions: .options(disableSandbox: true)))
+               ```
+            \(errorMessage)
+            """
+        )
     }
 
     private func logUnexpectedImportErrorIfNeeded(in path: AbsolutePath, error: Error, manifest: Manifest) {
