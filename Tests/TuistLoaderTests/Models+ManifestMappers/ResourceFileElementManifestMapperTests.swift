db275b794b507a44f7d2a7769554733afeb0a779
Hilton Campbell <hcampbell@acorns.com>
2025-05-29 13:24:31 -0700

    Update ResourceFileElementManifestMapperTests

--- Tests/TuistLoaderTests/Models+ManifestMappers/ResourceFileElementManifestMapperTests.swift
+++ Tests/TuistLoaderTests/Models+ManifestMappers/ResourceFileElementManifestMapperTests.swift
@@ -43,7 +43,7 @@ final class ResourceFileElementManifestMapperTests: TuistUnitTestCase {
         }
     }
 
-    func test_from_outputs_a_warning_when_no_files_found() async throws {
+    func test_from_when_no_files_found() async throws {
         try await withMockedDependencies {
             // Given
             let temporaryPath = try temporaryPath()
@@ -65,14 +65,43 @@ final class ResourceFileElementManifestMapperTests: TuistUnitTestCase {
             )
 
             // Then
-            XCTAssertPrinterOutputContains(
+            XCTAssertPrinterOutputNotContains(
                 "No files found at: \(rootDirectory.appending(components: "Resources", "**"))"
             )
             XCTAssertEqual(model, [])
         }
     }
 
-    func test_from_outputs_a_warning_when_no_files_found_in_opaque_directory() async throws {
+    func test_from_outputs_a_warning_when_specific_file_not_found() async throws {
+        try await withMockedDependencies {
+            // Given
+            let temporaryPath = try temporaryPath()
+            let rootDirectory = temporaryPath
+            let generatorPaths = GeneratorPaths(
+                manifestDirectory: temporaryPath,
+                rootDirectory: rootDirectory
+            )
+
+            try await fileSystem.makeDirectory(at: rootDirectory.appending(component: "Resources"))
+
+            let manifest = ProjectDescription.ResourceFileElement.glob(pattern: "Resources/Image.png")
+
+            // When
+            let model = try await XcodeGraph.ResourceFileElement.from(
+                manifest: manifest,
+                generatorPaths: generatorPaths,
+                fileSystem: fileSystem
+            )
+
+            // Then
+            XCTAssertPrinterOutputContains(
+                "No files found at: \(rootDirectory.appending(components: "Resources", "Image.png"))"
+            )
+            XCTAssertEqual(model, [])
+        }
+    }
+
+    func test_from_when_no_files_found_in_opaque_directory() async throws {
         try await withMockedDependencies {
             // Given
             let temporaryPath = try temporaryPath()
@@ -99,7 +128,7 @@ final class ResourceFileElementManifestMapperTests: TuistUnitTestCase {
             )
 
             // Then
-            XCTAssertPrinterOutputContains(
+            XCTAssertPrinterOutputNotContains(
                 "No files found at: \(assetsDirectory.appending(components: "**"))"
             )
             XCTAssertEqual(model, [])
