8196415decaa98367977a1bfd3489ee6349e8427
Pedro <pedro@pepicrft.me>
2025-06-02 22:36:53 +0200

    Use a pool lock when running acceptance tests that require a simulator

--- Sources/TuistTesting/TestCase/TuistTest.swift
+++ Sources/TuistTesting/TestCase/TuistTest.swift
@@ -167,23 +167,3 @@ public enum TuistTest {
         return paths
     }
 }
-
-public struct TuistTestMockedDependenciesTrait: TestTrait, SuiteTrait, TestScoping {
-    let forwardingLogs: Bool
-
-    public func provideScope(
-        for _: Test,
-        testCase _: Test.Case?,
-        performing function: @Sendable () async throws -> Void
-    ) async throws {
-        try await _withMockedDependencies(forwardLogs: forwardingLogs) {
-            try await function()
-        }
-    }
-}
-
-extension Trait where Self == TuistTestMockedDependenciesTrait {
-    public static func withMockedDependencies(forwardingLogs: Bool = false) -> Self {
-        return Self(forwardingLogs: forwardingLogs)
-    }
-}
