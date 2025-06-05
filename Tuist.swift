79f9eafcae803f5e96f052627fe50a781452db2f
fortmarek <marekfort@me.com>
2025-06-05 23:37:41 +0200

    Remove Tuist.swift

--- Tuist.swift
+++
@@ -1,9 +0,0 @@
-import ProjectDescription
-
-let config = Config(
-    fullHandle: "tuist/tuist",
-    swiftVersion: .init("5.10"),
-    generationOptions: .options(
-        optionalAuthentication: true
-    )
-)
