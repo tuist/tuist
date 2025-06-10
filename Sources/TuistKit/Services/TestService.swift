f9b43e0e11ac8d515acb4ed7599034dab173bef8
Simon Cook <simon.cook@trademe.co.nz>
2025-06-11 00:55:42 +1200

    Further fix for issue where builds were not run when building for testing but not specifying a test plan (#7643)
    * Fix an issue where building for testing would incorrectly determine there were no tests to run when building a scheme with multiple test plans and none specified
    
    * Update Sources/TuistKit/Services/TestService.swift
    
    * Update Sources/TuistKit/Services/TestService.swift
    
    * Further fix issue where build aborts when it shouldn't
    
    * Add test
    
    ---------
    
    Co-authored-by: Marek Fořt <marekfort@me.com>

--- Sources/TuistKit/Services/TestService.swift
+++ Sources/TuistKit/Services/TestService.swift
@@ -781,7 +781,8 @@ final class TestService { // swiftlint:disable:this type_body_length
             testPlan: testPlanConfiguration?.testPlan,
             testTargets: testTargets,
             skipTestTargets: skipTestTargets,
-            graphTraverser: graphTraverser
+            graphTraverser: graphTraverser,
+            action: action
         )
         else {
             throw TestServiceError.schemeWithoutTestableTargets(
