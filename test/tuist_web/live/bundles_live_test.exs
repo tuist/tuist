b6bd60094a11db16ca31c9f3165d4aca965a8be0
Marek Fořt <marekfort@me.com>
2025-06-20 09:50:28 +0200

    Fix server error when previous download size is nil (#1986)
    Resolves:
    https://appsignal.com/tuist-cloud/sites/6607b64d83eb6789e61c8ba7/exceptions/incidents/918/samples/timestamp/2025-06-20T05:37:00Z
    
    The previous bundle download size might be `nil` even when the previous
    bundle exists, so we need to check for that when deriving the trend.

--- test/tuist_web/live/bundles_live_test.exs
+++ test/tuist_web/live/bundles_live_test.exs
@@ -63,4 +63,35 @@ defmodule TuistWeb.BundlesLiveTest do
     # Then
     assert has_element?(lv, "span", "0 MB")
   end
+
+  test "defaults to 0.0% download size trend when previous bundle download_size is nil", %{
+    conn: conn,
+    organization: organization,
+    project: project
+  } do
+    # Given
+    _previous_bundle =
+      BundlesFixtures.bundle_fixture(
+        project: project,
+        name: "TestApp",
+        install_size: 1000,
+        download_size: nil,
+        inserted_at: DateTime.add(DateTime.utc_now(), -31, :day)
+      )
+
+    _last_bundle =
+      BundlesFixtures.bundle_fixture(
+        project: project,
+        name: "TestApp",
+        install_size: 1500,
+        download_size: 1000,
+        inserted_at: DateTime.utc_now()
+      )
+
+    # When
+    {:ok, lv, _html} = live(conn, ~p"/#{organization.account.name}/#{project.name}/bundles")
+
+    # Then
+    assert has_element?(lv, "#widget-download-size span", "0.0%")
+  end
 end
