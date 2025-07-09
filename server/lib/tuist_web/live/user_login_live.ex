f7c429216f9b165aa1ae3dd517709086fff82398
Christoph Schmatzler <christoph@tuist.dev>
2025-07-09 16:40:50 +0200

    Fix CI failures (#7798)
    I'm clearly not used to having enough checks that the list is
    scrollable. Oops.
    
    Signed-off-by: Christoph Schmatzler <christoph@tuist.dev>

--- server/lib/tuist_web/live/user_login_live.ex
+++ server/lib/tuist_web/live/user_login_live.ex
@@ -129,7 +129,7 @@ defmodule TuistWeb.UserLoginLive do
               error={Flash.get(@flash, :error)}
               show_error_message={false}
               required
-              tabindex="1"
+              tabindex={1}
             />
             <.text_input
               field={@form[:password]}
@@ -140,14 +140,14 @@ defmodule TuistWeb.UserLoginLive do
               error={Flash.get(@flash, :error)}
               show_error_message={false}
               required
-              tabindex="2"
+              tabindex={2}
             />
             <div data-part="remember-me">
               <.checkbox
                 id="remember_me"
                 field={@form[:remember_me]}
                 label={gettext("Keep me logged in")}
-                tabindex="3"
+                tabindex={3}
               />
               <.link_button
                 navigate={~p"/users/reset_password"}
@@ -156,7 +156,7 @@ defmodule TuistWeb.UserLoginLive do
                 label={gettext("Forgot password?")}
               />
             </div>
-            <.button variant="primary" size="large" label={gettext("Log in")} tabindex="4" />
+            <.button variant="primary" size="large" label={gettext("Log in")} tabindex={4} />
           </.form>
         </div>
         <div :if={@mail_configured?} data-part="bottom-link">
