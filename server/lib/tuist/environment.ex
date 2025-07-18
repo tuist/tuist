42aed1f631aa3f6f03be9fe21483f1d2240455b0
Pedro <pedro@pepicrft.me>
2025-07-18 13:05:43 +0200

    Revert "Normalize secrets handling to use string keys consistently"
    This reverts commit bd062aa9860011bbeabb60e0834975bcc64ec052.

--- server/lib/tuist/environment.ex
+++ server/lib/tuist/environment.ex
@@ -503,13 +503,11 @@ defmodule Tuist.Environment do
 
     default_value = Keyword.get(opts, :default_value)
 
-    string_keys = Enum.map(keys, &to_string/1)
-
     value =
       if System.get_env(env_variable) do
         System.get_env(env_variable)
       else
-        get_in(secrets, string_keys)
+        get_in(secrets, keys)
       end
 
     if is_nil(value) do
@@ -538,7 +536,7 @@ defmodule Tuist.Environment do
         |> File.read!()
         |> YamlElixir.read_from_string()
 
-      to_string_map(secrets_map)
+      to_atom_map(secrets_map)
     else
       master_key_path = Path.join("priv/secrets", "#{Atom.to_string(env())}.key")
       master_key_env_variable = "MASTER_KEY"
@@ -562,11 +560,11 @@ defmodule Tuist.Environment do
     end
   end
 
-  defp to_string_map(map) when is_map(map) do
-    Map.new(map, fn {k, v} -> {to_string(k), to_string_map(v)} end)
+  defp to_atom_map(map) when is_map(map) do
+    Map.new(map, fn {k, v} -> {String.to_atom(k), to_atom_map(v)} end)
   end
 
-  defp to_string_map(value) do
+  defp to_atom_map(value) do
     value
   end
 end
