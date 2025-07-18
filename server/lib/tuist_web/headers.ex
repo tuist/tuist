d2d036ef9b84b41009c0e64558034c3d28f2ef51
Pedro Piñera Buendía <663605+pepicrft@users.noreply.github.com>
2025-07-18 18:30:39 +0200

    Fix CLI version header handling and improve code formatting (#7882)

--- server/lib/tuist_web/headers.ex
+++ server/lib/tuist_web/headers.ex
@@ -6,13 +6,20 @@ defmodule TuistWeb.Headers do
 
   def cli_version_header, do: @cli_version_header
 
+  def get_cli_version_string(conn) do
+    case {:version_string, conn |> Plug.Conn.get_req_header(cli_version_header()) |> List.first()} do
+      {:version_string, version_string} when not is_nil(version_string) -> version_string
+      _ -> nil
+    end
+  end
+
   def get_cli_version(conn) do
-    with {:version_string, version_string} when not is_nil(version_string) <-
-           {:version_string, conn |> Plug.Conn.get_req_header(cli_version_header()) |> List.first()},
-         {:version, {:ok, version}} <- {:version, Version.parse(version_string)} do
+    with version_string when not is_nil(version_string) <- get_cli_version_string(conn),
+         {:ok, version} <- Version.parse(version_string) do
       version
     else
-      _ -> nil
+      _ ->
+        nil
     end
   end
 
