ec5c88b92372d08a849319ed6ab65bd53c66c22a
Pedro Piñera Buendía <663605+pepicrft@users.noreply.github.com>
2025-08-18 07:57:34 +0200

    Update server/config/config.exs
    Co-authored-by: Christoph Schmatzler <christoph@tuist.dev>

--- server/config/config.exs
+++ server/config/config.exs
@@ -84,7 +84,7 @@ config :peep, :bucket_calculator, Tuist.PromEx.Buckets
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-# Using the default EST storage leads to [lock contention](https://github.com/akoutmos/prom_ex/issues/248#issuecomment-2709045234)
+# Using the default ETS storage leads to [lock contention](https://github.com/akoutmos/prom_ex/issues/248#issuecomment-2709045234)
 # and causes the CPU clogging with cascading effects (e.g. connections dropping).
 # This configures prom_ex to use a different storage using [this](https://github.com/plausible/analytics/pull/5130/)
 # as a reference
