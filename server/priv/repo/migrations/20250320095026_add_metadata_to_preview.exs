6e268b41134c23003f8b1fc9a186e18b6870a87b
Christoph Schmatzler <christoph@schmatzler.com>
2025-08-22 11:04:05 +0000

    remove if_exists
    Signed-off-by: Christoph Schmatzler <christoph@schmatzler.com>

--- server/priv/repo/migrations/20250320095026_add_metadata_to_preview.exs
+++ server/priv/repo/migrations/20250320095026_add_metadata_to_preview.exs
@@ -13,12 +13,12 @@ defmodule Tuist.Repo.Migrations.AddMetadataToPreview do
   end
 
   def down do
-    drop_if_exists index(:previews, [:project_id, :git_branch])
+    drop index(:previews, [:project_id, :git_branch])
 
     alter table(:previews) do
-      remove_if_exists :ran_by_account_id, references(:accounts, on_delete: :nilify_all)
-      remove_if_exists :git_commit_sha, :string
-      remove_if_exists :git_branch, :string
+      remove :ran_by_account_id, references(:accounts, on_delete: :nilify_all)
+      remove :git_commit_sha, :string
+      remove :git_branch, :string
     end
   end
 end
