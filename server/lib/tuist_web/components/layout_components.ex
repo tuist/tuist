e245e8922fae280491323c59a3c00477a580177c
Pedro <pedro@pepicrft.me>
2025-09-05 17:12:25 +0200

    Fix

--- server/lib/tuist_web/components/layout_components.ex
+++ server/lib/tuist_web/components/layout_components.ex
@@ -106,7 +106,7 @@ defmodule TuistWeb.LayoutComponents do
   def head_analytics_scripts(assigns) do
     posthog_opts =
       Map.merge(
-        %{api_host: "https://eu.i.posthog.com", person_profiles: "identified_only"},
+        %{api_host: Tuist.Environment.posthog_url(), person_profiles: "identified_only"},
         if(TuistWeb.Authentication.authenticated?(assigns),
           do: %{bootstrap: %{distinctID: TuistWeb.Authentication.current_user(assigns).id}},
           else: %{persistence: "memory"}
@@ -150,7 +150,7 @@ defmodule TuistWeb.LayoutComponents do
     </script>
     <script :if={Tuist.Environment.analytics_enabled?()} nonce={get_csp_nonce()}>
       !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init Ce js Ls Te Fs Ds capture Ye calculateEventProperties Us register register_once register_for_session unregister unregister_for_session Ws getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSurveysLoaded onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey canRenderSurveyAsync identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty Bs zs createPersonProfile Hs Ms Gs opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing get_explicit_consent_status is_capturing clear_opt_in_out_capturing Ns debug L qs getPageViewId captureTraceFeedback captureTraceMetric".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-      posthog.init('phc_fpR9c0Hs5H5VXUsupU1I0WlEq366FaZH6HJR3lRIWVR', <%= raw JSON.encode!(@posthog_opts) %>)
+      posthog.init('<%= Tuist.Environment.posthog_api_key() %>', <%= raw JSON.encode!(@posthog_opts) %>)
     </script>
     <script
       :if={Tuist.Environment.analytics_enabled?() and not is_nil(@posthog_identity)}
