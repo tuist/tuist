0f946734d9f97a6d5fe38192a33327758d76ca71
Christoph Schmatzler <christoph@schmatzler.com>
2025-09-06 13:06:38 +0200

    cs/disable-http2

--- server/lib/tuist/environment.ex
+++ server/lib/tuist/environment.ex
@@ -204,7 +204,7 @@ defmodule Tuist.Environment do
   def s3_protocols(secrets \\ secrets()) do
     case get([:s3, :protocol], secrets) do
       protocol when is_binary(protocol) -> [String.to_atom(protocol)]
-      _ -> [:http2, :http1]
+      _ -> [:http1]
     end
   end
 
