6b6b047c368eb503b79af51982ed8f16ad040dc1
Christoph Schmatzler <christoph@schmatzler.com>
2025-09-08 12:22:28 +0000

    finish page design

--- server/lib/tuist_web/marketing/live/marketing_qa_live.ex
+++ server/lib/tuist_web/marketing/live/marketing_qa_live.ex
@@ -5,33 +5,21 @@ defmodule TuistWeb.Marketing.MarketingQALive do
 
   @mautic_qa_segment 3
 
-  def mount(params, session, socket) do
+  def mount(_params, session, socket) do
     socket =
       socket
       |> attach_hook(:assign_current_path, :handle_params, fn _params, url, socket ->
         uri = URI.parse(url)
-        current_path = if(is_nil(uri.query), do: uri.path, else: "#{uri.path}?#{uri.query}")
+        current_path = if uri.query, do: "#{uri.path}?#{uri.query}", else: uri.path
         {:cont, assign(socket, current_path: current_path)}
       end)
       |> TuistWeb.Authentication.mount_current_user(session)
-      |> then(
-        &if(is_nil(socket.assigns[:current_user]),
-          do: &1,
-          else:
-            assign(&1,
-              in_list_message:
-                if(Tuist.Mautic.email_in_segment?(&1.assigns[:current_user].email, @mautic_qa_segment),
-                  do: gettext("You are already in the waiting list")
-                )
-            )
-        )
-      )
-      |> assign(:form, to_form(%{"email" => ""}))
+      |> assign_initial_state()
 
     {:ok, socket}
   end
 
-  def handle_params(params, _url, socket) do
+  def handle_params(_params, _url, socket) do
     {:noreply,
      socket
      |> assign(:head_title, gettext("Tuist QA"))
@@ -45,18 +33,35 @@ defmodule TuistWeb.Marketing.MarketingQALive do
      |> assign(:head_description, gettext("Automate testing your apps for Apple platforms using agents."))}
   end
 
-  def handle_event("add-to-waiting-list", params, socket) do
-    current_user = socket.assigns[:current_user]
+  def handle_event("add-to-waiting-list", %{"email" => email}, socket) do
+    {:ok, _} = Tuist.Mautic.add_email_to_segment(email, @mautic_qa_segment)
 
-    email =
-      if is_nil(current_user) do
-        params["email"]
-      else
-        current_user.email
+    socket =
+      socket
+      |> assign(:in_waiting_list?, true)
+      |> assign(:email_submitted?, true)
+
+    {:noreply, socket}
+  end
+
+  defp assign_initial_state(socket) do
+    socket
+    |> assign_waiting_list_status()
+    |> assign(:form, to_form(%{"email" => ""}))
+    |> assign(:email_submitted?, false)
+  end
+
+  defp assign_waiting_list_status(socket) do
+    in_waiting_list? =
+      case socket.assigns[:current_user] do
+        nil -> false
+        current_user -> user_in_waiting_list?(current_user.email)
       end
 
-    {:ok, _} = Tuist.Mautic.add_email_to_segment(email, @mautic_qa_segment)
-    socket = assign(socket, :in_list_message, gettext("Added to the waiting list"))
-    {:noreply, socket}
+    assign(socket, :in_waiting_list?, in_waiting_list?)
+  end
+
+  defp user_in_waiting_list?(email) do
+    Tuist.Mautic.email_in_segment?(email, @mautic_qa_segment)
   end
 end
