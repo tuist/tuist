5cb05e94919d790824fb6388ddd858c0d7d1aeea
Pedro <pedro@pepicrft.me>
2025-09-15 17:03:48 +0200

    Give control to the caller to select the selected targets for caching

--- cli/Sources/TuistCache/TargetReplacementDecider.swift
+++ cli/Sources/TuistCache/TargetReplacementDecider.swift
@@ -1,9 +1,8 @@
-import Foundation
 import TuistCore
 import XcodeGraph
 
 /// A decider that determines whether a target should be replaced with a cached binary.
-protocol TargetReplacementDeciding {
+public protocol TargetReplacementDeciding {
     /// Determines whether a target should be replaced with a cached binary.
     /// - Parameters:
     ///   - project: The project containing the target.
@@ -13,18 +12,25 @@ protocol TargetReplacementDeciding {
 }
 
 /// A decider that chooses to replace only external targets.
-struct ExternalOnlyTargetReplacementDecider: TargetReplacementDeciding {
-    func shouldReplace(project: Project, target _: Target) -> Bool {
-        project.isExternal
+public struct ExternalOnlyTargetReplacementDecider: TargetReplacementDeciding {
+    public init() {}
+
+    public func shouldReplace(project: Project, target _: Target) -> Bool {
+        switch project.type {
+        case .external:
+            true
+        case .local:
+            false
+        }
     }
 }
 
 /// A decider that chooses to replace all targets, except for those that are explicitly excluded.
-struct AllPossibleTargetReplacementDecider: TargetReplacementDeciding {
+public struct AllPossibleTargetReplacementDecider: TargetReplacementDeciding {
     private let exceptionNames: Set<String>
     private let exceptionTags: Set<String>
 
-    init(exceptions: Set<TargetQuery>) {
+    public init(exceptions: Set<TargetQuery>) {
         var names = Set<String>()
         var tags = Set<String>()
         for exception in exceptions {
@@ -39,7 +45,7 @@ struct AllPossibleTargetReplacementDecider: TargetReplacementDeciding {
         exceptionTags = tags
     }
 
-    func shouldReplace(project _: Project, target: Target) -> Bool {
+    public func shouldReplace(project _: Project, target: Target) -> Bool {
         if exceptionNames.contains(target.name) {
             return false
         }
@@ -47,16 +53,5 @@ struct AllPossibleTargetReplacementDecider: TargetReplacementDeciding {
             return false
         }
         return true
-    }
-}
-
-extension Project {
-    fileprivate var isExternal: Bool {
-        switch type {
-        case .external:
-            return true
-        case .local:
-            return false
-        }
     }
 }
