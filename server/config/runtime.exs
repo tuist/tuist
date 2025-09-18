b535f42dfe137ad1b3317a4616791595cf187181
Marek Fořt <marekfort@me.com>
2025-09-18 18:27:13 +0200

    chore(server): don't run cron oban jobs in dev (#8239)

--- server/config/runtime.exs
+++ server/config/runtime.exs
@@ -357,7 +357,7 @@ config :tuist, Oban,
     {Oban.Plugins.Lifeline, rescue_after: to_timeout(minute: 30)},
     {Oban.Plugins.Cron,
      crontab:
-       if(Tuist.Environment.tuist_hosted?(),
+       if(Tuist.Environment.tuist_hosted?() and env in [:prod, :stag, :can],
          do: [
            {"0 10 * * 1-5", Tuist.Ops.DailySlackReportWorker},
            {"0 * * * 1-5", Tuist.Ops.HourlySlackReportWorker},
