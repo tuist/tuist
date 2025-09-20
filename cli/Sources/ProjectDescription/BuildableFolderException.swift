b1ea440f85b48bfb9b73ce361f6110cb1391102f
Pedro <pedro@pepicrft.me>
2025-09-20 11:01:04 +0200

    Add interfaces

---
+++ cli/Sources/ProjectDescription/BuildableFolderException.swift
@@ -0,0 +1,17 @@
+/// Represents exceptions for a buildable folder, such as files to exclude or specific compiler flags to apply.
+public struct BuildableFolderException: Sendable, Codable, Equatable, Hashable {
+    /// A list of absolute paths to files excluded from the buildable folder.
+    public var excluded: [Path]
+
+    /// A dictionary mapping files (referenced by their absolute path) to the compiler flags to apply.
+    public var compilerFlags: [Path: String]
+
+    /// Creates a new exception for a buildable folder.
+    /// - Parameters:
+    ///   - exclued: An array of absolute paths to files that should be excluded from the buildable folder.
+    ///   - compilerFlags: A dictionary mapping absolute file paths to specific compiler flags to apply to those files.
+    public init(excluded: [Path], compilerFlags: [Path: String]) {
+        self.excluded = excluded
+        self.compilerFlags = compilerFlags
+    }
+}
