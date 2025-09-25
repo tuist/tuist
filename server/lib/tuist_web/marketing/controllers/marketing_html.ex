c79ab935d30e2096ebb189c99119a544754dc5b4
Pedro Piñera <pedro@pepicrft.me>
2025-09-25 17:55:07 +0200

    New website

--- server/lib/tuist_web/marketing/controllers/marketing_html.ex
+++ server/lib/tuist_web/marketing/controllers/marketing_html.ex
@@ -1,8 +1,11 @@
 defmodule TuistWeb.Marketing.MarketingHTML do
   use TuistWeb, :html
+  use Noora
 
   import TuistWeb.Marketing.MarketingComponents
   import TuistWeb.Marketing.MarketingLogos
+  import TuistWeb.Marketing.MarketingOrgLogos
+  import TuistWeb.Marketing.TestimonialOrgLogos
 
   embed_templates "marketing_html/*"
 end
