0cfd5ab9a420999a3a5961313a1d162f4ac335d7
Pedro Piñera <pedro@pepicrft.me>
2025-10-02 16:07:09 +0200

    Reapply "Create the ClickHouse command_events table in on-premise environments"
    This reverts commit 3449e2ee923c6eefe1f2a5fda868ce3847b83075.

--- server/priv/ingest_repo/migrations/20250702083710_create_command_events_clickhouse.exs
+++ server/priv/ingest_repo/migrations/20250702083710_create_command_events_clickhouse.exs
@@ -3,7 +3,8 @@ defmodule Tuist.ClickHouseRepo.Migrations.CreateCommandEventsClickhouse do
 
   def up do
     # In production, these are created by Clickhouse Pipes.
-    if Tuist.Environment.dev?() || Tuist.Environment.test?() do
+    if Tuist.Environment.dev?() || Tuist.Environment.test?() ||
+         not Tuist.Environment.tuist_hosted?() do
       # excellent_migrations:safety-assured-for-next-line raw_sql_executed
       execute """
        CREATE TABLE command_events
