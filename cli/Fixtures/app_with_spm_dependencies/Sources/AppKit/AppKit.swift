cc771462b653eb726e99a5225af68864ea6a4b9f
Pedro Piñera <pedro@pepicrft.me>
2025-10-07 15:39:12 +0200

    Revert change

--- cli/Fixtures/app_with_spm_dependencies/Sources/AppKit/AppKit.swift
+++ cli/Fixtures/app_with_spm_dependencies/Sources/AppKit/AppKit.swift
@@ -1,4 +1,3 @@
-import AIProxy
 import Alamofire
 import FirebaseCrashlytics
 
@@ -6,12 +5,5 @@ public enum AppKit {
     public static func start() {
         // Use Alamofire to make sure it links fine
         _ = AF.download("http://www.tuist.io")
-        AIProxy.configure(
-            logLevel: .debug,
-            printRequestBodies: false, // Flip to true for library development
-            printResponseBodies: false, // Flip to true for library development
-            resolveDNSOverTLS: true,
-            useStableID: false, // Please see the docstring if you'd like to enable this
-        )
     }
 }
