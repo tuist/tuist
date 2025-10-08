5075b65186a525b4c7017176d03fee949fb2c18a
Pedro Piñera <pedro@pepicrft.me>
2025-10-08 13:46:25 +0200

    Add project

---
+++ cli/Fixtures/xcode_project_with_ios_app_and_cas/CAS/CASApp.swift
@@ -0,0 +1,17 @@
+//
+//  CASApp.swift
+//  CAS
+//
+//  Created by Pedro Piñera Buendía on 08.10.25.
+//
+
+import SwiftUI
+
+@main
+struct CASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cli/Fixtures/xcode_project_with_ios_app_and_cas/CAS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CAS
+//
+//  Created by Pedro Piñera Buendía on 08.10.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
