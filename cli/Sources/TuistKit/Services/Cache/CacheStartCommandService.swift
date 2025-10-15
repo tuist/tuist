3cbb2570b434de88e77f8e5dd0f014cf78f470f3
fortmarek <marekfort@me.com>
2025-10-15 10:05:33 +0200

    Remove fatalError in CacheStartCommandService

--- cli/Sources/TuistKit/Services/Cache/CacheStartCommandService.swift
+++ cli/Sources/TuistKit/Services/Cache/CacheStartCommandService.swift
@@ -10,6 +10,17 @@ import TuistRootDirectoryLocator
 import TuistServer
 import TuistSupport
 
+enum CacheStartCommandServiceError: Equatable, LocalizedError {
+    case missingFullHandle
+    
+    var errorDescription: String? {
+        switch self {
+        case .missingFullHandle:
+            return "The 'Tuist.swift' file is missing a fullHandle. See how to set up a Tuist project at: https://docs.tuist.dev/en/server/introduction/accounts-and-projects#projects"
+        }
+    }
+}
+
 struct CacheStartCommandService {
     private let configLoader: ConfigLoading
     private let serverEnvironmentService: ServerEnvironmentServicing
@@ -30,8 +41,9 @@ struct CacheStartCommandService {
     ) async throws {
         let config = try await configLoader.loadConfig(path: path)
 
-        guard let fullHandle = config.fullHandle
-        else { fatalError() }
+        guard let fullHandle = config.fullHandle else {
+            throw CacheStartCommandServiceError.missingFullHandle
+        }
         let socketPath = Environment.current.stateDirectory
             .appending(component: "\(fullHandle.replacingOccurrences(of: "/", with: "_")).sock")
         if try await !fileSystem.exists(socketPath.parentDirectory, isDirectory: true) {
