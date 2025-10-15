526a48bec6f5159800e922d6eea3ffb4dc8d0f11
fortmarek <marekfort@me.com>
2025-10-15 12:39:39 +0200

    chore(cli): add back cache acceptance test

--- cli/Tests/TuistCacheEEAcceptanceTests/TuistCacheEEAcceptanceTests.swift
+++ cli/Tests/TuistCacheEEAcceptanceTests/TuistCacheEEAcceptanceTests.swift
@@ -198,55 +198,54 @@ struct TuistCacheEEAcceptanceTests {
         TuistTest.expectLogs("There are no tests to run, exiting early..")
     }
 
-//    TODO: Uncomment this once CAS is on canary
-//    @Test(
-//        .inTemporaryDirectory,
-//        .withMockedEnvironment(inheritingVariables: ["PATH", "TUIST_URL"]),
-//        .withMockedNoora,
-//        .withMockedLogger(forwardLogs: true),
-//        .withFixtureConnectedToCanary("xcode_project_with_ios_app_and_cas"),
-//        .withTestingSimulator("iPhone 17")
-//    ) func xcode_project_with_ios_app_and_cas() async throws {
-//        // Given
-//        let fixtureDirectory = try #require(TuistTest.fixtureDirectory)
-//        let xcodeprojPath = fixtureDirectory.appending(component: "App.xcodeproj")
-//        let simulator = try #require(Simulator.testing)
-//        let fileSystem = FileSystem()
-//        let temporaryDirectory = try #require(FileSystem.temporaryTestDirectory)
-//        let environment = try #require(Environment.mocked)
-//        environment.stateDirectory = try await fileSystem.currentWorkingDirectory()
-//
-//        let backgroundTask = Task {
-//            while !Task.isCancelled {
-//                try await TuistTest.run(CacheStartCommand.self, ["--path", fixtureDirectory.pathString])
-//            }
-//        }
-//
-//        defer {
-//            backgroundTask.cancel()
-//        }
-//
-//        let fixtureFullHandle = try #require(TuistTest.fixtureFullHandle)
-//        let remoteCacheServicePath = environment.stateDirectory
-//            .appending(component: "\(fixtureFullHandle.replacingOccurrences(of: "/", with: "_")).sock")
-//
-//        let arguments = [
-//            "-scheme", "App",
-//            "-destination", simulator.description,
-//            "-project", xcodeprojPath.pathString,
-//            "-derivedDataPath", temporaryDirectory.pathString,
-//            "CODE_SIGN_IDENTITY=",
-//            "CODE_SIGNING_REQUIRED=NO",
-//            "CODE_SIGNING_ALLOWED=NO",
-//            "COMPILATION_CACHE_REMOTE_SERVICE_PATH=\(remoteCacheServicePath.pathString)"
-//        ]
-//        try await TuistTest.run(XcodeBuildBuildCommand.self, arguments)
-//        TuistTest.expectLogs("note: 0 hits / 60 cacheable tasks (0%)")
-//        resetUI()
-//
-//        try await fileSystem.remove(temporaryDirectory)
-//
-//        try await TuistTest.run(XcodeBuildBuildCommand.self, arguments)
-//        TuistTest.expectLogs("note: 60 hits / 60 cacheable tasks (100%)")
-//    }
+    @Test(
+        .inTemporaryDirectory,
+        .withMockedEnvironment(inheritingVariables: ["PATH"]),
+        .withMockedNoora,
+        .withMockedLogger(forwardLogs: true),
+        .withFixtureConnectedToCanary("xcode_project_with_ios_app_and_cas"),
+        .withTestingSimulator("iPhone 17")
+    ) func xcode_project_with_ios_app_and_cas() async throws {
+        // Given
+        let fixtureDirectory = try #require(TuistTest.fixtureDirectory)
+        let xcodeprojPath = fixtureDirectory.appending(component: "App.xcodeproj")
+        let simulator = try #require(Simulator.testing)
+        let fileSystem = FileSystem()
+        let temporaryDirectory = try #require(FileSystem.temporaryTestDirectory)
+        let environment = try #require(Environment.mocked)
+        environment.stateDirectory = try await fileSystem.currentWorkingDirectory()
+
+        let backgroundTask = Task {
+            while !Task.isCancelled {
+                try await TuistTest.run(CacheStartCommand.self, ["--path", fixtureDirectory.pathString])
+            }
+        }
+
+        defer {
+            backgroundTask.cancel()
+        }
+
+        let fixtureFullHandle = try #require(TuistTest.fixtureFullHandle)
+        let remoteCacheServicePath = environment.stateDirectory
+            .appending(component: "\(fixtureFullHandle.replacingOccurrences(of: "/", with: "_")).sock")
+
+        let arguments = [
+            "-scheme", "App",
+            "-destination", simulator.description,
+            "-project", xcodeprojPath.pathString,
+            "-derivedDataPath", temporaryDirectory.pathString,
+            "CODE_SIGN_IDENTITY=",
+            "CODE_SIGNING_REQUIRED=NO",
+            "CODE_SIGNING_ALLOWED=NO",
+            "COMPILATION_CACHE_REMOTE_SERVICE_PATH=\(remoteCacheServicePath.pathString)"
+        ]
+        try await TuistTest.run(XcodeBuildBuildCommand.self, arguments)
+        TuistTest.expectLogs("note: 0 hits / 60 cacheable tasks (0%)")
+        resetUI()
+
+        try await fileSystem.remove(temporaryDirectory)
+
+        try await TuistTest.run(XcodeBuildBuildCommand.self, arguments)
+        TuistTest.expectLogs("note: 60 hits / 60 cacheable tasks (100%)")
+    }
 }
