1e2f9c66f854999aeed1f8a5ebb9a5f82e6593f4
Pedro Piñera <pedro@pepicrft.me>
2025-10-19 19:14:07 +0200

    Add FAQ section

--- server/assets/marketing_next/js/hooks.js
+++ server/assets/marketing_next/js/hooks.js
@@ -1,11 +1,13 @@
 import { NavbarDropdown } from "./hooks/navbar-dropdown.js";
 import { MobileMenu } from "./hooks/mobile-menu.js";
 import { MobileMenuDropdown } from "./hooks/mobile-menu-dropdown.js";
+import { FaqAccordion } from "./hooks/faq-accordion.js";
 
 const Hooks = {
   NavbarDropdown,
   MobileMenu,
   MobileMenuDropdown,
+  FaqAccordion,
 };
 
 export { Hooks };
