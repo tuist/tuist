e0784b9e59ee0759caefca8c9ccd0b35d9c27dcf
Christoph Schmatzler <christoph@schmatzler.com>
2025-10-29 14:51:31 +0000

    more vibes
    Signed-off-by: Christoph Schmatzler <christoph@schmatzler.com>

--- cache/lib/cache_web/plugs/conditional_parser.ex
+++ cache/lib/cache_web/plugs/conditional_parser.ex
@@ -6,21 +6,20 @@ defmodule CacheWeb.Plugs.ConditionalParser do
   don't have request bodies and parsing adds unnecessary overhead.
   """
 
-  def init(_opts), do: nil
+  @parser_opts Plug.Parsers.init(
+                 parsers: [:urlencoded, :multipart, :json],
+                 pass: ["*/*"],
+                 json_decoder: Phoenix.json_library(),
+                 body_reader: {CacheWeb.Plugs.CacheBodyReader, :read_body, []}
+               )
+
+  def init(_opts), do: @parser_opts
 
   def call(%Plug.Conn{method: "GET"} = conn, _opts) do
     conn
   end
 
-  def call(conn, _opts) do
-    Plug.Parsers.call(
-      conn,
-      Plug.Parsers.init(
-        parsers: [:urlencoded, :multipart, :json],
-        pass: ["*/*"],
-        json_decoder: Phoenix.json_library(),
-        body_reader: {CacheWeb.Plugs.CacheBodyReader, :read_body, []}
-      )
-    )
+  def call(conn, parser_opts) do
+    Plug.Parsers.call(conn, parser_opts)
   end
 end
