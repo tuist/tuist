f0382cf18cfb1ca33dcdc768dcec836bbeddc328
Pedro Piñera Buendía <663605+pepicrft@users.noreply.github.com>
2025-10-30 19:11:36 +0100

    feat(cli): Add support for passing arguments to SwiftPM (#8544)

--- Tuist.swift
+++ Tuist.swift
@@ -1,11 +1,12 @@
 import ProjectDescription
 
-let config = Config(
+let tuist = Tuist(
     fullHandle: "tuist/tuist",
-    swiftVersion: .init("5.10"),
-    generationOptions: .options(
-        optionalAuthentication: true,
-        disableSandbox: true,
-        enableCaching: true
+    project: .tuist(
+        generationOptions: .options(
+            optionalAuthentication: true,
+            disableSandbox: true,
+            enableCaching: true
+        )
     )
 )
