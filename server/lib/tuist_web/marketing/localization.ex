092bce71a0dc43e87f07fb9e8388366f2af2ebe4
Pedro Piñera <pedro@pepicrft.me>
2025-11-11 12:25:52 +0100

    Add Korean language & testimonials

--- server/lib/tuist_web/marketing/localization.ex
+++ server/lib/tuist_web/marketing/localization.ex
@@ -10,7 +10,7 @@ defmodule TuistWeb.Marketing.Localization do
 
   import Plug.Conn
 
-  @additional_locales ["ru"]
+  @additional_locales ["ko", "ru"]
 
   def init(:put_locale), do: :put_locale
   def init(:redirect_to_localized_route), do: :redirect_to_localized_route
