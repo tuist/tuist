210d1e6cf61c467b9d9b3c18c81340815b84ab71
Christoph Schmatzler <christoph@schmatzler.com>
2025-11-11 10:58:07 +0000

    test
    Signed-off-by: Christoph Schmatzler <christoph@schmatzler.com>

--- cache/lib/cache_web/controllers/up_controller.ex
+++ cache/lib/cache_web/controllers/up_controller.ex
@@ -2,6 +2,6 @@ defmodule CacheWeb.UpController do
   use CacheWeb, :controller
 
   def index(conn, _params) do
-    send_resp(conn, :ok, "UP! " <> System.get_env("KAMAL_VERSION"))
+    send_resp(conn, :ok, "UP! Version: " <> System.get_env("KAMAL_VERSION"))
   end
 end
