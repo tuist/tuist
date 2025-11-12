5ae402c1e6c3ea5475a30411c2f2d7e288495d23
Christoph Schmatzler <christoph@schmatzler.com>
2025-11-12 15:48:42 +0000

    fix(cache): ci

--- cache/platform/users.nix
+++ cache/platform/users.nix
@@ -10,4 +10,12 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL/I+/2QT47raegzMIyhwMEPKarJP/+Ox9ewA4ZFJwk/ cschmatzler@tahani"
     ];
   };
+
+  users.users.github-actions = {
+    isNormalUser = true;
+    extraGroups = ["docker"];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII0j+FOf7IUXTuH8oMXRCxsnqhvAek+HT7gSF8mRIf5q github-actions@github"
+    ];
+  };
 }
