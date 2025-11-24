a18cdeb104df0f4e34dfde6d3deee8293be2c2f0
Pedro Piñera <pedro@pepicrft.me>
2025-11-24 10:47:21 +0100

    test(server): update test to use existing iframe template
    Use the actual lone_wolf template that exists for the smart-before-fast
    blog post to properly test the success scenario.

--- server/test/tuist_web/controllers/marketing_blog_iframe_controller_test.exs
+++ server/test/tuist_web/controllers/marketing_blog_iframe_controller_test.exs
@@ -13,13 +13,11 @@ defmodule TuistWeb.Marketing.MarketingBlogIframeControllerTest do
 
     test "renders template when id parameter is provided", %{conn: conn} do
       # When
-      conn = get(conn, "/blog/2025/11/6/zero-to-many/iframe.html?id=lone_wolf")
+      conn = get(conn, "/blog/2025/11/17/smart-before-fast/iframe.html?id=lone_wolf")
 
       # Then
-      # The template rendering will fail if the template doesn't exist,
-      # but the controller should attempt to render it
-      # This test verifies the controller doesn't crash with the id parameter
-      assert conn.status in [200, 500]
+      assert response(conn, 200) =~ "viz-container"
+      assert get_resp_header(conn, "content-type") == ["text/html; charset=utf-8"]
     end
   end
 end
