86ecc37937a68a5268fa22a3f171b82ea953f678
Pedro Piñera <pedro@pepicrft.me>
2025-12-18 15:52:18 +0100

    Make some progress in the case studies page

--- server/lib/tuist/marketing/case_studies/case_parser.ex
+++ server/lib/tuist/marketing/case_studies/case_parser.ex
@@ -1,11 +1,10 @@
 defmodule Tuist.Marketing.CaseStudies.CaseParser do
+  @moduledoc false
   def parse(_path, contents) do
     [frontmatter_string, body] =
       contents |> String.replace(~r/^---\n/, "") |> String.split(["\n---\n"], parts: 2)
 
-    frontmatter =
-      frontmatter_string
-      |> YamlElixir.read_from_string!()
+    frontmatter = YamlElixir.read_from_string!(frontmatter_string)
 
     {frontmatter, body}
   end
