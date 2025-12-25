31a8c239d053ace182230d618cea47b7afcb8662
fortmarek <marekfort@me.com>
2025-12-25 22:02:42 +0100

    Format code

--- server/lib/tuist_web/live/project_settings_live.ex
+++ server/lib/tuist_web/live/project_settings_live.ex
@@ -227,7 +227,8 @@ defmodule TuistWeb.ProjectSettingsLive do
       |> assign(schedule_form_channel_id: channel_id)
       |> assign(schedule_form_channel_name: channel_name)
       |> push_event("close-dropdown", %{id: "slack-channel-dropdown"})
-      # |> push_event("js-exec", %{to: "#slack-channel-dropdown", attr: "data-close"})
+
+    # |> push_event("js-exec", %{to: "#slack-channel-dropdown", attr: "data-close"})
 
     {:noreply, socket}
   end
