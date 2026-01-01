0235f0e1395cad18358d8d901ba4e3b880e98925
Pedro Piñera <pedro@pepicrft.me>
2026-01-01 11:32:40 +0100

    refactor(server): align GradleController with existing cache controllers
    Use LoaderPlug and AuthorizationPlug instead of custom plugs to follow
    the established patterns used by CASController and KeyValueController.

--- server/lib/tuist_web/controllers/api/cache/gradle_controller.ex
+++ server/lib/tuist_web/controllers/api/cache/gradle_controller.ex
@@ -11,24 +11,21 @@ defmodule TuistWeb.API.Cache.GradleController do
   """
   use TuistWeb, :controller
 
-  alias Tuist.Accounts.AuthenticatedAccount
-  alias Tuist.Projects
   alias Tuist.Storage
   alias TuistWeb.Authentication
+  alias TuistWeb.Plugs.LoaderPlug
 
   @max_artifact_size 100_000_000
 
-  plug :validate_cache_scope
-  plug :load_project
+  plug LoaderPlug
+  plug TuistWeb.API.Authorization.AuthorizationPlug, :cache
 
-  def load(conn, %{"hash" => hash}) do
-    account = conn.assigns.account
-    project = conn.assigns.project
-    authenticated_account = conn.assigns.authenticated_account
+  def load(%{assigns: %{selected_project: project, selected_account: account}} = conn, %{"hash" => hash}) do
+    current_subject = Authentication.authenticated_subject(conn)
     key = gradle_key(account, project, hash)
 
-    if Storage.object_exists?(key, authenticated_account) do
-      stream = Storage.stream_object(key, authenticated_account)
+    if Storage.object_exists?(key, current_subject) do
+      stream = Storage.stream_object(key, current_subject)
 
       conn
       |> put_resp_content_type("application/octet-stream")
@@ -39,16 +36,14 @@ defmodule TuistWeb.API.Cache.GradleController do
     end
   end
 
-  def save(conn, %{"hash" => hash}) do
-    account = conn.assigns.account
-    project = conn.assigns.project
-    authenticated_account = conn.assigns.authenticated_account
+  def save(%{assigns: %{selected_project: project, selected_account: account}} = conn, %{"hash" => hash}) do
+    current_subject = Authentication.authenticated_subject(conn)
     key = gradle_key(account, project, hash)
 
     case Plug.Conn.read_body(conn, length: @max_artifact_size) do
       {:ok, body, conn} ->
-        unless Storage.object_exists?(key, authenticated_account) do
-          Storage.put_object(key, body, authenticated_account)
+        unless Storage.object_exists?(key, current_subject) do
+          Storage.put_object(key, body, current_subject)
         end
 
         send_resp(conn, :ok, "")
@@ -76,67 +71,5 @@ defmodule TuistWeb.API.Cache.GradleController do
         {:error, :closed} -> {:halt, conn}
       end
     end)
-  end
-
-  defp validate_cache_scope(conn, _opts) do
-    case Authentication.authenticated_subject(conn) do
-      %AuthenticatedAccount{scopes: scopes} = authenticated_account ->
-        if has_cache_scope?(scopes) do
-          assign(conn, :authenticated_account, authenticated_account)
-        else
-          conn
-          |> put_status(:forbidden)
-          |> json(%{error: "Token does not have cache access"})
-          |> halt()
-        end
-
-      _ ->
-        conn
-        |> put_status(:forbidden)
-        |> json(%{error: "Account token required for Gradle cache access"})
-        |> halt()
-    end
-  end
-
-  defp has_cache_scope?(scopes) do
-    Enum.any?(scopes, fn scope ->
-      scope in ["project:cache:read", "project:cache:write"]
-    end)
-  end
-
-  defp load_project(conn, _opts) do
-    account_handle = conn.params["account_handle"]
-    project_handle = conn.params["project_handle"]
-    authenticated_account = conn.assigns.authenticated_account
-
-    with {:ok, project} <- Projects.get_project_by_slug("#{account_handle}/#{project_handle}") do
-      if can_access_project?(authenticated_account, project) do
-        conn
-        |> assign(:account, authenticated_account.account)
-        |> assign(:project, project)
-      else
-        conn
-        |> put_status(:forbidden)
-        |> json(%{error: "Token does not have access to this project"})
-        |> halt()
-      end
-    else
-      {:error, _} ->
-        conn
-        |> put_status(:not_found)
-        |> json(%{error: "Project not found"})
-        |> halt()
-    end
-  end
-
-  defp can_access_project?(authenticated_account, project) do
-    cond do
-      authenticated_account.all_projects ->
-        authenticated_account.account.id == project.account_id
-
-      true ->
-        project.id in authenticated_account.project_ids and
-          authenticated_account.account.id == project.account_id
-    end
   end
 end
