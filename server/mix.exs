98fb8035dad208baa4de6cbc21ca9745df089943
fortmarek <marekfort@me.com>
2026-01-07 13:59:16 +0100

    Minor UI updates

--- server/mix.exs
+++ server/mix.exs
@@ -133,7 +133,7 @@ defmodule Tuist.MixProject do
       {:ecto_ch, "~> 0.8.3"},
       (System.get_env("NOORA_LOCAL") &&
          {:noora, path: "../../Noora/web"}) ||
-        {:noora, "== 0.50.0"},
+        {:noora, "== 0.52.0"},
       {:zstream, "~> 0.6"},
       {:cloak_ecto, "~> 1.3.0"},
       {:boruta, git: "https://github.com/malach-it/boruta_auth", branch: "master"},
