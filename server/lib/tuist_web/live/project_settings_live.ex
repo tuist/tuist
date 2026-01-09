1e98cb3dcc56ae4d91c7831782b95af446783b13
Asmit Malakannawar <asmitbm2952002@gmail.com>
2026-01-09 21:47:19 +0530

    fix(server): fixed failing CI

--- server/lib/tuist_web/live/project_settings_live.ex
+++ server/lib/tuist_web/live/project_settings_live.ex
@@ -6,8 +6,6 @@ defmodule TuistWeb.ProjectSettingsLive do
   alias Tuist.Authorization
   alias Tuist.Projects
   alias Tuist.Projects.Project
-  alias Tuist.Slack.Client, as: SlackClient
-  alias Tuist.Slack.Reports
   alias TuistWeb.Helpers.OpenGraph
 
   @impl true
@@ -25,7 +23,6 @@ defmodule TuistWeb.ProjectSettingsLive do
       |> assign(rename_project_form: rename_project_form)
       |> assign(delete_project_form: delete_project_form)
       |> assign(:head_title, "#{dgettext("dashboard_projects", "Settings")} · #{selected_project.name} · Tuist")
-      |> assign_schedule_form_defaults(selected_project)
       |> assign(OpenGraph.og_image_assigns("settings"))
 
     {:ok, socket}
