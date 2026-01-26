33b8c6735739a7854b4cc252b933546ca1527aa1
Christoph Schmatzler <christoph@tuist.dev>
2026-01-26 12:36:48 +0100

    chore(cache,server): move dependencies from github to hex (#9215)

--- server/runner/mix.exs
+++ server/runner/mix.exs
@@ -25,7 +25,7 @@ defmodule Runner.MixProject do
   defp deps do
     [
       {:burrito, "~> 1.0"},
-      {:langchain, git: "https://github.com/brainlid/langchain", branch: "main"},
+      {:langchain, "~> 0.4"},
       {:briefly, "~> 0.5.0"},
       {:slipstream, "~> 1.2.0"},
       {:web_driver_client, "~> 0.2.0"},
