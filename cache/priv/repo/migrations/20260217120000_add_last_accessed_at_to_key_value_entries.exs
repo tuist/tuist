6159bf99090a971e3828371d7c952a8f1e17d23c
Christoph Schmatzler <christoph@schmatzler.com>
2026-02-19 14:09:24 +0000

    fix(cache): use reversible change in last_accessed_at migration

--- cache/priv/repo/migrations/20260217120000_add_last_accessed_at_to_key_value_entries.exs
+++ cache/priv/repo/migrations/20260217120000_add_last_accessed_at_to_key_value_entries.exs
@@ -1,19 +1,11 @@
 defmodule Cache.Repo.Migrations.AddLastAccessedAtToKeyValueEntries do
   use Ecto.Migration
 
-  def up do
+  def change do
     alter table(:key_value_entries) do
       add :last_accessed_at, :utc_datetime_usec, null: false, default: fragment("(strftime('%Y-%m-%dT%H:%M:%f', 'now'))")
     end
 
     create index(:key_value_entries, [:last_accessed_at])
-  end
-
-  def down do
-    drop index(:key_value_entries, [:last_accessed_at])
-
-    alter table(:key_value_entries) do
-      remove :last_accessed_at
-    end
   end
 end
