7a32c7d3f56fdfe4e0757f5ad701522d58df37f8
Christoph Schmatzler <christoph@schmatzler.com>
2026-02-19 15:13:46 +0000

    fix(cache); fix migration
    Signed-off-by: Christoph Schmatzler <christoph@schmatzler.com>

--- cache/priv/repo/migrations/20260217120000_add_last_accessed_at_to_key_value_entries.exs
+++ cache/priv/repo/migrations/20260217120000_add_last_accessed_at_to_key_value_entries.exs
@@ -3,8 +3,13 @@ defmodule Cache.Repo.Migrations.AddLastAccessedAtToKeyValueEntries do
 
   def change do
     alter table(:key_value_entries) do
-      add :last_accessed_at, :utc_datetime_usec, null: false, default: fragment("(strftime('%Y-%m-%dT%H:%M:%f', 'now'))")
+      add :last_accessed_at, :utc_datetime_usec
     end
+
+    execute(
+      "UPDATE key_value_entries SET last_accessed_at = strftime('%Y-%m-%dT%H:%M:%f', 'now') WHERE last_accessed_at IS NULL",
+      ""
+    )
 
     create index(:key_value_entries, [:last_accessed_at])
   end
