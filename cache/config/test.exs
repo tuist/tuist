f2a84c9a69e7d1051ca1a8cd0bac121dd17338d4
Christoph Schmatzler <christoph@schmatzler.com>
2026-02-25 14:14:10 +0000

    fix(cache): revert bad timeout overrides and make all tests async

--- cache/config/test.exs
+++ cache/config/test.exs
@@ -8,11 +8,9 @@ config :cache, Cache.Repo,
   database: Path.expand("../test.sqlite3", __DIR__),
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2,
-  show_sensitive_data_on_connection_error: false,
-  journal_mode: :wal,
-  busy_timeout: 60_000
+  show_sensitive_data_on_connection_error: false
 
-config :cache, Cache.SQLiteBuffer, shutdown_ms: 0, flush_interval_ms: to_timeout(hour: 1), flush_timeout_ms: 120_000
+config :cache, Cache.SQLiteBuffer, shutdown_ms: 0, flush_interval_ms: to_timeout(hour: 1)
 
 config :cache, CacheWeb.Endpoint,
   http: [ip: {127, 0, 0, 1}, port: 4002],
