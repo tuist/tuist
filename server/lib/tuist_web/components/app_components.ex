b6a6c2f26bfddee725fce029ef8c5d38ebe164ea
Christoph Schmatzler <christoph@schmatzler.com>
2026-02-27 13:37:33 +0000

    refactor(server): extract skeleton_box component and add configurable skeleton_legend widths

--- server/lib/tuist_web/components/app_components.ex
+++ server/lib/tuist_web/components/app_components.ex
@@ -43,16 +43,29 @@ defmodule TuistWeb.AppComponents do
     """
   end
 
+  attr(:title_width, :string, default: nil, doc: "Optional CSS width for the title placeholder.")
+  attr(:value_width, :string, default: nil, doc: "Optional CSS width for the value placeholder.")
+
   def skeleton_legend(assigns) do
     ~H"""
     <div class="tuist-legend">
       <div data-part="header">
         <div data-part="indicator"></div>
-        <span data-part="title" class="tuist-loading-skeleton">
+        <span
+          data-part="title"
+          class="tuist-loading-skeleton"
+          style={@title_width && "min-width: #{@title_width}"}
+        >
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         </span>
       </div>
-      <span data-part="value" class="tuist-loading-skeleton">&nbsp;&nbsp;&nbsp;</span>
+      <span
+        data-part="value"
+        class="tuist-loading-skeleton"
+        style={@value_width && "min-width: #{@value_width}"}
+      >
+        &nbsp;&nbsp;&nbsp;
+      </span>
     </div>
     """
   end
@@ -64,6 +77,29 @@ defmodule TuistWeb.AppComponents do
     <div
       class="noora-chart tuist-loading-skeleton"
       style={@height && "height: #{@height}"}
+    >
+      &nbsp;
+    </div>
+    """
+  end
+
+  attr(:width, :string, required: true, doc: "CSS width for the box.")
+  attr(:height, :string, required: true, doc: "CSS height for the box.")
+  attr(:border_radius, :string, default: nil, doc: "Optional CSS border-radius override.")
+
+  def skeleton_box(assigns) do
+    ~H"""
+    <div
+      class="tuist-loading-skeleton"
+      style={
+        [
+          "width: #{@width}",
+          "height: #{@height}",
+          @border_radius && "border-radius: #{@border_radius}"
+        ]
+        |> Enum.reject(&is_nil/1)
+        |> Enum.join("; ")
+      }
     >
       &nbsp;
     </div>
