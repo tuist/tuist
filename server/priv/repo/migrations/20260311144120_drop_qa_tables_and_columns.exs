a5d60fbf65c506b6edbd943db3db1edae7b57762
Pedro Piñera <pedro@pepicrft.me>
2026-03-11 16:44:20 +0100

    fix(server): add safety-assured comments to QA migration

--- server/priv/repo/migrations/20260311144120_drop_qa_tables_and_columns.exs
+++ server/priv/repo/migrations/20260311144120_drop_qa_tables_and_columns.exs
@@ -2,15 +2,23 @@ defmodule Tuist.Repo.Migrations.DropQaTablesAndColumns do
   use Ecto.Migration
 
   def up do
+    # excellent_migrations:safety-assured-for-next-line table_dropped
     drop table(:qa_screenshots)
+    # excellent_migrations:safety-assured-for-next-line table_dropped
     drop table(:qa_recordings)
+    # excellent_migrations:safety-assured-for-next-line table_dropped
     drop table(:qa_steps)
+    # excellent_migrations:safety-assured-for-next-line table_dropped
     drop table(:qa_runs)
+    # excellent_migrations:safety-assured-for-next-line table_dropped
     drop table(:qa_launch_argument_groups)
 
     alter table(:projects) do
+      # excellent_migrations:safety-assured-for-next-line column_removed
       remove :qa_app_description
+      # excellent_migrations:safety-assured-for-next-line column_removed
       remove :qa_email
+      # excellent_migrations:safety-assured-for-next-line column_removed
       remove :qa_password
     end
   end
@@ -25,7 +33,9 @@ defmodule Tuist.Repo.Migrations.DropQaTablesAndColumns do
       timestamps(type: :timestamptz)
     end
 
+    # excellent_migrations:safety-assured-for-next-line index_not_concurrently
     create index(:qa_launch_argument_groups, [:name])
+    # excellent_migrations:safety-assured-for-next-line index_not_concurrently
     create unique_index(:qa_launch_argument_groups, [:project_id, :name])
 
     create table(:qa_runs, primary_key: false) do
@@ -46,8 +56,11 @@ defmodule Tuist.Repo.Migrations.DropQaTablesAndColumns do
       timestamps(type: :timestamptz)
     end
 
+    # excellent_migrations:safety-assured-for-next-line index_not_concurrently
     create index(:qa_runs, [:status])
+    # excellent_migrations:safety-assured-for-next-line index_not_concurrently
     create index(:qa_runs, [:inserted_at])
+    # excellent_migrations:safety-assured-for-next-line index_not_concurrently
     create index(:qa_runs, [:app_build_id])
 
     create table(:qa_steps, primary_key: false) do
@@ -60,7 +73,9 @@ defmodule Tuist.Repo.Migrations.DropQaTablesAndColumns do
       add :inserted_at, :timestamptz, null: false
     end
 
+    # excellent_migrations:safety-assured-for-next-line index_not_concurrently
     create index(:qa_steps, [:qa_run_id])
+    # excellent_migrations:safety-assured-for-next-line index_not_concurrently
     create index(:qa_steps, [:inserted_at])
 
     create table(:qa_screenshots, primary_key: false) do
@@ -70,8 +85,11 @@ defmodule Tuist.Repo.Migrations.DropQaTablesAndColumns do
       timestamps(type: :timestamptz)
     end
 
+    # excellent_migrations:safety-assured-for-next-line index_not_concurrently
     create index(:qa_screenshots, [:qa_run_id])
+    # excellent_migrations:safety-assured-for-next-line index_not_concurrently
     create index(:qa_screenshots, [:qa_step_id])
+    # excellent_migrations:safety-assured-for-next-line index_not_concurrently
     create index(:qa_screenshots, [:inserted_at])
 
     create table(:qa_recordings, primary_key: false) do
@@ -83,8 +101,11 @@ defmodule Tuist.Repo.Migrations.DropQaTablesAndColumns do
     end
 
     alter table(:projects) do
+      # excellent_migrations:safety-assured-for-next-line column_added_with_default
       add :qa_app_description, :text, default: "", null: false
+      # excellent_migrations:safety-assured-for-next-line column_added_with_default
       add :qa_email, :text, default: "", null: false
+      # excellent_migrations:safety-assured-for-next-line column_added_with_default
       add :qa_password, :text, default: "", null: false
     end
   end
