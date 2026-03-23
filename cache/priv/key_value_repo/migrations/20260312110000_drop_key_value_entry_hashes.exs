3de990ef819f6cb4630cb1230e38f6528285448b
Christoph Schmatzler <christoph@schmatzler.com>
2026-03-23 12:41:15 +0000

    perf: do not drop entry hashes

--- cache/priv/key_value_repo/migrations/20260312110000_drop_key_value_entry_hashes.exs
+++
@@ -1,9 +0,0 @@
-defmodule Cache.KeyValueRepo.Migrations.DropKeyValueEntryHashes do
-  use Ecto.Migration
-
-  def change do
-    drop_if_exists(index(:key_value_entry_hashes, [:account_handle, :project_handle, :cas_hash]))
-    drop_if_exists(unique_index(:key_value_entry_hashes, [:key_value_entry_id, :cas_hash]))
-    drop_if_exists(table(:key_value_entry_hashes))
-  end
-end
