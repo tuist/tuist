a6bfc2a14eb8ebd8fabf610d82b5f68209e2bb18
Asmit Malakannawar <asmitbm2952002@gmail.com>
2026-04-22 12:35:31 +0200

    page level filters for test runs and test cases

--- server/lib/tuist_web/live/test_runs_live.ex
+++ server/lib/tuist_web/live/test_runs_live.ex
@@ -366,11 +366,15 @@ defmodule TuistWeb.TestRunsLive do
 
     search = params["search"] || ""
 
-    flop_filters = [
-      %{field: :project_id, op: :==, value: project.id},
-      %{field: :status, op: :!=, value: "in_progress"}
-      | build_flop_filters(filters, search)
-    ]
+    {start_datetime, end_datetime} = socket.assigns.analytics_period
+
+    flop_filters =
+      [
+        %{field: :project_id, op: :==, value: project.id},
+        %{field: :status, op: :!=, value: "in_progress"},
+        %{field: :ran_at, op: :>=, value: start_datetime},
+        %{field: :ran_at, op: :<=, value: end_datetime}
+      ] ++ build_flop_filters(filters, search) ++ page_level_environment_filters(socket.assigns)
 
     options = %{
       filters: flop_filters,
@@ -428,4 +432,8 @@ defmodule TuistWeb.TestRunsLive do
 
     flop_filters ++ ran_by_flop_filters ++ search_filters
   end
+
+  defp page_level_environment_filters(%{analytics_environment: "ci"}), do: [%{field: :is_ci, op: :==, value: true}]
+  defp page_level_environment_filters(%{analytics_environment: "local"}), do: [%{field: :is_ci, op: :==, value: false}]
+  defp page_level_environment_filters(_), do: []
 end
