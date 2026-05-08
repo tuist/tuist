dcd51eb511fbb11209b33b28ec8e935871ff701b
Pedro Piñera Buendía <pedro@pepicrft.me>
2026-05-08 11:38:05 +0200

    feat(server): add Georgian locale support

--- server/lib/tuist/locale.ex
+++ server/lib/tuist/locale.ex
@@ -5,6 +5,7 @@ defmodule Tuist.Locale do
     %{code: "en", label: "English", native: "English"},
     %{code: "es", label: "Spanish", native: "Castellano"},
     %{code: "ja", label: "Japanese", native: "日本語"},
+    %{code: "ka", label: "Georgian", native: "ქართული"},
     %{code: "ko", label: "Korean", native: "한국어"},
     %{code: "ru", label: "Russian", native: "Русский"},
     %{code: "yue_Hant", label: "Cantonese", native: "廣東話"},
