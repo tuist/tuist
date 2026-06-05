2863af6903d4063ef6e10394b65c530924055b20
fortmarek <marekfort@me.com>
2026-06-05 10:26:10 +0200

    Revert "tmp: trigger warm build validation (revert before merge)"
    This reverts commit 5700821a4010c4075ec010e010426a871687a9e5.

--- server/lib/tuist/base_64.ex
+++ server/lib/tuist/base_64.ex
@@ -2,8 +2,6 @@ defmodule Tuist.Base64 do
   @moduledoc ~S"""
   A module to deal with base64 encoding and decoding.
   """
-
-  # TEMP: warm-build validation marker (reverted before merge)
   def encode(data) do
     :base64.encode(data)
   end
