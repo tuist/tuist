6bcfd87bc000ecf63ae1064104078806e144a484
Marek Fořt <marekfort@me.com>
2026-06-11 14:28:29 +0200

    fix(cli): pin SwifterPM revision (#11229)

--- Package.swift
+++ Package.swift
@@ -1796,7 +1796,10 @@ let package = Package(
         .package(id: "swiftlang.swift-docc-plugin", from: "1.4.6"),
         .package(name: "XCResultNIF", path: "server/native/xcresult_nif"),
         .package(id: "stephencelis.SQLite_swift", from: "0.16.0"),
-        .package(url: "https://github.com/tuist/swifterpm", exact: "0.8.3"),
+        .package(
+            url: "https://github.com/tuist/swifterpm",
+            revision: "672a723f962cf243f1b6de65274c744c51e82acc"
+        ),
     ],
     targets: targets,
     swiftLanguageModes: [.v5]
