686fcb25962850bf6ae4c50be98c546ea00f7e49
Pedro Piñera Buendía <pedro@pepicrft.me>
2026-06-29 10:37:34 +0200

    fix(server): exclude continuous integration rows from ran by filters

--- server/lib/tuist_web/live/test_runs_live.ex
+++ server/lib/tuist_web/live/test_runs_live.ex
@@ -425,6 +425,12 @@ defmodule TuistWeb.TestRunsLive do
         %{value: :ci, operator: op} ->
           [%{field: :is_ci, op: op, value: true}]
 
+        %{value: value, operator: :==} when not is_nil(value) ->
+          [
+            %{field: :is_ci, op: :==, value: false},
+            %{field: :account_id, op: :==, value: value}
+          ]
+
         %{value: value, operator: op} when not is_nil(value) ->
           [%{field: :account_id, op: op, value: value}]
 
