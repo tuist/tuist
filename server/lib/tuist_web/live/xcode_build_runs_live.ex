7d7cb9737fad4898b9078b8ef769339609968a58
Marek Fořt <marekfort@me.com>
2026-07-09 19:16:45 +0200

    fix(noora): normalize filter operators for Flop (#11726)

--- server/lib/tuist_web/live/xcode_build_runs_live.ex
+++ server/lib/tuist_web/live/xcode_build_runs_live.ex
@@ -168,10 +168,7 @@ defmodule TuistWeb.XcodeBuildRunsLive do
     {ran_by, filters} = Enum.split_with(filters, &(&1.id == "ran_by"))
     {tags_filters, filters} = Enum.split_with(filters, &(&1.id == "custom_tags"))
 
-    flop_filters =
-      filters
-      |> Enum.map(&normalize_text_filter_operator/1)
-      |> Filter.Operations.convert_filters_to_flop()
+    flop_filters = Filter.Operations.convert_filters_to_flop(filters)
 
     ran_by_flop_filters =
       Enum.flat_map(ran_by, fn
@@ -203,10 +200,6 @@ defmodule TuistWeb.XcodeBuildRunsLive do
 
     flop_filters ++ ran_by_flop_filters ++ tag_flop_filters
   end
-
-  defp normalize_text_filter_operator(%Filter.Filter{operator: :"!=~"} = filter), do: %{filter | operator: :not_ilike}
-
-  defp normalize_text_filter_operator(filter), do: filter
 
   defp define_filters(project, schemes, configurations, tags) do
     base = [
