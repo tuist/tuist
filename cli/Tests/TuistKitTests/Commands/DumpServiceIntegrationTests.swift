9921b013a83c2c906c7680fe3a854d0aa5d7f416
Pedro Piñera Buendía <pedro@pepicrft.me>
2026-07-15 18:22:57 +0200

    test(cli): update project dump expectation

--- cli/Tests/TuistKitTests/Commands/DumpServiceIntegrationTests.swift
+++ cli/Tests/TuistKitTests/Commands/DumpServiceIntegrationTests.swift
@@ -77,6 +77,9 @@ final class DumpServiceTests: TuistTestCase {
                 }
               },
               "organizationName": "tuist",
+              "packageDependencies": [
+
+              ],
               "packages": [
 
               ],
