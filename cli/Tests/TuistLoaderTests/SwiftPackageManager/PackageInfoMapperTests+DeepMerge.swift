9e6e85310cef7b4d473d202b48c397070c64ec85
jonurq-peya <jonatan.urquiza@pedidosya.com>
2026-08-06 20:19:31 -0300

    remove unused test file

--- cli/Tests/TuistLoaderTests/SwiftPackageManager/PackageInfoMapperTests+DeepMerge.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import XCTest
-import ProjectDescription
-import TSCBasic
-@testable import TuistLoader
-
-final class PackageInfoMapperDeepMergeTests: XCTestCase {
-    // Tests for the new deep merge logic will go here
-}
